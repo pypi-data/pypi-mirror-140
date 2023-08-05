34df5220a355ef9b8b596535ae2bd722ac67a349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:55 +0000

    Add package gdsfactory/4.2.10/gdsfactory-4.2.10-py3-none-any.whl

---
+++ packages/gdsfactory/gdsfactory-4.2.10-py3-none-any.whl/gdsfactory/simulation/gtidy3d/__init__.py
@@ -0,0 +1,35 @@
+"""gdsfactory tidy3d plugin
+
+[tidy3D is a fast GPU based commercial FDTD solver](https://simulation.cloud/)
+"""
+
+from gdsfactory.simulation.gtidy3d import materials
+from gdsfactory.simulation.gtidy3d.get_results import get_results
+from gdsfactory.simulation.gtidy3d.get_simulation import (
+    get_simulation,
+    plot_simulation,
+    plot_simulation_xz,
+    plot_simulation_yz,
+)
+from gdsfactory.simulation.gtidy3d.write_sparameters import (
+    write_sparameters,
+    write_sparameters_1x1,
+    write_sparameters_batch,
+    write_sparameters_batch_1x1,
+    write_sparameters_crossing,
+)
+
+__version__ = "0.0.2"
+__all__ = [
+    "plot_simulation",
+    "plot_simulation_xz",
+    "plot_simulation_yz",
+    "get_simulation",
+    "get_results",
+    "materials",
+    "write_sparameters",
+    "write_sparameters_crossing",
+    "write_sparameters_1x1",
+    "write_sparameters_batch",
+    "write_sparameters_batch_1x1",
+]
