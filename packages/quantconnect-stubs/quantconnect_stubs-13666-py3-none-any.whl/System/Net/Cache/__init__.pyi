53ce9d454f2cfbcf19ab39e805fab9d096b0ecdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:29 +0000

    Add package quantconnect-stubs/13666/quantconnect_stubs-13666-py3-none-any.whl

---
+++ packages/quantconnect-stubs/quantconnect_stubs-13666-py3-none-any.whl/System/Net/Cache/__init__.pyi
@@ -0,0 +1,44 @@
+import typing
+
+import System
+import System.Net.Cache
+
+
+class RequestCacheLevel(System.Enum):
+    """This class has no documentation."""
+
+    Default = 0
+
+    BypassCache = 1
+
+    CacheOnly = 2
+
+    CacheIfAvailable = 3
+
+    Revalidate = 4
+
+    Reload = 5
+
+    NoCacheNoStore = 6
+
+
+class RequestCachePolicy(System.Object):
+    """This class has no documentation."""
+
+    @property
+    def Level(self) -> int:
+        """This property contains the int value of a member of the System.Net.Cache.RequestCacheLevel enum."""
+        ...
+
+    @typing.overload
+    def __init__(self) -> None:
+        ...
+
+    @typing.overload
+    def __init__(self, level: System.Net.Cache.RequestCacheLevel) -> None:
+        ...
+
+    def ToString(self) -> str:
+        ...
+
+
