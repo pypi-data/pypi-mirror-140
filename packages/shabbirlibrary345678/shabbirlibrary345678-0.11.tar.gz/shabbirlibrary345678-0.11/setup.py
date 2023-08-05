d10a422a8fe4be41e090dc2af1208246862af603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:36 +0000

    Add package shabbirlibrary345678/0.11/shabbirlibrary345678-0.11.tar.gz

---
+++ packages/shabbirlibrary345678/shabbirlibrary345678-0.11.tar.gz/shabbirlibrary345678-0.11/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='shabbirlibrary345678',
+      version='0.11',
+      description='operation & greeting library',
+      packages=['shabbirlibrary'],
+      author_email='shabbirg89@gmail.com',
+      zip_safe=False)
