2223391895698648305e927cb7d890f7569fd15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:50 +0000

    Add package little-helpers/0.0.5/little_helpers-0.0.5.tar.gz

---
+++ packages/little-helpers/little_helpers-0.0.5.tar.gz/little_helpers-0.0.5/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='little_helpers',
+    version='0.0.5',
+    packages=find_packages(where='src'),
+    install_requires=['numpy', 'scikit-learn', 'scipy', 'statsmodels', 'sympy', 'python-docx', 'latex2mathml', 'lxml']
+)
