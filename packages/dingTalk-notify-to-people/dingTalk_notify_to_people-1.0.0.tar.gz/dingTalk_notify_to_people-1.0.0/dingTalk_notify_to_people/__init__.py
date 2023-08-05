1c7ebcef75d2dff8fc5debdbf81b2e66473a8088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:51 +0000

    Add package dingTalk-notify-to-people/1.0.0/dingTalk_notify_to_people-1.0.0.tar.gz

---
+++ packages/dingTalk-notify-to-people/dingTalk_notify_to_people-1.0.0.tar.gz/dingTalk_notify_to_people-1.0.0/dingTalk_notify_to_people/__init__.py
@@ -0,0 +1,9 @@
+"""
+  @Project     : sentry_dingTalk_notify_to_people
+  @Time        : 2022/02/24 13:35:48
+  @File        : __init__.py
+  @Author      : MrChen
+  @Software    : VSCode
+  @Desc        : 
+"""
+name = "dingTalk_notify_to_people"
