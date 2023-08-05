f729a8d056d3ab77bdc469a7c2d8733b16fc0748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:06 +0000

    Add package Name-factor/1.0.0/Name%20factor-1.0.0.tar.gz

---
+++ packages/Name-factor/Name%20factor-1.0.0.tar.gz/Name factor-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setup(
+  name = "Name factor",
+  version = "1.0.0",
+  description = "Module description.",
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
+  url = "https://www.discordapp.com/users/771212911606628363",
+  author = "Author's name",
+  author_email = "devpsingh2020@gmail.com",
+  license = "GNU General Public License v3 (GPLv3)",
+  packages=["module_name"],
+  classifiers = [
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+  "Operating System :: OS Independent",
+],
+  zip_safe=True,
+  python_requires = ">=3.0",
+)
