3c5172de5e04695c65dd8d911205cc692c6f18f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:00 +0000

    Add package abalin-nameday/0.0.4/abalin_nameday-0.0.4-py3-none-any.whl

---
+++ packages/abalin-nameday/abalin_nameday-0.0.4-py3-none-any.whl/abalin_nameday/abalin_nameday.py
@@ -0,0 +1,32 @@
+import json
+import requests
+import subprocess
+
+from . import version
+
+class namedayRequestor:
+
+    def __init__(self, country, timezone ):
+        #   Init variables
+        self.country = country
+        self.timezone = timezone
+        self.data = "{"
+
+    def GetData(self):
+        self.data +="\"module\":{"
+        self.data +="\"version\":\""
+        self.data += version.currentVersion
+        self.data +="\""
+        self.data +="},"
+        self.data +="\"namedays\":"
+        try:
+            self.data += requests.post(f"https://nameday.abalin.net/api/V1/today?country={self.country}&timezone={self.timezone}").text
+        except:
+            self.data += "{\"Error\": \"Failed to connect to service. Try again later.\"}"
+
+        #   Finish JSON string
+        self.data += "}"
+
+        #   Return JSON string containing all the result
+        return (self.data)
+
