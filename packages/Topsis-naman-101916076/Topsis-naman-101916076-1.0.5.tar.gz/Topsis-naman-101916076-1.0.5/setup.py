824b5a1bec7e325d8f509db1b91cf1b4c24c13c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:40 +0000

    Add package Topsis-naman-101916076/1.0.5/Topsis-naman-101916076-1.0.5.tar.gz

---
+++ packages/Topsis-naman-101916076/Topsis-naman-101916076-1.0.5.tar.gz/Topsis-naman-101916076-1.0.5/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="Topsis-naman-101916076",
+    version="1.0.5",
+    description="TOPSIS method for multiple-criteria decision making (MCDM).",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Naman Kalsotra",
+    author_email="Kalsotranaman@gmail.com",
+    license="SELF",
+    classifiers=[
+        
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["topsis"],
+    include_package_data=True,
+    install_requires=["pandas"],
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis.__main__:main",
+        ]
+    },
+)
