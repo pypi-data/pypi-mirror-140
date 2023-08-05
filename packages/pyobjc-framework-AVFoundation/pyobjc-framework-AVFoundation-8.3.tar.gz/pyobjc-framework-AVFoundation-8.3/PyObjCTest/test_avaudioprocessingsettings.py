d46b70777e15c62fdfaff6140a17d4ed5ffaccc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:30 +0000

    Add package pyobjc-framework-AVFoundation/8.3/pyobjc-framework-AVFoundation-8.3.tar.gz

---
+++ packages/pyobjc-framework-AVFoundation/pyobjc-framework-AVFoundation-8.3.tar.gz/pyobjc-framework-AVFoundation-8.3/PyObjCTest/test_avaudioprocessingsettings.py
@@ -0,0 +1,18 @@
+import AVFoundation
+from PyObjCTools.TestSupport import TestCase, min_os_level
+
+
+class TestAVAudioProcessingSettings(TestCase):
+    def test_constants(self):
+        self.assertEqual(AVFoundation.AVAudioSpatializationFormatNone, 0)
+        self.assertEqual(AVFoundation.AVAudioSpatializationFormatMonoAndStereo, 0x3)
+        self.assertEqual(AVFoundation.AVAudioSpatializationFormatMultichannel, 0x4)
+        self.assertEqual(
+            AVFoundation.AVAudioSpatializationFormatMonoStereoAndMultichannel, 0x7
+        )
+
+    @min_os_level("10.9")
+    def testConstants10_9(self):
+        self.assertIsInstance(AVFoundation.AVAudioTimePitchAlgorithmTimeDomain, str)
+        self.assertIsInstance(AVFoundation.AVAudioTimePitchAlgorithmSpectral, str)
+        self.assertIsInstance(AVFoundation.AVAudioTimePitchAlgorithmVarispeed, str)
