7c38b6b2bfc831b10ce3074334d71a31ec263d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:42 +0000

    Add package robusta-cli/0.8.27/robusta-cli-0.8.27.tar.gz

---
+++ packages/robusta-cli/robusta-cli-0.8.27.tar.gz/robusta-cli-0.8.27/src/robusta/core/sinks/msteams/msteams_sink.py
@@ -0,0 +1,22 @@
+from ...reporting import Finding
+from ....integrations.msteams.sender import MsTeamsSender
+from ..sink_base import SinkBase
+from .msteams_sink_params import MsTeamsSinkConfigWrapper
+
+
+class MsTeamsSink(SinkBase):
+    def __init__(
+        self,
+        sink_config: MsTeamsSinkConfigWrapper,
+        account_id: str,
+        cluster_name: str,
+        signing_key: str,
+    ):
+        super().__init__(sink_config.ms_teams_sink)
+        self.account_id = account_id
+        self.cluster_name = cluster_name
+        self.signing_key = signing_key
+        self.webhook_url = sink_config.ms_teams_sink.webhook_url
+
+    def write_finding(self, finding: Finding, platform_enabled: bool):
+        MsTeamsSender.send_finding_to_ms_teams(self.webhook_url, finding, platform_enabled, self.cluster_name)
