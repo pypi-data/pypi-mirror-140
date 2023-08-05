172424779d3fed3276ba5529f41872f0d4efca7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:02 +0000

    Add package ajd-distributions/1.0/ajd-distributions-1.0.tar.gz

---
+++ packages/ajd-distributions/ajd-distributions-1.0.tar.gz/ajd-distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ajd-distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['ajd_distributions'],
+      author = 'Arkojyoti Das',
+      author_email = 'arkojyotidas18@gmail.com',
+      zip_safe=False)
