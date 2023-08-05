716c5fc7edcace4ca801cb3b3396388c6d231b54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:20:54 +0000

    Add package FlaskSimpleAuth/7.0/FlaskSimpleAuth-7.0.tar.gz

---
+++ packages/FlaskSimpleAuth/FlaskSimpleAuth-7.0.tar.gz/FlaskSimpleAuth-7.0/demo/pass.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+#
+# generate password authenticated users
+#
+
+import sys
+from passlib.context import CryptContext
+
+if len(sys.argv) <= 1:
+    print(f"Usage: {sys.argv[0]} bcrypt|plaintext|… [login1:pass1] …")
+    sys.exit(0)
+
+scheme = sys.argv[1]
+
+pm = CryptContext(schemes=[scheme],
+                  bcrypt__default_rounds=4,    # about 2 ms
+                  bcrypt__default_ident='2y')  # apache compatible
+sep = " "
+print("INSERT INTO Auth(login, email, upass, admin) VALUES")
+for lp in sys.argv[2:]:
+    login, mdp = lp.split(":", 1)
+    print(f"{sep} ('{login}', '{login}@school.org', '{pm.hash(mdp)}', TRUE)")
+    sep = ","
+print(";")
