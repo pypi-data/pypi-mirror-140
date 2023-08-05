8e508f7d4771bd4117035068480883965103c50f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:38 +0000

    Add package vsfieldkit/0.2.0/vsfieldkit-0.2.0.tar.gz

---
+++ packages/vsfieldkit/vsfieldkit-0.2.0.tar.gz/vsfieldkit-0.2.0/README.md
@@ -0,0 +1,15 @@
+# vsfieldkit
+Collection of functions for working with interlaced content in VapourSynth. It
+does not have any hard dependencies outside of
+[VapourSynth](http://www.vapoursynth.com/).
+
+Included functions:  
+`vsfieldkit.assume_bff(clip)`  
+`vsfieldkit.assume_progressive(clip)`  
+`vsfieldkit.assume_tff(clip)`  
+`vsfieldkit.double(clip)`  
+`vsfieldkit.group_by_combed(clip)`  
+`vsfieldkit.group_by_field_order(clip)`  
+`vsfieldkit.scan_interlaced(clip)`
+
+See [the documentation](https://vsfieldkit.readthedocs.io/) for more information.
---
+++ packages/vsfieldkit/vsfieldkit-0.2.0.tar.gz/vsfieldkit-0.2.0/vsfieldkit/__init__.py
@@ -0,0 +1,14 @@
+from vsfieldkit.deinterlacing import bob
+from vsfieldkit.scanning import scan_interlaced
+from vsfieldkit.types import (ChromaSubsampleScanning,
+                              InterlacedScanPostProcessor)
+from vsfieldkit.util import (assume_bff, assume_progressive, assume_tff,
+                             double, group_by_combed, group_by_field_order)
+
+VERSION = 0, 2, 0
+
+SCAN_BLENDED = ChromaSubsampleScanning.SCAN_BLENDED
+SCAN_LATEST = ChromaSubsampleScanning.SCAN_LATEST
+SCAN_UPSAMPLED = ChromaSubsampleScanning.SCAN_UPSAMPLED
+
+BLEND_VERTICALLY = InterlacedScanPostProcessor.BLEND_VERTICALLY
