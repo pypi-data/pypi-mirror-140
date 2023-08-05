af7a16c30f744fa519dc59b69c861fdf79d38d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:22 +0000

    Add package bodyguard/0.0.28/bodyguard-0.0.28.tar.gz

---
+++ packages/bodyguard/bodyguard-0.0.28.tar.gz/bodyguard-0.0.28/src/bodyguard/exceptions.py
@@ -0,0 +1,48 @@
+#------------------------------------------------------------------------------
+# Libraries
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+# Main
+#------------------------------------------------------------------------------
+class WrongInputException(Exception):
+    """
+    Exception raised when input is not a pd.DataFrame
+    """
+    def __init__(self, input_name, provided_input, allowed_inputs, message="\nUser-provided argument '{0}' is currently '{1}'.\nArgument must be one of: '{2}'"):
+        self.input_name = input_name
+        self.provided_input = provided_input
+        self.allowed_inputs = allowed_inputs
+        self.message = message
+        super().__init__(self.message)    
+
+    def __str__(self):
+        
+        return self.message.format(self.input_name,self.provided_input, self.allowed_inputs)
+
+
+class WrongInputTypeException(Exception):
+    """
+    Exception raised when input is not a pd.DataFrame
+    """
+    def __init__(self, input_name, provided_input, allowed_inputs, message="\nUser-provided argument '{0}' is currently an instance of type '{1}'.\nArgument must be an instance of one of: '{2}'"):
+        self.input_name = input_name
+        self.provided_input = provided_input
+        self.allowed_inputs = allowed_inputs
+        self.message = message
+        super().__init__(self.message)    
+
+    def __str__(self):
+        
+        return self.message.format(self.input_name,type(self.provided_input), self.allowed_inputs)
+
+
+def print_warning(msg=""):
+    
+    print(
+f"""
+------------------------- WARNING -------------------------
+{msg}
+-----------------------------------------------------------
+"""
+        )
