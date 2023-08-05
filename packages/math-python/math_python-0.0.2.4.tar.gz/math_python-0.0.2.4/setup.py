b374129b15f4918b7549d7c835d9420d4134c6d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:58 +0000

    Add package math-python/0.0.2.4/math_python-0.0.2.4.tar.gz

---
+++ packages/math-python/math_python-0.0.2.4.tar.gz/math_python-0.0.2.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='math_python',
+      version='0.0.2.4',
+      description='Gaussian and Binomial distributions',
+      packages=['math_python'],
+      author_email='vertikal2312312@yandex.ru',
+      zip_safe=False)
