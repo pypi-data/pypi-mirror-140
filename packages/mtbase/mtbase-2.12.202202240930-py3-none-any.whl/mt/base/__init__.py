6b4eb797cd3728851ce3057940056bb1e42f5902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:05 +0000

    Add package mtbase/2.12.202202240930/mtbase-2.12.202202240930-py3-none-any.whl

---
+++ packages/mtbase/mtbase-2.12.202202240930-py3-none-any.whl/mt/base/__init__.py
@@ -0,0 +1,39 @@
+'''Minh-Tri Pham's base package for Python.
+
+
+# Variable prefix naming convention
+
+On 2022/02/24, I introduced a prefix naming convention for variables. All variable names have been
+gradually updated since then. The convention is described in the following paragraphs.
+
+Variables can have a prefix in the form of a letter followed by an underscore. 'l_' stands for a
+list of things. 't_' stands for a tuple of things. 'a_' stands for a :class:`numpy.ndarray` which
+is an array of things. 'b_' stands for a :class:`tensorflow.Tensor` which is a batch of things. A
+special case 'i_' stands for an image of things, which represents a 2D grid with shape (H,W) of
+things.
+
+Nested collections can be prefixed with multiple letters followed by an underscore, using the same
+convention as above. For example, 'll_' stands for a list of lists of things. Prefix 'bi_' stands
+for a tensorflow batch of images of things.
+
+'''
+
+from .with_utils import dummy_scope, join_scopes
+from .logging import make_logger, logger, init as _log_init
+from .deprecated import deprecated_func
+from .casting import cast, castable
+from .exec import debug_exec
+
+home_dirpath = _log_init._home_dirpath
+temp_dirpath = _log_init._temp_dirpath
+
+__all__ = ['logger', 'home_dirpath', 'temp_dirpath', 'deprecated_func', 'cast', 'castable']
+
+
+from .const import *
+
+
+# backdoor to debug the process
+from .debug_process import listen as _listen
+_listen()
+
