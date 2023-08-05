6953e6b1f99bff7f0b16576c28009553dc9dacf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:53 +0000

    Add package pyobjc-framework-Intents/8.3/pyobjc-framework-Intents-8.3.tar.gz

---
+++ packages/pyobjc-framework-Intents/pyobjc-framework-Intents-8.3.tar.gz/pyobjc-framework-Intents-8.3/PyObjCTest/test_inperson.py
@@ -0,0 +1,14 @@
+from PyObjCTools.TestSupport import TestCase, min_os_level
+import Intents
+
+
+class TestINPerson(TestCase):
+    @min_os_level("10.12")
+    def testConstants(self):
+        self.assertEqual(Intents.INPersonSuggestionTypeNone, 0)
+        self.assertEqual(Intents.INPersonSuggestionTypeSocialProfile, 1)
+        self.assertEqual(Intents.INPersonSuggestionTypeInstantMessageAddress, 2)
+
+    @min_os_level("12.0")
+    def test_methods(self):
+        self.assertResultIsBOOL(Intents.INPerson.isContactSuggestion)
