69fa2d9d1017744874863d11e80c17bcf420f522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:54 +0000

    Add package pbat/0.0.3/pbat-0.0.3.tar.gz

---
+++ packages/pbat/pbat-0.0.3.tar.gz/pbat-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('readme.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    packages = find_packages(),
+    name = 'pbat',
+    version='0.0.3',
+    author="Stanislav Doronin",
+    author_email="mugisbrows@gmail.com",
+    url='https://github.com/mugiseyebrows/pbat',
+    description='Batch file preprocessor',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    install_requires = [],
+    entry_points={
+        'console_scripts': [
+            'pbat = pbat.compile:main',
+            'pbat.compile = pbat.compile:main',
+            'pbat.watch = pbat.watch:main'
+        ]
+    }
+)
