799a009c02d5df83f185fcc716863073361a193d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:44 +0000

    Add package create-gitlab-project/0.0.1/create_gitlab_project-0.0.1-py3-none-any.whl

---
+++ packages/create-gitlab-project/create_gitlab_project-0.0.1-py3-none-any.whl/create_gitlab_project/constants.py
@@ -0,0 +1,11 @@
+JENKINSFILE_PATH = "/template-files/Jenkinsfile"
+README_PATH = "/template-files/README.md"
+
+JENKINSFILE_REPLACEMENT_ITEMS = {
+    "pipeline_type": "{{PIPELINE_TYPE}}",
+    "project_type": "{{PARAMS_PROJECT_TYPE}}",
+    "app_name": "{{PARAMS_APP_NAME}}",
+    "namespace": "{{PARAMS_NAMESPACE}}"
+}
+
+DIR_NAME = ""
---
+++ packages/create-gitlab-project/create_gitlab_project-0.0.1-py3-none-any.whl/create_gitlab_project/template-files/README.md
@@ -0,0 +1,4 @@
+This project is created by Jenkins, please follow below steps to continue
+- Update iac/Jenkinsfile
+- Update iac/Dockerfile
+- Update iac/deployment.yml
