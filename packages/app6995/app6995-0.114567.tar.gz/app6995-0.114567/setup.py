8241f89bbdefd8e9f52a4478db7db2138be6d9c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:40 +0000

    Add package app6995/0.114567/app6995-0.114567.tar.gz

---
+++ packages/app6995/app6995-0.114567.tar.gz/app6995-0.114567/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='app6995',
+      version='0.114567',
+      description='operation & greeting library',
+      packages=['app'],
+      author_email='piyush@gmail.com',
+      zip_safe=False)
