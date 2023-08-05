b87028433e5cb997610fc208fd1af95a7b660dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:19:48 +0000

    Add package Juwell/2.1.7/Juwell-2.1.7-py3-none-any.whl

---
+++ packages/Juwell/Juwell-2.1.7-py3-none-any.whl/Juwel/Main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+from Juwel import Json_transf
+#import Json_transf
+
+
+
+def main():
+    Json_transf.json_transformer()
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
