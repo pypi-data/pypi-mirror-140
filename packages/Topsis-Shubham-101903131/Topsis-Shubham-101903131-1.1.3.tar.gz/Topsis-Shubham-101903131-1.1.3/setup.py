d4d71f8b7ebd61a411ae1e9ba5990f5f40208df6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:05 +0000

    Add package Topsis-Shubham-101903131/1.1.3/Topsis-Shubham-101903131-1.1.3.tar.gz

---
+++ packages/Topsis-Shubham-101903131/Topsis-Shubham-101903131-1.1.3.tar.gz/Topsis-Shubham-101903131-1.1.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="Topsis-Shubham-101903131",
+    version="1.1.3",
+    description="A Python package implementing TOPSIS technique.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author="Shubham Trivedi",
+    author_email="strivedi_be19@thapar.edu",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["topsis_py"],
+    include_package_data=True,
+    install_requires=['numpy',
+                      'pandas'
+                      ],
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis_py.topsis:main",
+        ]
+    },
+)
