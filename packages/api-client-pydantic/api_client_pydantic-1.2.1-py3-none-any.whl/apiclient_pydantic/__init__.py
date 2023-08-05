bab9105c95c9ffd742f634bf745efc59319746d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:06 +0000

    Add package api-client-pydantic/1.2.1/api_client_pydantic-1.2.1-py3-none-any.whl

---
+++ packages/api-client-pydantic/api_client_pydantic-1.2.1-py3-none-any.whl/apiclient_pydantic/__init__.py
@@ -0,0 +1,8 @@
+from .serializers import serialize, serialize_all_methods, serialize_request, serialize_response
+
+__all__ = (
+    'serialize',
+    'serialize_all_methods',
+    'serialize_request',
+    'serialize_response',
+)
