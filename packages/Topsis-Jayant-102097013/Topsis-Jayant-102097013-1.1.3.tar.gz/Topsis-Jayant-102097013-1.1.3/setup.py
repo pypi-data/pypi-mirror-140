e81213fa1cac71de05378b2e77baa7232ceb28a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:35 +0000

    Add package Topsis-Jayant-102097013/1.1.3/Topsis-Jayant-102097013-1.1.3.tar.gz

---
+++ packages/Topsis-Jayant-102097013/Topsis-Jayant-102097013-1.1.3.tar.gz/Topsis-Jayant-102097013-1.1.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(  
+  name = 'Topsis-Jayant-102097013',
+  packages = ['topsis'],
+  version = '1.1.3',
+  license='MIT',
+  description = 'API and CLI tool to calculate Topsis, CLI tool inputs CSV/Excel files',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author = 'Jayant Katia',
+  author_email = 'jayantkatia65@gmail.com',
+  url = 'https://github.com/jayantkatia/topsis',
+  download_url = 'https://github.com/jayantkatia/topsis/archive/refs/tags/v1.1.3.tar.gz',
+  keywords = ['topsis', 'python', 'pypi', 'csv', 'xlsx', 'xls', 'cli'],
+  install_requires=[
+          'numpy',
+          'pandas',
+      ],
+  entry_points={
+    'console_scripts': [
+      'topsis = topsis.topsis:main'
+      ]
+  },
+)
