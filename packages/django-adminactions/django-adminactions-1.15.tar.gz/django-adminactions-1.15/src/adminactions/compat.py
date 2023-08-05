810376f2c4887a07fad56b3e20ea5f92ad8543ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:44 +0000

    Add package django-adminactions/1.15/django-adminactions-1.15.tar.gz

---
+++ packages/django-adminactions/django-adminactions-1.15.tar.gz/django-adminactions-1.15/src/adminactions/compat.py
@@ -0,0 +1,15 @@
+import django
+import django.db.transaction as t
+
+
+class NoCommit(t.Atomic):
+    def __exit__(self, exc_type, exc_value, traceback):
+        super().__exit__(Exception, Exception(), None)
+
+
+if django.VERSION[0] == 2:
+    def nocommit(using=None, savepoint=True):
+        return NoCommit(using, savepoint)
+else:
+    def nocommit(using=None, savepoint=True, durable=False):
+        return NoCommit(using, savepoint, durable)
