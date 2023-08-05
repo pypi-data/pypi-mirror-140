fc8125d037ab0ef5848b642fb60ac8e2afb95b3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:52 +0000

    Add package check-11234/0.1/check-11234-0.1.tar.gz

---
+++ packages/check-11234/check-11234-0.1.tar.gz/check-11234-0.1/check-11234/operation.py
@@ -0,0 +1,17 @@
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
+def greet():
+    return "Hello World"
+
+def square(a):
+    return a*a
---
+++ packages/check-11234/check-11234-0.1.tar.gz/check-11234-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='check-11234',
+    version='0.1',
+    description='A test package',
+    packages = ['check-11234'],
+    author_email= 'piyushshukla356@gmail.com',
+    zip_safe=False
+)
+
