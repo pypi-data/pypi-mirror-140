e36ac8ee1a2082df194426813fa5f27a708208ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:06 +0000

    Add package shabbir999/9.1199/shabbir999-9.1199.tar.gz

---
+++ packages/shabbir999/shabbir999-9.1199.tar.gz/shabbir999-9.1199/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='shabbir999',
+      version='9.1199',
+      description='operation & greeting library',
+      packages=['shabbir-library'],
+      author_email='shab@gmail.com',
+      zip_safe=False)
