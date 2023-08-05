f77af8668ac0debf6122bad85819d689ae9261cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:27:40 +0000

    Add package databricks-sql-connector/2.0.0b0/databricks-sql-connector-2.0.0b0.tar.gz

---
+++ packages/databricks-sql-connector/databricks-sql-connector-2.0.0b0.tar.gz/databricks-sql-connector-2.0.0b0/src/databricks/sql/exc.py
@@ -0,0 +1,45 @@
+### PEP-249 Mandated ###
+class Error(Exception):
+    pass
+
+
+class Warning(Exception):
+    pass
+
+
+class InterfaceError(Error):
+    pass
+
+
+class DatabaseError(Error):
+    pass
+
+
+class InternalError(DatabaseError):
+    pass
+
+
+class OperationalError(DatabaseError):
+    pass
+
+
+class ProgrammingError(DatabaseError):
+    pass
+
+
+class IntegrityError(DatabaseError):
+    pass
+
+
+class DataError(DatabaseError):
+    pass
+
+
+class NotSupportedError(DatabaseError):
+    pass
+
+
+### Custom error classes ###
+class InvalidServerResponseError(OperationalError):
+    """ Thrown if the server does not set the initial namespace correctly"""
+    pass
