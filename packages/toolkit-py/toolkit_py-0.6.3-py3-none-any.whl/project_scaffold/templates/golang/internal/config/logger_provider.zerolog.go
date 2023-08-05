81e77a1ef1f9f01b783539f10d6a194f430493fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:20:21 +0000

    Add package toolkit-py/0.6.3/toolkit_py-0.6.3-py3-none-any.whl

---
+++ packages/toolkit-py/toolkit_py-0.6.3-py3-none-any.whl/project_scaffold/templates/golang/internal/config/logger_provider.zerolog.go
@@ -0,0 +1,34 @@
+{{GOLANG_HEADER}}
+
+package {{GOLANG_PACKAGE}}
+
+import (
+	"github.com/rs/zerolog"
+
+	"{{GOLANG_MODULE}}/internal/event"
+)
+
+
+func (c *config) initLogger() {
+	writerOnce.Do(func() {
+		if c.DetachServer() {
+			event.SetLogger(zerolog.New(c.LogWriter()).
+				With().
+				CallerWithSkipFrameCount(3).
+				Logger(),
+			)
+
+			event.SetLevel(c.LogLevel())
+		}
+	})
+}
+
+func (c *config) LogLevel() zerolog.Level {
+	level, err := zerolog.ParseLevel(c.settings.Logger.Level)
+
+	if err != nil {
+		return zerolog.InfoLevel
+	}
+
+	return level
+}
