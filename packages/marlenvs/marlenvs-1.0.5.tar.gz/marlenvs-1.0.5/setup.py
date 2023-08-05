2ff03f52251b68ed0c1036bd1353c42c5c34c080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:36 +0000

    Add package marlenvs/1.0.5/marlenvs-1.0.5.tar.gz

---
+++ packages/marlenvs/marlenvs-1.0.5.tar.gz/marlenvs-1.0.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+		name='marlenvs',
+		version='1.0.5',
+		author='Lukas König',
+		author_email='lukasmkoenig@gmx.net',
+        packages=find_packages(),
+		python_requires='>=3.9',
+		install_requires=['gym', 'numpy', 'wheel', 'pyglet'],
+		decription="Multi-Agent Reinforcement Learning environments for gym."
+	)
