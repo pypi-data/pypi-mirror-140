40816209494f282d83df87e6eec66cb8eebbb45a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:20:18 +0000

    Add package karp-backend/6.0.5/karp-backend-6.0.5.tar.gz

---
+++ packages/karp-backend/karp-backend-6.0.5.tar.gz/karp-backend-6.0.5/karp/foundation/commands.py
@@ -0,0 +1,34 @@
+import abc
+from typing import Any, Generic, TypeVar
+
+import injector
+
+
+CommandType = TypeVar('CommandType')
+
+
+class CommandHandler(Generic[CommandType]):
+    def execute(self, command: CommandType) -> Any:
+        raise NotImplementedError()
+
+
+class Command:
+    pass
+
+
+class CommandBus(abc.ABC):
+    @abc.abstractmethod
+    def dispatch(self, command: Command) -> None:
+        raise NotImplementedError
+
+
+class InjectorCommandBus(CommandBus):
+    def __init__(self, injector: injector.Injector) -> None:
+        self._injector = injector
+
+    def dispatch(self, command: Command) -> None:
+        cmd_cls = type(command)
+        cmd_handler = self._injector.get(
+            CommandHandler[cmd_cls])  # type: ignore
+
+        cmd_handler.execute(command)
