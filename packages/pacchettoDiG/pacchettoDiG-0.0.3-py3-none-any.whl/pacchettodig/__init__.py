b5efb02c907e6dfebdc16d74d4b1e04d4378e465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:20 +0000

    Add package pacchettoDiG/0.0.3/pacchettoDiG-0.0.3-py3-none-any.whl

---
+++ packages/pacchettoDiG/pacchettoDiG-0.0.3-py3-none-any.whl/pacchettodig/__init__.py
@@ -0,0 +1,18 @@
+__version__= "0.0.3"
+__doc__= "Useless string"
+
+
+
+import sys
+from twiggy import quick_setup,log
+import numpy as np
+from scipy import stats
+from scipy.special import factorial
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from scipy.fft import fft, ifft
+from pckg.module2 import *
+from pckg.module1 import *
+
+quick_setup()
+logger= log.name('lossaggregation')
