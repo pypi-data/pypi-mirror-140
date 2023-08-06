c2281a9dd26c0813c3560b5aaf5b19b3226ea4dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:33 +0000

    Add package pynets/1.28/pynets-1.28-py3.7.egg

---
+++ packages/pynets/pynets-1.28-py3.7.egg/pynets/core/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """Derek Pisner"""
+__email__ = "dpisner@utexas.edu"
+
+import matplotlib
+from . import cloud, interfaces, nodemaker, thresholding, utils, workflows
+import warnings
+
+matplotlib.use('Agg')
+warnings.filterwarnings("ignore")
