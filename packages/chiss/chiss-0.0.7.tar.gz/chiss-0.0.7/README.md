f7707f7a52334c64758cb6409188c0c5b6a9b105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:46 +0000

    Add package chiss/0.0.7/chiss-0.0.7.tar.gz

---
+++ packages/chiss/chiss-0.0.7.tar.gz/chiss-0.0.7/README.md
@@ -0,0 +1,7 @@
+# chissProject
+
+<<<<<<< HEAD
+Mon but : Aider les rats de france
+=======
+Mon but : Aider les rats de france
+>>>>>>> 2aa1e4385475c26587ac81c400ed66278cebbda3
---
+++ packages/chiss/chiss-0.0.7.tar.gz/chiss-0.0.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.0.7'
+DESCRIPTION = "Le module chiss pour les crevards"
+LONG_DESCRIPTION = "Le module chiss a été créer avec amour pour détécter les plus gros rat de france"
+
+setup(
+    name='chiss',
+    version=VERSION,
+    author="jachou",
+    author_email="jachoumartayan@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    url='https://github.com/Jachou-yt/chissProject',
+    packages=find_packages('src'),
+    install_requires=[],
+    keywords=['python','rat','crevard','ratus','fr','pince'],
+    classifiers=[
+    ]
+)
+
