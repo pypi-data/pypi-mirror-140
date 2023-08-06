94c7df8d59576b78b0f9319b8beacd35aad11d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:28 +0000

    Add package dsnd-distributions-SAS/0.1/dsnd_distributions_SAS-0.1.tar.gz

---
+++ packages/dsnd-distributions-SAS/dsnd_distributions_SAS-0.1.tar.gz/dsnd_distributions_SAS-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd_distributions_SAS',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['dsnd_distributions_SAS'],
+      author = 'Samara Sadeek',
+      zip_safe=False)
