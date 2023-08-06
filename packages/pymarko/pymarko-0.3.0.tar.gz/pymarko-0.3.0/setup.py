26c83d3768d2ef1e0487548d9f95f48f0321de5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:45 +0000

    Add package pymarko/0.3.0/pymarko-0.3.0.tar.gz

---
+++ packages/pymarko/pymarko-0.3.0.tar.gz/pymarko-0.3.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pymarko']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['colorama']
+
+setup_kwargs = {
+    'name': 'pymarko',
+    'version': '0.3.0',
+    'description': 'A multicast framework and tools',
+    'long_description': '# PyMarko\n\n**in development**\n\n# MIT License\n\n**Copyright (c) 2018 Kevin J. Walchko**\n\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the "Software"), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all\ncopies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\nSOFTWARE.\n',
+    'author': 'walchko',
+    'author_email': 'walchko@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://pypi.org/project/pymarko/',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8',
+}
+
+
+setup(**setup_kwargs)
