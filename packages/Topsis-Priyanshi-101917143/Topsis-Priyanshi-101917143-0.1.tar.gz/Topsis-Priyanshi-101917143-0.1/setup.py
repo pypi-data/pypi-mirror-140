0684f4583780624ce8ee6efd80b42e0d05b379f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:00 +0000

    Add package Topsis-Priyanshi-101917143/0.1/Topsis-Priyanshi-101917143-0.1.tar.gz

---
+++ packages/Topsis-Priyanshi-101917143/Topsis-Priyanshi-101917143-0.1.tar.gz/Topsis-Priyanshi-101917143-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+  name = 'Topsis-Priyanshi-101917143',        
+  version = '0.1',      
+  license='MIT',        
+  description = 'Topsis implementation',  
+  author = 'Priyanshi Gangwar',                   
+  author_email = 'gangwarpriyanshi@gmail.com',
+  url = '',     
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License', 
+    'Programming Language :: Python :: 3',     
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
