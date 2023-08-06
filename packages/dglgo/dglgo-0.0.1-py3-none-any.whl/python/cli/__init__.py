2387ed86a4dd919ac2d065a38aac9987ea6ee5ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:45 +0000

    Add package dglgo/0.0.1/dglgo-0.0.1-py3-none-any.whl

---
+++ packages/dglgo/dglgo-0.0.1-py3-none-any.whl/python/cli/__init__.py
@@ -0,0 +1,10 @@
+from .cli import *
+# from .co
+# import typer
+# from .config_cli import config
+
+# app = typer.Typer()
+# app.add_typer(config_app, name="config")
+
+# if __name__ == "__main__":
+#     app()
---
+++ packages/dglgo/dglgo-0.0.1-py3-none-any.whl/python/cli/cli.py
@@ -0,0 +1,21 @@
+import typer
+# import contextlib
+# import io
+# output = io.StringIO()
+# with contextlib.redirect_stdout(output):
+# from ..pipeline import *
+# from ..model import *
+# from .config_cli import config_app
+# from .train_cli import train
+# from .export_cli import export
+
+app = typer.Typer(no_args_is_help=True)
+# app.add_typer(config_app, name="config", no_args_is_help=True)
+# app.command(help="Train the model", no_args_is_help=True)(train)
+# app.command(help="Export the python file from config", no_args_is_help=True)(export)
+
+def main():
+    app()
+
+if __name__ == "__main__":
+    app()
