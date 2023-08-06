db016fdd8a01728d2aa610def4616c879d038949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:29 +0000

    Add package pyjdict/1.0.6/pyjdict-1.0.6.tar.gz

---
+++ packages/pyjdict/pyjdict-1.0.6.tar.gz/pyjdict-1.0.6/src/jdict/transformer.py
@@ -0,0 +1,58 @@
+import ast
+from typing import Any, Final
+
+JDICT: Final[str] = "jdict"  # WPS226 Found string literal over-use
+
+
+class JdictTransformer(ast.NodeTransformer):
+    """
+    The visitor class of the node that traverses,
+    the abstract syntax tree and calls the visitor function
+    for each node found. Inherits from class NodeTransformer
+    """
+
+    def visit_Module(self, node: ast.AST) -> Any:
+        """
+        Method imports jdict module into ast
+        :param node: CodeType
+        :return: node
+        """
+        visited_node = self.generic_visit(node)
+        import_node = ast.ImportFrom(
+            module=JDICT, names=[ast.alias(name=JDICT)], level=0
+        )
+        visited_node.body.insert(0, import_node)
+        return visited_node
+
+    def visit_Name(self, node: ast.Name) -> Any:
+        """
+        Method checks if the id dict and modifies it to jdict
+        :param node:
+        :return: node
+        """
+        if node.id == "dict":
+            node.id = JDICT
+        return self.generic_visit(node)
+
+    def visit_Dict(self, node: ast.AST) -> Any:
+        """
+        Method goes into the dict node and modifies it to jdict
+        :param node:
+        :return:
+        """
+        node = self.generic_visit(node)
+        name_node = ast.Name(id="jdict", ctx=ast.Load())
+        return ast.Call(func=name_node, args=[node], keywords=[])
+
+
+def transform(src: str) -> Any:
+    """
+    Transforms the given source to use jdict to replace built in structures.
+    :param src:  str
+    :return: new_tree
+    """
+    tree: Final = ast.parse(src)
+    transformer: Final = JdictTransformer()
+    new_tree = transformer.visit(tree)
+    ast.fix_missing_locations(new_tree)
+    return new_tree
---
+++ packages/pyjdict/pyjdict-1.0.6.tar.gz/pyjdict-1.0.6/test/unit/test_typed_jdict.py
@@ -0,0 +1,63 @@
+import json
+from typing import Final, Optional
+from unittest import TestCase, main
+
+from jdict import jdict, set_json_decoder
+
+
+class Point2D(jdict):
+    x: Final[int]
+    y: int
+    label: Optional[str]
+
+
+class TestTypedJdict(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        set_json_decoder(json)
+        cls.point = Point2D(x=10, y=25, label="first point")
+
+    def _validate_point(self, p) -> None:
+        self.assertEqual(10, p.x)
+        if issubclass(int, type(p.y)):
+            self.assertEqual(25, p.y)
+        else:
+            self.assertNotEqual(25, p.y)
+        if hasattr(p, "label"):
+            self.assertEqual("first point", p.label)
+
+    def test_normal_initialization(self):
+        self._validate_point(self.point)
+
+    def test_no_optional_initialization(self):
+        self._validate_point(Point2D(x=10, y=25))
+
+    def test_wrong_type(self):
+        p = Point2D(x=10, y="25", label="first point")  # PyCharm will not complain :-(
+        self._validate_point(p)
+
+    def test_missing_argument(self):
+        self.assertRaises(
+            TypeError, Point2D, x=10, label="first point"
+        )  # PyCharm will not complain, but it fails in run-time
+
+    def test_update_existing(self):
+        self.point.y = 45
+        self.assertEqual(45, self.point.y)
+
+    def test_update_optional(self):
+        p = Point2D(x=10, y=25)
+        p.label = "missing label"
+        self.assertEqual("missing label", p.label)
+
+    def test_update_final(self):
+        self.point.x = 45  # static type checkers and linters will complain about Final
+        self.assertEqual(45, self.point.x)
+
+    def test_set_non_existent(self):
+        self.point.z = 45
+        self.assertEqual(45, self.point.z)
+
+
+if __name__ == "__main__":
+    main()
