1d5e0d1d332d976c8657cb5cf46216add19ffbc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:49 +0000

    Add package sparkpipelineframework/1.0.11/sparkpipelineframework-1.0.11-py3-none-any.whl

---
+++ packages/sparkpipelineframework/sparkpipelineframework-1.0.11-py3-none-any.whl/spark_pipeline_framework/logger/yarn_logger.py
@@ -0,0 +1,23 @@
+from os import environ
+from sys import stderr
+from logging import Logger, getLogger, StreamHandler, Formatter, INFO
+from typing import Union, TextIO
+
+
+def get_logger(name: str, level: Union[int, str] = INFO) -> Logger:
+    logger: Logger = getLogger(name)
+    level = environ.get("LOGLEVEL") or level
+    logger.setLevel(level)
+
+    if logger.handlers:
+        pass
+    else:
+        stream_handler: StreamHandler[TextIO] = StreamHandler(stderr)
+        stream_handler.setLevel(level=level)
+        # noinspection SpellCheckingInspection
+        formatter: Formatter = Formatter(
+            "%(asctime)s.%(msecs)03d %(levelname)s %(module)s %(lineno)d - %(funcName)s: %(message)s"
+        )
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
+    return logger
