2a80da62efa2c770d244c0f229de9483abaca1eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:29 +0000

    Add package bitsoframework/1.0.79/bitsoframework-1.0.79-py3-none-any.whl

---
+++ packages/bitsoframework/bitsoframework-1.0.79-py3-none-any.whl/bitsoframework/utils/numbers.py
@@ -0,0 +1,10 @@
+import decimal
+
+
+def drop_zeros(number):
+    if not isinstance(number, decimal.Decimal):
+        number = decimal.Decimal(number)
+
+    result = number.normalize()
+    # e.g 22000 --> Decimal('2.2E+4')
+    return result.__trunc__() if not result % 1 else float(result)
