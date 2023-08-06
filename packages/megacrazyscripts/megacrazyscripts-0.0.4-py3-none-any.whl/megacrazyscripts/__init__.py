b6fdd1c76e370cd3cd84241e9d12001881872e8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:10 +0000

    Add package megacrazyscripts/0.0.4/megacrazyscripts-0.0.4-py3-none-any.whl

---
+++ packages/megacrazyscripts/megacrazyscripts-0.0.4-py3-none-any.whl/megacrazyscripts/__init__.py
@@ -0,0 +1,6 @@
+from jcow_utils.converter import converter
+from jcow_utils.mix_audio_image import mix_audio_image
+from jcow_utils.slowedreverb import slowedreverb
+from jcow_utils.thumbnail_img import thumbnail_img
+from jcow_utils.video_img import video_img
+from jcow_utils.ytdw import ytdw
