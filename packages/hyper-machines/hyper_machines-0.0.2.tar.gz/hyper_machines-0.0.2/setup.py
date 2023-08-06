2a6826a001099a1a8d20659ac36020b3313e44c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:48 +0000

    Add package hyper-machines/0.0.2/hyper_machines-0.0.2.tar.gz

---
+++ packages/hyper-machines/hyper_machines-0.0.2.tar.gz/hyper_machines-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="hyper_machines",
+    version="0.0.2",
+    packages=["hyper_machines"],
+    url="https://github.com/Hypercookie/PythonStateMachine",
+    license="MIT",
+    author="hypercookie",
+    author_email="hypercookie@gmx.de",
+    description="Machines for Python",
+    download_url = "https://github.com/Hypercookie/PythonStateMachine/archive/refs/tags/0.0.2.tar.gz",
+
+)
+
