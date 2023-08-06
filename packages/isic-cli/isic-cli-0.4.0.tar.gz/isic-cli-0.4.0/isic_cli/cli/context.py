f7fccede066d50988030aa118c9b3eba27ac9d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:53 +0000

    Add package isic-cli/0.4.0/isic-cli-0.4.0.tar.gz

---
+++ packages/isic-cli/isic-cli-0.4.0.tar.gz/isic-cli-0.4.0/isic_cli/cli/context.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from girder_cli_oauth_client import GirderCliOAuthClient
+
+from isic_cli.session import IsicCliSession
+
+
+@dataclass
+class IsicContext:
+    oauth: GirderCliOAuthClient
+    session: IsicCliSession
+    logged_in: bool
+    env: str  # One of dev/sandbox/prod
+    user: Optional[dict] = None
+    verbose: Optional[bool] = False
