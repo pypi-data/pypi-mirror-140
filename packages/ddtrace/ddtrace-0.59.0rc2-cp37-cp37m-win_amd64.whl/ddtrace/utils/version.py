c7e708d2b9805e832d6ea4f7e26ee87ebe4cd34f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:19:40 +0000

    Add package ddtrace/0.59.0rc2/ddtrace-0.59.0rc2-cp37-cp37m-win_amd64.whl

---
+++ packages/ddtrace/ddtrace-0.59.0rc2-cp37-cp37m-win_amd64.whl/ddtrace/utils/version.py
@@ -0,0 +1,8 @@
+from ..internal.utils.version import parse_version  # noqa
+from ..vendor.debtcollector.removals import removed_module
+
+
+removed_module(
+    module="ddtrace.utils.version",
+    removal_version="1.0.0",
+)
