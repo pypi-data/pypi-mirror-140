695434d2a4af00df03fb1c7eb6c1e7f33e7dc8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:12 +0000

    Add package django-loci/1.0/django-loci-1.0.tar.gz

---
+++ packages/django-loci/django-loci-1.0.tar.gz/django-loci-1.0/django_loci/channels/asgi.py
@@ -0,0 +1,25 @@
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.urls import path
+
+from django_loci.channels.base import location_broadcast_path
+from django_loci.channels.consumers import LocationBroadcast
+
+channel_routing = ProtocolTypeRouter(
+    {
+        'websocket': AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(
+                    [
+                        path(
+                            location_broadcast_path,
+                            LocationBroadcast.as_asgi(),
+                            name='LocationChannel',
+                        )
+                    ]
+                )
+            )
+        )
+    }
+)
