2a1fc11f3d4dbeccc0b33c14dcd573c180f34a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:18:23 +0000

    Add package rath/0.1.10/rath-0.1.10.tar.gz

---
+++ packages/rath/rath-0.1.10.tar.gz/rath-0.1.10/rath/links/compose.py
@@ -0,0 +1,58 @@
+import asyncio
+from os import link
+from typing import List
+from rath.links.base import ContinuationLink, Link, TerminatingLink
+from rath.operation import Operation
+
+
+class ComposedLink(TerminatingLink):
+    def __init__(self, links: List[Link]):
+        assert isinstance(
+            links[-1], TerminatingLink
+        ), "Last link must be a TerminatingLink"
+        for link in links[:-1]:
+            assert isinstance(
+                link, ContinuationLink
+            ), f"All but the last must be ContinuationLinks: check {link}"
+        self.links = links
+        self.first_link = links[0]
+
+    def __call__(self, rath):
+        for i in range(len(self.links) - 1):
+            self.links[i](rath, self.links[i + 1])
+
+        self.links[-1](rath)  # last one gets only rath
+
+    async def aconnect(self):
+        await self.first_link.aconnect()
+
+    async def adisconnect(self):
+        await self.first_link.adisconnect()
+
+    def connect(self):
+        self.first_link.connect()
+
+    def disconnect(self):
+        self.first_link.disconnect()
+
+    async def aquery(self, operation: Operation):
+        return await self.first_link.aquery(operation)
+
+    async def asubscribe(self, operation: Operation):
+        async for result in self.first_link.asubscribe(operation):
+            yield result
+
+    def query(self, operation: Operation):
+        return self.first_link.query(operation)
+
+    def subscribe(self, operation: Operation):
+        for result in self.first_link.subscribe(operation):
+            yield result
+
+
+def compose(*links: List[Link]) -> ComposedLink:
+    """
+    Composes a list of Links into a single Link.
+    """
+
+    return ComposedLink(links)
