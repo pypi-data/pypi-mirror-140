d3f934bb421bfa02439d225c9fd00fb14ea294a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:36 +0000

    Add package pytest-flake8-v2/1.2.3/pytest-flake8-v2-1.2.3.tar.gz

---
+++ packages/pytest-flake8-v2/pytest-flake8-v2-1.2.3.tar.gz/pytest-flake8-v2-1.2.3/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+"""Install package."""
+
+import setuptools
+
+setuptools.setup(
+    name="pytest-flake8-v2",
+    version="1.2.3",
+    description="pytest plugin to check FLAKE8 requirements",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Testing",
+        "Operating System :: OS Independent",
+    ],
+    license="BSD License",
+    author="Thorsten Lockert",
+    author_email="tholo@sigmasoft.com",
+    maintainer="Ilja Orlovs",
+    maintainer_email="vrghost@gmail.com",
+    url="https://github.com/VRGhost/pytest-flake8",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    py_modules=[
+        "pytest_flake8",
+    ],
+    entry_points={
+        "pytest11": ["flake8 = pytest_flake8"],
+    },
+    install_requires=[
+        "flake8>=4.0",
+        "pytest>=7.0",
+    ],
+    python_requires=">=3.6",
+)
