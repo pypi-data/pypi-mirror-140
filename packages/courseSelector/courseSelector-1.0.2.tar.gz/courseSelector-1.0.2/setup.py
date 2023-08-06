213e17cbef643784e1704c0bfcc07a7615925abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:18:44 +0000

    Add package courseSelector/1.0.2/courseSelector-1.0.2.tar.gz

---
+++ packages/courseSelector/courseSelector-1.0.2.tar.gz/courseSelector-1.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='courseSelector',
+    version='1.0.2',
+    author='Tiancheng Jiao',
+    author_email='jtc1246@outlook.com',
+    url='https://github.com/jtc1246/courseSelector',
+    description='An automatic course selection program for SJTU-JI',
+    packages=['courseSelector'],
+    install_requires=['urllib3'],
+    python_requires='>=3',
+    platforms=["all"],
+    license='GPL-2.0 License',
+    entry_points={
+        'console_scripts': [
+            'help=courseSelector:help',
+            'check=courseSelector:check',
+            'printCourseList=courseSelector:printCourseList',
+            'waitEmptySpace=courseSelector:waitEmptySpace',
+            'fastSelect=courseSelector:fastSelect',
+            'luckyDraw=courseSelector:luckyDraw'
+        ]
+    }
+)
+
+
