0a98a3b0e0f5fda4b50544c809ebef5d51616a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:07 +0000

    Add package prime-test/0.2.6/prime_test-0.2.6.tar.gz

---
+++ packages/prime-test/prime_test-0.2.6.tar.gz/prime_test-0.2.6/src/prime_test/prime.py
@@ -0,0 +1,13 @@
+import random
+def test(n, k = 40):
+    if n < 2 or (n % 2 == 0 and n > 2): return False
+    if n in [2, 3]: return True
+    s = n - 1
+    for _ in range(k):
+        a = random.randrange(2, n - 1)
+        x = pow(a, s, n)
+        if x in [1, -1]:
+            continue
+        else:
+            return False
+    return True
