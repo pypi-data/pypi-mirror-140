c843972e682f3759257ede87127b60068328de3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:33 +0000

    Add package pyfinra/0.2.1/pyfinra-0.2.1.tar.gz

---
+++ packages/pyfinra/pyfinra-0.2.1.tar.gz/pyfinra-0.2.1/pyfinra/financials/tools/finraStockID.py
@@ -0,0 +1,23 @@
+import requests
+import json
+from . import cookieGetter
+
+
+
+def getFinraStockID(ticker):
+
+  cookies = cookieGetter.get() 
+
+  s = requests.Session()
+  s.cookies = cookies
+
+  url = f"http://finra-markets.morningstar.com/acb.jsp?&condition=ST,FE,FC,FO,2,1,7&acbinstid=FINRA&kw={ticker}"
+  response = s.get( url )
+  symbols = json.loads(response.text)
+  secId = ""
+  for symbol in symbols["result"]:
+      if symbol["AC001"] == ticker.upper():
+          secId = symbol["SecId"]
+          break
+  return secId
+
