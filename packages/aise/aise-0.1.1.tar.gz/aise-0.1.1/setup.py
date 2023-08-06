4a04baadc9c8707a49d297e1403cdc351bcc5665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:20:07 +0000

    Add package aise/0.1.1/aise-0.1.1.tar.gz

---
+++ packages/aise/aise-0.1.1.tar.gz/aise-0.1.1/setup.py
@@ -0,0 +1,46 @@
+import io
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+
+
+setup(
+    name="aise",
+    version="0.1.1",
+    url="https://github.com/johnjrhunglin/aise",
+    license='MIT',
+
+    author="John J. H. Lin",
+    author_email="john.jrhunglin@gmail.com",
+
+    description="Functions used to analyze data in science education",
+    long_description=read("README.rst"),
+
+    packages=find_packages(exclude=('tests',)),
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    include_package_data=True,
+    package_data={'': ['dataset/data/*.csv']},
+)
