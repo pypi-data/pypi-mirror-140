79bafd26054fe6aafe8daa5ff72cec8ddb554299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:17 +0000

    Add package strictdoc/0.0.20/strictdoc-0.0.20-py3-none-any.whl

---
+++ packages/strictdoc/strictdoc-0.0.20-py3-none-any.whl/strictdoc/backend/sdoc/models/type_system.py
@@ -0,0 +1,47 @@
+from typing import List
+
+
+class GrammarElementField:
+    pass
+
+
+class GrammarElementFieldString(GrammarElementField):
+    def __init__(self, parent, title: str, required: str):
+        self.parent = parent
+        self.title: str = title
+        self.required: bool = required == "True"
+
+
+class GrammarElementFieldSingleChoice(GrammarElementField):
+    def __init__(  # pylint: disable=too-many-arguments
+        self,
+        parent,
+        title: str,
+        options: List[str],
+        required: str,
+    ):
+        self.parent = parent
+        self.title: str = title
+        self.options: List[str] = options
+        self.required: bool = required == "True"
+
+
+class GrammarElementFieldMultipleChoice(GrammarElementField):
+    def __init__(  # pylint: disable=too-many-arguments
+        self,
+        parent,
+        title: str,
+        options: List[str],
+        required: str,
+    ):
+        self.parent = parent
+        self.title: str = title
+        self.options: List[str] = options
+        self.required: bool = required == "True"
+
+
+class GrammarElementFieldTag(GrammarElementField):
+    def __init__(self, parent, title: str, required: str):
+        self.parent = parent
+        self.title: str = title
+        self.required: bool = required == "True"
