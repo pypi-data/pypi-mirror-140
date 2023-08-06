fa902760d49f3a7f1caecc18a93e9af028b88fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:39 +0000

    Add package mypy-gitlab-code-quality/0.0.11/mypy-gitlab-code-quality-0.0.11.tar.gz

---
+++ packages/mypy-gitlab-code-quality/mypy-gitlab-code-quality-0.0.11.tar.gz/mypy-gitlab-code-quality-0.0.11/mypy_gitlab_code_quality/__init__.py
@@ -0,0 +1,36 @@
+import json
+import re
+from base64 import b64encode
+from collections.abc import Hashable, Sequence
+from sys import byteorder, hash_info, stdin
+from typing import Dict, Optional, Union
+
+SEVERITY = {
+    "note": "info",
+    "error": "major",
+}
+
+
+def get_hash(tpl: Sequence[Hashable]) -> str:
+    return b64encode(hash(tpl).to_bytes(hash_info.width // 8, byteorder, signed=True)).decode()
+
+
+def parse_line(line: str) -> Optional[Dict[str, Union[str, Dict[str, Union[str, Dict[str, int]]]]]]:
+    match = re.fullmatch(r"(?P<path>.+?):(?P<line>\d+):(?:\d+:)?\s(?P<error_level>\w+):\s(?P<description>.+)", line)
+    if match is None:
+        return None
+    return {
+        "description": match["description"],
+        "fingerprint": get_hash(match.groups()),
+        "severity": SEVERITY.get(match["error_level"], "unknown"),
+        "location": {
+            "path": match["path"],
+            "lines": {
+                "begin": int(match["line"]),
+            },
+        },
+    }
+
+
+def main() -> None:
+    print(json.dumps(list(filter(None, (parse_line(line.rstrip("\n")) for line in stdin))), indent="\t"))
