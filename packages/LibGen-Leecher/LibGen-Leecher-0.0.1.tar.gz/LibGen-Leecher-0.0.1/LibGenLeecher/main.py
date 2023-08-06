dfbb6b2433046031df026fd47626aec7284889e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:29 +0000

    Add package LibGen-Leecher/0.0.1/LibGen-Leecher-0.0.1.tar.gz

---
+++ packages/LibGen-Leecher/LibGen-Leecher-0.0.1.tar.gz/LibGen-Leecher-0.0.1/LibGenLeecher/main.py
@@ -0,0 +1,52 @@
+from libgen_api import LibgenSearch
+import urllib.request
+import pprint
+
+print("\nWelcome to LibGen Leecher\n")
+
+def main():
+    download_file(url)
+
+def download_file(url):
+    book_download = book.replace(":","")
+    response = urllib.request.urlopen(url)
+    file = open(book_download + ".pdf", 'wb')
+    file.write(response.read())
+    file.close()
+    print("\nDownload complete. Enjoy!")
+
+def search():
+    try:
+        global book
+        book = str(input("What is the book title? "))
+        book_year = input("What is the year the book released? ")
+        s = LibgenSearch()
+        title_filters = {"Extension": "pdf", "Year": book_year}
+        results = s.search_title_filtered(book, title_filters, exact_match=True)
+        
+        pp = pprint.PrettyPrinter(indent=2, width=150)
+        print("\nResults: \n")
+        results_items = results[0].items()
+        results_filters = list(results_items)[1:8]
+        pp.pprint(results_filters)
+
+    except IndexError:
+        print("\nNo results. Try refining your search\n")
+        search()
+    
+    if results:
+        item_to_download = results[0]
+        download_links = s.resolve_download_links(item_to_download)
+        global url
+        url = download_links["Cloudflare"]
+        
+        correct_book = str(input("\nIs this the book you're looking for? [y/n] "))
+        if (correct_book.lower() == "y") or (correct_book.lower() == "yes"):
+            main()
+        elif (correct_book.lower() == "n") or (correct_book.lower() == "no"):
+            print("\nPlease refine your search\n")
+            search()
+        else:
+            print("\nInvalid input. \nExiting.")
+            quit()
+search()
