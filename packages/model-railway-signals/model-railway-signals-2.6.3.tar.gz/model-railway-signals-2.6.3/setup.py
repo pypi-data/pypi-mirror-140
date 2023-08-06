a79ea96d5855846f1195ced1ed1ac068267a0a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:34 +0000

    Add package model-railway-signals/2.6.3/model-railway-signals-2.6.3.tar.gz

---
+++ packages/model-railway-signals/model-railway-signals-2.6.3.tar.gz/model-railway-signals-2.6.3/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup
+from setuptools import find_packages
+
+print (find_packages())
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="model-railway-signals",
+    version="2.6.3",
+    packages=find_packages(),
+    include_package_data=True,
+    description="Create your own DCC model railway signalling scheme",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/johnrm174/model-railway-signalling",
+    author="johnrm174",
+    author_email="johnrm17418@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    install_requires=["pyserial","paho-mqtt"]
+
+)
