a2a38ef728cee7c5ee269f759f98bad392e57a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:23 +0000

    Add package toolchest-client/0.8.7/toolchest-client-0.8.7.tar.gz

---
+++ packages/toolchest-client/toolchest-client-0.8.7.tar.gz/toolchest-client-0.8.7/toolchest_client/tools/__init__.py
@@ -0,0 +1,11 @@
+from .tool import Tool
+from .alphafold import AlphaFold
+from .bowtie2 import Bowtie2
+from .cellranger import CellRangerCount
+from .kraken2 import Kraken2
+from .megahit import Megahit
+from .shi7 import Shi7
+from .shogun import ShogunAlign, ShogunFilter
+from .star import STARInstance
+from .test import Test
+from .unicycler import Unicycler
