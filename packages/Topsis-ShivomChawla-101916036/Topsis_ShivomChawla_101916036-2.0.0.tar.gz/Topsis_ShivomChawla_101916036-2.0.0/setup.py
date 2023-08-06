a9721b3d148edff6bd3e219f65d3e4c5d440ec4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:20 +0000

    Add package Topsis-ShivomChawla-101916036/2.0.0/Topsis_ShivomChawla_101916036-2.0.0.tar.gz

---
+++ packages/Topsis-ShivomChawla-101916036/Topsis_ShivomChawla_101916036-2.0.0.tar.gz/Topsis_ShivomChawla_101916036-2.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="Topsis_ShivomChawla_101916036",
+    version="2.0.0",
+    author="Shivom",
+    author_email="schawla_be19@thapar.edu",
+    description="A small package to work with topsis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shivom2k/Topsis_ShivomChawla_101916036",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
