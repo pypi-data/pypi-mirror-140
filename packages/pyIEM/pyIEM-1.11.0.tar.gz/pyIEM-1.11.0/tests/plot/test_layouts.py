2350d965e8329c9ec8f9354c961647842532feea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:01 +0000

    Add package pyIEM/1.11.0/pyIEM-1.11.0.tar.gz

---
+++ packages/pyIEM/pyIEM-1.11.0.tar.gz/pyIEM-1.11.0/tests/plot/test_layouts.py
@@ -0,0 +1,34 @@
+"""Test pyiem.plot.layouts."""
+
+# third party
+import pytest
+
+# local
+from pyiem.plot.layouts import figure_axes, figure
+
+
+@pytest.mark.mpl_image_compare(tolerance=0.1)
+def test_dpi():
+    """Test setting a dpi via autoplot context."""
+    fig, ax = figure_axes(
+        apctx={"_r": "169", "_dpi": "30"},
+    )
+    ax.plot([0, 1], [0, 1])
+    return fig
+
+
+@pytest.mark.mpl_image_compare(tolerance=0.1)
+def test_crawl_before_walk():
+    """Test that we can do basic things."""
+    fig, ax = figure_axes(
+        title="This is my Fancy Pants Title.",
+        subtitle="This is my Fancy Pants SubTitle.",
+    )
+    ax.plot([0, 1], [0, 1])
+    return fig
+
+
+def test_figsize():
+    """Test that figsize gets set properly."""
+    fig = figure(apctx={"_r": "169"})
+    assert fig.get_size_inches()[0] == 12.8
