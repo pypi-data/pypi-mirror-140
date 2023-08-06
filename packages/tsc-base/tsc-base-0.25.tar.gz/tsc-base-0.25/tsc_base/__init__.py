e76d510c899accbfa5b505cd3e0e0f8aa787868f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:51 +0000

    Add package tsc-base/0.25/tsc-base-0.25.tar.gz

---
+++ packages/tsc-base/tsc-base-0.25.tar.gz/tsc-base-0.25/tsc_base/__init__.py
@@ -0,0 +1,3 @@
+from .func import get_func_para, cover_dict, get, arg_extreme_dict, merge_dict, opt_value_dict, any_value_dict, pair_to_dict, dict_to_pair, put, recursion_stat, replace
+from .dependency import Excel, range_time, Bib
+from .web import bd_translate
