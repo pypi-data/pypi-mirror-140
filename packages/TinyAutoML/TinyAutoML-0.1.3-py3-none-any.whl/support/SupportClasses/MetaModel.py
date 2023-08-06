dbc5e9bb75808fc5af712077f202bbd7721149c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:57 +0000

    Add package TinyAutoML/0.1.3/TinyAutoML-0.1.3-py3-none-any.whl

---
+++ packages/TinyAutoML/TinyAutoML-0.1.3-py3-none-any.whl/support/SupportClasses/MetaModel.py
@@ -0,0 +1,95 @@
+import logging
+from datetime import datetime
+from typing import Any
+
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+from sklearn.base import BaseEstimator
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score, TimeSeriesSplit, StratifiedKFold, RandomizedSearchCV
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+
+from ..constants.GLOBAL_PARAMS import WINDOW
+from ..constants.gsp import estimators_params
+
+
+class MetaModel(BaseEstimator):
+    """
+    Meta estimator that trains other estimator to find the best
+    """
+
+    def __init__(self, grid_search: bool, n_splits=10):
+        self.best_estimator_ = None
+        self.best_estimator_index = None
+        self.estimators = [("rfc", RandomForestClassifier()),
+                           ("Logistic Regression", LogisticRegression(fit_intercept=True)),
+                           ('Gaussian Naive Bayes', GaussianNB()),
+                           ('LDA', LinearDiscriminantAnalysis()),
+                           ('AdaBoost', AdaBoostClassifier(DecisionTreeClassifier(max_depth=1),
+                                                            n_estimators= 200,
+                                                            algorithm= "SAMME.R",
+                                                            learning_rate=0.5)),
+                           ('xgb', xgb.XGBClassifier(learning_rate=0.02, n_estimators=600, objective='binary:logistic',
+                                                    silent=True,use_label_encoder=False))]
+        self.scores = pd.DataFrame()
+        self.n_splits = n_splits
+        self.grid_search = grid_search
+
+    def fit(self, X: pd.DataFrame, y: pd.Series, ) -> Any:
+
+        logging.info("Training models")
+
+        y_train = y[WINDOW:]
+
+        assert len(y[y == 1]) / len(y) <= 0.7, "The target is unbalanced"
+
+        if (X.index.dtype != datetime):
+            cv = StratifiedKFold(n_splits=self.n_splits)
+        else:
+            cv = TimeSeriesSplit(n_splits=self.n_splits)
+
+        dict_scores = {'mean': []}
+        if self.grid_search:
+            for estimator in self.estimators:
+                logging.info("\t----> {0}".format(estimator[0]))
+                if estimator[0] in estimators_params:
+                    clf = RandomizedSearchCV(estimator=estimator[1], param_distributions=estimators_params[estimator[0]], scoring='accuracy',
+                                       n_jobs=-1, cv=cv,refit=True)
+                    clf.fit(X, y_train)
+                    b_ix = np.argmax(clf.cv_results_['mean_test_score'])
+                    dict_scores['mean'].append(clf.cv_results_['mean_test_score'][b_ix])
+                else:
+                    cvs = cross_val_score(estimator[1], X, y_train, cv=cv, scoring='accuracy', n_jobs=-1)
+                    dict_scores['mean'].append(cvs.mean())
+
+            self.scores = pd.DataFrame(dict_scores, index=[estimator[0] for estimator in self.estimators])
+
+        else:
+            self.scores = pd.DataFrame(
+                data=[cross_val_score(estimator[1], X, y_train, cv=cv, scoring='accuracy', n_jobs=-1) for estimator in
+                      self.estimators], index=[estimator[0] for estimator in self.estimators],
+                columns=['Split {0}'.format(i) for i in range(self.n_splits)])
+
+            self.scores['mean'] = self.scores.mean(axis=1)
+
+        self.best_estimator_index = self.scores['mean'].argmax()
+
+        logging.info("The best estimator is {0} with a cross-validation accuracy (in Sample) of {1}".format(
+            self.estimators[self.best_estimator_index][0], self.scores['mean'].iloc[self.best_estimator_index]))
+
+        self.best_estimator_ = self.estimators[self.best_estimator_index][1].fit(X, y_train)
+
+        return self
+
+    def predict(self, X: pd.Series) -> pd.Series:
+        return self.best_estimator_.predict(X)
+
+    def predict_proba(self, X: pd.Series) -> pd.Series:
+        return self.best_estimator_.predict_proba(X)
+
+    def transform(self,X: pd.Series):
+        return X
---
+++ packages/TinyAutoML/TinyAutoML-0.1.3-py3-none-any.whl/support/SupportClasses/OneRulerForAll.py
@@ -0,0 +1,90 @@
+from datetime import datetime
+
+import pandas as pd
+import logging
+from sklearn.base import BaseEstimator
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import TimeSeriesSplit, StratifiedKFold, RandomizedSearchCV
+from sklearn.naive_bayes import GaussianNB
+
+from ..constants.GLOBAL_PARAMS import WINDOW
+from ..constants.gsp import estimators_params
+
+
+class OneRulerForAll(BaseEstimator):
+    """
+    Meta estimator that trains other estimator to find the best
+    """
+
+    def __init__(self, grid_search: bool, n_splits=10, ruler=None):
+
+        if ruler is None:
+            self.ruler = RandomForestClassifier()
+            self.ruler_name = 'rfc'
+        else:
+            self.ruler = ruler
+        self.estimators = [("rfc", RandomForestClassifier()),
+                           ("Logistic Regression", LogisticRegression(fit_intercept=True)),
+                           ('Gaussian Naive Bayes', GaussianNB()),
+                           ('LDA', LinearDiscriminantAnalysis()),
+                           ]
+        self.n_splits = n_splits
+        self.grid_search = grid_search
+
+    def fit(self, X: pd.DataFrame, y: pd.Series):
+
+        logging.info("Training models...")
+
+        y_train = y[WINDOW:]
+
+        assert len(y[y == 1]) / len(y) <= 0.7, "The target is unbalanced"
+
+        if X.index.dtype != datetime:
+            cv = StratifiedKFold(n_splits=self.n_splits)
+        else:
+            cv = TimeSeriesSplit(n_splits=self.n_splits)
+
+        if self.grid_search:
+            for estimator in self.estimators:
+                logging.info("\t----> {0}".format(estimator[0]))
+                if estimator[0] in estimators_params:
+                    clf = RandomizedSearchCV(estimator=estimator[1],
+                                             param_distributions=estimators_params[estimator[0]], scoring='accuracy',
+                                             n_jobs=-1, cv=cv)
+                    clf.fit(X, y_train)
+
+                    estimator[1].set_params(**clf.best_params_)
+
+                estimator[1].fit(X, y_train)
+
+            interm_ds = pd.DataFrame({estimator[0]: estimator[1].predict(X) for estimator in self.estimators})
+
+        else:
+            interm_ds = pd.DataFrame(
+                {estimator[0]: estimator[1].fit(X, y_train).predict(X) for estimator in self.estimators})
+
+        # self.final_estimator = GridSearchCV(estimator=xgb.XGBClassifier(use_label_encoder=False),
+        #                                           param_grid=estimators_params['xgb'],
+        #                                           scoring='accuracy',
+        #                                           n_jobs=-1, cv=cv, refit=True, verbose=0)
+
+        if self.ruler_name in estimators_params:
+            clf = RandomizedSearchCV(estimator=RandomForestClassifier(),
+                                     param_distributions=estimators_params[self.ruler_name], scoring='accuracy',
+                                     n_jobs=-1, cv=cv, refit=True)
+        self.ruler = clf.fit(interm_ds, y_train)
+
+        return self
+
+    def predict(self, X: pd.Series) -> pd.Series:
+        interm_ds = pd.DataFrame({estimator[0]: estimator[1].predict(X) for estimator in self.estimators})
+        return self.ruler.predict(interm_ds)
+
+    def predict_proba(self, X: pd.Series) -> pd.Series:
+        interm_ds = pd.DataFrame({estimator[0]: estimator[1].predict(X) for estimator in self.estimators})
+        return self.ruler.predict_proba(interm_ds)
+
+    def transform(self, X: pd.Series):
+        return X
