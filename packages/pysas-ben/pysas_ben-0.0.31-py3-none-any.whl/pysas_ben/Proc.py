1f9693e13df4a3674b29a6242c1423cfada45796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:44 +0000

    Add package pysas-ben/0.0.31/pysas_ben-0.0.31-py3-none-any.whl

---
+++ packages/pysas-ben/pysas_ben-0.0.31-py3-none-any.whl/pysas_ben/Proc.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+
+def Proc_Sort(df,by,ascending=True,nodupkey=False,keep=None):
+    """
+    Sorting the Datasets with given columns
+    :df: Dataframe name
+    :by: Columns by which want to be sorted as space separated string
+    :ascending: order in which columns have to be sorted,can be
+    passed as list eg: [True,False,True]
+    :nodupkey: True to eliminate duplicate rows
+    :keep: to return df with required columns passed as string separated
+    """
+    cols = by.split()
+    df = df.sort_values(by=cols,ascending=ascending)
+    if nodupkey == True:
+        df.drop_duplicates(inplace=True)
+    if keep != None:
+        cols = keep.split()
+        df = df[cols]
+
+    return df
