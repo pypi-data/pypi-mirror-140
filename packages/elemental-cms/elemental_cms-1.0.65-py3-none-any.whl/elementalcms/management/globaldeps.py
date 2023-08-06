0daff4803e3a2d44c5ad64f7ecbcb1a0e06b34ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:18 +0000

    Add package elemental-cms/1.0.65/elemental_cms-1.0.65-py3-none-any.whl

---
+++ packages/elemental-cms/elemental_cms-1.0.65-py3-none-any.whl/elementalcms/management/globaldeps.py
@@ -0,0 +1,88 @@
+import click
+from cloup import constraint, option, command, pass_context
+from cloup.constraints import RequireExactly
+
+from .globaldepscommands import List, Create, Push, Pull, Remove
+
+
+class GlobalDeps(click.Group):
+
+    def __init__(self):
+        super(GlobalDeps, self).__init__()
+        self.name = 'global-deps'
+        self.add_command(self.list)
+        self.add_command(self.create)
+        self.add_command(self.push)
+        self.add_command(self.pull)
+        self.add_command(self.remove)
+
+    @staticmethod
+    @command(name='list',
+             help='List all previously pushed global dependencies.')
+    @pass_context
+    def list(ctx):
+        List(ctx).exec()
+
+    @staticmethod
+    @command(name='create', help='Create a new global dependency on the local workspace.')
+    @option('--dep',
+            '-d',
+            required=True,
+            nargs=2,
+            help='Name and type for the gobal dep(s) to be created; name must be unique, lowercased and it can not '
+                 'contains special characters, and the type can be any of: text/css application/javascript or module. '
+                 'For example create --dep jquery application/javascript')
+    @pass_context
+    def create(ctx, dep):
+        Create(ctx).exec(dep[0], dep[1])
+
+    @staticmethod
+    @command(name='push',
+             help='Push global dep(s) specs to the CMS database.')
+    @option('--all',
+            is_flag=True,
+            help='Push all global dependencies from the local workspace.')
+    @option('--dep',
+            '-d',
+            nargs=2,
+            multiple=True,
+            help='Name and type for the global dep(s) to be pushed. '
+                 'For example: push -d bootstrap text/css -d bootstrap application/javascript')
+    @constraint(RequireExactly(1), ['all', 'dep'])
+    @pass_context
+    def push(ctx, **params):
+        if params['all']:
+            Push(ctx).exec('*')
+            return
+        Push(ctx).exec(params['dep'])
+
+    @staticmethod
+    @command(name='pull',
+             help='Pull global dep(s) specs from the CMS database.')
+    @option('--all',
+            is_flag=True,
+            help='Pull all global dependencies into the local workspace.')
+    @option('--dep',
+            '-d',
+            nargs=2,
+            multiple=True,
+            help='Name and type for the global dep(s) to be pulled. '
+                 'For example: pull -d bootstrap application/javascript')
+    @constraint(RequireExactly(1), ['all', 'dep'])
+    @pass_context
+    def pull(ctx, **params):
+        if params['all']:
+            Pull(ctx).exec('*')
+            return
+        Pull(ctx).exec(params['dep'])
+
+    @staticmethod
+    @command(name='remove', help='Remove global dependencies from the CMS database.')
+    @option('--dep',
+            '-d',
+            nargs=2,
+            help='Name and type for the global dependency to be removed. '
+                 'For example: remove --dep bootstrap application/javascript')
+    @pass_context
+    def remove(ctx, dep):
+        Remove(ctx).exec(dep[0], dep[1])
---
+++ packages/elemental-cms/elemental_cms-1.0.65-py3-none-any.whl/elementalcms/management/globaldepscommands/remove.py
@@ -0,0 +1,36 @@
+import time
+import os
+import click
+from bson import json_util
+
+from elementalcms.core import ElementalContext
+from elementalcms.services.global_deps import GetMe, RemoveOne, GetOne
+
+
+class Remove:
+
+    def __init__(self, ctx):
+        self.context: ElementalContext = ctx.obj['elemental_context']
+
+    def exec(self, name, _type):
+        get_me_result = GetMe(self.context.cms_db_context).execute(name, _type)
+        if get_me_result.is_failure():
+            click.echo(f'Global dependency {name} ({_type}) does not exist.')
+            return
+        dep = get_me_result.value()
+        self.build_backup(dep)
+        RemoveOne(self.context.cms_db_context).execute(dep['_id'])
+        click.echo(f'Global dependency {name} ({_type}) removed successfully.')
+
+    def build_backup(self, dep):
+        click.echo('Building backup...')
+        root_folder_path = self.context.cms_core_context.GLOBAL_DEPS_FOLDER
+        type_folder_name = dep['type'].replace('/', '_')
+        sufix = round(time.time())
+        backups_folder_path = f'{root_folder_path}/{type_folder_name}/.bak'
+        if not os.path.exists(backups_folder_path):
+            os.makedirs(backups_folder_path)
+        spec_backup_file_path = f'{backups_folder_path}/{dep["name"]}-{sufix}.json'
+        spec_backup_file = open(spec_backup_file_path, mode='w', encoding='utf-8')
+        spec_backup_file.write(json_util.dumps(dep, indent=4))
+        spec_backup_file.close()
