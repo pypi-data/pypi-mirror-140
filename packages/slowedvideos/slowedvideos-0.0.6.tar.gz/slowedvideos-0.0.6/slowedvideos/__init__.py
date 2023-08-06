41690bf1313ef8b35489c785e6b25525b3c11262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:51 +0000

    Add package slowedvideos/0.0.6/slowedvideos-0.0.6.tar.gz

---
+++ packages/slowedvideos/slowedvideos-0.0.6.tar.gz/slowedvideos-0.0.6/slowedvideos/__init__.py
@@ -0,0 +1,4 @@
+from slowedvideos.downloadurl.downloadurl import downloadurl
+from slowedvideos.exportvideo.exportvideo import exportvideo
+from slowedvideos.makeimages.makeimages import makevideo, makethumb
+from slowedvideos.makeslowed.makeslowed import makeslowed
