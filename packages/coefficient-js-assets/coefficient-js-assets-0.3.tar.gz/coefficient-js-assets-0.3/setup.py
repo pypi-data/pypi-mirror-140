1bce6fbe2b9f5153d3475514fbff07ed159166eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:11 +0000

    Add package coefficient-js-assets/0.3/coefficient-js-assets-0.3.tar.gz

---
+++ packages/coefficient-js-assets/coefficient-js-assets-0.3.tar.gz/coefficient-js-assets-0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='coefficient-js-assets',
+    version='0.3',
+    license='MIT',
+    author="John Sandall",
+    author_email='contact@coefficient.ai',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    package_data={'': ['*.js', '*.css']},
+    url='https://github.com/john-sandall/',
+    keywords='js',
+    install_requires=[
+          '',
+      ],
+
+)
