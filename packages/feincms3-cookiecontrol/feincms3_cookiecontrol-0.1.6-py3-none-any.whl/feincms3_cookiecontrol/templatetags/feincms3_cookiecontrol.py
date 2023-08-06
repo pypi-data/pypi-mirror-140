0102ca4c2644a66ed47f8139b4aabca2c810d025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:55 +0000

    Add package feincms3-cookiecontrol/0.1.6/feincms3_cookiecontrol-0.1.6-py3-none-any.whl

---
+++ packages/feincms3-cookiecontrol/feincms3_cookiecontrol-0.1.6-py3-none-any.whl/feincms3_cookiecontrol/templatetags/feincms3_cookiecontrol.py
@@ -0,0 +1,31 @@
+from django import template
+
+from feincms3_cookiecontrol.models import panel_data
+
+
+register = template.Library()
+
+
+@register.inclusion_tag("feincms3_cookiecontrol/panel.html")
+def feincms3_cookiecontrol_panel(page):
+    panel = panel_data()
+
+    # only show modify button on legal_page
+    if not panel["legalPage"]:
+        return {"panel": panel}
+
+    if hasattr(page, "translations") and panel["legalPage"] in {
+        p.id for p in page.translations()
+    }:
+        return {"panel": panel}
+
+    panel.pop("modify")
+    return {"panel": panel}
+
+
+@register.inclusion_tag("feincms3_cookiecontrol/panel.html")
+def feincms3_cookiecontrol(*, modify_button=True):
+    panel = panel_data()
+    if not modify_button:
+        panel.pop("modify")
+    return {"panel": panel}
