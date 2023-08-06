9641b6843758346b7d597cea1511ca3b16e5836f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:26 +0000

    Add package backendpy/0.1.6a1/backendpy-0.1.6a1-py3-none-any.whl

---
+++ packages/backendpy/backendpy-0.1.6a1-py3-none-any.whl/backendpy/logging.py
@@ -0,0 +1,42 @@
+import logging as _logging
+
+NOTSET = _logging.NOTSET
+DEBUG = _logging.DEBUG
+INFO = _logging.INFO
+WARNING = _logging.WARNING
+ERROR = _logging.ERROR
+CRITICAL = _logging.CRITICAL
+
+
+class Logger(_logging.Logger):
+
+    PINK = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    ORANGE = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+
+    def debug(self, msg, *args, **kwargs):
+        super().debug(f"{self.BLUE}{msg}{self.ENDC}", *args, **kwargs)
+
+    def info(self, msg, *args, **kwargs):
+        super().info(f"{self.GREEN}{msg}{self.ENDC}", *args, **kwargs)
+
+    def warning(self, msg, *args, **kwargs):
+        super().warning(f"{self.ORANGE}{msg}{self.ENDC}", *args, **kwargs)
+
+    def error(self, msg, *args, **kwargs):
+        super().error(f"{self.RED}{msg}{self.ENDC}", *args, **kwargs)
+
+    def exception(self, msg, *args, exc_info=True, **kwargs):
+        super().exception(f"{self.RED}{msg}{self.ENDC}", *args, exc_info=exc_info, **kwargs)
+
+    def critical(self, msg, *args, **kwargs):
+        super().critical(f"{self.RED}{msg}{self.ENDC}", *args, **kwargs)
+
+
+def get_logger(name, level=DEBUG):
+    _logging.setLoggerClass(Logger)
+    _logging.basicConfig(level=level)
+    return _logging.getLogger(name)
---
+++ packages/backendpy/backendpy-0.1.6a1-py3-none-any.whl/backendpy/unittest.py
@@ -0,0 +1,26 @@
+import unittest
+import asyncio
+import functools
+
+
+class AsyncTestCase(unittest.TestCase):
+    def __init__(self, methodName='runTest'):
+        self._event_loop = asyncio.get_event_loop()
+        super().__init__(methodName=methodName)
+
+    def async_test(self, coro):
+        @functools.wraps(coro)
+        def wrapper(*args, **kwargs):
+            self._event_loop.run_until_complete(coro(*args, **kwargs))
+        return wrapper
+
+    def __getattribute__(self, item):
+        attr = object.__getattribute__(self, item)
+        if asyncio.iscoroutinefunction(attr):
+            return self.async_test(attr)
+        return attr
+
+
+class TestCase(unittest.TestCase):
+    def __init__(self, methodName='runTest'):
+        super().__init__(methodName=methodName)
