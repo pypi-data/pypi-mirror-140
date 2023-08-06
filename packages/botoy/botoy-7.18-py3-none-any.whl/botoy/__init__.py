1561ae41ec928ff2bcb0bd6d4803eca67f882411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:23 +0000

    Add package botoy/7.18/botoy-7.18-py3-none-any.whl

---
+++ packages/botoy/botoy-7.18-py3-none-any.whl/botoy/__init__.py
@@ -0,0 +1,39 @@
+"""
+============================================
+= Github: https://github.com/opq-osc/botoy =
+============================================
+"""
+from .__version__ import check_version
+from .action import Action
+from .async_action import AsyncAction
+from .async_client import AsyncBotoy
+from .client import Botoy
+from .collection import Emoticons, EventNames, MsgTypes
+from .config import jconfig
+from .log import logger
+from .model import EventMsg, FriendMsg, GroupMsg
+from .runner import run
+from .sugar import Picture, S, Text, Voice
+
+check_version()
+del check_version
+
+__all__ = [
+    "Action",
+    "AsyncAction",
+    "AsyncBotoy",
+    "Botoy",
+    "Emoticons",
+    "EventNames",
+    "MsgTypes",
+    "jconfig",
+    "logger",
+    "EventMsg",
+    "FriendMsg",
+    "GroupMsg",
+    "Picture",
+    "Text",
+    "Voice",
+    "S",
+    "run",
+]
