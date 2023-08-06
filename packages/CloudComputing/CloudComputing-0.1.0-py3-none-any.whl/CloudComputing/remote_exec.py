133ec44cfaa83a32bfa3c949fafdc239b673bbc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:49 +0000

    Add package CloudComputing/0.1.0/CloudComputing-0.1.0-py3-none-any.whl

---
+++ packages/CloudComputing/CloudComputing-0.1.0-py3-none-any.whl/CloudComputing/remote_exec.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+import tempfile as tf
+from . import vars
+
+def remote_exec(path, rdir="./", verbose=True, logfile=None):
+    # If localhost, return
+    if 'localhost' in vars.ssh_host or '127.0.0.1' in vars.ssh_host:
+        print("Running on local machine...")
+        return
+    print("Running from file: {}".format(path))
+    # Open the calling script (from path) and read the file
+    fin = open(path, 'r')
+    # Split the script and take everything after separator
+    s = fin.read().split("__file__")[-1]
+    s = s[s.find("\n")+1:len(s)]   
+    # Do we need to import CloudComputing? 
+    if "CloudComputing" in s or "cc" in s:
+        s = "import CloudComputing as cc\n" + s
+    # Write to file
+    tmp = os.path.join(tf.gettempdir(), os.urandom(8).hex() + '.py')
+    fout = open(tmp, 'w')
+    fout.write(s)
+    fout.close()
+    # CD to remote dir (if any)
+    cmd = cmd = "/usr/bin/ssh -p {} {}".format(vars.ssh_port, vars.ssh_host)
+    cmd = cmd + " 'cd {} &&".format(rdir)
+    # Copy the temp file (script) to the remote working dir
+    os.system("/usr/bin/scp -P {} {} {}:{}".format(vars.ssh_port, tmp, vars.ssh_host, rdir))
+    # Run over SSH
+    cmd = cmd + " python {}'".format(tmp.split("/")[-1]) # Remove "/tmp/" from the filename
+    print(cmd)
+    if not verbose:
+        cmd = cmd + " 1>/dev/null 2>&1"
+    if not logfile is None:
+        print("Logging to file: {}".format(logfile))
+        cmd = cmd + " > {}".format(logfile)
+    subprocess.Popen(cmd, shell=True)
+    # Remove the local temp file
+    os.system("rm {}".format(tmp))
+    # Remove the remote temp file
+    os.system("/usr/bin/ssh -p {} {} 'rm {}/{}'".format(vars.ssh_port, vars.ssh_host, rdir, tmp.split("/")[-1]))
+    # Exit to prevent the calling script to run locally
+    exit(0)
