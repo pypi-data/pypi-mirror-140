5e542b2e2a9f7935b68e53c7f9be9337899a2b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:47 +0000

    Add package krules-dev-support/0.11/krules-dev-support-0.11.tar.gz

---
+++ packages/krules-dev-support/krules-dev-support-0.11.tar.gz/krules-dev-support-0.11/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='krules-dev-support',
+    version="0.11",
+    author="Alberto Degli Esposti",
+    author_email="alberto@arispot.tech",
+    description="KRules dev utilities",
+    license="Apache Licence 2.0",
+    keywords="krules rules engine sane-build",
+    url="https://github.com/airspot-dev/krules",
+    packages=find_packages(),
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'Jinja2==3.0.1',
+        'sane-build==7.2',
+        'python-dotenv==0.19.0',
+        'PyYAML==5.4.1',
+        'MarkupSafe==2.0.1',
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
