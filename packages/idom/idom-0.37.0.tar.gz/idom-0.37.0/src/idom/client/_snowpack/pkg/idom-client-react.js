abdf5ef04885f515d01ba0e2c29bd2076b53d39e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:43 +0000

    Add package idom/0.37.0/idom-0.37.0.tar.gz

---
+++ packages/idom/idom-0.37.0.tar.gz/idom-0.37.0/src/idom/client/_snowpack/pkg/idom-client-react.js
@@ -0,0 +1,5 @@
+import './common/index-6ed86a98.js';
+import './common/index-21e68f69.js';
+export { L as LayoutServerInfo, m as mountWithLayoutServer } from './common/server-00cb8973.js';
+import './common/htm.module-dd7abb54.js';
+import './common/index-d5d712a4.js';
