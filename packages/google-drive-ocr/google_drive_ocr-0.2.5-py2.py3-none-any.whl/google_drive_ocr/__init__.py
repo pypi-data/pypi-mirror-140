493258480a5b48b609c04a1c7a6479c5c0e17ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:51 +0000

    Add package google-drive-ocr/0.2.5/google_drive_ocr-0.2.5-py2.py3-none-any.whl

---
+++ packages/google-drive-ocr/google_drive_ocr-0.2.5-py2.py3-none-any.whl/google_drive_ocr/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Google OCR (Drive API v3)."""
+
+###############################################################################
+
+__author__ = """Hrishikesh Terdalkar"""
+__email__ = 'hrishikeshrt@linuxmail.org'
+__version__ = '0.2.5'
+
+###############################################################################
+
+from .application import GoogleOCRApplication  # noqa
