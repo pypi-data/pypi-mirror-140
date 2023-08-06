776b041218bb4099b45085a0ea63d520bfa66439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:15 +0000

    Add package vlutils/0.1.12/vlutils-0.1.12-py3-none-any.whl

---
+++ packages/vlutils/vlutils-0.1.12-py3-none-any.whl/vlutils/utils.py
@@ -0,0 +1,37 @@
+from typing import Any
+import re
+import yaml
+from io import StringIO
+
+
+def strNoneorEmpty(string: Any):
+    return string is None or not (string or string.strip())
+
+def _alignYAML(str, pad=0, aligned_colons=False):
+    props = re.findall(r'^\s*[\S]+:', str, re.MULTILINE)
+    if not props:
+        return str
+    longest = max([len(i) for i in props]) + pad
+    if aligned_colons:
+        return ''.join([i+'\n' for i in map(
+                    lambda str: re.sub(r'^(\s*.+?[^:#]): \s*(.*)',
+                        lambda m: m.group(1) + ''.ljust(longest-len(m.group(1))-1-pad) + ':'.ljust(pad+1) + m.group(2), str, re.MULTILINE),
+                    str.split('\n'))])
+    else:
+        return ''.join([i+'\n' for i in map(
+                    lambda str: re.sub(r'^(\s*.+?[^:#]: )\s*(.*)',
+                        lambda m: m.group(1) + ''.ljust(longest-len(m.group(1))+1) + m.group(2), str, re.MULTILINE),
+                    str.split('\n'))])
+
+def pPrint(d: dict) -> str:
+    """Print dict prettier.
+
+    Args:
+        d (dict): The input dict.
+
+    Returns:
+        str: Resulting string.
+    """
+    with StringIO() as stream:
+        yaml.safe_dump(d, stream, default_flow_style=False)
+        return _alignYAML(stream.getvalue(), pad=1, aligned_colons=True)
