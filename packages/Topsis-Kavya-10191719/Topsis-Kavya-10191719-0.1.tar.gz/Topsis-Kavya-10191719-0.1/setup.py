ea23890bebd1e56256e140c809b5a3a65af03c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:29 +0000

    Add package Topsis-Kavya-10191719/0.1/Topsis-Kavya-10191719-0.1.tar.gz

---
+++ packages/Topsis-Kavya-10191719/Topsis-Kavya-10191719-0.1.tar.gz/Topsis-Kavya-10191719-0.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+  name = 'Topsis-Kavya-10191719',
+  version = '0.1',
+  license='MIT', 
+  description = 'Calculates the topsis score from all int and float columns in data.',
+  author = 'Kavya',
+  author_email = 'kavya.jaswani2001@gmail.com',
+  url = '',
+  keywords = ['TOPSIS'],
+  install_requires=[
+          'pandas'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7'
+  ],
+)
