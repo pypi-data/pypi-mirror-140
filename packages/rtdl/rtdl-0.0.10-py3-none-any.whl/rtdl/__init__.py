08f2eac091b63868ff3dfe2b5cf6b73763c3a5e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:13 +0000

    Add package rtdl/0.0.10/rtdl-0.0.10-py3-none-any.whl

---
+++ packages/rtdl/rtdl-0.0.10-py3-none-any.whl/rtdl/__init__.py
@@ -0,0 +1,19 @@
+"""Revisiting Tabular Deep Learning."""
+
+__version__ = '0.0.10'
+
+from . import data  # noqa
+from .functional import geglu, reglu  # noqa
+from .modules import (  # noqa
+    GEGLU,
+    MLP,
+    CategoricalFeatureTokenizer,
+    CLSToken,
+    FeatureTokenizer,
+    FTTransformer,
+    MultiheadAttention,
+    NumericalFeatureTokenizer,
+    ReGLU,
+    ResNet,
+    Transformer,
+)
