cc1e5967930c17e8a0b443f01652ae3a0b07626b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:13 +0000

    Add package cqml/0.2.6/cqml-0.2.6.tar.gz

---
+++ packages/cqml/cqml-0.2.6.tar.gz/cqml-0.2.6/src/cqml/keys.py
@@ -0,0 +1,26 @@
+#
+# Magic Keys
+#
+
+DB='default'
+cAlias='|'
+kAbove='above'
+kArgs='args'
+kCast='as_number'
+kCols='cols'
+kDoc='+doc'
+kExt='file_ext'
+kFunc='function'
+kGroup='group'
+kInner='inner'
+kJoinType='join_type'
+kKeepJoin='keep_join'
+kReverse='descend'
+kSave='save'
+kSelect='select'
+kSkip='skip'
+kSkipSave='skip_save'
+kSkipUpload='skip_upload'
+kSort='sort'
+kSQL='sql'
+kUniq='ensure_unique'
