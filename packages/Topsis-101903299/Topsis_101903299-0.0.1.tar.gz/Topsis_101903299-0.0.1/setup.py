1f05ba9d61b84c730a5ff7e2a800234f18751c88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:56 +0000

    Add package Topsis-101903299/0.0.1/Topsis_101903299-0.0.1.tar.gz

---
+++ packages/Topsis-101903299/Topsis_101903299-0.0.1.tar.gz/Topsis_101903299-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="Topsis_101903299",
+    version="0.0.1",
+    description="Topsis",
+    long_description="Topsis 101903299",
+    long_description_content_type="text/markdown",
+    author="Rishav Wadhwa",
+    author_email="rishavwadhwa2@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["Topsis_101903299"],
+    include_package_data=True,
+    install_requires=[],
+    
+)
