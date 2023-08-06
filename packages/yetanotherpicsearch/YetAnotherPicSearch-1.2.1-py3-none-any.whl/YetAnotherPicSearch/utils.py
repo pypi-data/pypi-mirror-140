411e7cd891fe7bd8ab03a1fb37d0010528ef21c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:27 +0000

    Add package yetanotherpicsearch/1.2.1/YetAnotherPicSearch-1.2.1-py3-none-any.whl

---
+++ packages/yetanotherpicsearch/YetAnotherPicSearch-1.2.1-py3-none-any.whl/YetAnotherPicSearch/utils.py
@@ -0,0 +1,39 @@
+import base64
+from io import BytesIO
+
+import httpx
+from pyquery import PyQuery
+
+
+# 将图片转化为 base64
+async def get_pic_base64_by_url(url: str, proxy: str) -> str:
+    async with httpx.AsyncClient(proxies=proxy) as client:
+        r = await client.get(url)
+        image_buffer = BytesIO(r.content)
+        return str(base64.b64encode(image_buffer.getvalue()), encoding="utf-8")
+
+
+async def handle_img(url: str, proxy: str, hide_img: bool) -> str:
+    if hide_img:
+        return ""
+    img_base64 = await get_pic_base64_by_url(url, proxy)
+    if img_base64:
+        return f"[CQ:image,file=base64://{img_base64}]"
+    return f"图片下载失败: {url}"
+
+
+async def get_source(url: str, proxy: str) -> str:
+    async with httpx.AsyncClient(proxies=proxy) as client:
+        if "danbooru.donmai.us" in url:
+            return PyQuery((await client.get(url)).content)(".image-container").attr(
+                "data-normalized-source"
+            )
+        elif "konachan.com" in url or "yande.re" in url:
+            return PyQuery((await client.get(url)).content)(
+                "#stats li:contains(Source) a"
+            ).attr("href")
+        elif "gelbooru.com" in url:
+            return PyQuery((await client.get(url)).content)(
+                "#tag-list li:contains(Source) a"
+            ).attr("href")
+    return ""
