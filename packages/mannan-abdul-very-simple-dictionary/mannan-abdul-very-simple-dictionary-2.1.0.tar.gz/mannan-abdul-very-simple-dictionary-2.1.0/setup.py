e951aad90261a4c3174de219050b25441cc661f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:41 +0000

    Add package mannan-abdul-very-simple-dictionary/2.1.0/mannan-abdul-very-simple-dictionary-2.1.0.tar.gz

---
+++ packages/mannan-abdul-very-simple-dictionary/mannan-abdul-very-simple-dictionary-2.1.0.tar.gz/mannan-abdul-very-simple-dictionary-2.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="mannan-abdul-very-simple-dictionary",
+    author="Mannan Abdul",
+    version="2.1.0",
+    packages=["dictionary"],
+    install_requires=[
+        "requests>=2.23.0",
+    ],
+    python_requires=">=3.8",
+
+)
