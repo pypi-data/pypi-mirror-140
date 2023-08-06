e7cab6b20034a082fd26b26535793a276ec7581e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:27 +0000

    Add package slowedvideos/0.1.3/slowedvideos-0.1.3.tar.gz

---
+++ packages/slowedvideos/slowedvideos-0.1.3.tar.gz/slowedvideos-0.1.3/slowedvideos/makeimages/makethumb.py
@@ -0,0 +1,43 @@
+def makethumb(cover, output):
+
+    prefix = 'Thumbnail -'
+
+    from PIL import Image, ImageDraw, ImageEnhance, ImageFilter 
+    from math import trunc
+
+    # Middle
+    middle = (trunc((X-x)/2), trunc((Y-y)/2))
+
+    # Import
+    print(prefix, f'Importing {cover} ...')
+    tb = Image.open(cover)
+    cv = Image.open(cover)
+
+    # Cover
+    print(prefix, 'Resizing cover...')
+    x, y = (626, 626)
+    cv = cv.resize((x, y), resample=0, box=None)
+    
+    # Thumb Resize
+    print(prefix, 'Resizing background...')
+    X, Y = (1920, 1080)
+    tb = tb.resize((X, Y), resample=0, box=None)
+    
+    # Add a shadow (Black square with blur)
+    print(prefix, 'Creating the shadow...')
+    square = Image.new(mode = "RGBA", size = (2500, 2500), color = (0, 0, 0))
+    tb.paste(square, middle)
+
+    # Blur and brightness
+    print(prefix, 'Bluring background...')
+    tb = ImageEnhance.Brightness(tb).enhance(0.4)
+    tb = tb.filter(ImageFilter.GaussianBlur(120))
+    tb = tb.copy()
+
+    # Mix into file
+    print(prefix, 'Merging cover to background...')
+    tb.paste(cv, middle)
+    print(prefix, f'Exporting thumbnail as {output} ...')
+    tb.save(output)
+    
+    return print(prefix, 'Done')
