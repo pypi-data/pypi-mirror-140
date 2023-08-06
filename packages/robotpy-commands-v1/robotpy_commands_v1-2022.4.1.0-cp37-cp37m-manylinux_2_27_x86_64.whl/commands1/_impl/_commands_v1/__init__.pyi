6e50a76743cf795ce582ec60b1265198d3c7614d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:36:46 +0000

    Add package robotpy-commands-v1/2022.4.1.0/robotpy_commands_v1-2022.4.1.0-cp37-cp37m-manylinux_2_27_x86_64.whl

---
+++ packages/robotpy-commands-v1/robotpy_commands_v1-2022.4.1.0-cp37-cp37m-manylinux_2_27_x86_64.whl/commands1/_impl/_commands_v1/__init__.pyi
@@ -0,0 +1,41 @@
+from __future__ import annotations
+import commands1._impl._commands_v1
+import typing
+
+__all__ = [
+    "PIDSourceType",
+    "button",
+    "command"
+]
+
+
+class PIDSourceType():
+    """
+    Members:
+
+      kDisplacement
+
+      kRate
+    """
+    def __eq__(self, other: object) -> bool: ...
+    def __getstate__(self) -> int: ...
+    def __hash__(self) -> int: ...
+    def __init__(self, value: int) -> None: ...
+    def __int__(self) -> int: ...
+    def __ne__(self, other: object) -> bool: ...
+    def __repr__(self) -> str: ...
+    def __setstate__(self, state: int) -> None: ...
+    @property
+    def name(self) -> str:
+        """
+        :type: str
+        """
+    @property
+    def value(self) -> int:
+        """
+        :type: int
+        """
+    __members__: dict # value = {'kDisplacement': <PIDSourceType.kDisplacement: 0>, 'kRate': <PIDSourceType.kRate: 1>}
+    kDisplacement: commands1._impl._commands_v1.PIDSourceType # value = <PIDSourceType.kDisplacement: 0>
+    kRate: commands1._impl._commands_v1.PIDSourceType # value = <PIDSourceType.kRate: 1>
+    pass
