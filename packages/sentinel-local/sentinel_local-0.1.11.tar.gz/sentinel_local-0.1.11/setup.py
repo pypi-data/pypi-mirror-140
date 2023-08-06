7d74929eb8d3f3a3918fdcc51bf0fbe9c8bf2a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:44 +0000

    Add package sentinel-local/0.1.11/sentinel_local-0.1.11.tar.gz

---
+++ packages/sentinel-local/sentinel_local-0.1.11.tar.gz/sentinel_local-0.1.11/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import pathlib
+
+VERSION = '0.1.11'
+DESCRIPTION = 'Local Processing of Data Based on CXL Created Algorithms'
+HERE = pathlib.Path(__file__).parent
+README = (HERE/"README.md").read_text()
+
+
+setup(
+    name="sentinel_local",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=README,
+    long_description_content_type="text/markdown", 
+    url="https://github.com/cxl-garage/sentinel-laptop",
+    author="Sam Kelly",
+    author_email="sam@conservationxlabs.org",
+    py_modules = ['sentinel_local'],
+    classifiers=[ 
+        "License :: OSI Approved :: MIT License", 
+        "Programming Language :: Python :: 3", 
+        "Programming Language :: Python :: 3.7", 
+   ], 
+    packages=find_packages(),
+    includepackagedata=True, 
+    install_requires=["docker","GPUtil","numpy","pandas","Pillow","requests","tqdm","google-cloud-artifact-registry"], 
+    entrypoints={ 
+        "console_scripts":[ 
+            "sentinel_run=sentinel_local.main:run", 
+            "sentinel_download=sentinel_local.main:download", 
+        ]
+    } 
+    
+)
