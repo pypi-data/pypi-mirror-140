5e9b8072e648a092b9c62f35594ba6cd9f224651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:15 +0000

    Add package probnode/0.1.1/probnode-0.1.1-py3-none-any.whl

---
+++ packages/probnode/probnode-0.1.1-py3-none-any.whl/probnode/P.py
@@ -0,0 +1,9 @@
+from probnode.interface.ievent import IEvent
+from probnode.probability.probability import BaseProbabilityExpression, ProbabilityExpression
+
+
+def P(expression):
+  if isinstance(expression, IEvent):
+    return ProbabilityExpression.from_event(expression)
+  if isinstance(expression, BaseProbabilityExpression):
+    return expression
