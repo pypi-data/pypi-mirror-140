798465e7495cdf496832001cef0bf5da4158bb75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:19:11 +0000

    Add package ddtrace/0.59.0rc2/ddtrace-0.59.0rc2-cp27-cp27m-manylinux2010_i686.whl

---
+++ packages/ddtrace/ddtrace-0.59.0rc2-cp27-cp27m-manylinux2010_i686.whl/ddtrace/utils/attr.py
@@ -0,0 +1,9 @@
+from ..internal.utils.attr import T  # noqa
+from ..internal.utils.attr import from_env  # noqa
+from ..vendor.debtcollector.removals import removed_module
+
+
+removed_module(
+    module="ddtrace.utils.attr",
+    removal_version="1.0.0",
+)
---
+++ packages/ddtrace/ddtrace-0.59.0rc2-cp27-cp27m-manylinux2010_i686.whl/ddtrace/utils/cache.py
@@ -0,0 +1,15 @@
+from ..internal.utils.cache import CachedMethodDescriptor  # noqa
+from ..internal.utils.cache import F  # noqa
+from ..internal.utils.cache import M  # noqa
+from ..internal.utils.cache import S  # noqa
+from ..internal.utils.cache import T  # noqa
+from ..internal.utils.cache import cached  # noqa
+from ..internal.utils.cache import cachedmethod  # noqa
+from ..internal.utils.cache import miss  # noqa
+from ..vendor.debtcollector.removals import removed_module
+
+
+removed_module(
+    module="ddtrace.utils.cache",
+    removal_version="1.0.0",
+)
