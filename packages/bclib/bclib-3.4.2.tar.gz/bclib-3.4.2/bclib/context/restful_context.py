faa83d033d35103f3185cb5cf9b23ec3df839827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:38:10 +0000

    Add package bclib/3.4.2/bclib-3.4.2.tar.gz

---
+++ packages/bclib/bclib-3.4.2.tar.gz/bclib-3.4.2/bclib/context/restful_context.py
@@ -0,0 +1,14 @@
+import json
+from typing import TYPE_CHECKING
+from bclib.utility import DictEx
+from ..context.json_base_request_context import JsonBaseRequestContext
+
+if TYPE_CHECKING:
+    from .. import dispatcher
+
+
+class RESTfulContext(JsonBaseRequestContext):
+    def __init__(self, cms_object: dict, dispatcher: 'dispatcher.IDispatcher') -> None:
+        super().__init__(cms_object, dispatcher)
+        self.body = DictEx(self.cms.form) if self.cms.form else DictEx(
+            json.loads(self.cms.request.body)) if self.cms.request.body else None
