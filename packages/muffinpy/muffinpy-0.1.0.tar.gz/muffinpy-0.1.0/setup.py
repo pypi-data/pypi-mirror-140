805b7c6efb785ee1755572d54845215c230fa9ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:49 +0000

    Add package muffinpy/0.1.0/muffinpy-0.1.0.tar.gz

---
+++ packages/muffinpy/muffinpy-0.1.0.tar.gz/muffinpy-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='muffinpy',
+    version='0.1.0',    
+    description='A example Python package',
+    url='https://github.com/MuffinDev9/muffinpy',
+    packages=['muffinpy'],
+    install_requires=[]
+)
