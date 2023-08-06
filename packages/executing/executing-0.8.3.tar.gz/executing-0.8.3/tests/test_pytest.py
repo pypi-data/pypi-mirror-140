19f45a779a1471524f46ff48fa2b7be1702fc032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:25 +0000

    Add package executing/0.8.3/executing-0.8.3.tar.gz

---
+++ packages/executing/executing-0.8.3.tar.gz/executing-0.8.3/tests/test_pytest.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+from littleutils import SimpleNamespace
+
+from executing.executing import is_ipython_cell_code
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+
+def test_pytest():
+    from tests.utils import tester
+
+    lst = [1, 2, 3]
+    lst2 = tester(lst)
+    assert lst == lst2
+    lst3 = tester(lst + [4])
+    assert (
+            [1, 2, 3, 4]
+            == lst3
+    ), 'message'
+    x = tester.x
+    assert x is tester
+
+
+def test_ipython_cell_code():
+    assert is_ipython_cell_code(
+        SimpleNamespace(
+            co_name="<cell line: 1>",
+            co_filename="tmp/ipykernel_3/foo",
+        )
+    )
+
+    assert not is_ipython_cell_code(
+        SimpleNamespace(
+            co_name="<cell line: 1",
+            co_filename="tmp/ipykernel_3/foo",
+        )
+    )
+
+    assert not is_ipython_cell_code(
+        SimpleNamespace(
+            co_name="<cell line: 1>",
+            co_filename="tmp/ipykernel_3",
+        )
+    )
