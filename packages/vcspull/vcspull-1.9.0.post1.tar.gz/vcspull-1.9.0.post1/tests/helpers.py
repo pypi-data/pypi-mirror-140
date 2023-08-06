ab96382d2399319b87ab3894575d90fc3b6ede64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:09 +0000

    Add package vcspull/1.9.0.post1/vcspull-1.9.0.post1.tar.gz

---
+++ packages/vcspull/vcspull-1.9.0.post1.tar.gz/vcspull-1.9.0.post1/tests/helpers.py
@@ -0,0 +1,38 @@
+"""Helpers for vcspull."""
+import os
+
+
+class EnvironmentVarGuard(object):
+
+    """Class to help protect the environment variable properly.
+
+    May be used as context manager.
+    Vendorize to fix issue with Anaconda Python 2 not
+    including test module, see #121.
+    """
+
+    def __init__(self):
+        self._environ = os.environ
+        self._unset = set()
+        self._reset = dict()
+
+    def set(self, envvar, value):
+        if envvar not in self._environ:
+            self._unset.add(envvar)
+        else:
+            self._reset[envvar] = self._environ[envvar]
+        self._environ[envvar] = value
+
+    def unset(self, envvar):
+        if envvar in self._environ:
+            self._reset[envvar] = self._environ[envvar]
+            del self._environ[envvar]
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *ignore_exc):
+        for envvar, value in self._reset.items():
+            self._environ[envvar] = value
+        for unset in self._unset:
+            del self._environ[unset]
