530ce97b4b1a0681c6bba2b2367836515c0a05fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:16 +0000

    Add package tcpb/0.8.1/tcpb-0.8.1.tar.gz

---
+++ packages/tcpb/tcpb-0.8.1.tar.gz/tcpb-0.8.1/examples/molden_file.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# Basic energy calculation
+import sys
+
+from qcelemental.models import AtomicInput, Molecule
+
+from tcpb import TCProtobufClient as TCPBClient
+
+if len(sys.argv) != 3:
+    print("Usage: {} host port".format(sys.argv[0]))
+    exit(1)
+
+
+# Water system
+atoms = ["O", "H", "H"]
+geom = [0.0, 0.0, 0.0, 0.0, 1.5, 0.0, 0.0, 0.0, 1.5]  # in bohr
+
+molecule = Molecule(symbols=atoms, geometry=geom)
+atomic_input = AtomicInput(
+    molecule=molecule,
+    model={
+        "method": "pbe0",
+        "basis": "6-31g",
+    },
+    driver="energy",
+    keywords={
+        "closed_shell": True,
+        "restricted": True,
+        "molden": True,
+        "imd_orbital_type": "whole_c",
+    },
+)
+
+with TCPBClient(host=sys.argv[1], port=int(sys.argv[2])) as TC:
+    result = TC.compute(atomic_input)
+
+print(result)
+print(result.extras["molden"])
