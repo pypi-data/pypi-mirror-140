b16817c2cc91925e145d7cf0bfe242bbf832c51d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:53 +0000

    Add package Windows-Toasts/0.2.0/windows-toasts-0.2.0.tar.gz

---
+++ packages/Windows-Toasts/windows-toasts-0.2.0.tar.gz/windows-toasts-0.2.0/src/windows_toasts/__init__.py
@@ -0,0 +1,29 @@
+from .toast_audio import AudioSource, ToastAudio
+from .toast_types import (
+    ToastDuration,
+    ToastImageAndText1,
+    ToastImageAndText2,
+    ToastImageAndText3,
+    ToastImageAndText4,
+    ToastText1,
+    ToastText2,
+    ToastText3,
+    ToastText4
+)
+from .windows_toasts import InteractableWindowsToaster, WindowsToaster
+
+__all__ = [
+    "AudioSource",
+    "ToastAudio",
+    "ToastDuration",
+    "ToastImageAndText1",
+    "ToastImageAndText2",
+    "ToastImageAndText3",
+    "ToastImageAndText4",
+    "ToastText1",
+    "ToastText2",
+    "ToastText3",
+    "ToastText4",
+    "InteractableWindowsToaster",
+    "WindowsToaster"
+]
