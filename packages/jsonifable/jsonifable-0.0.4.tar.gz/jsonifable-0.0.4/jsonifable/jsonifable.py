059ab93095f250c2006c0df425c6e4997e42b271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:31 +0000

    Add package jsonifable/0.0.4/jsonifable-0.0.4.tar.gz

---
+++ packages/jsonifable/jsonifable-0.0.4.tar.gz/jsonifable-0.0.4/jsonifable/jsonifable.py
@@ -0,0 +1,47 @@
+import typing as tp
+import json
+
+
+T = tp.TypeVar('T')
+
+_CONVERTABLE_TYPES = [dict, str, int, float, bool, tuple, list, set]
+
+def Jsonifable(cls: T) -> T:
+    """
+    ## Jsonifable
+    Adds a to_json method to your class, which after being called shall
+    convert your class' properties and values into a JSON formatted string.
+
+    If the decorated class contains another class, it will convert those aswell.
+
+    ### Params:
+    * cls -> class that you wish to decorate
+
+    ### Returns:
+    Decorated class
+    """
+    
+    orign_init = cls.__init__
+
+    def __init__(self, *args, **kwargs):
+        orign_init(self, *args, **kwargs)
+
+        def to_json(self) -> str:
+            obj = {}
+
+            for var in vars(self):
+                attr = getattr(self, var)
+
+                if hasattr(attr, 'to_json'):
+                    obj[var] = json.loads(attr.to_json())
+                elif type(attr) in _CONVERTABLE_TYPES:
+                    obj[var] = attr
+                else:
+                    obj[var] = json.loads(to_json(attr))
+
+            return json.dumps(obj)
+
+        cls.to_json = to_json
+
+    cls.__init__ = __init__
+    return cls
---
+++ packages/jsonifable/jsonifable-0.0.4.tar.gz/jsonifable-0.0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Simple decorator for making classes easily convertable to JSON'
+
+setup(
+    name='jsonifable',
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=['jsonifable'],
+    author="Maciej Oliwa (avery)",
+    author_email="maciejoliwa0906@gmail.com",
+    keywords=['python', 'json', 'class', 'dataclass', 'tojson'],    
+)
