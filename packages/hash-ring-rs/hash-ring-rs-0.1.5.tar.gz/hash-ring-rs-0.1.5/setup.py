d8e63016b9a88243d2700549e8759047cbda57d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:28 +0000

    Add package hash-ring-rs/0.1.5/hash-ring-rs-0.1.5.tar.gz

---
+++ packages/hash-ring-rs/hash-ring-rs-0.1.5.tar.gz/hash-ring-rs-0.1.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+from setuptools_rust import RustExtension, Binding
+
+setup(
+    name="hash-ring-rs",
+    version="0.1.5",
+    rust_extensions=[RustExtension("hash_ring_rs", binding=Binding.PyO3)],
+    zip_safe=False
+)
