7156ce82d74ca5b6a8c9f04ab9644d52435b5153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:15 +0000

    Add package jmapc/0.0.0/jmapc-0.0.0.tar.gz

---
+++ packages/jmapc/jmapc-0.0.0.tar.gz/jmapc-0.0.0/jmapc/types/jmap/methods_thread.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import List
+
+from dataclasses_json import config
+
+from ... import constants
+from .methods import JMAPGet, JMAPGetResponse
+from .models import JMAPThread
+
+
+@dataclass
+class JMAPThreadGet(JMAPGet):
+    @classmethod
+    def name(cls) -> str:
+        return "Thread/get"
+
+    @classmethod
+    def using(cls) -> set[str]:
+        return set([constants.JMAP_URN_MAIL])
+
+
+@dataclass
+class JMAPThreadGetResponse(JMAPGetResponse):
+    data: List[JMAPThread] = field(metadata=config(field_name="list"))
---
+++ packages/jmapc/jmapc-0.0.0.tar.gz/jmapc-0.0.0/jmapc/types/util.py
@@ -0,0 +1,70 @@
+from dataclasses import dataclass, fields
+from datetime import datetime
+from typing import Any, Dict, Optional
+
+import dataclasses_json
+import dateutil.parser
+
+
+def datetime_encode(dt: Optional[datetime]) -> Optional[str]:
+    if not dt:
+        return None
+    return f"{dt.replace(tzinfo=None).isoformat()}Z"
+
+
+def datetime_decode(value: Optional[str]) -> Optional[datetime]:
+    if not value:
+        return None
+    return dateutil.parser.isoparse(value)
+
+
+@dataclass
+class JMAPResultReference(dataclasses_json.DataClassJsonMixin):
+    dataclass_json_config = dataclasses_json.config(
+        letter_case=dataclasses_json.LetterCase.CAMEL,  # type: ignore
+    )["dataclasses_json"]
+    name: str
+    path: str
+    result_of: str
+
+
+class JsonDataClass(dataclasses_json.DataClassJsonMixin):
+    dataclass_json_config = dataclasses_json.config(
+        letter_case=dataclasses_json.LetterCase.CAMEL,  # type: ignore
+        undefined=dataclasses_json.Undefined.EXCLUDE,
+        exclude=lambda f: f is None,  # type: ignore
+    )["dataclasses_json"]
+
+    def _fix_refs(
+        self, data: Dict[str, dataclasses_json.core.Json]
+    ) -> Dict[str, dataclasses_json.core.Json]:
+        for k in [key for key in data.keys() if not key.startswith("#")]:
+            v = data[k]
+            if isinstance(v, dict):
+                if len(v.keys()) == len(fields(JMAPResultReference)):
+                    try:
+                        JMAPResultReference.from_dict(v)
+                        new_key = f"#{k}"
+                        if new_key in data:
+                            raise Exception(
+                                f"Reference key {new_key} already exists"
+                            )
+                        data[new_key] = v
+                        del data[k]
+                        continue
+                    except KeyError:
+                        pass
+                data[k] = self._fix_refs(v)
+        return data
+
+    def to_dict(
+        self, *args: Any, account_id: Optional[str] = None, **kwargs: Any
+    ) -> Dict[str, dataclasses_json.core.Json]:
+        if account_id:
+            self.account_id: Optional[str] = account_id
+        result = super().to_dict(*args, **kwargs)
+        result = self._fix_refs(result)
+        import pprint
+
+        pprint.pprint(result)
+        return result
