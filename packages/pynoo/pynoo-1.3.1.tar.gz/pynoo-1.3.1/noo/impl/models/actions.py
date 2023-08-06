5ecba350bdc61ad62d2dbc9acaab85fc1dcfcaab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:21:58 +0000

    Add package pynoo/1.3.1/pynoo-1.3.1.tar.gz

---
+++ packages/pynoo/pynoo-1.3.1.tar.gz/pynoo-1.3.1/noo/impl/models/actions.py
@@ -0,0 +1,34 @@
+from typing import List, Literal, Optional, Union
+
+from pydantic import BaseModel
+
+
+class Action(BaseModel):
+    action: str
+
+
+class ReplaceAction(Action):
+    action: Literal["replace"]
+    files: List[str]
+    src: str
+    dest: str
+
+
+class DeleteAction(Action):
+    action: Literal["delete"]
+    files: List[str]
+
+
+class CreateAction(Action):
+    action: Literal["create"]
+    file: str
+    content: Optional[str] = None
+
+
+class RenameAction(Action):
+    action: Literal["rename"]
+    file: str
+    dest: str
+
+
+ActionType = Union[ReplaceAction, DeleteAction, CreateAction, RenameAction, Action]
