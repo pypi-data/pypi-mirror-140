11bef528ffc83a83ca421f736b16ce5ec53e056f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:52 +0000

    Add package Topsis-PreetinderKaur-101903403/0.0.1/Topsis-PreetinderKaur-101903403-0.0.1.tar.gz

---
+++ packages/Topsis-PreetinderKaur-101903403/Topsis-PreetinderKaur-101903403-0.0.1.tar.gz/Topsis-PreetinderKaur-101903403-0.0.1/README.txt
@@ -0,0 +1,7 @@
+This package is used to implement topsis in python. As parameters, we require input file as .csv, Weights, Impacts and name of output csv file.
+
+To install the package use -
+>>pip install Topsis-Preetinder-101903403==0.0.1
+
+
+Eg. - topsis.topsis("data.csv","1,2,1,1","-,+,+,+", "output.csv")
---
+++ packages/Topsis-PreetinderKaur-101903403/Topsis-PreetinderKaur-101903403-0.0.1.tar.gz/Topsis-PreetinderKaur-101903403-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='Topsis-PreetinderKaur-101903403',
+  version='0.0.1',
+  description='Topsis impelementation in python',
+  long_description_content_type='text/markdown',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Preetinder Kaur',
+  author_email='dhillonpreeti2001@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Topsis', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
