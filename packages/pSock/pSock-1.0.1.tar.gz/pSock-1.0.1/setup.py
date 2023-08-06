d01f0637c1e1975ec2d948ebebbbdf57c65ec064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:52 +0000

    Add package pSock/1.0.1/pSock-1.0.1.tar.gz

---
+++ packages/pSock/pSock-1.0.1.tar.gz/pSock-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import os, codecs
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.1'
+DESCRIPTION = 'pSock is a socket / threading module that helps developers and students to approach Server-Client creation and much more.'
+
+setup(
+    name="pSock",
+    version=VERSION,
+    author="Andrea Vaccaro (ANDRVV)",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    keywords=["python", "socket", "threading", "server", "client"],
+)
