1f8653084a20a814966a3a8350d6ce088da7f5df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:51 +0000

    Add package python-mecab-ner/1.0.7/python_mecab_ner-1.0.7-py3-none-any.whl

---
+++ packages/python-mecab-ner/python_mecab_ner-1.0.7-py3-none-any.whl/python_mecab_ner/service/mecab_parser.py
@@ -0,0 +1,189 @@
+import copy
+
+import _mecab
+from collections import namedtuple
+from typing import Generator
+
+from mecab import MeCabError
+from python_mecab_ner.domain.mecab_domain import MecabWordFeature
+
+
+def delete_pattern_from_string(string, pattern, index, nofail=False):
+    """ 문자열에서 패턴을 찾아서 *로 변환해주는 기능 """
+
+    # raise an error if index is outside of the string
+    if not nofail and index not in range(len(string)):
+        raise ValueError("index outside given string")
+
+    # if not erroring, but the index is still not in the correct range..
+    if index < 0:  # add it to the beginning
+        return pattern + string
+    if index > len(string):  # add it to the end
+        return string + pattern
+
+    len_pattern = len(pattern)
+    blank_pattern = len(pattern) * "*"
+    # insert the new string between "slices" of the original
+    return string[:index] + blank_pattern + string[index + len_pattern:]
+
+STRING_NOT_FOUND = -1
+
+Feature = namedtuple('Feature', [
+    'pos',
+    'semantic',
+    'has_jongseong',
+    'reading',
+    'type',
+    'start_pos',
+    'end_pos',
+    'expression',
+])
+
+
+def _create_lattice(sentence):
+    lattice = _mecab.Lattice()
+    lattice.add_request_type(_mecab.MECAB_ALLOCATE_SENTENCE)  # Required
+    lattice.set_sentence(sentence)
+
+    return lattice
+
+
+def _get_mecab_feature(node) -> MecabWordFeature:
+    # Reference:
+    # - http://taku910.github.io/mecab/learn.html
+    # - https://docs.google.com/spreadsheets/d/1-9blXKjtjeKZqsf4NzHeYJCrr49-nXeRF6D80udfcwY
+    # - https://bitbucket.org/eunjeon/mecab-ko-dic/src/master/utils/dictionary/lexicon.py
+
+    # feature = <pos>,<semantic>,<has_jongseong>,<reading>,<type>,<start_pos>,<end_pos>,<expression>
+    values = node.feature.split(',')
+    assert len(values) == 8
+
+    values = [value if value != '*' else None for value in values]
+    feature = dict(zip(Feature._fields, values))
+    feature['has_jongseong'] = {'T': True, 'F': False}.get(feature['has_jongseong'])
+
+    return MecabWordFeature(node.surface, **feature)
+
+
+class MecabParser:
+
+    """
+    문장을 형태소 분석하는 클래스.
+    형태소 분석시 형태소 분석 토큰, 스페이스 분석 토큰의 인덱스 위치도 함께 저장
+    """
+
+    FIRST_WORD = 0
+    type_list = ["Compound", "Inflect"]
+
+    def __init__(self, sentence: str, dicpath=''):
+        argument = ''
+
+        if dicpath != '':
+            argument = '-d %s' % dicpath
+
+        self.tagger = _mecab.Tagger(argument)
+        self.sentence = sentence
+        self.sentence_token = self.sentence.split()
+
+    def _get_space_token_idx(self, mecab_word_feature: MecabWordFeature) -> int:
+
+        """
+        스페이스로 토큰 분석한 인덱스 값 반환
+        :param mecab_word_feature: 메캅 단어 특성데이터
+        :return: 스페이스 토큰 분석한 결과
+        """
+
+        for idx_token, sentence_token_item in enumerate(self.sentence_token):
+
+            index_string = sentence_token_item.find(mecab_word_feature.word)
+            if index_string != STRING_NOT_FOUND:
+
+                self.sentence_token[idx_token] = delete_pattern_from_string(sentence_token_item, mecab_word_feature.word, index_string)
+
+                return idx_token
+
+        return False
+
+    def gen_mecab_token_feature(self) -> Generator:
+
+        """
+        메캅으로 형태소 분석한 토큰 제너레이터로 반환
+        스페이스로 분석한 토큰의 정보와 형태소로 분석한 토큰의 정보 포함
+        """
+
+        lattice = _create_lattice(self.sentence)
+
+        if not self.tagger.parse(lattice):
+            raise MeCabError(self.tagger.what())
+
+        for mecab_token_idx, mecab_token in enumerate(lattice):
+            mecab_token_feature = _get_mecab_feature(mecab_token)
+            mecab_token_feature.mecab_token_idx = mecab_token_idx
+
+            space_token_idx = self._get_space_token_idx(mecab_token_feature)
+
+            if space_token_idx is not False:
+                mecab_token_feature.space_token_idx = space_token_idx
+                mecab_token_feature.word = mecab_token_feature.word.lower()
+
+                yield mecab_token_feature
+
+    def tokenize_mecab_compound(self) -> Generator:
+
+        """
+        메캅으로 분석한 토큰 제너레이터로 반환 결과 중에 복합여, 굴절형태소 있는 경우 토큰화
+        """
+
+        for compound_include_item in self.gen_mecab_token_feature():
+            if compound_include_item.type in self.type_list:
+                compound_item_list = compound_include_item.expression.split("+")
+                for compound_item in compound_item_list:
+                    word, pos_tag, _ = compound_item.split("/")
+                    copy_compound_include_item = copy.deepcopy(compound_include_item)
+                    copy_compound_include_item.word = word
+                    yield word, copy_compound_include_item
+
+            else:
+                yield compound_include_item.word, compound_include_item
+
+    def gen_mecab_compound_token_feature(self) -> Generator:
+
+        """
+
+        :return: 복합어를 분해한 메캅 토큰 순서가 들어간 단어
+        """
+
+        for idx, x in enumerate(list(self.tokenize_mecab_compound())):
+            copy_x = copy.deepcopy(x)
+            copy_x[1].mecab_token_compound_idx = idx
+            yield copy_x
+
+    def get_word_from_mecab_compound(self, is_list=False):
+
+        """
+        메캅으로 분해된 문장에서 단어만 추출
+        :param is_list: 리스트로 반환 여부
+        :return: 메캅으로 분해된 문장에서 단어만 포함된 문장
+        """
+
+        if is_list:
+            return [x[self.FIRST_WORD] for x in list(self.gen_mecab_compound_token_feature())]
+
+        return " ".join([x[self.FIRST_WORD] for x in list(self.gen_mecab_compound_token_feature())])
+
+
+if __name__ == "__main__":
+
+        test_sentence = "나는 서울대병원에 갔어"
+
+        mecab_parse_results = list(
+            MecabParser(test_sentence).gen_mecab_token_feature())
+
+        for idx, mecab_parse_item in enumerate(mecab_parse_results):
+            print(mecab_parse_item)
+
+        mecab_parse_results = list(
+            MecabParser(test_sentence).gen_mecab_compound_token_feature())
+
+        for idx, mecab_parse_item in enumerate(mecab_parse_results):
+            print(mecab_parse_item)
---
+++ packages/python-mecab-ner/python_mecab_ner-1.0.7-py3-none-any.whl/python_mecab_ner/service/mecab_reader.py
@@ -0,0 +1,189 @@
+from typing import List, Generator
+from pathlib import Path
+from collections import defaultdict
+
+from python_mecab_ner.domain.mecab_exception import MecabDataReaderException
+from python_mecab_ner.service.mecab_parser import MecabParser
+
+
+class CategoryData:
+    """
+    large_cat, small_cat에 word 리스트로 저장
+    """
+    def __init__(self):
+        self.large_cat = defaultdict(list)
+
+    def add(self, small_cat, word):
+        self.large_cat[small_cat].extend(word)
+
+
+class DataUtility:
+
+    """
+    데이터 읽고 쓰기 유틸리티
+    """
+
+    @staticmethod
+    def read_txt(path):
+        """텍스트 파일 읽는 메소드"""
+        with open(path, "r", encoding='utf-8-sig') as file:
+            txt_list = file.read().splitlines()
+            return txt_list
+
+    @staticmethod
+    def write_txt(path: str, txt_list: List, is_sort=False):
+        """텍스트 파일 쓰는 메소드"""
+        if is_sort:
+            txt_list = sorted(list(txt_list), key=len, reverse=True)
+
+        with open(path, "a", encoding='UTF8') as file:
+            for idx, txt_item in enumerate(txt_list):
+                data = str(txt_item) + "\n"
+                file.write(data)
+
+
+class MecabDataController:
+
+    """
+    데이터 읽고 쓰는 기능
+    """
+
+    FIRST_WORD = 0
+    MECAB_WORD = 1
+    ORIGIN_WORD = 0
+
+    MECAB_DATA = "mecab_data"
+    ITEM_BOUNDARY = ","
+    CATEGORY_SPLITER = "_"
+    FORMAT_SUFFIX = ".txt"
+    SMALL_CAT_DIVIDER = "#"
+
+    def __init__(self, ner_path: str = None, clear_mecab_dir=True):
+
+        """
+        :param ner_path: ner 데이터 경로
+        :param clear_mecab_dir: 기존 메캅 디렉터리 삭제 여부
+        """
+
+        self.ner_path = ner_path or "./"
+        self._clear_mecab_dir = clear_mecab_dir
+        self._set_mecab_path(self.ner_path)
+
+    def _set_mecab_path(self, ner_path: str) -> None:
+
+        """
+        메캅 데이터 경로 생성
+        - 경로가 정상 디렉터리인지 확인
+        :param ner_path: ner_path 입력 경로
+        :param clear_dir: 경로 전부 삭제할 것인지 여부
+        """
+
+        if not Path(ner_path).is_dir():
+            raise MecabDataReaderException("Please check if directory is proper")
+
+        self.mecab_path = Path(__file__).parent.parent.joinpath("data", self.MECAB_DATA)
+
+        if self._clear_mecab_dir:
+            self._clear_dir()
+
+        if not Path(self.mecab_path).exists():
+            Path(self.mecab_path).mkdir()
+
+    def _clear_dir(self):
+
+        """
+        메캅 데이터 저장 디렉터리 데이터 전부 삭제하는 메소드
+        - ner_example인 경우 제외
+        """
+
+        try:
+            for path_item in Path(self.mecab_path).iterdir():
+
+                if path_item.stem.startswith("ner_example"): # 예시코드 삭제 방지 코드
+                    continue
+
+                Path(path_item).unlink()
+
+        except FileNotFoundError:
+            pass
+
+    @classmethod
+    def read_category(cls, datas: List) -> Generator:
+
+        """
+        데이터에서 헤더, 내용을 나눠서 반환하는 메소드
+        SMALL_CAT로 시작되지 않을시 small_category 리스트로 반환
+        """
+
+        small_cat, *words = datas
+        small_cat_words = []
+
+        for word in words:
+            if cls.SMALL_CAT_DIVIDER in word:
+                yield small_cat, sorted(small_cat_words, key=len, reverse=True)
+                small_cat = word
+                small_cat_words = []
+                continue
+
+            if word == '':
+                continue
+
+            small_cat_words.append(word)
+
+        yield small_cat, sorted(small_cat_words, key=len, reverse=True)
+
+    @classmethod
+    def gen_all_mecab_category_data(cls, path, use_mecab_parser=False) -> Generator:
+
+        """
+        경로에 있는 데이터 읽은 뒤 카테고리 데이터셋으로 반환
+        :param path: 데이터 읽는 경로. mecab_data or ner_data경로 접근 가능
+        :param use_mecab_parser: 메캅 파서 사용시 분해되서 반환.
+        :return: 카테고리이름, 데이터 반환
+        """
+
+        for path_item in Path(path).iterdir():
+            if Path(path_item).suffix != cls.FORMAT_SUFFIX:
+                continue
+
+            txt_data = DataUtility.read_txt(path_item)
+            large_category = path_item.stem
+            c_d = CategoryData()
+
+            if not txt_data[cls.FIRST_WORD].startswith(cls.SMALL_CAT_DIVIDER):
+                txt_data.insert(cls.FIRST_WORD, cls.SMALL_CAT_DIVIDER + path_item.stem)
+
+            for small_cat_word in cls.read_category(txt_data):
+
+                small_cat, words = small_cat_word
+
+                if use_mecab_parser:
+                    words = [(word, MecabParser(sentence=word).get_word_from_mecab_compound()) for word in words]
+
+                c_d.add(small_cat, words)
+
+            yield large_category, dict(c_d.large_cat)
+
+    def write_category(self) -> None:
+
+        """
+        카테고리별로 데이터 저장하는 메소드
+        ner_path에서 데이터 읽어서 mecab_path에 데이터 저장
+        """
+
+        for data_item in self.gen_all_mecab_category_data(path=self.ner_path, use_mecab_parser=True):
+            category, content = data_item
+
+            file_name = category + self.FORMAT_SUFFIX
+            mecab_write_path = self.mecab_path.joinpath(file_name)
+
+            if file_name.startswith("ner_example"): # 예시코드 실행시 추가 방지 코드
+                continue
+
+            for content_key_item in content.keys():
+                mecab_write_list = [content_key_item,]
+                mecab_write_list.extend([str(x[self.ORIGIN_WORD]) + self.ITEM_BOUNDARY + str(x[self.MECAB_WORD]) for x in
+                             content[content_key_item]])
+                DataUtility.write_txt(path=str(mecab_write_path), txt_list=mecab_write_list)
+
+
