a5b1c02f35bdff7d32ed75df57ee38c4f99cfd40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:51 +0000

    Add package jeeachinto/0.0.0/jeeachinto-0.0.0.tar.gz

---
+++ packages/jeeachinto/jeeachinto-0.0.0.tar.gz/jeeachinto-0.0.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jeeachinto",
+    version="0.0.0",
+    author="IISS Luigi Dell' Erba",
+    author_email="me@domysh.com",
+    install_requires=[],
+    description="Use connect to hosts in an easier way",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/naodellerba/jeeachinto",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    python_requires='>=3.6',
+)
