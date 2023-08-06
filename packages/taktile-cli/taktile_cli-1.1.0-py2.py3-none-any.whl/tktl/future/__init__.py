8deeef4239050876f232681a318d8cfc9c227451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:14 +0000

    Add package taktile-cli/1.1.0/taktile_cli-1.1.0-py2.py3-none-any.whl

---
+++ packages/taktile-cli/taktile_cli-1.1.0-py2.py3-none-any.whl/tktl/future/__init__.py
@@ -0,0 +1,13 @@
+from fastapi import BackgroundTasks  # noqa: F401
+from taktile_client import ArrowClient, RestClient  # noqa: F401
+
+from .registration.decorator import Tktl  # noqa: F401
+from .registration.endpoints import (  # noqa: F401
+    ArrowEndpoint,
+    EndpointResponse,
+    ProfiledEndpoint,
+    TypedEndpoint,
+)
+from .settings import Settings
+
+settings = Settings()
