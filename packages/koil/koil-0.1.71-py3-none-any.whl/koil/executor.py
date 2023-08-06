b7649bfa08fa2a1ee43f03055caef0c9ece4dd2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:09 +0000

    Add package koil/0.1.71/koil-0.1.71-py3-none-any.whl

---
+++ packages/koil/koil-0.1.71-py3-none-any.whl/koil/executor.py
@@ -0,0 +1,38 @@
+from concurrent.futures import ThreadPoolExecutor
+import asyncio
+import threading
+from koil.vars import *
+
+
+class KoiledExecutor(ThreadPoolExecutor):
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            *args,
+            **kwargs,
+        )
+
+    def submit(self, fn, *args, **kwargs):
+        raise Exception("Do not call this directly. Await asubmit instead")
+
+    async def asubmit(self, fn, *args, **kwargs):
+        copy_loop = asyncio.get_event_loop()
+
+        assert (
+            copy_loop.is_running()
+        ), "Loop is not running. You shouldn't be using this"
+
+        cancel_event = threading.Event()
+
+        def wrap(*args, **kwargs):
+            current_loop.set(copy_loop)
+            current_cancel_event.set(cancel_event)
+            t = fn(*args, **kwargs)
+            current_loop.set(None)
+            current_cancel_event.set(None)
+            return t
+
+        try:
+            return await asyncio.wrap_future(super().submit(wrap, *args, **kwargs))
+        except asyncio.CancelledError:
+            cancel_event.set()
+            raise
