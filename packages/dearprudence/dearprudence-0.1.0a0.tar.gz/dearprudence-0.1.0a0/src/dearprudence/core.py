44ff6909879c8171fb744c7df191421bb18559ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:16 +0000

    Add package dearprudence/0.1.0a0/dearprudence-0.1.0a0.tar.gz

---
+++ packages/dearprudence/dearprudence-0.1.0a0.tar.gz/dearprudence-0.1.0a0/src/dearprudence/core.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+
+
+__all__ = ["Cmip6Record", "SimpleRun", "DtrRun"]
+
+
+@dataclass
+class Cmip6Record:
+    activity_id: str
+    experiment_id: str
+    table_id: str
+    variable_id: str
+    source_id: str
+    institution_id: str
+    member_id: str
+    grid_label: str
+    version: str
+
+
+@dataclass
+class SimpleRun:
+    target: str
+    variable_id: str
+    historical: Cmip6Record
+    ssp: Cmip6Record
+
+
+@dataclass
+class DtrRun:
+    target: str
+    variable_id: str
+    tasmin: SimpleRun
+    tasmax: SimpleRun
