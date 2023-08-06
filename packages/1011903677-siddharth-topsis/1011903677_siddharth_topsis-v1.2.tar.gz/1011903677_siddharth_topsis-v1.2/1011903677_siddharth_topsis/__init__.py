b7346530f91e7db4b43e8eac97290f18d8644ed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:45 +0000

    Add package 1011903677-siddharth-topsis/1.2/1011903677_siddharth_topsis-v1.2.tar.gz

---
+++ packages/1011903677-siddharth-topsis/1011903677_siddharth_topsis-v1.2.tar.gz/1011903677_siddharth_topsis-v1.2/1011903677_siddharth_topsis/__init__.py
@@ -0,0 +1,2 @@
+from 1011903677_siddharth_topsis.topsis import rank
+__version__='v1.2'
---
+++ packages/1011903677-siddharth-topsis/1011903677_siddharth_topsis-v1.2.tar.gz/1011903677_siddharth_topsis-v1.2/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = '1011903677_siddharth_topsis',
+  packages = ['1011903677_siddharth_topsis'],
+  version = 'v1.2',
+  license='MIT',
+  description = '',
+  author = 'siddharth ',
+  author_email = 'sraghuvanshi_be19@thapar.edu',
+  url = '',
+  download_url = '',
+  keywords = ['topsis','topsis score','rank','Thapar'],
+  install_requires=['numpy','pandas' ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
