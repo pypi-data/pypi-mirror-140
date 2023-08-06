b5619e4b7c25fef225a8fd91f2ed890e29ee3a20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:54 +0000

    Add package cachecore/0.2.1/cachecore-0.2.1.tar.gz

---
+++ packages/cachecore/cachecore-0.2.1.tar.gz/cachecore-0.2.1/src/cachecore/caches/__init__.py
@@ -0,0 +1,5 @@
+from cachecore.caches.base import CacheInterface, BaseCache
+from cachecore.caches.dummy import DummyCache
+from cachecore.caches.file import FileCache
+from cachecore.caches.local import LocalCache
+from cachecore.caches.redis import RedisCache
---
+++ packages/cachecore/cachecore-0.2.1.tar.gz/cachecore-0.2.1/src/cachecore/utils.py
@@ -0,0 +1,18 @@
+from math import ceil
+import time
+
+
+def ttl_to_exptime(ttl):
+    if ttl is None:
+        return None
+    return time.time() + ttl
+
+
+def ttl_remaining(exp_time):
+    if exp_time is None:
+        return None
+    return max(0, ceil(exp_time - time.time()))
+
+
+def is_expired(exp_time):
+    return ttl_remaining(exp_time) == 0
