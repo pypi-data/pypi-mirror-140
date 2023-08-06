348f0ee7b3e6f240edd57a9bfacd93c5a8c9968c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:40 +0000

    Add package ipptemplate/0.1.5/ipptemplate-0.1.5-py3-none-any.whl

---
+++ packages/ipptemplate/ipptemplate-0.1.5-py3-none-any.whl/package_example/example_class.py
@@ -0,0 +1,28 @@
+##  @package PACKAGENAME
+#   DESCRIPTION
+
+import sys
+import os
+
+
+##  CLASS DESCRIPTION
+#   \version 0.1.0 <br>
+#   \details
+class ExampleClass(object):
+
+
+    ##  This is the constructor
+    #   @param
+    #   @param
+    def __init__(self):
+        print("ExampleClass init")
+        pass
+
+
+
+
+if __name__ == '__main__':
+    pass
+
+
+
