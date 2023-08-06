01198750f31fb2aba48b223062731686f349ee49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:56 +0000

    Add package hatyan/2.5.0/hatyan-2.5.0-py3-none-any.whl

---
+++ packages/hatyan/hatyan-2.5.0-py3-none-any.whl/hatyan/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+.. include:: ../README.md
+"""
+
+__author__ = """Jelmer Veenstra"""
+__email__ = 'jelmer.veenstra@deltares.nl'
+__version__ = '2.5.0'
+
+from hatyan.analysis_prediction import *
+from hatyan.astrog import *
+from hatyan.components import *
+from hatyan.foreman_core import *
+from hatyan.hatyan_core import *
+from hatyan.timeseries import *
+from hatyan.wrapper_RWS import *
+from hatyan.getonlinedata import *
