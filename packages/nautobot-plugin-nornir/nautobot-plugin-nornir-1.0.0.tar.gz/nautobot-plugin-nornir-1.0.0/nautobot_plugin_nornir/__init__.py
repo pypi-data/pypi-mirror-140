89394a6355b81c955fc83bfaa7fedc48597b06b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:26 +0000

    Add package nautobot-plugin-nornir/1.0.0/nautobot-plugin-nornir-1.0.0.tar.gz

---
+++ packages/nautobot-plugin-nornir/nautobot-plugin-nornir-1.0.0.tar.gz/nautobot-plugin-nornir-1.0.0/nautobot_plugin_nornir/__init__.py
@@ -0,0 +1,24 @@
+"""Plugin declaration for nautobot_plugin_nornir."""
+
+__version__ = "1.0.0"
+
+from nautobot.extras.plugins import PluginConfig
+
+
+class NornirConfig(PluginConfig):
+    """Plugin configuration for nautobot_plugin_nornir."""
+
+    name = "nautobot_plugin_nornir"
+    verbose_name = "Nautobot Plugin for Nornir"
+    version = __version__
+    author = "Network to Code, LLC"
+    description = "A plugin/library for using Nornir within Nautobot."
+    base_url = "plugin-nornir"
+    required_settings = []
+    # min_version = "0"
+    # max_version = "100"
+    default_settings = {}
+    caching_config = {}
+
+
+config = NornirConfig  # pylint:disable=invalid-name
