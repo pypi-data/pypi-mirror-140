196cb110904daf98f3fb2c0105396100873c73ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:38 +0000

    Add package nautilus-librarian/0.3.2/nautilus-librarian-0.3.2.tar.gz

---
+++ packages/nautilus-librarian/nautilus-librarian-0.3.2.tar.gz/nautilus-librarian-0.3.2/tests/test_nautilus_librarian/test_domain/test_file_locator.py
@@ -0,0 +1,29 @@
+from nautilus_librarian.domain.file_locator import (
+    file_locator,
+    get_base_image_filename_from_gold_image,
+)
+from nautilus_librarian.mods.namecodes.domain.filename import Filename
+
+
+def test_gold_image_localization():
+    gold_image_filename = Filename("000001-32.600.2.tif")
+
+    folder = file_locator(gold_image_filename)
+
+    assert folder == "data/000001/32"
+
+
+def test_base_image_localization():
+    gold_image_filename = Filename("000001-52.600.2.tif")
+
+    folder = file_locator(gold_image_filename)
+
+    assert folder == "data/000001/52"
+
+
+def it_should_return_the_corresponding_base_image_filename_from_the_gold_image():
+    gold_image_filename = Filename("000001-32.600.2.tif")
+
+    base_image_filename = get_base_image_filename_from_gold_image(gold_image_filename)
+
+    assert base_image_filename == Filename("000001-52.600.2.tif")
