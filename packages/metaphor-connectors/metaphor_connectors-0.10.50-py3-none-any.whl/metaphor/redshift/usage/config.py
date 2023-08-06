8c2d3258cb46fb4be7699aaae9571f47d6ad0f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:44 +0000

    Add package metaphor-connectors/0.10.50/metaphor_connectors-0.10.50-py3-none-any.whl

---
+++ packages/metaphor-connectors/metaphor_connectors-0.10.50-py3-none-any.whl/metaphor/redshift/usage/config.py
@@ -0,0 +1,14 @@
+from dataclasses import field as dataclass_field
+from typing import Set
+
+from pydantic.dataclasses import dataclass
+
+from metaphor.redshift.config import RedshiftRunConfig
+
+
+@dataclass
+class RedshiftUsageRunConfig(RedshiftRunConfig):
+    lookback_days: int = 30
+
+    # Query filter to exclude certain usernames from the processing
+    excluded_usernames: Set[str] = dataclass_field(default_factory=lambda: set())
