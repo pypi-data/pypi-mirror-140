dccb4e05cbba95e600f69b47e11fedba7c07f0d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:18 +0000

    Add package megacrazyscripts/0.1.5/megacrazyscripts-0.1.5.tar.gz

---
+++ packages/megacrazyscripts/megacrazyscripts-0.1.5.tar.gz/megacrazyscripts-0.1.5/megacrazyscripts/ytdw.py
@@ -0,0 +1,24 @@
+def ytdw(url, output):
+
+    prefix = '(ytdw)'
+    
+    from megacrazyscripts import converter
+    from youtube_dl import YoutubeDL
+    from os import remove
+    
+    output_temp = output + '.wav'
+
+    # Download audio
+    print(prefix, 'Downloading audio...')
+    audio = YoutubeDL({'format':'bestaudio', 'audio-format':'wavs', 'audio-quality':'0', 'outtmpl':output_temp})
+    audio.download([url])
+
+    # Convert temporary file to wav
+    print(prefix, f'Converting {output_temp} to wav...')
+    #converter(output_temp)
+
+    # Remove first file
+    print(prefix, 'Removing first file...')
+    #remove(output_temp)
+    
+    return print(prefix, 'Done')
