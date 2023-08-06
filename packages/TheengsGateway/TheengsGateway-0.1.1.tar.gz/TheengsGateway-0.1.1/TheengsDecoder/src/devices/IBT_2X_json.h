1118072dc7ee6267c4463405901fc5231e383f7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:38 +0000

    Add package TheengsGateway/0.1.1/TheengsGateway-0.1.1.tar.gz

---
+++ packages/TheengsGateway/TheengsGateway-0.1.1.tar.gz/TheengsGateway-0.1.1/TheengsDecoder/src/devices/IBT_2X_json.h
@@ -0,0 +1,34 @@
+const char* _IBT_2X_json = "{\"brand\":\"Inkbird\",\"model\":\"iBBQ\",\"model_id\":\"IBT-2X\",\"condition\":[\"name\",\"index\",0,\"iBBQ\",\"&\",\"manufacturerdata\",\"=\",24,\"index\",0,\"0000\"],\"properties\":{\"tempc\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",16,4,true,false],\"post_proc\":[\"/\",10]},\"tempc2\":{\"decoder\":[\"value_from_hex_data\",\"manufacturerdata\",20,4,true,false],\"post_proc\":[\"/\",10]}}}";
+
+/*R""""(
+{
+   "brand":"Inkbird",
+   "model":"iBBQ",
+   "model_id":"IBT-2X",
+   "condition":["name", "index", 0, "iBBQ","&","manufacturerdata", "=", 24, "index", 0, "0000"],
+   "properties":{
+      "tempc":{
+         "decoder":["value_from_hex_data", "manufacturerdata", 16, 4, true, false],
+         "post_proc":["/", 10]
+      },
+      "tempc2":{
+         "decoder":["value_from_hex_data", "manufacturerdata", 20, 4, true, false],
+         "post_proc":["/", 10]
+      }
+   }
+})"""";*/
+
+const char* _IBT_2X_json_props = "{\"properties\":{\"tempc\":{\"unit\":\"°C\",\"name\":\"temperature\"},\"tempc2\":{\"unit\":\"°C\",\"name\":\"temperature\"}}}";
+/*R""""(
+{
+   "properties":{
+      "tempc":{
+         "unit":"°C",
+         "name":"temperature"
+      },
+      "tempc2":{
+         "unit":"°C",
+         "name":"temperature"
+      }
+   }
+})"""";*/
