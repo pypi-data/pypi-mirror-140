cced6e551b16eed37a7b564f35a7d9e67d76ac5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:43 +0000

    Add package x3d/4.0.56/x3d-4.0.56.tar.gz

---
+++ packages/x3d/x3d-4.0.56.tar.gz/x3d-4.0.56/setup.py
@@ -0,0 +1,56 @@
+"""
+setup.py is configuration information for the *x3d* PyPi project.
+"""
+
+# https://docs.python.org/3/distutils/setupscript.html
+
+import setuptools
+
+DISTUTILS_DEBUG='verbose'
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="x3d",
+    version="4.0.56", 
+    author="Don Brutzman",
+    author_email="brutzman@nps.edu",
+    maintainer="Don Brutzman",
+    maintainer_email="brutzman@nps.edu",
+    description="Package support for Extensible 3D (X3D) Graphics International Standard (IS)",
+### https://stackoverflow.com/questions/9977889/how-to-include-license-file-in-setup-py-script
+    license_files = ('license.txt',),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+#   download_url=""
+    url="https://www.web3d.org/x3d/stylesheets/python/python.html",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.10", # not 3.10.x
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Multimedia :: Graphics :: 3D Modeling",
+        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
+        "Topic :: Multimedia :: Graphics :: Presentation",
+        "Topic :: Multimedia :: Graphics :: Viewers",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Text Processing :: Markup :: VRML",
+    ],
+    data_files=[
+        ("examples",["examples/PythonX3dSmokeTests.py"])
+    ],
+    keywords=[
+        "X3D","3D","graphics","Web3D"
+    ],
+###     "Topic :: Text Processing :: Markup :: X3D",
+)
+
+### package_dir={'x3d': 'dist/x3d'}, # testing...
