88daaddcd9dbc1520e5f972989ef7ab2b071b8fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:41 +0000

    Add package Topsis-Anushri-101903678/1.2/Topsis_Anushri_101903678-v1.2.tar.gz

---
+++ packages/Topsis-Anushri-101903678/Topsis_Anushri_101903678-v1.2.tar.gz/Topsis_Anushri_101903678-v1.2/Topsis_Anushri_101903678/__init__.py
@@ -0,0 +1,2 @@
+from Topsis_Anushri_101903678.topsis import rank
+__version__='v1.2'
---
+++ packages/Topsis-Anushri-101903678/Topsis_Anushri_101903678-v1.2.tar.gz/Topsis_Anushri_101903678-v1.2/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'Topsis_Anushri_101903678',        
+  packages = ['Topsis_Anushri_101903678'],  
+  version = 'v1.2',      
+  license='MIT',       
+  description = 'Topsis Score and Rank generator',
+  author = 'Anushri Jain',                
+  author_email = 'ajain4_be19@thapar.edu',      
+  url = 'https://github.com/anushri2325/Topsis_Anushri_101903678',   
+  download_url = '',    
+  keywords = ['topsis','topsis score','rank','Thapar'],
+  install_requires=['numpy','pandas' ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',    
+    'Intended Audience :: Developers',   
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
