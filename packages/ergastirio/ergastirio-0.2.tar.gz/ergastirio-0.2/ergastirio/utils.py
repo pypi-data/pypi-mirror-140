fdb7aa5c31fc055add2a3e14fc4610fd11854649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:06 +0000

    Add package ergastirio/0.2/ergastirio-0.2.tar.gz

---
+++ packages/ergastirio/ergastirio-0.2.tar.gz/ergastirio-0.2/ergastirio/utils.py
@@ -0,0 +1,7 @@
+def disable_widget(widgets):
+    for widget in widgets:
+        widget.setEnabled(False)   
+
+def enable_widget(widgets):
+    for widget in widgets:
+        widget.setEnabled(True)  
