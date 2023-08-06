b5c4402f5967a38632437c407dd7d638d18ed785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:03 +0000

    Add package Topsis-Gaurvi-401903031/0.0.1/Topsis-Gaurvi-401903031-0.0.1.tar.gz

---
+++ packages/Topsis-Gaurvi-401903031/Topsis-Gaurvi-401903031-0.0.1.tar.gz/Topsis-Gaurvi-401903031-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A package for doing TOPSIS on the data'
+LONG_DESCRIPTION = 'a package for TOPSIS'
+
+# Setting up
+setup(
+    name="Topsis-Gaurvi-401903031",
+    version=VERSION,
+    author="Gaurvi Rajwanshi",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python' 'Topsis'],
+    classifiers=[
+        
+    ]
+)
