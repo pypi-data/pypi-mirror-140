1b01ee407cab5cebc469b42eff20c7fe86e0fdd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:42 +0000

    Add package example-package-vdande/1.0.3/example-package-vdande-1.0.3.tar.gz

---
+++ packages/example-package-vdande/example-package-vdande-1.0.3.tar.gz/example-package-vdande-1.0.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-package-vdande",
+    version="1.0.3",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
