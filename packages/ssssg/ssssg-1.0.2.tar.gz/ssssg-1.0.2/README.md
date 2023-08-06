021f44d60c03c4e5c9c1a5b221ba01356f5edb6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:53 +0000

    Add package ssssg/1.0.2/ssssg-1.0.2.tar.gz

---
+++ packages/ssssg/ssssg-1.0.2.tar.gz/ssssg-1.0.2/README.md
@@ -0,0 +1,6 @@
+# SSSSG - Stupidly Simple Static Site Generator
+## TO-DO
+- Alias ssssg with s4g
+- update deploy job
+- Add lint testing
+- update lint job
---
+++ packages/ssssg/ssssg-1.0.2.tar.gz/ssssg-1.0.2/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+__version__ = "1.0.2"
+
+
+if __name__ == "__main__":
+    setup(
+        version=__version__
+    )
