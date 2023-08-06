812ba045fc0097195e03b5f09c7be13119a8cd9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:03 +0000

    Add package iot-gh/2.0.1/iot_gh-2.0.1-py3-none-any.whl

---
+++ packages/iot-gh/iot_gh-2.0.1-py3-none-any.whl/iot_gh/IoTGreenhouseService.py
@@ -0,0 +1,17 @@
+
+
+from iot_gh.GHService import GHService
+
+class IoTGreenhouseService(GHService):
+    """A container class for IoT Greenhouse services implemented on the 
+    Raspberry Pi.
+    """
+
+    def __init__(self, pi=None):
+        import spidev
+
+        if not pi:
+            import pigpio
+            pi = pigpio.pi()
+        spi = spidev.SpiDev()
+        super().__init__(pi, spi)
