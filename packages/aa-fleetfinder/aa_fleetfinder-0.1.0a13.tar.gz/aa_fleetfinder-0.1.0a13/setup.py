8f1f6c23b258881c9f6974e00180c02eacffbabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:01 +0000

    Add package aa-fleetfinder/0.1.0a13/aa_fleetfinder-0.1.0a13.tar.gz

---
+++ packages/aa-fleetfinder/aa_fleetfinder-0.1.0a13.tar.gz/aa_fleetfinder-0.1.0a13/setup.py
@@ -0,0 +1,14 @@
+"""
+setup
+"""
+
+# Third Party
+import pkg_resources
+import setuptools
+
+setuptools_version = pkg_resources.parse_version(setuptools.__version__)
+
+if setuptools_version < pkg_resources.parse_version("39.2"):
+    raise SystemExit("Please upgrade setuptools")
+
+setuptools.setup()
