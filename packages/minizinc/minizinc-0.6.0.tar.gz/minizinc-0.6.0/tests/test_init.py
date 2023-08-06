1df3db54b1fac0d7a9395d4f4577c5494827f05e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:53 +0000

    Add package minizinc/0.6.0/minizinc-0.6.0.tar.gz

---
+++ packages/minizinc/minizinc-0.6.0.tar.gz/minizinc-0.6.0/tests/test_init.py
@@ -0,0 +1,13 @@
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import minizinc
+
+
+def test_default_driver():
+    assert minizinc.default_driver is not None
+
+
+def test_version():
+    # Test normal behaviour
+    assert "MiniZinc" in minizinc.default_driver.minizinc_version
