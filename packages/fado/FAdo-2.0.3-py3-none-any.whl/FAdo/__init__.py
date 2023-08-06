25cb68be99afab4d0f8fc0beddf9a3063b334b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:22:20 +0000

    Add package fado/2.0.3/FAdo-2.0.3-py3-none-any.whl

---
+++ packages/fado/FAdo-2.0.3-py3-none-any.whl/FAdo/__init__.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+""" FAdo initialization"""
+version="2.0b.01"
+__all__=["fa","reex","common","cfg","fl","comboperations","ska","conversions","codes","transducers",
+         "rndfap","rndadfa","fio"]
+__package__="FAdo"
