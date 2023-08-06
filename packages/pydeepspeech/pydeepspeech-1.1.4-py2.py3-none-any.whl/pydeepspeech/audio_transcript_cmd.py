e824d122b144152bc04e1f26492564af65a0b5bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:16 +0000

    Add package pydeepspeech/1.1.4/pydeepspeech-1.1.4-py2.py3-none-any.whl

---
+++ packages/pydeepspeech/pydeepspeech-1.1.4-py2.py3-none-any.whl/pydeepspeech/audio_transcript_cmd.py
@@ -0,0 +1,42 @@
+import argparse
+import logging
+import sys
+
+from pydeepspeech.transcribe import transcribe
+
+# Debug helpers
+logging.basicConfig(stream=sys.stderr, level=logging.CRITICAL)
+
+
+def main(args):
+    parser = argparse.ArgumentParser(
+        description="Transcribe long audio files using webRTC VAD or use the streaming interface"
+    )
+    parser.add_argument(
+        "--aggressive",
+        type=int,
+        choices=range(4),
+        required=False,
+        help="Determines how aggressive filtering out non-speech is. (Interger between 0-3)",
+    )
+    parser.add_argument(
+        "--audio",
+        required=False,
+        help="Path to the audio file to run (WAV format)",
+    )
+    parser.add_argument(
+        "--model",
+        required=True,
+        help="Path to directory that contains all model files (output_graph and scorer)",
+    )
+    args = parser.parse_args(args)
+    if args.audio is not None:
+        logging.debug("Transcribing audio file @ %s" % args.audio)
+    else:
+        parser.print_help()
+        parser.exit()
+    transcribe(args.aggressive, args.audio, args.model)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
