a23c3fa827b1fa7892682f00f9de8590ef10b379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:00 +0000

    Add package banana-hep/0.6.1/banana-hep-0.6.1.tar.gz

---
+++ packages/banana-hep/banana-hep-0.6.1.tar.gz/banana-hep-0.6.1/src/banana/data/theories.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import pathlib
+
+import yaml
+
+from . import db, sql
+
+_here = pathlib.Path(__file__).parent
+
+# load default theory
+default_card = {}
+with open(_here / "theory_template.yaml", encoding="utf-8") as f:
+    default_card = yaml.safe_load(f)
+default_card = dict(sorted(default_card.items()))
+
+# db interface
+def load(session, updates):
+    # add hash
+    raw_records, df = sql.prepare_records(default_card, updates)
+    # insert new ones
+    sql.insertnew(session, db.Theory, df)
+    return raw_records
