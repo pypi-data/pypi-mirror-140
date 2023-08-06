45eb58c5139ff1f71375e9cbcc3097025213c2e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:58 +0000

    Add package git-dashboard/0.1.2/git_dashboard-0.1.2.tar.gz

---
+++ packages/git-dashboard/git_dashboard-0.1.2.tar.gz/git_dashboard-0.1.2/setup.py
@@ -0,0 +1,56 @@
+"""
+Pypi setup file
+"""
+
+import os
+
+from setuptools import setup, find_packages
+
+import git_dashboard
+
+PATH_ROOT = os.path.dirname(__file__)
+
+def load_requirements():
+    """Load requirements.txt with comment support"""
+    lines = []
+    comment_char = '#'
+    with open(os.path.join(PATH_ROOT, 'requirements.txt'), 'r', encoding='utf-8') as file:
+        lines += [ln.strip() for ln in file.readlines()]
+    reqs = []
+    for ln in lines:
+        # filter all comments
+        if comment_char in ln:
+            ln = ln[:ln.index(comment_char)]
+        if ln:  # if requirement is not empty
+            reqs.append(ln)
+    return reqs
+
+def load_readme():
+    """Load README.md as string"""
+    with open('README.md', encoding='utf-8') as inp:
+        return inp.read()
+
+setup(
+    name='git_dashboard',
+    version=git_dashboard.__version__,
+    description="Git dashboard",
+    author="Jung Ko",
+    author_email="kojung@gmail.com",
+    license="GPL",
+    platforms=['any'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    python_requires='>=3.6',
+    install_requires=load_requirements(),
+    scripts=[
+        'bin/git-dashboard',
+    ],
+    long_description=load_readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/kojung/git-dashboard',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ packages/git-dashboard/git_dashboard-0.1.2.tar.gz/git_dashboard-0.1.2/src/git_dashboard/__init__.py
@@ -0,0 +1,8 @@
+"""Package info"""
+
+__version__ = '0.1.2'
+__docs__ = 'Git Dashboard'
+__author__ = 'Jung Ko'
+__author_email__ = 'kojung@gmail.com'
+__homepage__ = 'https://github.com/kojung/git-dashboard'
+__license__ = 'GPL'
