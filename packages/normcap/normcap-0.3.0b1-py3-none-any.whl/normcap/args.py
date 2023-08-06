f042ca3cfb161db9f6016a9b25becf3aa5727153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:56 +0000

    Add package normcap/0.3.0b1/normcap-0.3.0b1-py3-none-any.whl

---
+++ packages/normcap/normcap-0.3.0b1-py3-none-any.whl/normcap/args.py
@@ -0,0 +1,41 @@
+"""Definition of CLI arguments."""
+
+import argparse
+
+from normcap.gui.constants import DEFAULT_SETTINGS, DESCRIPTION
+
+
+def create_argparser() -> argparse.ArgumentParser:
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(prog="normcap", description=DESCRIPTION)
+
+    for setting in DEFAULT_SETTINGS:
+        parser.add_argument(
+            f"-{setting.flag}",
+            f"--{setting.key}",
+            type=setting.type_,
+            help=setting.help,
+        )
+
+    parser.add_argument(
+        "-r",
+        "--reset",
+        action="store_true",
+        help="reset all settings to default values",
+        default=False,
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="print debug information to console",
+        default=False,
+    )
+    parser.add_argument(
+        "-V",
+        "--very-verbose",
+        action="store_true",
+        help="print more debug information to console",
+        default=False,
+    )
+    return parser
