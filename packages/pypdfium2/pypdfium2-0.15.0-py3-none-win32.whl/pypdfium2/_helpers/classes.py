5907d9c61138b9032c07eaf9d448bc238d065cd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:49:29 +0000

    Add package pypdfium2/0.15.0/pypdfium2-0.15.0-py3-none-win32.whl

---
+++ packages/pypdfium2/pypdfium2-0.15.0-py3-none-win32.whl/pypdfium2/_helpers/classes.py
@@ -0,0 +1,80 @@
+# SPDX-FileCopyrightText: 2022 geisserml <geisserml@gmail.com>
+# SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause 
+
+from pypdfium2._helpers import (
+    opener,
+    saver,
+    toc,
+    pdf_renderer,
+    page_renderer,
+)
+
+
+class PdfDocument:
+    """
+    Document class that maps the functional support model to an object-oriented API,
+    unifying the separate helpers.
+    
+    Parameters:
+        input_obj: The file or data to load using :func:`.open_pdf_auto`.
+        password: A password to unlock the PDF, if encrypted.
+    """
+    
+    def __init__(
+            self,
+            input_obj,
+            password = None,
+        ):
+        self._input_obj = input_obj
+        self._password = password
+        self._pdf, self._ld_data = opener.open_pdf_auto(
+            self._input_obj,
+            password = self._password,
+        )
+    
+    @property
+    def raw(self):
+        """ Get the raw PDFium ``FPDF_DOCUMENT`` handle. """
+        return self._pdf
+    
+    def close(self):
+        """
+        Close the document to release allocated memory. This method must be called when
+        done with processing the PDF.
+        """
+        return opener.close_pdf(self._pdf, self._ld_data)
+    
+    def save(self, *args, **kws):
+        """
+        Save the PDF into an output byte buffer (see :func:`.save_pdf`).
+        """
+        return saver.save_pdf(self._pdf, *args, **kws)
+    
+    def get_toc(self, max_depth=None):
+        """
+        Incrementally read the document's table of contents (see :func:`.get_toc`).
+        """
+        yield from toc.get_toc(
+            self._pdf,
+            max_depth = max_depth,
+        )
+    
+    def render_pdf(self, **kws):
+        """
+        Incrementally render multiple pages (see :func:`.render_pdf`).
+        """
+        yield from pdf_renderer.render_pdf(
+            self._input_obj,
+            password = self._password,
+            **kws
+        )
+    
+    def render_page(self, index, **kws):
+        """
+        Render a single page (see :func:`.render_page`).
+        """
+        return page_renderer.render_page(
+            self._pdf,
+            page_index = index,
+            **kws
+        )
