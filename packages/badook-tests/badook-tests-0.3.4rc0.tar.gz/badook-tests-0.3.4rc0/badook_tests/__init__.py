b71942fa8b2405f021c11b541473c39a951999fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:43 +0000

    Add package badook-tests/0.3.4rc0/badook-tests-0.3.4rc0.tar.gz

---
+++ packages/badook-tests/badook-tests-0.3.4rc0.tar.gz/badook-tests-0.3.4rc0/badook_tests/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+from badook_tests import *
+from badook_tests.data_test_case import DataTestCase
+from badook_tests.runner import BadookRuntimeTestResult, BadookBaseTestResult, BadookTestRunnerError
+
+__version__ = '0.3.4-pre.0'
