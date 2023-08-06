894d2917c72be7bcf9c92d4b639a96c658e34585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:48 +0000

    Add package jupyterlite-sphinx/0.4.0/jupyterlite-sphinx-0.4.0.tar.gz

---
+++ packages/jupyterlite-sphinx/jupyterlite-sphinx-0.4.0.tar.gz/jupyterlite-sphinx-0.4.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='jupyterlite-sphinx',
+    version='0.4.0',
+    package_dir={'': 'src'},
+    py_modules=['jupyterlite_sphinx'],
+    python_requires='>=3.7',
+    install_requires=[
+        'docutils',
+        'jupyterlite',
+        'sphinx>=1.8',
+        'jupyter_server',
+        'jupyterlab_server',
+    ],
+    author='Martin Renou',
+    author_email='martin.renou@gmail.com',
+    description='Sphinx extension for deploying JupyterLite',
+    license='MIT',
+    zip_safe=True,
+)
