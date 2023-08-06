1d551a80d172e352e637504c6c96daae60e128d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:39:49 +0000

    Add package dhlab/2.0.20/dhlab-2.0.20-py3-none-any.whl

---
+++ packages/dhlab/dhlab-2.0.20-py3-none-any.whl/dhlab/text/__init__.py
@@ -0,0 +1,6 @@
+from .conc_coll import Concordance, Collocations, Counts
+from .corpus import Corpus, Corpus_from_identifiers, Corpus_from_identifiers as CorpusFromIdentifiers
+from .dispersion import Dispersion
+from .nbtokenizer import Tokens, tokenize
+from .chunking import Chunks
+from .geo_data import GeoData
