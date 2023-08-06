bea73f359a8f05689b925e226fff8f5817521ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:53 +0000

    Add package cornflow-client/0.35.0/cornflow_client-0.35.0-py3-none-any.whl

---
+++ packages/cornflow-client/cornflow_client-0.35.0-py3-none-any.whl/cornflow_client/core/tools.py
@@ -0,0 +1,27 @@
+"""
+
+"""
+# Full imports
+import json
+
+# Partial imports
+from pytups import OrderSet
+
+
+def new_set(seq):
+    """
+    :param seq: a (hopefully unique) list of elements (tuples, strings, etc.)
+    Returns a new ordered set
+    """
+    return OrderSet(seq)
+
+
+def load_json(path):
+    with open(path) as json_file:
+        file = json.load(json_file)
+    return file
+
+
+def save_json(data, path):
+    with open(path, "w") as outfile:
+        json.dump(data, outfile)
