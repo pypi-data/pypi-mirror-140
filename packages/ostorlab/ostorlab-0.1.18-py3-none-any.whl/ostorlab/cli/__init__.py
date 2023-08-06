e089f23e3b234e3977b997a14519a0e104f2cbe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:24 +0000

    Add package ostorlab/0.1.18/ostorlab-0.1.18-py3-none-any.whl

---
+++ packages/ostorlab/ostorlab-0.1.18-py3-none-any.whl/ostorlab/cli/__init__.py
@@ -0,0 +1,6 @@
+"""Ostorlab cli package."""
+from ostorlab.cli import scan
+from ostorlab.cli import auth
+from ostorlab.cli import agent
+from ostorlab.cli import vulnz
+from ostorlab.cli import agentgroup
