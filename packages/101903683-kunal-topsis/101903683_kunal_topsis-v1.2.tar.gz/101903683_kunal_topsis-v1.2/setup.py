317d5dec1e7a26666bc3c66ea3abba4503d1ef43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:39 +0000

    Add package 101903683-kunal-topsis/1.2/101903683_kunal_topsis-v1.2.tar.gz

---
+++ packages/101903683-kunal-topsis/101903683_kunal_topsis-v1.2.tar.gz/101903683_kunal_topsis-v1.2/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = '101903683_kunal_topsis',
+  packages = ['101903683_kunal_topsis'],
+  version = 'v1.2',
+  license='MIT',
+  description = '',
+  author = 'Kunal Garg',
+  author_email = 'kgarg2_be19@thapar.edu',
+  url = 'https://github.com/Gargkunal02/101903683_kunal_topsis',
+  download_url = '',
+  keywords = ['topsis','topsis score','rank','Thapar'],
+  install_requires=['numpy','pandas' ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
