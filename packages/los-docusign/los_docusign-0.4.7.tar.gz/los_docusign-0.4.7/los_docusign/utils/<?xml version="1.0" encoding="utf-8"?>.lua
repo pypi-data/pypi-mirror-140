7202a9b5df3ac4075de7767ac699238e8a44bcf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:27 +0000

    Add package los-docusign/0.4.7/los_docusign-0.4.7.tar.gz

---
+++ packages/los-docusign/los_docusign-0.4.7.tar.gz/los_docusign-0.4.7/los_docusign/utils/<?xml version="1.0" encoding="utf-8"?>.lua
@@ -0,0 +1,390 @@
+<?xml version="1.0" encoding="utf-8"?>
+<DocuSignEnvelopeInformation
+	xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xmlns="http://www.docusign.net/API/3.0">
+	<EnvelopeStatus>
+		<RecipientStatuses>
+			<RecipientStatus>
+				<Type>Signer</Type>
+				<Email>tejas.reva@gmail.com</Email>
+				<UserName>Tejas Bhandari</UserName>
+				<RoutingOrder>1</RoutingOrder>
+				<Sent>2022-03-01T03:50:45.523</Sent>
+				<Delivered>2022-03-01T03:52:03.19</Delivered>
+				<Signed>2022-03-01T03:52:12.113</Signed>
+				<DeclineReason xsi:nil="true" />
+				<Status>Completed</Status>
+				<RecipientIPAddress>106.193.223.85</RecipientIPAddress>
+				<CustomFields>
+					<CustomField>borrower</CustomField>
+				</CustomFields>
+				<TabStatuses>
+					<TabStatus>
+						<TabType>Custom</TabType>
+						<Status>Signed</Status>
+						<XPosition>144</XPosition>
+						<YPosition>144</YPosition>
+						<TabLabel>organization_name</TabLabel>
+						<TabName />
+						<TabValue>LRT Capital</TabValue>
+						<DocumentID>1</DocumentID>
+						<PageNumber>1</PageNumber>
+						<OriginalValue>LRT Capital</OriginalValue>
+						<ValidationPattern />
+						<CustomTabType>Text</CustomTabType>
+					</TabStatus>
+					<TabStatus>
+						<TabType>Custom</TabType>
+						<Status>Signed</Status>
+						<XPosition>275</XPosition>
+						<YPosition>808</YPosition>
+						<TabLabel>pn_borrower_signer_1</TabLabel>
+						<TabName />
+						<TabValue>Tejas Bhandari</TabValue>
+						<DocumentID>1</DocumentID>
+						<PageNumber>1</PageNumber>
+						<OriginalValue>Tejas Bhandari</OriginalValue>
+						<ValidationPattern />
+						<CustomTabType>Text</CustomTabType>
+					</TabStatus>
+					<TabStatus>
+						<TabType>SignHere</TabType>
+						<Status>Signed</Status>
+						<XPosition>275</XPosition>
+						<YPosition>721</YPosition>
+						<TabLabel>borrower_signature</TabLabel>
+						<TabName>SignHere</TabName>
+						<TabValue />
+						<DocumentID>1</DocumentID>
+						<PageNumber>1</PageNumber>
+					</TabStatus>
+					<TabStatus>
+						<TabType>SignHere</TabType>
+						<Status>Signed</Status>
+						<XPosition>104</XPosition>
+						<YPosition>1042</YPosition>
+						<TabLabel>SIG_Signer_0_9807809d-962a-4058-971e-0d4cf77bc0b4_true_1</TabLabel>
+						<TabName>Tejas Bhandari</TabName>
+						<TabValue />
+						<DocumentID>3</DocumentID>
+						<PageNumber>2</PageNumber>
+					</TabStatus>
+					<TabStatus>
+						<TabType>Custom</TabType>
+						<Status>Signed</Status>
+						<XPosition>802</XPosition>
+						<YPosition>810</YPosition>
+						<TabLabel>pn_borrower_signer_2</TabLabel>
+						<TabName />
+						<TabValue />
+						<DocumentID>1</DocumentID>
+						<PageNumber>1</PageNumber>
+						<ValidationPattern />
+						<CustomTabType>Text</CustomTabType>
+					</TabStatus>
+					<TabStatus>
+						<TabType>Custom</TabType>
+						<Status>Signed</Status>
+						<XPosition>288</XPosition>
+						<YPosition>313</YPosition>
+						<TabLabel>pn_coborrower_3_signer_1</TabLabel>
+						<TabName />
+						<TabValue />
+						<DocumentID>1</DocumentID>
+						<PageNumber>2</PageNumber>
+						<ValidationPattern />
+						<CustomTabType>Text</CustomTabType>
+					</TabStatus>
+					<TabStatus>
+						<TabType>Custom</TabType>
+						<Status>Signed</Status>
+						<XPosition>817</XPosition>
+						<YPosition>310</YPosition>
+						<TabLabel>pn_coborrower_3_signer_2</TabLabel>
+						<TabName />
+						<TabValue />
+						<DocumentID>1</DocumentID>
+						<PageNumber>2</PageNumber>
+						<ValidationPattern />
+						<CustomTabType>Text</CustomTabType>
+					</TabStatus>
+					<TabStatus>
+						<TabType>Custom</TabType>
+						<Status>Signed</Status>
+						<XPosition>281</XPosition>
+						<YPosition>1067</YPosition>
+						<TabLabel>pn_coborrower_1_signer_1</TabLabel>
+						<TabName />
+						<TabValue />
+						<DocumentID>1</DocumentID>
+						<PageNumber>1</PageNumber>
+						<ValidationPattern />
+						<CustomTabType>Text</CustomTabType>
+					</TabStatus>
+					<TabStatus>
+						<TabType>Custom</TabType>
+						<Status>Signed</Status>
+						<XPosition>821</XPosition>
+						<YPosition>1065</YPosition>
+						<TabLabel>pn_coborrower_1_signer_2</TabLabel>
+						<TabName />
+						<TabValue />
+						<DocumentID>1</DocumentID>
+						<PageNumber>1</PageNumber>
+						<ValidationPattern />
+						<CustomTabType>Text</CustomTabType>
+					</TabStatus>
+					<TabStatus>
+						<TabType>Custom</TabType>
+						<Status>Signed</Status>
+						<XPosition>821</XPosition>
+						<YPosition>1298</YPosition>
+						<TabLabel>pn_coborrower_2_signer_2</TabLabel>
+						<TabName />
+						<TabValue />
+						<DocumentID>1</DocumentID>
+						<PageNumber>1</PageNumber>
+						<ValidationPattern />
+						<CustomTabType>Text</CustomTabType>
+					</TabStatus>
+					<TabStatus>
+						<TabType>Custom</TabType>
+						<Status>Signed</Status>
+						<XPosition>283</XPosition>
+						<YPosition>1296</YPosition>
+						<TabLabel>pn_coborrower_2_signer_1</TabLabel>
+						<TabName />
+						<TabValue />
+						<DocumentID>1</DocumentID>
+						<PageNumber>1</PageNumber>
+						<ValidationPattern />
+						<CustomTabType>Text</CustomTabType>
+					</TabStatus>
+					<TabStatus>
+						<TabType>Custom</TabType>
+						<Status>Signed</Status>
+						<XPosition>296</XPosition>
+						<YPosition>560</YPosition>
+						<TabLabel>pn_coborrower_4_signer_1</TabLabel>
+						<TabName />
+						<TabValue />
+						<DocumentID>1</DocumentID>
+						<PageNumber>2</PageNumber>
+						<ValidationPattern />
+						<CustomTabType>Text</CustomTabType>
+					</TabStatus>
+					<TabStatus>
+						<TabType>Custom</TabType>
+						<Status>Signed</Status>
+						<XPosition>810</XPosition>
+						<YPosition>558</YPosition>
+						<TabLabel>pn_coborrower_4_signer_2</TabLabel>
+						<TabName />
+						<TabValue />
+						<DocumentID>1</DocumentID>
+						<PageNumber>2</PageNumber>
+						<ValidationPattern />
+						<CustomTabType>Text</CustomTabType>
+					</TabStatus>
+					<TabStatus>
+						<TabType>DateSigned</TabType>
+						<Status>Signed</Status>
+						<XPosition>215</XPosition>
+						<YPosition>858</YPosition>
+						<TabLabel>Date Signed f2a10241-ee0d-4610-8e34-7dc12570014a</TabLabel>
+						<TabName>DateSigned</TabName>
+						<TabValue>3/1/2022</TabValue>
+						<DocumentID>1</DocumentID>
+						<PageNumber>1</PageNumber>
+					</TabStatus>
+					<TabStatus>
+						<TabType>DateSigned</TabType>
+						<Status>Signed</Status>
+						<XPosition>525</XPosition>
+						<YPosition>1138</YPosition>
+						<TabLabel>Date Signed</TabLabel>
+						<TabName>Tejas Bhandari</TabName>
+						<TabValue>3/1/2022</TabValue>
+						<DocumentID>3</DocumentID>
+						<PageNumber>2</PageNumber>
+					</TabStatus>
+				</TabStatuses>
+				<RecipientAttachment>
+					<Attachment>
+						<Data>PEZvcm1EYXRhPjx4ZmRmPjxmaWVsZHM+PGZpZWxkIG5hbWU9Im9yZ2FuaXphdGlvbl9uYW1lIj48dmFsdWU+TFJUIENhcGl0YWw8L3ZhbHVlPjwvZmllbGQ+PGZpZWxkIG5hbWU9InBuX2JvcnJvd2VyX3NpZ25lcl8xIj48dmFsdWU+VGVqYXMgQmhhbmRhcmk8L3ZhbHVlPjwvZmllbGQ+PGZpZWxkIG5hbWU9InBuX2JvcnJvd2VyX3NpZ25lcl8yIj48dmFsdWU+PC92YWx1ZT48L2ZpZWxkPjxmaWVsZCBuYW1lPSJwbl9jb2JvcnJvd2VyXzNfc2lnbmVyXzEiPjx2YWx1ZT48L3ZhbHVlPjwvZmllbGQ+PGZpZWxkIG5hbWU9InBuX2NvYm9ycm93ZXJfM19zaWduZXJfMiI+PHZhbHVlPjwvdmFsdWU+PC9maWVsZD48ZmllbGQgbmFtZT0icG5fY29ib3Jyb3dlcl8xX3NpZ25lcl8xIj48dmFsdWU+PC92YWx1ZT48L2ZpZWxkPjxmaWVsZCBuYW1lPSJwbl9jb2JvcnJvd2VyXzFfc2lnbmVyXzIiPjx2YWx1ZT48L3ZhbHVlPjwvZmllbGQ+PGZpZWxkIG5hbWU9InBuX2NvYm9ycm93ZXJfMl9zaWduZXJfMiI+PHZhbHVlPjwvdmFsdWU+PC9maWVsZD48ZmllbGQgbmFtZT0icG5fY29ib3Jyb3dlcl8yX3NpZ25lcl8xIj48dmFsdWU+PC92YWx1ZT48L2ZpZWxkPjxmaWVsZCBuYW1lPSJwbl9jb2JvcnJvd2VyXzRfc2lnbmVyXzEiPjx2YWx1ZT48L3ZhbHVlPjwvZmllbGQ+PGZpZWxkIG5hbWU9InBuX2NvYm9ycm93ZXJfNF9zaWduZXJfMiI+PHZhbHVlPjwvdmFsdWU+PC9maWVsZD48ZmllbGQgbmFtZT0iRGF0ZVNpZ25lZCI+PHZhbHVlPjMvMS8yMDIyPC92YWx1ZT48L2ZpZWxkPjwvZmllbGRzPjwveGZkZj48L0Zvcm1EYXRhPg==</Data>
+						<Label>DSXForm</Label>
+					</Attachment>
+				</RecipientAttachment>
+				<AccountStatus>Active</AccountStatus>
+				<FormData>
+					<xfdf>
+						<fields>
+							<field name="organization_name">
+								<value>LRT Capital</value>
+							</field>
+							<field name="pn_borrower_signer_1">
+								<value>Tejas Bhandari</value>
+							</field>
+							<field name="pn_borrower_signer_2">
+								<value />
+							</field>
+							<field name="pn_coborrower_3_signer_1">
+								<value />
+							</field>
+							<field name="pn_coborrower_3_signer_2">
+								<value />
+							</field>
+							<field name="pn_coborrower_1_signer_1">
+								<value />
+							</field>
+							<field name="pn_coborrower_1_signer_2">
+								<value />
+							</field>
+							<field name="pn_coborrower_2_signer_2">
+								<value />
+							</field>
+							<field name="pn_coborrower_2_signer_1">
+								<value />
+							</field>
+							<field name="pn_coborrower_4_signer_1">
+								<value />
+							</field>
+							<field name="pn_coborrower_4_signer_2">
+								<value />
+							</field>
+							<field name="DateSigned">
+								<value>3/1/2022</value>
+							</field>
+						</fields>
+					</xfdf>
+				</FormData>
+				<RecipientId>62f93a8f-3bc1-4755-aeee-8f05e9b075ba</RecipientId>
+			</RecipientStatus>
+			<RecipientStatus>
+				<Type>Signer</Type>
+				<Email>tejas_bhandari88@yahoo.com</Email>
+				<UserName>Tejas - Owner  Bhandari</UserName>
+				<RoutingOrder>4</RoutingOrder>
+				<Sent>2022-03-01T03:52:13.863</Sent>
+				<Delivered>2022-03-01T03:52:41.36</Delivered>
+				<Signed>2022-03-01T03:52:48.83</Signed>
+				<DeclineReason xsi:nil="true" />
+				<Status>Completed</Status>
+				<RecipientIPAddress>106.193.223.85</RecipientIPAddress>
+				<CustomFields>
+					<CustomField>owner</CustomField>
+				</CustomFields>
+				<TabStatuses>
+					<TabStatus>
+						<TabType>SignHere</TabType>
+						<Status>Signed</Status>
+						<XPosition>75</XPosition>
+						<YPosition>1256</YPosition>
+						<TabLabel>SIG_Guarantor_0_7f7c0fa5-777c-4cdb-85e4-15a40d0aa6d0_true_1</TabLabel>
+						<TabName>Tejas - Owner  Bhandari</TabName>
+						<TabValue />
+						<DocumentID>3</DocumentID>
+						<PageNumber>2</PageNumber>
+					</TabStatus>
+					<TabStatus>
+						<TabType>DateSigned</TabType>
+						<Status>Signed</Status>
+						<XPosition>496</XPosition>
+						<YPosition>1352</YPosition>
+						<TabLabel>Date Signed</TabLabel>
+						<TabName>Tejas - Owner  Bhandari</TabName>
+						<TabValue>3/1/2022</TabValue>
+						<DocumentID>3</DocumentID>
+						<PageNumber>2</PageNumber>
+					</TabStatus>
+				</TabStatuses>
+				<RecipientAttachment>
+					<Attachment>
+						<Data>PEZvcm1EYXRhPjx4ZmRmPjxmaWVsZHM+PGZpZWxkIG5hbWU9IkRhdGVTaWduZWQiPjx2YWx1ZT4zLzEvMjAyMjwvdmFsdWU+PC9maWVsZD48L2ZpZWxkcz48L3hmZGY+PC9Gb3JtRGF0YT4=</Data>
+						<Label>DSXForm</Label>
+					</Attachment>
+				</RecipientAttachment>
+				<AccountStatus>Active</AccountStatus>
+				<FormData>
+					<xfdf>
+						<fields>
+							<field name="DateSigned">
+								<value>3/1/2022</value>
+							</field>
+						</fields>
+					</xfdf>
+				</FormData>
+				<RecipientId>83c72a94-1665-41a4-b853-2621cf83b176</RecipientId>
+			</RecipientStatus>
+		</RecipientStatuses>
+		<TimeGenerated>2022-03-01T03:52:50.265938</TimeGenerated>
+		<EnvelopeID>2695db11-b0c7-4eeb-95b3-1415f4892bd0</EnvelopeID>
+		<Subject>Please DocuSign: LRT Capital Loan Application</Subject>
+		<UserName>Direct Forgiveness Sandbox</UserName>
+		<Email>dfsandbox@thesummitgrp.com</Email>
+		<Status>Completed</Status>
+		<Created>2022-03-01T03:50:43.93</Created>
+		<Sent>2022-03-01T03:52:13.91</Sent>
+		<Delivered>2022-03-01T03:52:41.657</Delivered>
+		<Signed>2022-03-01T03:52:48.83</Signed>
+		<Completed>2022-03-01T03:52:48.83</Completed>
+		<ACStatus>Original</ACStatus>
+		<ACStatusDate>2022-03-01T03:50:43.93</ACStatusDate>
+		<ACHolder>Direct Forgiveness Sandbox</ACHolder>
+		<ACHolderEmail>dfsandbox@thesummitgrp.com</ACHolderEmail>
+		<ACHolderLocation>DocuSign</ACHolderLocation>
+		<SigningLocation>Online</SigningLocation>
+		<SenderIPAddress>106.193.223.85 </SenderIPAddress>
+		<EnvelopePDFHash />
+		<CustomFields />
+		<AutoNavigation>true</AutoNavigation>
+		<EnvelopeIdStamping>true</EnvelopeIdStamping>
+		<AuthoritativeCopy>false</AuthoritativeCopy>
+		<DocumentStatuses>
+			<DocumentStatus>
+				<ID>2</ID>
+				<Name>Loan Application.pdf</Name>
+				<TemplateName />
+				<Sequence>1</Sequence>
+			</DocumentStatus>
+			<DocumentStatus>
+				<ID>1</ID>
+				<Name>Loan Application Consent.pdf</Name>
+				<TemplateName />
+				<Sequence>2</Sequence>
+			</DocumentStatus>
+			<DocumentStatus>
+				<ID>3</ID>
+				<Name>InitialDisclosures.pdf</Name>
+				<TemplateName />
+				<Sequence>3</Sequence>
+			</DocumentStatus>
+		</DocumentStatuses>
+	</EnvelopeStatus>
+	<DocumentPDFs>
+		<DocumentPDF>
+			<Name>Loan Application.pdf</Name>
+			<PDFBytes></PDFBytes>
+			<DocumentID>2</DocumentID>
+			<DocumentType>CONTENT</DocumentType>
+		</DocumentPDF>
+		<DocumentPDF>
+			<Name>Loan Application Consent.pdf</Name>
+			<PDFBytes></PDFBytes>
+			<DocumentID>1</DocumentID>
+			<DocumentType>CONTENT</DocumentType>
+		</DocumentPDF>
+		<DocumentPDF>
+			<Name>InitialDisclosures.pdf</Name>
+			<PDFBytes></PDFBytes>
+			<DocumentID>3</DocumentID>
+			<DocumentType>CONTENT</DocumentType>
+		</DocumentPDF>
+		<DocumentPDF>
+			<Name>CertificateOfCompletion_2695db11-b0c7-4eeb-95b3-1415f4892bd0.pdf</Name>
+			<PDFBytes></PDFBytes>
+			<DocumentType>SUMMARY</DocumentType>
+		</DocumentPDF>
+	</DocumentPDFs>
+</DocuSignEnvelopeInformation>
