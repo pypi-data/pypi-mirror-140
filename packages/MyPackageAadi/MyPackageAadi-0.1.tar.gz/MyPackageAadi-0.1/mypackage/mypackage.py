f1cbe232171a4787421d84033ce362678632fed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:04 +0000

    Add package MyPackageAadi/0.1/MyPackageAadi-0.1.tar.gz

---
+++ packages/MyPackageAadi/MyPackageAadi-0.1.tar.gz/MyPackageAadi-0.1/mypackage/mypackage.py
@@ -0,0 +1,5 @@
+def developer(name):
+	print(f"Hi {name}, the game dev Aadi here!!!")
+
+def yeet():
+	print("SIKE THATS THE WRONG FUNCTION OOOOOO")
---
+++ packages/MyPackageAadi/MyPackageAadi-0.1.tar.gz/MyPackageAadi-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name = 'MyPackageAadi',
+	version = '0.01',
+	description = 'Thiss is my first package',
+	author = 'Aadi Tripathi',
+	packages=['mypackage'],
+	zip_safe=False)
