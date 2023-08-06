6b4ea430ae4f19ab397e7c106a71e5e534ec87ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:35 +0000

    Add package django-forestadmin/1.1.2/django-forestadmin-1.1.2.tar.gz

---
+++ packages/django-forestadmin/django-forestadmin-1.1.2.tar.gz/django-forestadmin-1.1.2/django_forest/apps.py
@@ -0,0 +1,19 @@
+import os
+import urllib3
+from distutils.util import strtobool
+
+from django.apps import AppConfig
+
+disable_warnings = os.getenv('URLLIB3_DISABLE_WARNINGS', 'False')
+try:
+    disable_warnings = strtobool(disable_warnings)
+except Exception:
+    disable_warnings = False
+finally:
+    if disable_warnings:
+        urllib3.disable_warnings()
+
+
+class ForestConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_forest'
