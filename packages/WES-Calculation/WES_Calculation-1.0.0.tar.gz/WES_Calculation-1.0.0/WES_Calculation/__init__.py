dbcfe20506647d82a00ed13c3fb2559beab99aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:52 +0000

    Add package WES-Calculation/1.0.0/WES_Calculation-1.0.0.tar.gz

---
+++ packages/WES-Calculation/WES_Calculation-1.0.0.tar.gz/WES_Calculation-1.0.0/WES_Calculation/__init__.py
@@ -0,0 +1,10 @@
+'''
+Author: BDFD
+Date: 2021-10-27 18:42:50
+LastEditTime: 2022-02-24 14:32:30
+LastEditors: BDFD
+Description: In User Settings Edit
+FilePath: \5.2-PyPi-WES_Calculation\WES_Calculation\__init__.py
+'''
+
+from WES_Calculation.templateproj import add_one
