b6cab409dcdd18b44524787c0bf9d2aba4195185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:22:35 +0000

    Add package topsisOm/0.3/topsisOm-0.3.tar.gz

---
+++ packages/topsisOm/topsisOm-0.3.tar.gz/topsisOm-0.3/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'topsisOm',         
+  packages = ['topsisOm'],   
+  version = '0.3',      
+  license='MIT',       
+  description = 'This package helps to get the topsis score from the given dataframe',   # Give a short description about your library
+  author = 'Om Gupta',                   # Type in your name
+  author_email = 'omstringtheory@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/om-guptaa',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/om-guptaa/topsisOm/archive/refs/tags/v_03.tar.gz',    # I explain this later on
+  keywords = ['Topsis', 'Ranking'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'pandas',
+          'numpy',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
