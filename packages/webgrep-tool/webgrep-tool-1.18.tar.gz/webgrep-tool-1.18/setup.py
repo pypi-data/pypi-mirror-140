dde26f0850e0a6dedc863128beda444bce346b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:42 +0000

    Add package webgrep-tool/1.18/webgrep-tool-1.18.tar.gz

---
+++ packages/webgrep-tool/webgrep-tool-1.18.tar.gz/webgrep-tool-1.18/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+from os.path import abspath, dirname, join
+from setuptools import setup
+
+currdir = abspath(dirname(__file__))
+with open(join(currdir, 'README.md')) as f:
+    long_descr = f.read()
+
+setup(
+  name = "webgrep-tool",
+  author = "Alexandre D\'Hondt",
+  author_email = "alexandre.dhondt@gmail.com",
+  url = "https://github.com/dhondta/webgrep",
+  version = "1.18",
+  license = "GPLv3",
+  description = "Grep for a Web page with extra features like JS deobfuscation and OCR",
+  long_description=long_descr,
+  long_description_content_type='text/markdown',
+  keywords = ["grep", "webpage", "js-deobfuscator", "css-unminifier", "exif-metadata", "ocr", "steghide", "ctf-tools"],
+  scripts = ["webgrep"],
+  classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Console',
+    'Intended Audience :: End Users/Desktop',
+    'Intended Audience :: Information Technology',
+    'Intended Audience :: System Administrators',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Topic :: Security',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+  ],
+  install_requires=["bs4", "coloredlogs", "jsbeautifier", "requests"],
+  python_requires = '>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,<4',
+)
