19c38b77b17570a55bc43040735e34e48a6c9a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:15 +0000

    Add package wav-ytdw/0.0.3/wav_ytdw-0.0.3.tar.gz

---
+++ packages/wav-ytdw/wav_ytdw-0.0.3.tar.gz/wav_ytdw-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.3'
+DESCRIPTION = 'A basic python script to download audio the web.'
+
+# Setting up
+setup(
+    name="wav_ytdw",
+    version=VERSION,
+    author="JustCow",
+    author_email="<justcow@pm.me>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['ffmpeg', 'youtube_dl'],
+    keywords=['python', 'download', 'youtube download', 'audio', 'wav', 'justcow'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
