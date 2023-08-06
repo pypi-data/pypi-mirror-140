2ee9801a1e4f53adc0412f894b3fcf8d0d62a47f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:41 +0000

    Add package tycho-api/1.4.dev19/tycho-api-1.4.dev19.tar.gz

---
+++ packages/tycho-api/tycho-api-1.4.dev19.tar.gz/tycho-api-1.4.dev19/tycho/exceptions.py
@@ -0,0 +1,36 @@
+class TychoException(Exception):
+    def __init__(self, message, details=""):
+        super().__init__(message)
+        self.details = details
+
+
+class StartException(Exception):
+    def __init__(self, message, details=""):
+        super().__init__(message, details)
+
+
+class DeleteException(Exception):
+    def __init__(self, message, details=""):
+        super().__init__(message, details)
+
+
+class ContextException(TychoException):
+    def __init__(self, message, details=""):
+        super().__init__(message, details)
+
+
+class ModifyException(Exception):
+    def __init__(self, message, details=""):
+        super().__init__(message, details)
+
+
+class TemplateException(Exception):
+    def __init__(self, message, details=""):
+        super().__init__(message)
+        self.details = details
+
+
+class AppRegistryRenderException(Exception):
+    def __init__(self, message, details=""):
+        super().__init__(message, details)
+
