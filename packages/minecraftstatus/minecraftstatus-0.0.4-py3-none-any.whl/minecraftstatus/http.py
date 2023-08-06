df4bd68142ea129cabf7cdcdf157de1fbc4085d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:13 +0000

    Add package minecraftstatus/0.0.4/minecraftstatus-0.0.4-py3-none-any.whl

---
+++ packages/minecraftstatus/minecraftstatus-0.0.4-py3-none-any.whl/minecraftstatus/http.py
@@ -0,0 +1,23 @@
+from aiohttp import ClientSession
+import aiohttp
+
+
+
+__all__ = ("HTTPClient",)
+
+
+class HTTPClient:
+    def __init__(self):
+        self.session = None
+
+    async def _request(self, method: str, endpoint: str) -> dict:
+        self.session = ClientSession()
+        try:
+            resp = await self.session.request(method, endpoint)
+        except KeyError:
+            return resp
+        return resp
+
+    async def _close(self):
+        if self.session:
+            await self.session.close()
