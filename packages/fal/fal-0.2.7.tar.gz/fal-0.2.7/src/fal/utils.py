b21c277e69d5bd568ba089a1260697aecc2c49ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:14 +0000

    Add package fal/0.2.7/fal-0.2.7.tar.gz

---
+++ packages/fal/fal-0.2.7.tar.gz/fal-0.2.7/src/fal/utils.py
@@ -0,0 +1,16 @@
+"""Fal utilities."""
+from dbt.logger import print_timestamped_line
+from faldbt.project import DbtModel
+from typing import List
+
+
+def print_run_info(models: List[DbtModel], keyword: str, before):
+    """Print information on the current fal run."""
+    models_arr = []
+    for model in models:
+        models_arr.append(f"{model.name}: {', '.join(model.get_scripts(keyword, before))}")
+
+    models_str = "\n".join(models_arr)
+    print_timestamped_line(
+        f"Starting fal run for following models and scripts: \n{models_str}\n"
+    )
