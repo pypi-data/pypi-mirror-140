a1a9cd1a57848681239c17fda8d98791d885245f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:45 +0000

    Add package bbai/1.2.0/bbai-1.2.0-py3-none-manylinux1_x86_64.whl

---
+++ packages/bbai/bbai-1.2.0-py3-none-manylinux1_x86_64.whl/bbai/_computation/_response.py
@@ -0,0 +1,40 @@
+from collections import namedtuple
+
+ErrorResponse = namedtuple('ErrorResponse', ['message'])
+
+fit_glm_fields = [
+    'hyperparameters',
+    'aloocv',
+    'aloocvs',
+    'weights',
+    'intercepts',
+    'raw_weights',
+]
+FitGlmResponse = namedtuple('FitGlmResponse', fit_glm_fields)
+FitGlmResponse.__new__.__defaults__ = (None,) * len(fit_glm_fields)
+
+fit_glm_map_fields = [
+        'weights',
+        'hessian',
+]
+FitGlmMAPResponse = namedtuple('FitGlmMAPResponse', fit_glm_map_fields)
+FitGlmMAPResponse.__new__.__defaults__ = (None,) * len(fit_glm_map_fields)
+
+fit_bayesian_glm_fields = [
+    'weight_mean_vector',
+    'weight_covariance_matrix',
+    'noise_variance_mean',
+]
+FitBayesianGlmResponse = namedtuple('FitBayesianGlmResponse', fit_bayesian_glm_fields)
+FitBayesianGlmResponse.__new__.__defaults__ = (None,) * len(fit_bayesian_glm_fields)
+
+fit_sparse_glm_fields = [
+    'hyperparameters',
+    'aloocv',
+    'aloocvs',
+    'weights',
+    'intercepts',
+    'raw_weights',
+]
+FitSparseGlmResponse = namedtuple('FitSparseGlmResponse', fit_sparse_glm_fields)
+FitSparseGlmResponse.__new__.__defaults__ = (None,) * len(fit_sparse_glm_fields)
