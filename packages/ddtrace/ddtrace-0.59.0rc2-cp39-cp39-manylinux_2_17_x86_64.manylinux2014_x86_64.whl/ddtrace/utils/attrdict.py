2cb99890d9c1a8efe44826cea63053dd4b5f12b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:19:54 +0000

    Add package ddtrace/0.59.0rc2/ddtrace-0.59.0rc2-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ddtrace/ddtrace-0.59.0rc2-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ddtrace/utils/attrdict.py
@@ -0,0 +1,8 @@
+from ..internal.utils.attrdict import AttrDict  # noqa
+from ..vendor.debtcollector.removals import removed_module
+
+
+removed_module(
+    module="ddtrace.utils.attrdict",
+    removal_version="1.0.0",
+)
