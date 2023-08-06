e607ae46247aa1ac8179a3057d88af6b58198a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:37 +0000

    Add package APIFlask/0.12.0/APIFlask-0.12.0-py3-none-any.whl

---
+++ packages/APIFlask/APIFlask-0.12.0-py3-none-any.whl/apiflask/__init__.py
@@ -0,0 +1,18 @@
+from . import fields as fields
+from . import validators as validators
+from ._decorators import auth_required as auth_required
+from ._decorators import doc as doc
+from ._decorators import input as input
+from ._decorators import output as output
+from .app import APIFlask as APIFlask
+from .blueprint import APIBlueprint as APIBlueprint
+from .exceptions import abort as abort
+from .exceptions import HTTPError as HTTPError
+from .helpers import get_reason_phrase as get_reason_phrase
+from .helpers import pagination_builder as pagination_builder
+from .schemas import PaginationSchema as PaginationSchema
+from .schemas import Schema as Schema
+from .security import HTTPBasicAuth as HTTPBasicAuth
+from .security import HTTPTokenAuth as HTTPTokenAuth
+
+__version__ = '0.12.0'
