084ae50f13e0865645e8538460fafa4f72eb412f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:14 +0000

    Add package fast-agave/0.3.1.dev9/fast_agave-0.3.1.dev9-py3-none-any.whl

---
+++ packages/fast-agave/fast_agave-0.3.1.dev9-py3-none-any.whl/examples/validators.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+from cuenca_validations.types import QueryParams
+from fastapi import UploadFile
+from pydantic import BaseModel
+
+
+class AccountQuery(QueryParams):
+    name: Optional[str] = None
+    user_id: Optional[str] = None
+    is_active: Optional[bool] = None
+
+
+class TransactionQuery(QueryParams):
+    user_id: Optional[str] = None
+
+
+class AccountRequest(BaseModel):
+    name: str
+
+
+class AccountUpdateRequest(BaseModel):
+    name: str
+
+
+class FileQuery(QueryParams):
+    user_id: Optional[str] = None
+
+
+class CardQuery(QueryParams):
+    number: Optional[str] = None
+
+
+class FileUploadValidator(BaseModel):
+    file: UploadFile
