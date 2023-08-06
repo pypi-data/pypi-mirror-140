fd5ee7f08250c7f2e78677d160e431e0540a900f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:43 +0000

    Add package Topsis-Twinkle-101903209/0.0.1/Topsis-Twinkle-101903209-0.0.1.tar.gz

---
+++ packages/Topsis-Twinkle-101903209/Topsis-Twinkle-101903209-0.0.1.tar.gz/Topsis-Twinkle-101903209-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Topsis-Twinkle-101903209",
+    version = "0.0.1",
+    license = "MIT",
+    description = "A Python package to find TOPSIS for multi-criteria decision analysis method",
+    long_description = open('readme.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    long_description_content_type = "text/markdown",
+    author = "Twinkle Garg",
+    author_email = "ttwinkle_be19@thapar.edu",
+    keywords = ['topsis', 'UCS654', 'TIET'],
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = ['pandas', 'tabulate'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3' 
+    ]
+)
