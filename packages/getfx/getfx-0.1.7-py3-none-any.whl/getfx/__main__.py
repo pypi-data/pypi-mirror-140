539c3f08ccb5a8ee6cca8ba3e89231e94bbb4b39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:25 +0000

    Add package getfx/0.1.7/getfx-0.1.7-py3-none-any.whl

---
+++ packages/getfx/getfx-0.1.7-py3-none-any.whl/getfx/__main__.py
@@ -0,0 +1,13 @@
+"""
+
+Invoke command line
+==================
+
+When `getfx` is run from command line, it invokes FX retrieval using NBP API.
+
+"""
+
+
+if __name__ == '__main__':
+    from getfx.getfxnbp import init_cmd
+    init_cmd()
