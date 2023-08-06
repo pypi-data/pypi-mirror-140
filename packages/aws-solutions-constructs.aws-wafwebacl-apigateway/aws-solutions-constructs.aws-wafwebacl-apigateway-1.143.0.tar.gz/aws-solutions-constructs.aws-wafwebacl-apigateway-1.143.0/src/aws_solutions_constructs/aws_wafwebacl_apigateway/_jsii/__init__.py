b9c9f28a27432136a987e68815345fd5e5fe35a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:07 +0000

    Add package aws-solutions-constructs.aws-wafwebacl-apigateway/1.143.0/aws-solutions-constructs.aws-wafwebacl-apigateway-1.143.0.tar.gz

---
+++ packages/aws-solutions-constructs.aws-wafwebacl-apigateway/aws-solutions-constructs.aws-wafwebacl-apigateway-1.143.0.tar.gz/aws-solutions-constructs.aws-wafwebacl-apigateway-1.143.0/src/aws_solutions_constructs/aws_wafwebacl_apigateway/_jsii/__init__.py
@@ -0,0 +1,38 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_apigateway._jsii
+import aws_cdk.aws_autoscaling._jsii
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_elasticloadbalancingv2._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_wafv2._jsii
+import aws_cdk.core._jsii
+import aws_solutions_constructs.aws_apigateway_dynamodb._jsii
+import aws_solutions_constructs.aws_apigateway_iot._jsii
+import aws_solutions_constructs.aws_apigateway_kinesisstreams._jsii
+import aws_solutions_constructs.aws_apigateway_lambda._jsii
+import aws_solutions_constructs.aws_apigateway_sagemakerendpoint._jsii
+import aws_solutions_constructs.aws_apigateway_sqs._jsii
+import aws_solutions_constructs.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-solutions-constructs/aws-wafwebacl-apigateway",
+    "1.143.0",
+    __name__[0:-6],
+    "aws-wafwebacl-apigateway@1.143.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
