4ac11796f0a3bae1579f44bd7f11bdf31db4782b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:07 +0000

    Add package neorcli/3.1.0/neorcli-3.1.0.tar.gz

---
+++ packages/neorcli/neorcli-3.1.0.tar.gz/neorcli-3.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+long_description = 'NeorCloud CLI tool to manage your services from cli.'
+
+setup(
+    name='neorcli',
+    version='3.1.0',
+    author='Ali Ghotbizadeh',
+    author_email='ali.ghotbizadeh@gmail.com',
+    url='https://github.com/NeorCloud/neor-cli',
+    description='NeorCli tool',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'neorcli = src.cli:run'
+        ]
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    keywords='NeorCloud NeorCli',
+    install_requires=requirements,
+    zip_safe=False
+)
