6aa7d05bb39ec2503e78050406e2c9e91d7a3361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:44 +0000

    Add package Topsis-Piyush-101903753/0.0.1/Topsis-Piyush-101903753-0.0.1.tar.gz

---
+++ packages/Topsis-Piyush-101903753/Topsis-Piyush-101903753-0.0.1.tar.gz/Topsis-Piyush-101903753-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Topsis-Piyush-101903753",
+    version="0.0.1",
+    license="MIT",
+    description="A Python package to find TOPSIS for multi-criteria decision analysis method",
+    author="Piyush Sachdeva",
+    author_email="psachdeva_be19@thapar.edu",
+    url="",
+    keywords=[],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['pandas', 'tabulate'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
+)
