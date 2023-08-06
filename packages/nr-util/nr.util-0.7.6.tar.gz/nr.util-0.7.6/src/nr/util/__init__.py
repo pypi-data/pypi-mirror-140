a2aac3116f7286e3444d5d067b325483ef35de70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:19 +0000

    Add package nr-util/0.7.6/nr.util-0.7.6.tar.gz

---
+++ packages/nr-util/nr.util-0.7.6.tar.gz/nr.util-0.7.6/src/nr/util/__init__.py
@@ -0,0 +1,14 @@
+
+""" General purpose utility library. """
+
+__author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
+__version__ = "0.7.6"
+
+
+# Backwards compatibility
+from .functional._coalesce import coalesce
+from .chaindict import ChainDict
+from .optional import Optional
+from .orderedset import OrderedSet
+from .refreshable import Refreshable
+from .stream import Stream
