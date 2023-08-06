838d4687caf2c2f45bd65b70da4d316d8257fc32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:20 +0000

    Add package slowedvideos/0.1.1/slowedvideos-0.1.1-py3-none-any.whl

---
+++ packages/slowedvideos/slowedvideos-0.1.1-py3-none-any.whl/slowedvideos/downloadurl/downloadurl.py
@@ -0,0 +1,19 @@
+def downloadurl(url, output):
+
+    prefix = 'Download URL -'
+
+    from youtube_dl import YoutubeDL
+
+    output = output.split('.')
+    output = output[0] + '.mp3'
+    
+    # Set opts
+    print(prefix, 'Setting opts...')
+    ytdl_opts = { 'format': 'bestaudio/best', 'outtmpl':output, 'postprocessors':[{'key': 'FFmpegExtractAudio','preferredcodec': 'wav'}] }
+    print(prefix, ytdl_opts)
+    
+    # Download video
+    print(prefix, 'Downloading audio...')
+    YoutubeDL(ytdl_opts).download([url])
+
+    return print(prefix, 'Done')
