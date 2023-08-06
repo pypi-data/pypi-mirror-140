8eed2ff3e92a714ce5675a8a5b3df75aae20dae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:26 +0000

    Add package formfyxer/0.0.6/formfyxer-0.0.6.tar.gz

---
+++ packages/formfyxer/formfyxer-0.0.6.tar.gz/formfyxer-0.0.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='formfyxer',
+    version='0.0.6',
+    author='Suffolk LIT Lab',
+    author_email='litlab@suffolk.edu',
+    description='A tool for learning about and pre-processing pdf forms.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/SuffolkLITLab/FormFyxer',
+    project_urls = {
+        "Bug Tracker": "https://github.com/SuffolkLITLab/FormFyxer/issues"
+    },
+    license='MIT',
+    packages=['formfyxer'],
+    install_requires=['spacy',  'PyPDF2',  'pikepdf',  'textstat',  'requests',  'numpy',  'sklearn', 'joblib',  'nltk', 'boxdetect', 'pdf2image', 'reportlab', 'pdfminer.six', 'opencv-python'],
+    include_package_data = True
+)
