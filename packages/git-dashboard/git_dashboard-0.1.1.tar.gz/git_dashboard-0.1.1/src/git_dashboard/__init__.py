988a2e0a64e845ff3d8bb7b88455478d1440308b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:58 +0000

    Add package git-dashboard/0.1.1/git_dashboard-0.1.1.tar.gz

---
+++ packages/git-dashboard/git_dashboard-0.1.1.tar.gz/git_dashboard-0.1.1/src/git_dashboard/__init__.py
@@ -0,0 +1,8 @@
+"""Package info"""
+
+__version__ = '0.1.1'
+__docs__ = 'Git Dashboard'
+__author__ = 'Jung Ko'
+__author_email__ = 'kojung@gmail.com'
+__homepage__ = 'https://github.com/kojung/git-dashboard'
+__license__ = 'GPL'
