c8a9d5882e9621c2215a3a5ba2736c0936823230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:49 +0000

    Add package aitv/0.0.2/aitv-0.0.2.tar.gz

---
+++ packages/aitv/aitv-0.0.2.tar.gz/aitv-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.2'
+DESCRIPTION = 'A basic python script to merge an audio and an image into a video using ffmpeg.'
+
+# Setting up
+setup(
+    name="aitv",
+    version=VERSION,
+    author="JustCow",
+    author_email="<justcow@pm.me>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['ffmpeg-python'],
+    keywords=['python', 'video', 'audio', 'justcow'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
