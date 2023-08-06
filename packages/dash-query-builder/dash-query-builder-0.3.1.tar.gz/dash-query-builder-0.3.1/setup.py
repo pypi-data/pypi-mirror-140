4928208b59a76009d21e85d8c0fbe78ef1921bce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:19 +0000

    Add package dash-query-builder/0.3.1/dash-query-builder-0.3.1.tar.gz

---
+++ packages/dash-query-builder/dash-query-builder-0.3.1.tar.gz/dash-query-builder-0.3.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dash_query_builder']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dash>=2.0.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'dash-query-builder',
+    'version': '0.3.1',
+    'description': 'Dash Component based on react-awesome-query-builder',
+    'long_description': None,
+    'author': 'Tyler Baur',
+    'author_email': 'baur.tyler@protonmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
