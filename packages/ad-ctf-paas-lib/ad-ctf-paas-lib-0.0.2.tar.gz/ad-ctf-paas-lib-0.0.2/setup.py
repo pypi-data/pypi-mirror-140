9a1b56902e399d401da08a8920885188de3246dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:43 +0000

    Add package ad-ctf-paas-lib/0.0.2/ad-ctf-paas-lib-0.0.2.tar.gz

---
+++ packages/ad-ctf-paas-lib/ad-ctf-paas-lib-0.0.2.tar.gz/ad-ctf-paas-lib-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ad-ctf-paas-lib',
+    version='0.0.2',
+    packages=['checker'],
+    url='',
+    license='',
+    author='ivanh',
+    author_email='ivanhahanov13@gmail.com',
+    description='Library that help to write checkers'
+)
