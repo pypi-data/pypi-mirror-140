9f0cd7bcdf80ab5b375f4d0e180d3eef2b4e5752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:27 +0000

    Add package Topsis-Jayant-401903026/0.0.1/Topsis-Jayant-401903026-0.0.1.tar.gz

---
+++ packages/Topsis-Jayant-401903026/Topsis-Jayant-401903026-0.0.1.tar.gz/Topsis-Jayant-401903026-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='Topsis-Jayant-401903026',
+    version='0.0.1',
+    description='topsis-score',
+    py_modules=["assignment4"],
+    package_dir={'':'src'},
+)
