3df79b26e27d06beebd640dc153064dce9a4937a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:40 +0000

    Add package merge-koskidata2primusdata/1.0.0/merge_koskidata2primusdata-1.0.0.tar.gz

---
+++ packages/merge-koskidata2primusdata/merge_koskidata2primusdata-1.0.0.tar.gz/merge_koskidata2primusdata-1.0.0/merge_koskidata2primusdata/__init__.py
@@ -0,0 +1,7 @@
+"""merge_koskidata2primusdata - Utility which merging Koski and Primus CSV reports."""
+
+__version__ = "1.0.0"
+__author__ = "Pasi Ollikainen <pasi.ollikainen@outlook.com>"
+commit = True
+tag = True
+
