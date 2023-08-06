1f456e136a61e01f9edcbfd71e240f87ef7ede1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:57 +0000

    Add package hashily/0.0.6/hashily-0.0.6-py3-none-any.whl

---
+++ packages/hashily/hashily-0.0.6-py3-none-any.whl/utils/constants.py
@@ -0,0 +1,142 @@
+"""
+MIT License
+
+Copyright (c) 2022 TrueMyst
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+import string
+
+upper = string.ascii_uppercase
+lower = string.ascii_lowercase
+
+
+BaconDict = {
+    "A": "aaaaa",
+    "B": "aaaab",
+    "C": "aaaba",
+    "D": "aaabb",
+    "E": "aabaa",
+    "F": "aabab",
+    "G": "aabba",
+    "H": "aabbb",
+    "I": "abaaa",
+    "J": "abaab",
+    "K": "ababa",
+    "L": "ababb",
+    "M": "abbaa",
+    "N": "abbab",
+    "O": "abbba",
+    "P": "abbbb",
+    "Q": "baaaa",
+    "R": "baaab",
+    "S": "baaba",
+    "T": "baabb",
+    "U": "babaa",
+    "V": "babab",
+    "W": "babba",
+    "X": "babbb",
+    "Y": "bbaaa",
+    "Z": "bbaab",
+}
+
+MorseCodeDict = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    "0": "-----",
+    ", ": "--..--",
+    ".": ".-.-.-",
+    "?": "..--..",
+    "/": "-..-.",
+    "-": "-....-",
+    "(": "-.--.",
+    ")": "-.--.-",
+    " ": "/",
+}
+
+characters = {
+    "0": "A",
+    "1": "B",
+    "2": "C",
+    "3": "D",
+    "4": "E",
+    "5": "F",
+    "6": "G",
+    "7": "H",
+    "8": "I",
+    "9": "J",
+    "10": "K",
+    "11": "L",
+    "12": "M",
+    "13": "N",
+    "14": "O",
+    "15": "P",
+    "16": "Q",
+    "17": "R",
+    "18": "S",
+    "19": "T",
+    "20": "U",
+    "21": "V",
+    "22": "W",
+    "23": "X",
+    "24": "Y",
+    "25": "Z",
+    "26": "2",
+    "27": "3",
+    "28": "4",
+    "29": "5",
+    "30": "6",
+    "31": "7",
+    "=": "=",
+}
