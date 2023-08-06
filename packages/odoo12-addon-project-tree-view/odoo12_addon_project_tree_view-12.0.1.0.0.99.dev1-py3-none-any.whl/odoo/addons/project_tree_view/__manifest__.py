683882d4f491cc71393139c70c217fb695eda865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:40 +0000

    Add package odoo12-addon-project-tree-view/12.0.1.0.0.99.dev1/odoo12_addon_project_tree_view-12.0.1.0.0.99.dev1-py3-none-any.whl

---
+++ packages/odoo12-addon-project-tree-view/odoo12_addon_project_tree_view-12.0.1.0.0.99.dev1-py3-none-any.whl/odoo/addons/project_tree_view/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021-Coopdevs Treball SCCL (<https://coopdevs.org>)
+# - konykon - <kon.tsagari@coopdevs.org>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Project Tree View",
+    "version": "12.0.1.0.0",
+    "depends": ["project"],
+    "author": "Coopdevs Treball SCCL",
+    "category": "Project Management",
+    "website": "https://coopdevs.org",
+    "license": "AGPL-3",
+    "summary": """
+        Tree view for projects.
+    """,
+    "data": ["views/project_tree_view.xml"],
+    "installable": True,
+}
