fe807a71efebd0f5c8fcc63bec396f84a03ca844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:34 +0000

    Add package notecoin/0.10.8/notecoin-0.10.8.tar.gz

---
+++ packages/notecoin/notecoin-0.10.8.tar.gz/notecoin-0.10.8/notecoin/okex/v5/client/system.py
@@ -0,0 +1,24 @@
+import datetime
+from typing import Iterable, Optional, Union
+
+import pandas as pd
+from notecoin.okex.common.exceptions import OkexParamsException
+from notecoin.okex.v5.client.client import Client
+from notecoin.okex.v5.consts import *
+from notecoin.okex.v5.types import CcyType, InstType
+from notecoin.okex.v5.utils import enum_to_str, iterable_to_str
+
+
+class SystemAPI(Client):
+
+    def __init__(self, api_key, api_secret_key, passphrase, use_server_time=False, test=False, first=False):
+        Client.__init__(self, api_key, api_secret_key, passphrase, use_server_time, test, first)
+
+    def status(self, state: Optional[str] = None):
+        params = {}
+        if state is not None:
+            params['state'] = state
+
+        data = self._request_with_params(GET, INSTRUMENTS, params)["data"]
+
+        return data
