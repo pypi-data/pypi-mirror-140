8e61b7597900463ea2c17a8b85d90f54873795ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:34 +0000

    Add package logstack/0.0.4/logstack-0.0.4-py3-none-any.whl

---
+++ packages/logstack/logstack-0.0.4-py3-none-any.whl/logstack/serializers.py
@@ -0,0 +1,17 @@
+class LogSerializer():
+    data = dict()
+
+    def __init__(
+        self,
+        execution_status: bool = False,
+        message: str = "No message",
+        traceback: str = 'No traceback',
+        execution_time: float = 0.0,
+    ):
+        # TODO: Add data validation.
+        # TODO: Add log level?
+        # TODO: Add additional data serializer for sending any additional information as a JSON serializable dict.
+        self.data['execution_status'] = execution_status
+        self.data['execution_time'] = execution_time
+        self.data['message'] = message
+        self.data['traceback'] = traceback
