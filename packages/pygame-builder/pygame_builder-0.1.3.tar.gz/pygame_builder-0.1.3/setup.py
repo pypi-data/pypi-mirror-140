49fe1222abb34f2efd73c455191ec2fbb84c04a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:57 +0000

    Add package pygame-builder/0.1.3/pygame_builder-0.1.3.tar.gz

---
+++ packages/pygame-builder/pygame_builder-0.1.3.tar.gz/pygame_builder-0.1.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name="pygame_builder",
+    version="0.1.3",
+    author="Aaryan Dehade",
+    author_email="aaryandehade@adehade.tech",
+    description="A package to build games with ease.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dehadeaaryan/pygame-builder",
+    project_urls={
+        "Bug Tracker": "https://github.com/dehadeaaryan/pygame-builder/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
