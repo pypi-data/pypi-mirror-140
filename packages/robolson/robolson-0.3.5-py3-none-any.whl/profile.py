83326a3723e0b45e1631f2bbca7dbcf62dcf6b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:04 +0000

    Add package robolson/0.3.5/robolson-0.3.5-py3-none-any.whl

---
+++ packages/robolson/robolson-0.3.5-py3-none-any.whl/profile.py
@@ -0,0 +1,28 @@
+"""python interpreter profile
+Point your $PYTHONSTARTUP environment variable at this file."""
+
+import os
+
+from functools import reduce
+
+import rich.traceback
+from rich import pretty
+
+pretty.install()
+
+rich.traceback.install(show_locals=True)
+
+os.environ["PYTHONBREAKPOINT"] = "pdbr.set_trace"
+
+
+def compose(*functions):
+    """Compose multiple unary functions.  E.g., compose(plus_2, times_2, minus_2)"""
+    return reduce(lambda f, g: lambda x: g(f(x)), functions)
+
+
+try:
+    from ptpython.repl import embed
+except ImportError:
+    print("ptpython is not available: falling back to standard prompt")
+else:
+    embed(globals(), locals())
