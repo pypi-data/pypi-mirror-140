19ee6911d3a96efb44f7499e4b7b28aac31b645e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:47:25 +0000

    Add package mlcf/1.0.2/mlcf-1.0.2.tar.gz

---
+++ packages/mlcf/mlcf-1.0.2.tar.gz/mlcf-1.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+install_requirements = [
+    "tensorboard",
+    "torch-tb-profiler",
+    "geneticalgorithm2",
+    "TA-Lib",
+    "ritl"
+]
+
+setup(
+    install_requires=install_requirements,
+)
