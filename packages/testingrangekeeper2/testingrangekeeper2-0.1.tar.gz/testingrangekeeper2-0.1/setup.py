d86697430c2549083c0461df7103e9bebc344903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:38 +0000

    Add package testingrangekeeper2/0.1/testingrangekeeper2-0.1.tar.gz

---
+++ packages/testingrangekeeper2/testingrangekeeper2-0.1.tar.gz/testingrangekeeper2-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='testingrangekeeper2',
+    version='0.1',
+    author="Muhammad H Ali",
+    author_email='6hassan@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    keywords='example project',
+    install_requires=[],
+
+)
