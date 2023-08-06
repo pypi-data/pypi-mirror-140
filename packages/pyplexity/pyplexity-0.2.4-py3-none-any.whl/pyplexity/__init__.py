0ddefa27601b4967a8a53802341ce62a13c8aa2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:56 +0000

    Add package pyplexity/0.2.4/pyplexity-0.2.4-py3-none-any.whl

---
+++ packages/pyplexity/pyplexity-0.2.4-py3-none-any.whl/pyplexity/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.1.0'
+
+from pyplexity.perpl_computer import PerplexityComputer as PerplexityComputer
+from pyplexity.perpl_computer import PerplexityProcessor as PerplexityProcessor
