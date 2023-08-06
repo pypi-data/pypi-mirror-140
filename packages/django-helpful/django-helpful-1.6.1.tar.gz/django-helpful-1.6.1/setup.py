d1603d1b73d2015c31b8edc42fef4c9748dc1f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:41 +0000

    Add package django-helpful/1.6.1/django-helpful-1.6.1.tar.gz

---
+++ packages/django-helpful/django-helpful-1.6.1.tar.gz/django-helpful-1.6.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+# python setup.py register
+
+# python setup.py sdist upload
+# python setup.py sdist bdist_wheel
+# twine upload dist/*.whl
+
+setup(
+	name="django-helpful",
+	version="1.6.1",
+	description="Django helpful things",
+	long_description=open('README.rst').read(),
+	author="Stanislav Baltrunas",
+	author_email="stanislav@baltrunas.ru",
+	license="BSD",
+	url="https://github.com/Baltrunas/django-helpful",
+	# packages=find_packages(exclude=["tests.*", "tests"]),
+	packages=['helpful',],
+	package_data={
+		'templates' :['*'],
+		'static' :['*'],
+		'locale': ['*'],
+		'docs': ['*'],
+	},
+	include_package_data=True,
+	install_requires=[],
+	python_requires='>=2.6, <4',
+	classifiers=[
+		"Development Status :: 4 - Beta",
+		"Environment :: Web Environment",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: BSD License",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python",
+		"Framework :: Django",
+	],
+	zip_safe=False,
+)
