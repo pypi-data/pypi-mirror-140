c974c7a55825fbcb6a55e7de29d878e6a4e55151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:36 +0000

    Add package muygpys/0.5.0/muygpys-0.5.0-py3-none-any.whl

---
+++ packages/muygpys/muygpys-0.5.0-py3-none-any.whl/MuyGPyS/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2021 Lawrence Livermore National Security, LLC and other MuyGPyS
+# Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: MIT
+
+"""Public MuyGPyS modules and functions."""
+
+__version__ = "0.5.0"
+
+from MuyGPyS._src.config import config as config
