081c7f1e2992a6cd56fc3f2c37e326b0a504403d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:16 +0000

    Add package yascc/0.2.6/yascc-0.2.6.tar.gz

---
+++ packages/yascc/yascc-0.2.6.tar.gz/yascc-0.2.6/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['yascc']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['setuptools>=60.5.0,<61.0.0']
+
+extras_require = \
+{':python_version < "3.8"': ['importlib-metadata']}
+
+setup_kwargs = {
+    'name': 'yascc',
+    'version': '0.2.6',
+    'description': 'swap cases',
+    'long_description': None,
+    'author': 'urm8',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.6,<4.0',
+}
+from build import *
+build(setup_kwargs)
+
+setup(**setup_kwargs)
