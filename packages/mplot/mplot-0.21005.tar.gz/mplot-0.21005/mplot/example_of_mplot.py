31104643775f8ce5eb1775c584e1110a30075b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:12 +0000

    Add package mplot/0.21005/mplot-0.21005.tar.gz

---
+++ packages/mplot/mplot-0.21005.tar.gz/mplot-0.21005/mplot/example_of_mplot.py
@@ -0,0 +1,22 @@
+import plot_new as plot
+from mplot.color import *
+
+x = list(range(2010,2020))
+y = [1,1.1,1.24,1.18,1.5,1.46,1.99,2.0,2.34,2.55]
+mat = [1000,1024,1032,1054,1045,1046,1056,1035,1076,1053]
+def translate(alist,mult,add):
+    newlist = []
+    for elem in alist:
+        val = elem*mult+add
+        newlist.append(val)
+    return newlist
+
+yhigh = translate(y,1.23,0.1)
+ylow = translate(y,0.89,-0.1)
+pave = plot.line(x,y,legend = 'Average',color = BLUE)
+pbet = plot.areabetween(x,yhigh,ylow,legend = 'Range',color = BLUE,alpha = 0.2)
+pmat = plot.line(x,mat,legend = 'Matches',color = RED)
+plot.displaysubplots([[pave,pbet],[pmat,pmat]],'examplegraph',title = 'Example savings',
+             ylabel = 'Savings $')
+
+
