4d8aa127d11028214e0cf6784d4cca94e7474770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:41 +0000

    Add package SigFit/1.1.1/SigFit-1.1.1.tar.gz

---
+++ packages/SigFit/SigFit-1.1.1.tar.gz/SigFit-1.1.1/include/SigFit.h
@@ -0,0 +1,31 @@
+#ifndef __SIGFIT__
+#define __SIGFIT__
+
+#define PY_SSIZE_T_CLEAN
+#include "SineFit.h"
+#include <Python.h>
+
+static PyObject * FitThreeParam(PyObject* self, PyObject* args);
+
+static PyMethodDef SigFitMethods[] = {
+    {"FitThreeParam",  FitThreeParam, METH_VARARGS,
+     "Fits a sine wave to data with known freq."},
+    {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+static struct PyModuleDef sigfitmodule = {
+    PyModuleDef_HEAD_INIT,
+    "SigFit",   /* name of module */
+    NULL, /* module documentation, may be NULL */
+    -1,       /* size of per-interpreter state of the module,
+                 or -1 if the module keeps state in global variables. */
+    SigFitMethods
+};
+
+PyMODINIT_FUNC
+PyInit_SigFit(void)
+{
+    return PyModule_Create(&sigfitmodule);
+}
+
+#endif /* __SIGFIT__ */
---
+++ packages/SigFit/SigFit-1.1.1.tar.gz/SigFit-1.1.1/setup.py
@@ -0,0 +1,45 @@
+#
+#   Date: March 2nd 2022
+#   Author: Arash Roshanineshat
+#
+
+import subprocess
+
+from setuptools import setup, Extension
+import os
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+def pkgconfig(package, kw):
+    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
+    output = subprocess.getoutput(
+        'pkg-config --cflags --libs {}'.format(package))
+    for token in output.strip().split():
+        kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
+    return kw
+
+sigfit_sources = ['src/SineFit.c', 'src/SigFit.c']
+sigfit_includes = ['{}/include'.format(os.getcwd())]
+
+keywords = {
+    'sources' : sigfit_sources,
+    'include_dirs': sigfit_includes,
+    'libraries': ['m']
+}
+
+keywords = pkgconfig('python3', keywords)
+
+main_module = Extension('SigFit', **keywords)
+
+setup (name = 'SigFit',
+       license= "MIT License",
+       version = '1.1.1',
+       author = 'Arash Roshanineshat',
+       author_email = 'aroshanineshat@email.arizona.edu',
+       url = 'https://github.com/aroshanineshat/SigFit',
+       description = 'SigFit is a package to help you fit a sine wave to noisy sine data',
+       long_description = readme(),
+       long_description_content_type = "text/markdown", 
+       ext_modules = [main_module])
