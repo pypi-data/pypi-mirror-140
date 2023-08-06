17facf3ebcb66e9493472291083050e5fb167fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:15 +0000

    Add package nitransforms/22.0.0/nitransforms-22.0.0.tar.gz

---
+++ packages/nitransforms/nitransforms-22.0.0.tar.gz/nitransforms-22.0.0/nitransforms/io/__init__.py
@@ -0,0 +1,11 @@
+# emacs: -*- mode: python-mode; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""Read and write transforms."""
+from . import afni, fsl, itk, lta
+
+__all__ = [
+    "afni",
+    "fsl",
+    "itk",
+    "lta",
+]
