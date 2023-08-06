0632b48fcffd1bbf65ec5d70d18d70ba7f885732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:25 +0000

    Add package pycord-paging/1.0.0/pycord_paging-1.0.0-py3-none-any.whl

---
+++ packages/pycord-paging/pycord_paging-1.0.0-py3-none-any.whl/paginator/__init__.py
@@ -0,0 +1,10 @@
+from .paginator import *
+from .objects import *
+
+__title__ = 'pycord-paging'
+__summary__ = 'Pycord paginator for messages and embeds.'
+__uri__ = 'https://github.com/Tommodev-06/pycord-paging'
+__author__ = 'Tommodev'
+__version__ = '1.0.0'
+__license__ = 'MIT'
+__copyright__ = '2022-present Tommodev.'
