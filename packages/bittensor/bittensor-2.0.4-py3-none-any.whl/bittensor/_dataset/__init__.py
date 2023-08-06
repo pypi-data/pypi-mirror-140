f39e05771ee5d450478e205db4e55ca229ccbc62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:11 +0000

    Add package bittensor/2.0.4/bittensor-2.0.4-py3-none-any.whl

---
+++ packages/bittensor/bittensor-2.0.4-py3-none-any.whl/bittensor/_dataset/__init__.py
@@ -0,0 +1,167 @@
+""" Create and init the GenesisTextDataset class, which handles dataloading from ipfs
+"""
+
+# The MIT License (MIT)
+# Copyright © 2021 Yuma Rao
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+import argparse
+import os
+import copy
+
+import bittensor
+from . import dataset_impl
+
+class dataset:
+    """ Create and init the GenesisTextDataset class, which handles dataloading from ipfs
+    """
+    def __new__(
+            cls,
+            config: 'bittensor.config' = None,
+            block_size: int = None,
+            batch_size: int = None,
+            max_corpus_size:int = None,
+            num_workers: int = None,
+            dataset_name: list = [],
+            save_dataset: bool=None,
+            no_tokenizer: bool=None,
+            _mock:bool=None
+        ):
+        r""" Create and init the GenesisTextDataset class, which handles dataloading from ipfs.
+            Args:
+                config (:obj:`bittensor.Config`, `optional`): 
+                    bittensor.dataset.config()
+                block_size (:obj:`int`, `optional`):
+                    Number of text items to pull for each example.
+                batch_size (:obj:`int`, `optional`):
+                    Batch size.
+                max_corpus_size (:obj:`int`, `optional`):
+                    Maximum amount of data to download from IPFS into memory for training.
+                num_workers (:obj:`int`, `optional`):
+                    Number of workers for data loader.
+                dataset_name (:obj:`list`, `optional`):
+                    Which datasets to use (ArXiv, BookCorpus2, Books3, DMMathematics, EnronEmails, EuroParl, 
+                    Gutenberg_PG, HackerNews, NIHExPorter, OpenSubtitles, PhilPapers, UbuntuIRC, YoutubeSubtitles)).
+                save_dataset (:obj:`bool`, `optional`):
+                    Save the downloaded dataset or not.
+                no_tokenizer (:obj:`bool`, `optional`):
+                    To return non-tokenized text (EXPERIMENTAL, DO NOT USE)
+                _mock (:obj:`bool`, `optional`):
+                    For testing, if true the dataset if filled with fake text data.  
+        """   
+        if config == None: 
+            config = dataset.config()
+        config = copy.deepcopy( config )
+        config.dataset.block_size = block_size if block_size != None else config.dataset.block_size
+        config.dataset.batch_size = batch_size if batch_size != None else config.dataset.batch_size
+        config.dataset.max_corpus_size = max_corpus_size if max_corpus_size != None else config.dataset.max_corpus_size
+        config.dataset.num_workers = num_workers if num_workers != None else config.dataset.num_workers
+        config.dataset.dataset_name = dataset_name if dataset_name != [] else config.dataset.dataset_name
+        config.dataset.save_dataset = save_dataset if save_dataset != None else config.dataset.save_dataset
+        config.dataset.no_tokenizer = no_tokenizer if no_tokenizer != None else config.dataset.no_tokenizer
+        config.dataset._mock = _mock if _mock != None else config.dataset._mock
+        dataset.check_config( config )
+        if config.dataset._mock:
+            return dataset_impl.MockGenesisTextDataset(
+                block_size = config.dataset.block_size,
+                batch_size = config.dataset.batch_size,
+                max_corpus_size = config.dataset.max_corpus_size,
+                num_workers = config.dataset.num_workers,
+                dataset_name = config.dataset.dataset_name,
+                data_dir = config.dataset.data_dir,
+                save_dataset = config.dataset.save_dataset,
+                max_datasets = config.dataset.max_datasets,
+                no_tokenizer = config.dataset.no_tokenizer
+            )
+        else:
+            return dataset_impl.GenesisTextDataset(
+                block_size = config.dataset.block_size,
+                batch_size = config.dataset.batch_size,
+                max_corpus_size = config.dataset.max_corpus_size,
+                num_workers = config.dataset.num_workers,
+                dataset_name = config.dataset.dataset_name,
+                data_dir = config.dataset.data_dir,
+                save_dataset = config.dataset.save_dataset,
+                max_datasets = config.dataset.max_datasets,
+                no_tokenizer = config.dataset.no_tokenizer
+            )
+
+    @classmethod
+    def mock(cls):
+        return dataset( _mock = True )
+
+    @classmethod
+    def config(cls) -> 'bittensor.Config':
+        """ Get config from the argument parser 
+            Return: bittensor.config object
+        """
+        parser = argparse.ArgumentParser()
+        dataset.add_args( parser )
+        return bittensor.config( parser )
+
+    @classmethod
+    def add_args(cls, parser: argparse.ArgumentParser ):
+        """ Accept specific arguments from parser
+        """
+        try:
+            parser.add_argument('--dataset.batch_size', type=int, help='Batch size.', default = bittensor.defaults.dataset.batch_size)
+            parser.add_argument('--dataset.block_size', type=int, help='Number of text items to pull for each example..', default = bittensor.defaults.dataset.block_size)
+            parser.add_argument('--dataset.max_corpus_size',  type=int, help='Maximum amount of data to download from IPFS into memory for training.', default = bittensor.defaults.dataset.max_corpus_size)
+            parser.add_argument('--dataset.num_workers',  type=int, help='Number of workers for data loader.', default = bittensor.defaults.dataset.num_workers)
+            parser.add_argument('--dataset.dataset_name', type=str, required=False, nargs='*', action='store', help='Which datasets to use (ArXiv, BookCorpus2, Books3, DMMathematics, EnronEmails, EuroParl, Gutenberg_PG, HackerNews, NIHExPorter, OpenSubtitles, PhilPapers, UbuntuIRC, YoutubeSubtitles)).',
+                                                                    default = bittensor.defaults.dataset.dataset_name)
+            parser.add_argument('--dataset.data_dir', type=str, help='Where to save and load the data.', default = bittensor.defaults.dataset.data_dir)
+            parser.add_argument('--dataset.save_dataset', action='store_true', help='Save the downloaded dataset or not.', default = bittensor.defaults.dataset.save_dataset)
+            parser.add_argument('--dataset.max_datasets',  type=int, help='Number of datasets to load', default = bittensor.defaults.dataset.max_datasets)
+            parser.add_argument('--dataset.no_tokenizer', action='store_true', help='To return non-tokenized text (EXPERIMENTAL, DO NOT USE)',default=False)
+            parser.add_argument('--dataset._mock', action='store_true', help='To turn on dataset mocking for testing purposes.', default=False)
+
+        except argparse.ArgumentError:
+            # re-parsing arguments.
+            pass
+
+    @classmethod   
+    def help(cls):
+        """ Print help to stdout
+        """
+        parser = argparse.ArgumentParser()
+        cls.add_args( parser )
+        print (cls.__new__.__doc__)
+        parser.print_help()
+
+    @classmethod   
+    def add_defaults(cls, defaults):
+        """ Adds parser defaults to object from enviroment variables.
+        """
+        defaults.dataset = bittensor.Config()
+        defaults.dataset.batch_size = os.getenv('BT_DATASET_BATCH_SIZE') if os.getenv('BT_DATASET_BATCH_SIZE') != None else 10
+        defaults.dataset.block_size = os.getenv('BT_DATASET_BLOCK_SIZE') if os.getenv('BT_DATASET_BLOCK_SIZE') != None else 20
+        defaults.dataset.max_corpus_size = os.getenv('BT_DATASET_MAX_CORPUS_SIZE') if os.getenv('BT_DATASET_MAX_CORPUS_SIZE') != None else int(1e+4)
+        defaults.dataset.num_workers = os.getenv('BT_DATASET_NUM_WORKERS') if os.getenv('BT_DATASET_NUM_WORKERS') != None else 0
+        defaults.dataset.dataset_name = os.getenv('BT_DATASET_DATASET_NAME') if os.getenv('BT_DATASET_DATASET_NAME') != None else 'default'
+        defaults.dataset.data_dir = os.getenv('BT_DATASET_DATADIR') if os.getenv('BT_DATASET_DATADIR') != None else '~/.bittensor/data/'
+        defaults.dataset.save_dataset = os.getenv('BT_DATASET_SAVE_DATASET') if os.getenv('BT_DATASET_SAVE_DATASET') != None else False
+        defaults.dataset.max_datasets = os.getenv('BT_DATASET_max_datasets') if os.getenv('BT_DATASET_max_datasets') != None else 3
+
+    @classmethod
+    def check_config( cls, config: 'bittensor.Config' ):
+        """ Check config for batch size, block size, corpus size, num_workers and dataset
+        """
+        assert config.dataset.batch_size > 0, 'Batch size must be larger than 0'
+        assert config.dataset.block_size > 0, 'Block size must be larger than 0'
+        assert config.dataset.max_corpus_size > 0, 'max_corpus_size must be larger than 0'
+        assert config.dataset.num_workers >= 0, 'num_workers must be equal to or larger than 0'
+        assert isinstance(config.dataset.save_dataset, bool) , 'save_dataset must be True/False only'
