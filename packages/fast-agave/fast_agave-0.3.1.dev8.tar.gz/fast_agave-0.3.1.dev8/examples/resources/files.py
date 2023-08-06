17b0f3396c546cd253c3e2b48fc8349d4d00770c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:39 +0000

    Add package fast-agave/0.3.1.dev8/fast_agave-0.3.1.dev8.tar.gz

---
+++ packages/fast-agave/fast_agave-0.3.1.dev8.tar.gz/fast_agave-0.3.1.dev8/examples/resources/files.py
@@ -0,0 +1,30 @@
+from io import BytesIO
+
+from fast_agave.filters import generic_query
+from fastapi import BackgroundTasks
+from fastapi.responses import JSONResponse as Response
+
+from ..models import File as FileModel
+from ..validators import FileQuery, FileUploadValidator
+from .base import app
+
+
+@app.resource('/files')
+class File:
+    model = FileModel
+    query_validator = FileQuery
+    upload_validator = FileUploadValidator
+    get_query_filter = generic_query
+
+    @classmethod
+    async def download(cls, data: FileModel) -> BytesIO:
+        return BytesIO(bytes('Hello', 'utf-8'))
+
+    @classmethod
+    async def upload(
+        cls, request: FileUploadValidator, background_tasks: BackgroundTasks
+    ) -> Response:
+        request_file = request.file
+        file = FileModel(name=request_file.filename, user_id='US01')
+        await file.async_save()
+        return Response(content=file.to_dict(), status_code=201)
