64cfae355ffc6e7c82ef215ee551c8744d70d75c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:21:10 +0000

    Add package loliglio/0.0.2/loliglio-0.0.2.tar.gz

---
+++ packages/loliglio/loliglio-0.0.2.tar.gz/loliglio-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='loliglio',
+    version='0.0.2',
+    description='Loliglio allows you to extract data from the Riot API easily for your app development. Loliglio Framework is also intuitive and free.',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='https://conradofmf.gitbook.io/loliglio/',
+    author='Conrado Moreno',
+    author_email='cfmorenofernandez@gmail.com',
+    license='BSD License',
+    classifiers=classifiers,
+    keywords=['lol', 'library', 'league-of-legends'],
+    packages=find_packages(),
+    package_dir={'loliglio': ''},
+    install_requires=['']
+)
