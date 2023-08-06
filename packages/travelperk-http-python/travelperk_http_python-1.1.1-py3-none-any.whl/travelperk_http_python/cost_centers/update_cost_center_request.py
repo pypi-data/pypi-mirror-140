f586a810c18cf49a036150f746567eed4a805015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:18 +0000

    Add package travelperk-http-python/1.1.1/travelperk_http_python-1.1.1-py3-none-any.whl

---
+++ packages/travelperk-http-python/travelperk_http_python-1.1.1-py3-none-any.whl/travelperk_http_python/cost_centers/update_cost_center_request.py
@@ -0,0 +1,32 @@
+from typing import TYPE_CHECKING
+from .update_cost_center_input_params import UpdateCostCenterInputParams
+from travelperk_python_api_types.cost_centers.cost_centers.cost_center_detail import (
+    CostCenterDetail,
+)
+from travelperk_http_python.dataclass_wrapper.dataclass_wrapper import DataclassWrapper
+
+if TYPE_CHECKING:
+    from api.travelperk import TravelPerk
+
+
+class UpdateCostCenterRequest:
+    def __init__(self, id: int, travelperk: "TravelPerk"):
+        self.id = id
+        self.params = UpdateCostCenterInputParams()
+        self.travelperk = travelperk
+
+    def save(self) -> CostCenterDetail:
+        return DataclassWrapper.wrap(
+            CostCenterDetail,
+            self.travelperk.patch(
+                "/".join(["cost_centers", str(self.id)]), self.params.to_dict()
+            ),
+        )
+
+    def set_name(self, name: str) -> "UpdateCostCenterRequest":
+        self.params.set_name(name)
+        return self
+
+    def set_archive(self, archive: bool) -> "UpdateCostCenterRequest":
+        self.params.set_archive(archive)
+        return self
