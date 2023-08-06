c46e2e2b27602ff5e82fd452d7c48a45fdf57bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:06 +0000

    Add package glacier/0.3.3/glacier-0.3.3.tar.gz

---
+++ packages/glacier/glacier-0.3.3.tar.gz/glacier-0.3.3/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['glacier']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click-completion>=0.5.2,<0.6.0',
+ 'click-help-colors>=0.8,<0.9',
+ 'click>=7.1.2,<8.0.0',
+ 'typing-extensions>=4.1.1,<5.0.0']
+
+setup_kwargs = {
+    'name': 'glacier',
+    'version': '0.3.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Hiroki Konishi',
+    'author_email': 'relastle@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
