0542f96dbca406390fb0dbc8b433fa9a1f908909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:35 +0000

    Add package tensorflow-rocm/2.8.0/tensorflow_rocm-2.8.0-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/tensorflow-rocm/tensorflow_rocm-2.8.0-cp310-cp310-manylinux2014_x86_64.whl/tensorflow/include/external/local_config_rocm/rocm/rocm/include/hipsolver-version.h
@@ -0,0 +1,17 @@
+/* ************************************************************************
+ * Copyright 2020 Advanced Micro Devices, Inc.
+ * ************************************************************************ */
+
+#ifndef HIPSOLVER_VERSION_H_
+#define HIPSOLVER_VERSION_H_
+
+/* the configured version and settings
+ */
+// clang-format off
+#define hipsolverVersionMajor 1
+#define hipsolverVersionMinor 2
+#define hipsolverVersionPatch 0
+#define hipsolverVersionTweak 
+// clang-format on
+
+#endif
