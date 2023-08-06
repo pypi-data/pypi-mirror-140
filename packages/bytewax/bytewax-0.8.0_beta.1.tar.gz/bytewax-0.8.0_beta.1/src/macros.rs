7cb4b97ad50da2a78c1ba7aa2365032215768f99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:39 +0000

    Add package bytewax/0.8.0b1/bytewax-0.8.0_beta.1.tar.gz

---
+++ packages/bytewax/bytewax-0.8.0_beta.1.tar.gz/bytewax-0.8.0_beta.1/src/macros.rs
@@ -0,0 +1,13 @@
+//! Internal macros
+
+macro_rules! with_traceback {
+    ($py:expr, $pyfunc:expr) => {
+        // This would be the perfect use for the
+        // https://doc.rust-lang.org/nightly/unstable-book/language-features/try-blocks.html
+        // feature.
+        match (|| $pyfunc)() {
+            Ok(r) => r,
+            Err(err) => std::panic::panic_any(err),
+        }
+    };
+}
