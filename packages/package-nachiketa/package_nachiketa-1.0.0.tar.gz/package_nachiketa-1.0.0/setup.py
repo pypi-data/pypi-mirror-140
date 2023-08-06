51c831de582b573664089aa4b228485ce407e3c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:42 +0000

    Add package package-nachiketa/1.0.0/package_nachiketa-1.0.0.tar.gz

---
+++ packages/package-nachiketa/package_nachiketa-1.0.0.tar.gz/package_nachiketa-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="package_nachiketa",
+    version="1.0.0",
+    description="Code for fibonacci number",
+    long_description="It will return n-th fibonacci number",
+    author="Nachiketa",
+    packages=["package_nachiketa"],
+    install_requires=[],
+)
