c852e6a018abe6297a7885cafba44c4e70aedef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:23 +0000

    Add package jaws-libp/2.8.0a0/jaws-libp-2.8.0a0.tar.gz

---
+++ packages/jaws-libp/jaws-libp-2.8.0a0.tar.gz/jaws-libp-2.8.0a0/README.md
@@ -0,0 +1,14 @@
+# jaws-libp [![PyPI](https://img.shields.io/pypi/v/jaws-libp)](https://pypi.org/project/jaws-libp/)
+Reusable Python Classes for [JAWS](https://github.com/JeffersonLab/jaws).
+
+## Install
+```
+pip install jaws-libp
+```
+
+## API
+[Sphinx Docs](https://jeffersonlab.github.io/jaws-libp/)
+
+
+## See Also
+ - [Developer Notes](https://github.com/JeffersonLab/jaws-libp/wiki/Developer-Notes)
---
+++ packages/jaws-libp/jaws-libp-2.8.0a0.tar.gz/jaws-libp-2.8.0a0/src/jlab_jaws/avro/schemas/__init__.py
@@ -0,0 +1,30 @@
+"""
+   AVRO schemas (.avsc data files) for JAWS.
+
+    Schemas:
+
+    * :download:`AlarmActivationUnion.avsc </../../src/jlab_jaws/avro/schemas/AlarmActivationUnion.avsc>`
+    * :download:`AlarmClass.avsc </../../src/jlab_jaws/avro/schemas/AlarmClass.avsc>`
+    * :download:`AlarmLocation.avsc </../../src/jlab_jaws/avro/schemas/AlarmLocation.avsc>`
+    * :download:`AlarmOverrideKey.avsc </../../src/jlab_jaws/avro/schemas/AlarmOverrideKey.avsc>`
+    * :download:`AlarmOverrideSet.avsc </../../src/jlab_jaws/avro/schemas/AlarmOverrideSet.avsc>`
+    * :download:`AlarmOverrideUnion.avsc </../../src/jlab_jaws/avro/schemas/AlarmOverrideUnion.avsc>`
+    * :download:`AlarmPriority.avsc </../../src/jlab_jaws/avro/schemas/AlarmPriority.avsc>`
+    * :download:`AlarmInstance.avsc </../../src/jlab_jaws/avro/schemas/AlarmInstance.avsc>`
+    * :download:`AlarmState.avsc </../../src/jlab_jaws/avro/schemas/AlarmState.avsc>`
+    * :download:`DisabledOverride.avsc </../../src/jlab_jaws/avro/schemas/DisabledOverride.avsc>`
+    * :download:`EffectiveActivation.avsc </../../src/jlab_jaws/avro/schemas/EffectiveActivation.avsc>`
+    * :download:`EffectiveAlarm.avsc </../../src/jlab_jaws/avro/schemas/EffectiveAlarm.avsc>`
+    * :download:`EffectiveRegistration.avsc </../../src/jlab_jaws/avro/schemas/EffectiveRegistration.avsc>`
+    * :download:`FilteredOverride.avsc </../../src/jlab_jaws/avro/schemas/FilteredOverride.avsc>`
+    * :download:`IntermediateMonolog.avsc </../../src/jlab_jaws/avro/schemas/IntermediateMonolog.avsc>`
+    * :download:`LatchedOverride.avsc </../../src/jlab_jaws/avro/schemas/LatchedOverride.avsc>`
+    * :download:`MaskedOverride.avsc </../../src/jlab_jaws/avro/schemas/MaskedOverride.avsc>`
+    * :download:`OffDelayedOverride.avsc </../../src/jlab_jaws/avro/schemas/OffDelayedOverride.avsc>`
+    * :download:`OnDelayedOverride.avsc </../../src/jlab_jaws/avro/schemas/OnDelayedOverride.avsc>`
+    * :download:`OverrideList.avsc </../../src/jlab_jaws/avro/schemas/OverrideList.avsc>`
+    * :download:`ProcessorTransitions.avsc </../../src/jlab_jaws/avro/schemas/ProcessorTransitions.avsc>`
+    * :download:`ShelvedOverride.avsc </../../src/jlab_jaws/avro/schemas/ShelvedOverride.avsc>`
+
+    `View Schemas on GitHub <https://github.com/JeffersonLab/jaws-libp/tree/main/src/jlab_jaws/avro/schemas>`_
+"""
