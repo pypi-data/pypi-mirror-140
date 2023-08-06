571e1170787aa41b8050024f180d8808422dfd7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:55 +0000

    Add package taktile-auth/0.0.1a12/taktile-auth-0.0.1a12.tar.gz

---
+++ packages/taktile-auth/taktile-auth-0.0.1a12.tar.gz/taktile-auth-0.0.1a12/taktile_auth/entities/__init__.py
@@ -0,0 +1,3 @@
+from taktile_auth.entities.permission import Permission, PermissionDefinition
+from taktile_auth.entities.resource import Resource, ResourceDefinition
+from taktile_auth.entities.role import Role, RoleDefinition
