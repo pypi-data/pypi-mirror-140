7341651a9a076c4627c78aead6943bc1a612a4d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:27 +0000

    Add package Topsis-Ishav-101903773/0.1.1/Topsis_Ishav_101903773-0.1.1.tar.gz

---
+++ packages/Topsis-Ishav-101903773/Topsis_Ishav_101903773-0.1.1.tar.gz/Topsis_Ishav_101903773-0.1.1/Topsis_Ishav_101903773/__init__.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+from importlib import resources
+import io
+import sys
+
+class Incorrect_Params_no(Exception):
+    pass
+class col_nos_error(Exception):
+    pass
+class weight_error(Exception):
+    pass
+class impact_error(Exception):
+    pass
+#exception handling
+
+
+try:
+    if len(sys.argv)>5:
+        raise Incorrect_Params_no(" Number of Parameters are wrong")
+    if len(sys.argv)==5:
+        name=sys.argv[4]
+
+    if len(sys.argv)==4:
+        print("setting default output csv file name as 101903773.csv" )
+        name = "10190773.csv"
+
+    dframe=pd.read_csv(sys.argv[1])
+    if len(dframe.columns)<3:
+        raise col_nos_error("Input dataset has less than 3 columns,retry")
+    dfl=dframe
+    dframe=dframe.iloc[:,1:]
+    sz=len(dframe)
+    impacts=sys.argv[3]
+    
+    weights=sys.argv[2]
+    print(len(impacts.split(sep=",")), len(dframe.columns))
+
+    if len(impacts.split(sep=","))!=len(weights.split(sep=",") ):
+        raise weight_error("Weight and impact size different")
+    if len(impacts.split(sep=","))!=len(dframe.columns):
+        raise weight_error("Dataset and impact size different")
+    if len(weights.split(sep=","))!=len(dframe.columns):
+        raise weight_error("Dataset and weight size different")
+    
+    best=[]
+    worst=[]
+    #normalization of table
+    for i in dframe.columns:
+        arr=dframe.loc[:,i]
+        newarr=preprocessing.normalize(np.reshape(np.array(arr),(1,sz)))
+        dframe.loc[:,i]=np.reshape(newarr,(sz,1))
+
+        
+    wlist=weights.split(sep=",")
+    for i in range(len(dframe.columns)):
+        dframe.iloc[:,i]*=float(wlist[i])
+    ilist=impacts.split(sep=",")
+    for point in ilist:
+        if point not in ["+","-"]:
+            raise impact_error("ERROR : Impact values are incorrect...")
+            
+    for i in range(len(dframe.columns)):
+        if ilist[i]=="+":
+            best=np.append(best,dframe.iloc[:,i].max())
+            worst=np.append(worst,dframe.iloc[:,i].min())
+        elif ilist[i]=="-":
+            best=np.append(best,dframe.iloc[:,i].min())
+            worst=np.append(worst,dframe.iloc[:,i].max())
+    dframe=dframe.append(pd.Series(best,index=dframe.columns),ignore_index=True)
+    dframe=dframe.append(pd.Series(worst,index=dframe.columns),ignore_index=True)
+
+    dist_ib=[]
+    dist_iw=[]
+    for i in range(sz):
+        sumplus=0
+        summinus=0
+        for j in dframe.columns:
+            temp=dframe.loc[i,j]-dframe.loc[sz,j]
+            sumplus+=np.power(temp,2)
+            temp=dframe.loc[i,j]-dframe.loc[sz+1,j]
+            summinus+=np.power(temp,2)
+        dist_ib.append(np.sqrt(sumplus))
+        dist_iw.append(np.sqrt(summinus))
+    topsis=[]
+    for i in range(len(dist_iw)):
+        topsis.append(dist_iw[i]/(dist_iw[i]+dist_ib[i]))
+    dfl["Topsis Score"]=topsis
+    dict_rank={}
+    #rank determination
+    topsis.sort(reverse=True)
+    dict_rank[topsis[0]]=1
+    rank=2
+    for i in range(1,len(topsis)):
+        dict_rank[topsis[i]]=rank
+        rank+=1
+    rank_topsis=[]
+    for i in dfl["Topsis Score"]:
+        rank_topsis.append(dict_rank[i])
+    dfl["Rank"]=rank_topsis
+    dfl.to_csv(name,index=False)
+except Incorrect_Params_no as wnop:
+    print(wnop)
+except ValueError:
+    print("Incorrect value present")
+except FileNotFoundError:
+    print("Input value not found")
+except col_nos_error as nce:
+    print(nce)
+except weight_error as uwi:
+    print(uwi)
+except impact_error as ii:
+    print(ii)
