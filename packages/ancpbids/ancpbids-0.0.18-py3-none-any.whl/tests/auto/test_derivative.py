cf9497a8993b75d094b5e72f4d8f475fcd515b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:56 +0000

    Add package ancpbids/0.0.18/ancpbids-0.0.18-py3-none-any.whl

---
+++ packages/ancpbids/ancpbids-0.0.18-py3-none-any.whl/tests/auto/test_derivative.py
@@ -0,0 +1,24 @@
+from ancpbids import load_dataset, model
+from ..base_test_case import *
+
+
+class DerivativesTestCase(BaseTestCase):
+    def test_derivative_generated_by(self):
+        test_ds = load_dataset(SYNTHETIC_DIR)
+        fmriprep_folder = test_ds.derivatives.get_folder('fmriprep')
+        self.assertTrue(isinstance(fmriprep_folder, model.DerivativeFolder))
+        self.assertTrue(isinstance(fmriprep_folder.dataset_description, model.DerivativeDatasetDescriptionFile))
+        dddf = fmriprep_folder.dataset_description
+        self.assertEqual(1, len(dddf.GeneratedBy))
+        generated_by = dddf.GeneratedBy[0]
+        self.assertEqual("fmriprep", generated_by.Name)
+        self.assertEqual("1.1.0", generated_by.Version)
+        self.assertTrue(generated_by.Container)
+        container = generated_by.Container
+        self.assertEqual("abc", container.Type)
+        self.assertEqual("xyz", container.Tag)
+        self.assertEqual("test:abc/xyz", container.URI)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/ancpbids/ancpbids-0.0.18-py3-none-any.whl/tests/auto/test_schema.py
@@ -0,0 +1,20 @@
+import unittest
+
+from ancpbids import model
+from ..base_test_case import BaseTestCase
+
+
+class SchemaTestCase(BaseTestCase):
+    def test_entitmatching(self):
+        self.assertEqual('sub', model.fuzzy_match_entity_key('sub'))
+        self.assertEqual('sub', model.fuzzy_match_entity_key('subject'))
+        self.assertEqual('sub', model.fuzzy_match_entity_key('subjects'))
+        self.assertEqual('sub', model.fuzzy_match_entity_key('subjs'))
+
+        self.assertEqual('desc', model.fuzzy_match_entity_key('des'))
+        self.assertEqual('desc', model.fuzzy_match_entity_key('dscr'))
+        self.assertEqual('desc', model.fuzzy_match_entity_key('descriptions'))
+
+
+if __name__ == '__main__':
+    unittest.main()
