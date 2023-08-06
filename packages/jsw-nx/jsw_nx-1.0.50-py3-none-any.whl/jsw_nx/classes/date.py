15515b0f59fc50ab908ffd09514c971330e05b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:58 +0000

    Add package jsw-nx/1.0.50/jsw_nx-1.0.50-py3-none-any.whl

---
+++ packages/jsw-nx/jsw_nx-1.0.50-py3-none-any.whl/jsw_nx/classes/date.py
@@ -0,0 +1,43 @@
+import random
+import time
+from datetime import datetime
+
+STANDARD_FORMAT = {
+    'isoDateTime': '%Y-%m-%dT%H:%M:%SZ',
+    'datetime': '%Y-%m-%d %H:%M:%S',
+    'date': '%Y-%m-%d',
+    'time': '%H:%M:%S',
+    'month': '%Y-%m',
+    'dbdt': '%Y%m%d_%H%M%S'
+}
+
+
+class Date:
+    @classmethod
+    def format(cls, in_date=None, in_fmt='datetime'):
+        date = in_date or datetime.now()
+        fmt = STANDARD_FORMAT[in_fmt] or in_fmt
+        return date.strftime(fmt)
+
+    @classmethod
+    def now(cls):
+        return int(datetime.now().timestamp())
+
+    @classmethod
+    def create(cls, target):
+        if type(target) is str:
+            return datetime.fromisoformat(target)
+        if type(target) is int:
+            return datetime.fromtimestamp(target)
+        if type(target) is datetime:
+            return target
+        return None
+
+    @classmethod
+    def random_date(cls, start, end, in_fmt='%Y-%m-%d %H:%M:%S'):
+        prop = random.random()
+        fmt = STANDARD_FORMAT[in_fmt] or in_fmt
+        stime = time.mktime(time.strptime(start, fmt))
+        etime = time.mktime(time.strptime(end, fmt))
+        ptime = stime + prop * (etime - stime)
+        return time.strftime(fmt, time.localtime(ptime))
