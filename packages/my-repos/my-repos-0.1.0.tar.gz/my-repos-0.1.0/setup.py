eff82c4dd1863a7a72359ff01591dbd2062b2534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:32 +0000

    Add package my-repos/0.1.0/my-repos-0.1.0.tar.gz

---
+++ packages/my-repos/my-repos-0.1.0.tar.gz/my-repos-0.1.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['myrepos']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.4,<9.0.0']
+
+entry_points = \
+{'console_scripts': ['myrepos = myrepos.repos:cli']}
+
+setup_kwargs = {
+    'name': 'my-repos',
+    'version': '0.1.0',
+    'description': 'Keep an offline mirror of some git repositories',
+    'long_description': None,
+    'author': 'xtofl',
+    'author_email': 'kristoffel.pirard@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
