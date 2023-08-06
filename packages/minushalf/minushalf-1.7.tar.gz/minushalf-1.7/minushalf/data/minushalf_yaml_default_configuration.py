47e75f60b3cf45dd8078af405d12b5e27e595f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:16 +0000

    Add package minushalf/1.7/minushalf-1.7.tar.gz

---
+++ packages/minushalf/minushalf-1.7.tar.gz/minushalf-1.7/minushalf/data/minushalf_yaml_default_configuration.py
@@ -0,0 +1,133 @@
+"""
+Lists minushalf.yml parameters and their default values
+"""
+from enum import Enum, unique
+import aenum
+
+
+@unique
+class MinushalfParams(Enum):
+    """
+    minushalf.yaml parameters.
+    """
+
+    software = "software"
+    atomic_program = "atomic_program"
+    correction = "correction"
+
+    def __str__(self):
+        return str(self.name)
+
+    @staticmethod
+    def to_list():
+        """
+        Returns a list with the name of the parameters present in minushalf.yaml.
+        """
+        return list(map(lambda element: element.value, MinushalfParams))
+
+    @staticmethod
+    def to_dict():
+        """
+        Returns a dictionary with the name of the parameters present in minushalf.yaml.
+        """
+        values = map(lambda element: element.value, MinushalfParams)
+        keys = map(lambda element: element.__str__(), MinushalfParams)
+        return dict(zip(keys, values))
+
+
+class CorrectionDefaultParams(aenum.Enum, settings=aenum.NoAlias):
+    """
+    Default value of parameters in the correction tag.
+    """
+
+    correction_code = "v"
+    potfiles_folder = "minushalf_potfiles"
+    amplitude = 1.0
+    valence_cut_guess = None
+    conduction_cut_guess = None
+    tolerance = 0.01
+    fractional_valence_treshold = 10
+    fractional_conduction_treshold = 9
+    overwrite_vbm = []
+    overwrite_cbm = []
+    inplace = False
+
+    def __str__(self):
+        return str(self.name)
+
+    @staticmethod
+    def to_list():
+        """
+        Returns a list of default parameters.
+        """
+        return list(map(lambda element: element.value,
+                        CorrectionDefaultParams))
+
+    @staticmethod
+    def to_dict():
+        """
+        Returns a dictionary of default parameters.
+        """
+        values = map(lambda element: element.value, CorrectionDefaultParams)
+        keys = map(lambda element: element.__str__(), CorrectionDefaultParams)
+        return dict(zip(keys, values))
+
+
+@unique
+class VaspDefaultParams(Enum):
+    """
+    Default value of parameters in the vasp tag.
+    """
+
+    command = ['mpirun','vasp']
+
+    def __str__(self):
+        return str(self.name)
+
+    @staticmethod
+    def to_list():
+        """
+        Returns a list of default parameters.
+        """
+        return list(map(lambda element: element.value, VaspDefaultParams))
+
+    @staticmethod
+    def to_dict():
+        """
+        Returns a dictionary of default parameters.
+        """
+        values = map(lambda element: element.value, VaspDefaultParams)
+        keys = map(lambda element: element.__str__(), VaspDefaultParams)
+        return dict(zip(keys, values))
+
+
+@unique
+class AtomicProgramDefaultParams(Enum):
+    """
+    Default value of parameters in the atomic_program tag.
+    """
+
+    exchange_correlation_code = "pb"
+    calculation_code = "ae"
+    max_iterations = 100
+
+    def __str__(self):
+        return str(self.name)
+
+    @staticmethod
+    def to_list():
+        """
+        Returns a list of default parameters.
+        """
+        return list(
+            map(lambda element: element.value, AtomicProgramDefaultParams))
+
+    @staticmethod
+    def to_dict():
+        """
+        Returns a dictionary of default parameters.
+        """
+        values = map(lambda element: element.value, AtomicProgramDefaultParams)
+        keys = map(lambda element: element.__str__(),
+                   AtomicProgramDefaultParams)
+        return dict(zip(keys, values))
---
+++ packages/minushalf/minushalf-1.7.tar.gz/minushalf-1.7/setup.py
@@ -0,0 +1,47 @@
+"""
+Configuration setup for python project
+"""
+from os import path
+import setuptools
+from numpy.distutils.core import Extension, setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+atomic_program = Extension(name="minushalf.atomic_program",
+                           sources=[
+                               "minushalf/atomic_program/atm_cGuima3.f",
+                               "minushalf/atomic_program/atm_cGuima3.pyf",
+                           ],
+                           extra_compile_args=['-static'])
+
+setup(
+    name="minushalf",
+    version="1.7",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    author="Henrique Fernandes",
+    author_email="dftminushalf@gmail.com",
+    description=
+    "CLI to provides Pre processing tools for DFT -1/2 calculations",
+    long_description=long_description,
+    license="GPL",
+    install_requires=[
+        "pandas>=1.1.5",
+        "fortranformat>=0.2.5",
+        "Click>=7.1.2",
+        "pyfiglet>=0.8",
+        "loguru>=0.5.3",
+        "tabulate>=0.8.7",
+        "numpy<=1.21.0",
+        "pyyaml",
+        "scipy>=1.5.4",
+        "aenum>=3.0.0",
+    ],
+    entry_points="""
+        [console_scripts]
+        minushalf=minushalf.minushalf:minushalf
+    """,
+    ext_modules=[atomic_program],
+)
