3446a0351438656f5fafa52b800507b9c8c1cc76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:06 +0000

    Add package qq-bot/0.6.7/qq_bot-0.6.7-py3-none-any.whl

---
+++ packages/qq-bot/qq_bot-0.6.7-py3-none-any.whl/qqbot/__init__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from .api import (
+    listen_events,
+    GuildAPI,
+    GuildRoleAPI,
+    GuildMemberAPI,
+    ChannelAPI,
+    ChannelPermissionsAPI,
+    UserAPI,
+    AudioAPI,
+    MessageAPI,
+    WebsocketAPI,
+    MuteAPI,
+    AnnouncesAPI,
+    DmsAPI,
+    APIPermissionAPI,
+    ScheduleAPI,
+)
+
+from .async_api import (
+    async_listen_events,
+    AsyncGuildAPI,
+    AsyncGuildRoleAPI,
+    AsyncGuildMemberAPI,
+    AsyncChannelAPI,
+    AsyncChannelPermissionsAPI,
+    AsyncUserAPI,
+    AsyncAudioAPI,
+    AsyncMessageAPI,
+    AsyncWebsocketAPI,
+    AsyncMuteAPI,
+    AsyncAnnouncesAPI,
+    AsyncDmsAPI,
+    AsyncAPIPermissionAPI,
+    AsyncScheduleAPI,
+)
+from .core.network.ws.ws_intents_handler import HandlerType, Handler
+from .core.util import logging
+from .model import *
+
+logger = logging.getLogger()
