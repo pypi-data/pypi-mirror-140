73d4033917d4b386c8a718efcaac7514f3f8210b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:47 +0000

    Add package uploadgrampyapi/1.6.1/UploadgramPyAPI-1.6.1.tar.gz

---
+++ packages/uploadgrampyapi/UploadgramPyAPI-1.6.1.tar.gz/UploadgramPyAPI-1.6.1/README.md
@@ -0,0 +1,191 @@
+# UploadgramPyAPI
+![Alt](https://sun9-33.userapi.com/impg/uRrtaPGWd5M3u529Cr-PVUNORq-O4EwDjgwZyA/ak0CZq-ijFk.jpg?size=113x113&quality=96&sign=ce6642140fc672643d33e934d1235f7d&type=album "icon")
+
+This API can upload, download, remove and rename any files from the service [uploadgram.me](https://uploadgram.me) - a simple and fast file uploader that uses Telegram network as file storage.
+
+__If you download this module, you confirm your agreement with the [Terms of Service](https://uploadgram.me/terms.html) and [DMCA Policy](https://uploadgram.me/dmca.html) of [Uploadgram.me](https://uploadgram.me/)__
+
+
+[![Downloads](https://pepy.tech/badge/uploadgrampyapi)](https://pepy.tech/project/uploadgrampyapi)
+[![Downloads](https://pepy.tech/badge/uploadgrampyapi/month)](https://pepy.tech/project/uploadgrampyapi)
+[![Downloads](https://pepy.tech/badge/uploadgrampyapi/week)](https://pepy.tech/project/uploadgrampyapi)
+[![Supported Versions](https://img.shields.io/pypi/pyversions/uploadgrampyapi.svg)](https://pypi.org/project/uploadgrampyapi)
+[![PyPI](https://img.shields.io/pypi/v/uploadgrampyapi.svg)](https://pypi.org/project/uploadgrampyapi/)
+[![PyPi](https://img.shields.io/pypi/format/uploadgrampyapi)](https://pypi.org/project/uploadgrampyapi/)
+![GitHub top language](https://img.shields.io/github/languages/top/tankalxat34/UploadgramPyAPI)
+[![GitHub last commit](https://img.shields.io/github/last-commit/tankalxat34/uploadgrampyapi)](https://github.com/tankalxat34/UploadgramPyAPI/commits/main)
+[![GitHub Release Date](https://img.shields.io/github/release-date/tankalxat34/UploadgramPyAPI)](https://github.com/tankalxat34/UploadgramPyAPI/releases)
+
+[![releases note](https://img.shields.io/badge/-releases%20note-black?style=for-the-badge&logo=markdown)](https://github.com/tankalxat34/UploadgramPyAPI/blob/main/releases_note.md)
+[![github](https://img.shields.io/badge/-git%20hub-black?style=for-the-badge&logo=github)](https://github.com/tankalxat34/UploadgramPyAPI)
+
+[![GitHub Repo stars](https://img.shields.io/github/stars/tankalxat34/UploadgramPyAPI?style=social)](https://github.com/tankalxat34/UploadgramPyAPI)
+
+**Author: [tankalxat34](mailto:tankalxat34@gmail.com?subject=User%20of%20UploadgramPyAPI)**
+
+# Install
+In your CMD perform this command:
+
+```bat
+pip install UploadgramPyAPI
+```
+
+# Quickstart
+## Upload any file
+```py
+import UploadgramPyAPI
+up_file = UploadgramPyAPI.NewFile("D:\\image.jpg")
+up_file.upload()
+```
+
+## Delete any file
+```py
+import UploadgramPyAPI
+up_file = UploadgramPyAPI.File("611e5e6237f6fg", "e3da26e9dddd2e01b8c0831370695e9088a96ff81e262fc2g")
+up_file.delete()
+```
+
+## Rename any file
+```py
+import UploadgramPyAPI
+up_file = UploadgramPyAPI.File("611e5e6237f6fg", "e3da26e9dddd2e01b8c0831370695e9088a96ff81e262fc2g")
+up_file.rename("ItsNewNameForFile.jpg")
+```
+
+## Download any file
+```py
+import UploadgramPyAPI
+up_file = UploadgramPyAPI.File("611e5e6237f6fg", "e3da26e9dddd2e01b8c0831370695e9088a96ff81e262fc2g")
+up_file.download()
+```
+
+# Using
+Now we can looking, how to use UploadgramPyAPI
+
+## Upload new file
+
+### Step 1
+Firstly, you need to import the library:
+
+```py
+import UploadgramPyAPI
+```
+
+### Step 2
+Next if you want to upload new file on uploadgram.me you need write this in your file:
+
+```py
+up_file = UploadgramPyAPI.NewFile("D:\\image.jpg")
+```
+
+This string will preparing your file to upload.
+
+### Step 3
+Write this line after the previous one.
+
+```py
+up_file.upload()
+```
+
+Now file `image.jpg` was uploaded in uploadgram.me and you was get the dictionary like here
+```json
+{
+    "ok": "true", 
+    "url": "https://dl.uploadgram.me/611e5e6237f6fg", 
+    "delete": "e3da26e9dddd2e01b8c0831370695e9088a96ff81e262fc2g"
+}
+```
+
+UploadgramPyAPI can parse this json-responce and create new attributes: `url`, `key` and `url_import`. 
+
+
+## Important thing
+_______
+___Attribute `key` is a very important string for renaming and removing the file. You need to save its, else you won't do it.___
+_______
+
+
+
+
+
+Attribute `url_import` it's a simple url for import your file in a dashboard in https://dl.uploadgram.me.
+Its look like this:
+
+```json
+https://uploadgram.me/upload/#import:{"e3da26e9dddd2e01b8c0831370695e9088a96ff81e262fc2g": {"filename": "image.jpg", "size": 55604, "url": "https://dl.uploadgram.me/611e5e6237f6fg"}}
+```
+
+Open this url in your browser and uploaded file will appear on the website! You can see something like this:
+
+![Alt](https://sun9-54.userapi.com/impg/jppDL_T9_2FsDnc8pFLWdpqSzd91heDnbd8C4g/GJLh13On_aY.jpg?size=872x665&quality=96&sign=e32ba8f5877883060558882a1dd82345&type=album "slide")
+
+So your code can be looking like this:
+```py
+import UploadgramPyAPI
+up_file = UploadgramPyAPI.NewFile("D:\\image.jpg")
+up_file.upload()
+```
+
+## Delete file
+
+Also you can delete the file. Look!
+
+### Step 1
+
+If you have the `key` attribute for the file, you can delete its!
+
+Firstly, you need to write this strings for connect to server and file on it:
+
+```py
+import UploadgramPyAPI
+up_file = UploadgramPyAPI.File("611e5e6237f6fg", "e3da26e9dddd2e01b8c0831370695e9088a96ff81e262fc2g")
+up_file.delete()
+```
+
+Last string deleted the `image.jpg` from `uploadgram.me`. If you want to make sure, you can open `url` in your browser. You will get the 404-error:
+
+![Alt](https://sun9-78.userapi.com/impg/jKe2pjbifNJ7QNR3wvkiVWV7wzHebukEwd4Xlw/HbreA_TjlIg.jpg?size=922x665&quality=96&sign=b37ab6325819b5589ee8bbed9af61252&type=album "slide")
+
+## Rename file
+
+UploadgramPyAPI can rename the file.
+
+We need use the `key` attribute for rename the file. 
+
+The beginning is the same as in the previous steps:
+
+```py
+import UploadgramPyAPI
+up_file = UploadgramPyAPI.File("611e5e6237f6fg", "e3da26e9dddd2e01b8c0831370695e9088a96ff81e262fc2g")
+
+# this string will rename the filename
+up_file.rename("ItsNewNameForFile.jpg")
+```
+
+And now you can see this situation:
+
+![Alt](https://sun9-80.userapi.com/impg/LQagHS9h8wcDdqMtDBCMeyUlU5QZ_PKJ2Fd3jA/QFax68p4K_0.jpg?size=872x665&quality=96&sign=fcd29c8114e651fbcae85d17293442e4&type=album "slide")
+
+## Download file
+
+You can download the file from server:
+
+```py
+import UploadgramPyAPI
+up_file = UploadgramPyAPI.File("611e5e6237f6fg", "e3da26e9dddd2e01b8c0831370695e9088a96ff81e262fc2g")
+
+# this string will help download the file in the default download's folder
+up_file.download()
+```
+
+File will appear in the download's folder:
+
+![Alt](https://sun9-81.userapi.com/impg/2wvdZzEwgWKff9lzn2OIO1pXkG7yihLCMZxeyw/7Tzl3y78LJo.jpg?size=1021x850&quality=96&sign=6c43d00ea579a07566ba506b8032fa0e&type=album "slide")
+
+Also you can write path to save file:
+
+```py
+up_file.download("D:\\MyMainFolder\\")
+```
+
+It's enough! Successful use of the UploadgramPyAPI library!
---
+++ packages/uploadgrampyapi/UploadgramPyAPI-1.6.1.tar.gz/UploadgramPyAPI-1.6.1/UploadgramPyAPI.py
@@ -0,0 +1,310 @@
+"""
+********************************
+*------------------------------*
+*--------UploadgramPyAPI-------*
+*------------------------------*
+********************************
+Author: tankalxat34
+Description: 
+    This API can be upload, download, remove and rename any files from the service uploadgram.me.
+Important links:
+    - Uploadgram.me link: https://uploadgram.me/
+    - Terms of Service: https://uploadgram.me/terms.html
+    - DMCA Policy: https://uploadgram.me/dmca.html
+Contacts (API Author):
+    - github: https://github.com/tankalxat34/UploadgramPyAPI
+    - email: mailto:tankalxat34@gmail.com?subject=User%20of%20UploadgramPyAPI
+    - telegram: https://t.me/tankalxat34
+Contacts (Uploadgram Author):
+    - telegram channel: https://t.me/uploadgramme
+    - telegram author: https://t.me/pato05
+Other links:
+    - pypi link: https://pypi.org/project/uploadgrampyapi/
+    - pepy.tech statistic: https://pepy.tech/project/UploadgramPyAPI
+
+****************************************************************
+*----------------------Example of use:-------------------------*
+****************************************************************
+
+import UploadgramPyAPI
+
+# Upload any file
+up_new_file = UploadgramPyAPI.NewFile("D:\\image.jpg")
+up_new_file.upload()
+
+# Set up connection to file like here
+up_file = UploadgramPyAPI.File("611e5e6237f6fg", "e3da26e9dddd2e01b8c0831370695e9088a96ff81e262fc2g")
+# or like here
+# up_file = UploadgramPyAPI.File("e3da26e9dddd2e01b8c0831370695e9088a96ff81e262fc2g")
+# or like this
+# up_file = UploadgramPyAPI.File("611e5e6237f6fg")
+
+# Download any file
+up_file.download()
+
+# Rename any file
+up_file.rename("ItsNewNameForFile.jpg")
+
+# Delete any file
+up_file.delete()
+
+****************************************************************
+"""
+import random
+import getpass
+import json
+import os
+import os.path
+import requests
+
+# You can replace this user-agent any different
+global USER_AGENT
+USER_AGENT = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.2"
+
+# If you don't want to rename or delete the file you can use this const for param "key" in class "File"
+global NONE
+NONE = "none"
+
+# Const for default length of 'id' parameter
+global LENGTH_ID
+LENGTH_ID = 14
+
+# Const for default length of 'key' parameter
+global LENGTH_KEY
+LENGTH_KEY = 49
+
+
+class UploadgramConnectionError(Exception):
+    def __init__(self):
+        super().__init__("Uploadgram.me currently unavailable. Please, try again later.")
+
+
+class UploadgramUsingKeyError(Exception):
+    def __init__(self, text):
+        super().__init__('You can not ' + text + ' this file because you using the NONE-const for "key" parameter or you using "id" file instead of "key" parameter')
+
+
+class UploadgramInvalidKey(Exception):
+    def __init__(self, text):
+        super().__init__("Invalid key to " + text + " this file")
+
+
+class UploadgramFileIsNotAvalible(Exception):
+    def __init__(self, id):
+        super().__init__("This file " + id + " does not exists")
+
+
+class UploadgramInvalidValue(Exception):
+    def __init__(self, value, parameter):
+        super().__init__('Invalid value "' + value + '" for parameter "' + parameter + '"')
+
+
+class UploadgramInvalidLengthPattern(Exception):
+    def __init__(self, pattern_name, length):
+        super().__init__('Invalid length ' + str(length) + " for " + pattern_name)
+
+
+class ServiceRules:
+    def __init__(self, type="policies"):
+        """Class for getting text from `Terms of Service` and `DMCA Policy`
+
+        :param type:    Get string values: "policies" or "dmca"
+        """
+        self.req = requests.get("https://uploadgram.me/"+type+".txt", headers={"user-agent": USER_AGENT})
+        if self.req:
+            self.text = self.req.text
+        else:
+            if self.req.status_code == 404:
+                raise UploadgramInvalidValue(type, "type")
+            else:
+                raise UploadgramConnectionError()
+
+    def get(self): 
+        return self.text
+
+
+class Random:
+    def __init__(self, type="id", pattern_id="62"+11*"."+"g", pattern_key=48*"."+"g", place_for_symbol="."):
+        """Class for generate random key and id
+
+        :param type:                String parameter, that you want to get: "id" or "key"
+        :param pattern_id:          String pattern, that are using to generate file id
+        :param pattern_key:         String pattern, that are using to generate file key
+        :param place_for_symbol:    Any symbol that will be replaced on a random symbol
+        """
+        if type == "id" or type == "key":
+            self.type = type
+        else:
+            raise UploadgramInvalidValue(type, "type")
+        if len(pattern_id) != LENGTH_ID:
+            raise UploadgramInvalidLengthPattern("pattern_id", len(pattern_id))
+        if len(pattern_key) != LENGTH_KEY:
+            raise UploadgramInvalidLengthPattern("pattern_key", len(pattern_key))
+        if self.type == "id":
+            self.using_pattern = pattern_id
+        else:
+            self.using_pattern = pattern_key
+        self.pattern_id = pattern_id
+        self.pattern_key = pattern_key
+        self.length = len(self.using_pattern)
+        self.place_for_symbol = place_for_symbol
+        self.local_result = ""
+
+    def get(self):
+        """Returns random string for `key` or `id` parameter"""
+        self.local_result = self.using_pattern
+        for i in range(self.length):
+            if self.using_pattern[i] == self.place_for_symbol:
+                self.local_result = self.local_result.replace(self.place_for_symbol, hex(random.randint(0, 15))[2:], 1)
+        return self.local_result
+
+    def get_url(self):
+        return "https://dl.uploadgram.me/" + self.get()
+
+
+class File:
+    def __init__(self, id: str, key=NONE):
+        """
+        :param id:  Get id file that placed at the end URL for file
+        :param key: Key for rename and remove file from server
+        """
+        self.id = id
+        if len(self.id) > LENGTH_ID:
+            self.key = self.id
+        else:
+            self.key = key
+
+        self.url_import = None
+
+        try:
+            self.r = requests.get("https://api.uploadgram.me/get/" + self.id)
+        except requests.exceptions.ConnectionError:
+            raise UploadgramConnectionError()
+
+        if self.r:
+            self.json = self.r.json()
+
+            self.id = self.json["url"].split("/")[-1]
+            self.url = "https://dl.uploadgram.me/" + self.id
+
+            ### Create attibutes for class ###
+            self.name = self.json["filename"]
+            self.size = self.json["size"]
+            self.scanned = self.json["wasScanned"]
+
+            try:
+                self.userTelegramId = self.json["userTelegramId"]
+            except Exception:
+                self.userTelegramId = None
+
+            try:
+                self.userIp = self.json["userIp"]
+            except Exception:
+                self.userIp = None
+
+            if str(self.key) != NONE and len(self.key) > LENGTH_ID:
+                ### Create import link ###
+                dict_part_url_to_import = ({str(self.key): {'filename': os.path.basename(self.name),
+                                                               'size': int(self.size),
+                                                               'url': str(self.url)}})
+                self.url_import = "https://uploadgram.me/upload/#import:" + json.dumps(dict_part_url_to_import)
+            else:
+                self.url_import = None
+        else:
+            raise UploadgramFileIsNotAvalible(self.id)
+
+    def get_json(self):
+        """
+        Returns json response from answer
+        """
+        return self.json
+
+    def download(self, path=f"C:\\Users\\{getpass.getuser()}\\Downloads\\"):
+        """
+        Returns True if the file was download on your PC
+        """
+        with open(path + self.name, 'wb') as f:
+            f.write((requests.get(self.url + "?raw")).content)
+        return True
+
+    def delete(self):
+        """
+        Returns response from server if file deleted successfully
+        """
+        if self.key != NONE:
+            self.r_delete = requests.get("https://api.uploadgram.me/delete/" + self.key,
+                                     headers={"user-agent": USER_AGENT})
+        else:
+            raise UploadgramUsingKeyError("delete")
+        if self.r_delete:
+            return self.r_delete
+        else:
+            raise UploadgramInvalidKey("delete")
+
+    def rename(self, new_name: str):
+        """
+        Returns response from server if file renamed successfully
+        """
+        if self.key != NONE:
+            self.r_rename = requests.post("https://api.uploadgram.me/rename/" + self.key,
+                                      data={"new_filename": new_name,
+                                            "user-agent": USER_AGENT})
+        else:
+            raise UploadgramUsingKeyError("rename")
+        if self.r_rename:
+            return self.r_rename
+        else:
+            raise UploadgramInvalidKey("rename")
+
+
+class NewFile:
+    def __init__(self, path):
+        """
+        :param path:    Path to file what you want to upload
+        """
+        try:
+            self.check_to_available = requests.get("https://api.uploadgram.me/status", headers={"user-agent": USER_AGENT})
+            self.path = path
+            self.url_upload = 'https://api.uploadgram.me/upload'
+            self.readfile = open(self.path, "rb")
+
+            self.key = None
+            self.id = None
+            self.url = None
+            self.url_import = None
+
+            self.name = None
+            self.size = None
+            self.scanned = None
+            self.userTelegramId = None
+            self.userIp = None
+        except Exception:
+            raise UploadgramConnectionError()
+
+    def upload(self):
+        """return {'ok': True, 'url': 'https://dl.uploadgram.me/new_file_id', 'delete': 'delete_key'}"""
+        self.r = requests.post(self.url_upload,
+                               headers={"user-agent": USER_AGENT},
+                               files={"file_upload": self.readfile})
+        self.key = self.r.json()["delete"]
+        self.id = self.r.json()["url"].split("/")[-1]
+        self.url = self.r.json()["url"]
+
+        _local = File(self.id, self.key)
+        self.url_import = _local.url_import
+
+        self.name = _local.json["filename"]
+        self.size = _local.json["size"]
+        self.scanned = _local.json["wasScanned"]
+
+        try:
+            self.userTelegramId = _local.json["userTelegramId"]
+        except Exception:
+            pass
+
+        try:
+            self.userIp = _local.json["userIp"]
+        except Exception:
+            pass
+
+        return self.r.json()
