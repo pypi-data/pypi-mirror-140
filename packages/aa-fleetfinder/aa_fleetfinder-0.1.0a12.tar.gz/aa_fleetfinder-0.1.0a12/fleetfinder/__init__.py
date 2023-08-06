3f0ba3f90be369e4d2c4751ef34e5814a52596bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:28:53 +0000

    Add package aa-fleetfinder/0.1.0a12/aa_fleetfinder-0.1.0a12.tar.gz

---
+++ packages/aa-fleetfinder/aa_fleetfinder-0.1.0a12.tar.gz/aa_fleetfinder-0.1.0a12/fleetfinder/__init__.py
@@ -0,0 +1,15 @@
+"""
+init
+"""
+
+default_app_config: str = "fleetfinder.apps.FleetFinderConfig"
+
+github_url: str = "https://github.com/ppfeufer/aa-fleetfinder"
+
+__title__ = "Fleet Finder"
+__version__ = "0.1.0-alpha.12"
+
+__verbose_name__ = "Fleet Finder for Alliance Auth"
+__user_agent_name__ = "Fleet-Finder-for-Alliance-Auth"
+
+__user_agent__ = f"{__verbose_name__} v{__version__} {github_url}"
