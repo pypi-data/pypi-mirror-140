545f6a01163e439929e1cb68e9929b1d84206329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:05 +0000

    Add package DeltapatcherForDeltarando/1.0.0/DeltapatcherForDeltarando-1.0.0.tar.gz

---
+++ packages/DeltapatcherForDeltarando/DeltapatcherForDeltarando-1.0.0.tar.gz/DeltapatcherForDeltarando-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0' 
+DESCRIPTION = 'DELTARUNE Randovania Patcher'
+LONG_DESCRIPTION = 'The patcher for the DELTARUNE Randovania addition.'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="DeltapatcherForDeltarando", 
+        version=VERSION,
+        author="jonloveslegos",
+        author_email="<jonloveslegos@email.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=[],
+        classifiers= []
+)
