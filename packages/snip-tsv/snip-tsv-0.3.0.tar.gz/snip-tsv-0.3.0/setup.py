fc3c84d2776ffb538759ff7976b1582053750183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:45 +0000

    Add package snip-tsv/0.3.0/snip-tsv-0.3.0.tar.gz

---
+++ packages/snip-tsv/snip-tsv-0.3.0.tar.gz/snip-tsv-0.3.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+modules = \
+['snip_tsv']
+install_requires = \
+['matplotlib>=3.5.1,<4.0.0']
+
+entry_points = \
+{'console_scripts': ['snip = snip_tsv:main']}
+
+setup_kwargs = {
+    'name': 'snip-tsv',
+    'version': '0.3.0',
+    'description': 'Crop tabular data files',
+    'long_description': '# snip-tsv\n',
+    'author': 'Andrew Ross',
+    'author_email': 'andrew.ross.mail@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/andrewrosss/snip-tsv',
+    'package_dir': package_dir,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
