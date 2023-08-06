1fb2ba19a33b5b638428e758f0fb95fa0453a550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:38 +0000

    Add package lib-auth-py/0.1.2/lib-auth-py-0.1.2.tar.gz

---
+++ packages/lib-auth-py/lib-auth-py-0.1.2.tar.gz/lib-auth-py-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'lib-auth-py',
+    packages = ['lib_auth_py'],
+    version = '0.1.2',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Update folder',
+    author = 'Natarajan Kumar',
+    author_email = 'natarajan@albertinvent.com',
+    url = 'https://github.com/natarajanmolecule/lib-auth-py.git',
+    download_url = 'https://github.com/natarajanmolecule/lib-auth-py/archive/refs/tags/0.1.2.tar.gz',
+    keywords = ['jwt', 'auth'],
+    classifiers = [],
+)
