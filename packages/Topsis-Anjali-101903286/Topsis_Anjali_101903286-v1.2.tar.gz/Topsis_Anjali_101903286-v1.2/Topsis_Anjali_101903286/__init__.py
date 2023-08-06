fcdcfb6d7289494fcf6a7c95900fb4c0ed0e3875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:41 +0000

    Add package Topsis-Anjali-101903286/1.2/Topsis_Anjali_101903286-v1.2.tar.gz

---
+++ packages/Topsis-Anjali-101903286/Topsis_Anjali_101903286-v1.2.tar.gz/Topsis_Anjali_101903286-v1.2/Topsis_Anjali_101903286/__init__.py
@@ -0,0 +1,2 @@
+from Topsis_Anjali_101903286.topsis import rank
+__version__='v1.2'
---
+++ packages/Topsis-Anjali-101903286/Topsis_Anjali_101903286-v1.2.tar.gz/Topsis_Anjali_101903286-v1.2/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'Topsis_Anjali_101903286',        
+  packages = ['Topsis_Anjali_101903286'],  
+  version = 'v1.2',      
+  license='MIT',       
+  description = 'Topsis Score and Rank generator',
+  author = 'Anjali',                
+  author_email = 'aanjali_be19@thapar.edu',      
+  url = 'https://github.com/Anjali2502/Topsis_Anjali_101903286',   
+  download_url = '',    
+  keywords = ['topsis','topsis score','rank','Thapar'],
+  install_requires=['numpy','pandas' ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',    
+    'Intended Audience :: Developers',   
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
