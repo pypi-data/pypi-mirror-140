50a250a021251a236e0f00f7aad7e0deb3cc66e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:46 +0000

    Add package robogate/1.3.1/robogate-1.3.1-py3-none-any.whl

---
+++ packages/robogate/robogate-1.3.1-py3-none-any.whl/robogate/robogate.py
@@ -0,0 +1,26 @@
+import dataclasses
+import enum
+import sys
+from .robot import Robot
+import json
+import fileinput
+
+
+class EnhancedJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if dataclasses.is_dataclass(o):
+            return dataclasses.asdict(o)
+        if isinstance(o, enum.Enum):
+            return o.value
+        return super().default(o)
+
+def start_robot_war(robot: Robot):
+    playerId = input()
+    init = robot.initialize(playerId)
+    print(json.dumps(init, cls=EnhancedJSONEncoder))
+    for line in fileinput.input():
+        print(line, file=sys.stderr)
+        gameState = json.loads(input())
+        actions = robot.get_actions(gameState)
+        print(json.dumps(actions, cls=EnhancedJSONEncoder))
+
