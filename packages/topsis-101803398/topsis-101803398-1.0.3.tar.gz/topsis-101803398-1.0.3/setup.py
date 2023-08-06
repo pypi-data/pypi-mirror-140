8395c528e124d1a25cc749bc9075dc5ec36a686b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:30 +0000

    Add package topsis-101803398/1.0.3/topsis-101803398-1.0.3.tar.gz

---
+++ packages/topsis-101803398/topsis-101803398-1.0.3.tar.gz/topsis-101803398-1.0.3/setup.py
@@ -0,0 +1,35 @@
+import pathlib
+from setuptools import setup
+import codecs
+import os
+
+# Location of file directory
+HERE = pathlib.Path(__file__).parent
+
+# Readme
+README = (HERE / "README.md").read_text()
+
+VERSION = '1.0.3'
+DESCRIPTION = 'Topsis implementation'
+
+# Setting up
+setup(
+    name="topsis-101803398",
+    version=VERSION,
+    author="Harshit Verma",
+    author_email="iamharshitverma114@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url = "",
+    license="MIT",
+    long_description=README,
+    packages=["topsis"],
+    install_requires=["pandas", "numpy"],
+     classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
