d39043c89d783bc279a62d6d5ff9c2eed99ec78d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:19:44 +0000

    Add package foundrydeploy/0.1.2/foundrydeploy-0.1.2.tar.gz

---
+++ packages/foundrydeploy/foundrydeploy-0.1.2.tar.gz/foundrydeploy-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="foundrydeploy",
+    version="0.1.2",
+    description="basic wrapper around `forge create` and `cast send` for basic deployment flows",
+    url="https://github.com/joshieDo/foundrydeploy",
+    author="joshie",
+    author_email="",
+    license="",
+    packages=["foundrydeploy"],
+    install_requires=["loguru==0.6.0"],
+    classifiers=[],
+)
