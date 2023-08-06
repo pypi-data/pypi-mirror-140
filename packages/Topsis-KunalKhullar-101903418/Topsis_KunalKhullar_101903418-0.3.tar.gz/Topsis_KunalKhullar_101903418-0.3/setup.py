7710869e3d868d4545332b842df9c28ce25298df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:43 +0000

    Add package Topsis-KunalKhullar-101903418/0.3/Topsis_KunalKhullar_101903418-0.3.tar.gz

---
+++ packages/Topsis-KunalKhullar-101903418/Topsis_KunalKhullar_101903418-0.3.tar.gz/Topsis_KunalKhullar_101903418-0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+setup(
+  name = 'Topsis_KunalKhullar_101903418',         
+  packages = ['Topsis_Kunalkhullar_101903418'],   
+  version = '0.3',      
+  license='MIT',        
+  description = 'Implementation of TOPSIS',   
+  author = 'Kunal Khullar',                  
+  author_email = 'kkhullar_be19@thapar.edu',     
+  
+  keywords = ['TOPSIS'],   
+  install_requires=[           
+          'pandas',
+          'numpy',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'Programming Language :: Python :: 3.6',      
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+  ],
+)
