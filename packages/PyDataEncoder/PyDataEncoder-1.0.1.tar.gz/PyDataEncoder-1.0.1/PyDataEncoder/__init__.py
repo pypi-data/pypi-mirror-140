342ea01984ff1f712ff891d30302a1d307b73bd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:26 +0000

    Add package PyDataEncoder/1.0.1/PyDataEncoder-1.0.1.tar.gz

---
+++ packages/PyDataEncoder/PyDataEncoder-1.0.1.tar.gz/PyDataEncoder-1.0.1/PyDataEncoder/__init__.py
@@ -0,0 +1,1 @@
+from PyDataEncoder.PyDataEncoder import PyDataEncoder
---
+++ packages/PyDataEncoder/PyDataEncoder-1.0.1.tar.gz/PyDataEncoder-1.0.1/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+    name='PyDataEncoder',         
+    packages=['PyDataEncoder'],   
+    version='1.0.1',      
+    license='MIT',
+    description='Dataset Encoder Package',
+    author='Doulat',                   
+    author_email='ahmad.doulat@gmail.com',      
+    url='https://github.com/ahmaddoulat/PyDataEncoder',
+    download_url='https://github.com/ahmaddoulat/PyDataEncoder.git',
+    
+    keywords=['dataset', 'encoder', 'encoding', 'OneHotEncoder', 'OrdinalEncoder', 'FindnReplace', 'LabelEncoder', 'OneHotEncoder', 'ScikitOrdinalEncoder', 'ScikitOneHotEncoder'],
+    install_requires=[            
+        'pandas',
+        'sklearn',
+        'numpy',
+        'logging'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
