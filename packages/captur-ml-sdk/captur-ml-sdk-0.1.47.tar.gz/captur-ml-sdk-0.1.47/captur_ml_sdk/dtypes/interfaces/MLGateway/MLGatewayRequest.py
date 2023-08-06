64d7b72bc245e3ee00ac663a3311fb281021bf55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:40 +0000

    Add package captur-ml-sdk/0.1.47/captur-ml-sdk-0.1.47.tar.gz

---
+++ packages/captur-ml-sdk/captur-ml-sdk-0.1.47.tar.gz/captur-ml-sdk-0.1.47/captur_ml_sdk/dtypes/interfaces/MLGateway/MLGatewayRequest.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, root_validator
+from typing import Optional
+
+from captur_ml_sdk.dtypes.interfaces.MLGateway.MLMetaRequest import ModelMetaRequest
+from captur_ml_sdk.dtypes.interfaces.MLGateway.MLPredictRequest import ModelPredictRequest
+from captur_ml_sdk.dtypes.interfaces.MLGateway.MLLivePredictRequest import ModelLivePredictRequest
+from captur_ml_sdk.dtypes.interfaces.MLGateway.MLTrainRequest import ModelTrainRequest
+from captur_ml_sdk.dtypes.interfaces.MLGateway.MLEvalRequest import ModelEvaluateRequest
+
+
+class GatewayRequest(BaseModel):
+    meta: Optional[ModelMetaRequest]
+    predict: Optional[ModelPredictRequest]
+    live_predict: Optional[ModelLivePredictRequest]
+    train: Optional[ModelTrainRequest]
+    evaluate: Optional[ModelEvaluateRequest]
+
+    @root_validator
+    def request_must_have_predict_live_predict_train_or_evaluate(cls, values):
+        if not values.get("predict") \
+                and not values.get("live_predict") \
+                and not values.get("train") \
+                and not values.get("evaluate"):
+            raise ValueError(
+                "Request must include either 'predict', 'live_predict', 'train' or 'evaluate'")
+
+        return values
+
+# if __name__ == "__main__":
+#     print(MLGatewayRequest.schema_json(indent=2))
