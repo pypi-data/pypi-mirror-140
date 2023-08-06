cff9f5c44374d8ec201129387da5b0b8dc78025e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:32 +0000

    Add package edc-microbiology/0.1.3/edc-microbiology-0.1.3.tar.gz

---
+++ packages/edc-microbiology/edc-microbiology-0.1.3.tar.gz/edc-microbiology-0.1.3/edc_microbiology/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from edc_identifier.model_mixins import UniqueSubjectIdentifierFieldMixin
+from edc_model import models as edc_models
+from edc_model.models import HistoricalRecords
+from edc_sites.models import CurrentSiteManager, SiteModelMixin
+from edc_utils import get_utcnow
+
+from .model_mixins import (
+    BloodCultureModelMixin,
+    HistopathologyModelMixin,
+    SputumAfbModelMixin,
+    SputumCultureModelMixin,
+    SputumGenexpertModelMixin,
+    UrinaryLamModelMixin,
+    UrineCultureModelMixin,
+)
+
+
+class Microbiology(
+    UniqueSubjectIdentifierFieldMixin,
+    UrinaryLamModelMixin,
+    SputumGenexpertModelMixin,
+    SputumCultureModelMixin,
+    SputumAfbModelMixin,
+    UrineCultureModelMixin,
+    BloodCultureModelMixin,
+    HistopathologyModelMixin,
+    SiteModelMixin,
+    edc_models.BaseUuidModel,
+):
+
+    report_datetime = models.DateTimeField(default=get_utcnow)
+
+    on_site = CurrentSiteManager()
+    objects = models.Manager()
+    history = HistoricalRecords()
+
+    class Meta(edc_models.BaseUuidModel.Meta):
+        verbose_name = "Microbiology"
+        verbose_name_plural = "Microbiology"
