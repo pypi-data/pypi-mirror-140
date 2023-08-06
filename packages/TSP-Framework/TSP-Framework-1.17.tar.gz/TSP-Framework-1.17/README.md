4d2ea47d0fdcc2398de1557ebc4d3d893e74ced2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:27 +0000

    Add package TSP-Framework/1.17/TSP-Framework-1.17.tar.gz

---
+++ packages/TSP-Framework/TSP-Framework-1.17.tar.gz/TSP-Framework-1.17/README.md
@@ -0,0 +1,242 @@
+# Framework de algoritmos para resolver el Problema del Vendedor Viajero
+Framework para resolver el problema del vendedor viajero aplicando algoritmos metaheuristicos como el Simulated Annealing o Algoritmo Genetico.
+
+**Visualizacion**:
+<p align="center">
+<img src="https://media.giphy.com/media/uLzy84eyEANxqK83Iw/giphy.gif"/>
+</p>
+
+**GUI**:
+<p align="center">
+<img src="https://media.giphy.com/media/7I1c3ms9OekiCkcbQD/giphy.gif"/>
+</p>
+
+## Contenido
+* **codigo fuente:** codigo en lenguaje Python en la carpeta tspf/ para ser preparado y distribuido como paquete Python
+
+* **instancias:** instancias en formato TSPlib en la carpeta instances/
+
+## Uso sin instalacion (Recomendado)
+
+Para ejecutar de forma local, puede descargar el repositorio e instalar los requerimientos abriendo una terminal dentro de la carpeta del proyecto y luego ejecutar:
+```sh
+pip install -r requirements.txt
+```
+
+Luego, ejecutar python (python3 en Linux) tspf.py + los argumentos a utilizar definidos  desde la carpeta donde tenga el framework
+```sh
+python tspf.py --instance instances/burma14.tsp
+```
+
+## Instalacion como script Python
+
+El framework puede ser instaladado como paquete desde PyPi (https://pypi.org/project/TSP-Framework/) o GitHub, para esto utilice:
+
+```sh
+pip install TSP-Framework
+```
+o
+```sh
+pip install git+https://github.com/Javernaver/TSP-Framework.git
+```
+Una vez instalado tendra acceso al comando **tspf** el cual podra ser utilizado desde cualquier directorio. Recuerde que para utilizar estos comandos debe tener el directorio en las variables de entorno o PATH del sistema operativo.
+### Importante sobre comando tspf
+Al utilizar el comando **tspf** debe agregar igualmente el parametro con la instancia, ya que de otro modo no funcionara la instancia por defecto, debe utilizar una con el parametro -i o --instance <PATH> al archivo en formato TSPlib (http://comopt.ifi.uni-heidelberg.de/software/TSPLIB95/). Ejemplo de uso:
+
+```sh
+tspf --instance archivo.tsp
+```
+
+## Uso Online con Replit
+
+Puede utilizar el framework de forma online con Replit: https://replit.com/@Javernaver/TSP-Framework
+Se recomienda tener una cuenta en Replit y hacer fork del proyecto para una mejor experiencia y edicion del codigo fuente.
+
+## Descripcion
+
+### General
+
+* **TSPlibReader.py:** Modulo con la clase que implementa funciones para leer las instancias de TSP en el formato de la libreria de instancias TSPlib.
+
+* **Tsp.py:** Modulo con la clase que representa la instancia de TSP. Contiene los siguientes metodos:
+
+	* **compute_tour_length:** Calcula el costo de un tour
+
+	* **tsp_check_tour:** Revisa si un tour es correcto
+
+	* **random_tour:** Genera un tour aleatorio
+
+	* **greedy_nearest_n:** Genera un tour utilizando la heuristica del vecino mas cercano
+
+* **Tour.py:** Modulo con la clase que implementa una solucion del TSP. El constructor de clase permite definir si la solucion inicial es construida aleatoriamente, utilizando la heuristica del vecino mas cercano o una solucion secuencial. Contiene las siguientes variables y metodos:
+
+	* **current:** Solucion del TSP que se representa con un arreglo de enteros de tamano n+1, donde n son los nodos (ciudades) y la ultima ciudad del tour corresponde siempre a la primera ciudad. 
+
+	* **cost:** Costo del tour de la solucion
+
+	* **swap**: Metodo que aplica el movimiento swap a dos nodos en el tour actual
+
+	* **twoOptSwap:** Metodo que aplica el movimiento 2-opt a dos nodos en el tour actual
+
+	* **randomNeighbor:** Metodo que aplica aleatoriamente a dos nodos el movimiento definido en move_type (TSPMove.TWO_OPT o TSPMove.SWAP)
+
+  
+* **main.py:** Modulo principal que ejecuta el software uniendo todos los demas modulos
+ 
+
+* **AlgorithmsOptions.py:** Modulo con la clase que configura y lee todos los argumentos que puedan tener uno de los algoritmos a aplicar
+ 
+* **utilities.py:** Modulo distintas utilidades utilizadas por los demas modulos
+
+* **plot.py:** Modulo encargado de generar y mostrar los distintos graficos utilizados como los generados en la trayectoria de las soluciones 
+
+* **gui.py:** Modulo encargado de generar y mostrar la interfaz grafica de usuario 
+
+### Algoritmos
+
+* **SimulatedAnnealing.py:** Modulo con la clase que implementa el metodo de busqueda de Simulated Annealing
+
+	* **search:** Metodo que inicia la busqueda de Simulated Annealing comenzando de una solucion inicial
+
+	* **terminationCondition:** Metodo que revisa si la condicion de termino (temperatura minima, numero de evaluaciones o tiempo de ejecucion) se ha cumplido
+
+	* **getAcceptanceProbability:** Metodo que calcula la probabilidad de aceptar una solucion
+
+	* **reduceTemperature:** Metodo que reduce el valor de la temperatura siguiendo un esquema de enfriamiento seleccionado
+
+* **GeneticAlgorithm.py:** Modulo con la clase que implementa el metodo de busqueda de Algoritmo Genetico
+
+	* **search:** Metodo que inicia la busqueda comenzando de una poblacion inicial generada aleatoriamente
+
+	* **terminationCondition:** Metodo que revisa si la condicion de termino (numero de evaluaciones, iteraciones o tiempo de ejecucion) se ha cumplido
+
+* **Population.py:** Modulo con la clase que implementa todos los metodos para manipular las soluciones de la poblacion de algoritmo genetico
+
+	* **mutation:** Metodo que aplica un operador de mutacin a la poblacion
+
+	* **crossover:** Metodo que aplica un operador de crossover a los padres proporcionados
+
+	* **selectParents:** Metodo que selecciona dos padres aplicando un operador de seleccion
+
+	* **selectPopulation:** Metodo que selecciona la poblacion reduciendo su tamano en base a un operador de seleccion
+	
+* **LocalSearch.py:** Modulo con la clase que implementa el metodo de busqueda local
+
+	* **search:** Metodo que aplica la busqueda comenzando por una solucion inicial
+
+	* **swapSearch:** Metodo que aplica la busqueda local mediante movimientos swap
+
+	* **twoOptSearch:** Metodo que aplica la busqueda local mediante 2-opt
+
+	* **threeOptSearch:** Metodo que aplica la busqueda local mediante 3-opt
+
+	
+* **IteratedLocalSearch.py:** Modulo con la clase que implementa el metodo de busqueda local iterativo
+
+	* **search:** Metodo que aplica la busqueda comenzando por una solucion inicial
+
+	* **terminationCondition:** Metodo que revisa si la condicion de termino (numero de iteraciones o tiempo de ejecucion) se ha cumplido
+  
+
+## Argumentos
+
+Las opciones de los algoritmos se pueden cambiar en la clase AlgorithmsOptions.py, sin embargo, al crear la clase se le pueden entregar definiciones de las variables a definir, como por ejemplo: "options = AlgorithmsOptions(seed=98849,...)". O bien, si se es ejecutado directamente desde la linea de comando se le pueden pasar los siguientes argumentos:
+
+Argumentos Generales:
+  
+* **Metaheuristica:** Tipo de Metaheuristica a usar. Por defecto SA
+	* **SA:** Simulated Annealing 
+	* **GA:** Genetic Algorithm.
+	*  (-mh o --metaheuristic). **Ejemplo:** python tspf.py -mh SA
+
+* **Instancia:** Ruta al archivo de la instancia de TSP a resolver. Por defecto instances/burma14.tsp.
+	*  (-i o --instance **<PATH>**). **Ejemplo:** python tspf.py -i instances/eil51.tsp
+  
+* **Semilla**: Semilla para el generador de numeros aleatorios y todo los relacionado al modulo random de Python.
+	*  (-s o --seed **entero**). **Ejemplo:** python tspf.py -s 4854
+
+* **Movimiento:** Tipo de movimiento en formato TSPMove que se utilizara para la ejecucion. Los valores posibles son TSPMove.SWAP, TSPMove.TWO_OPT y TSPMove.THREE_OPT. Por defecto se utiliza swap. 
+	* (-mhm o --move **[ swap | 2opt | 3opt ]**). **Ejemplo:** python tspf.py --move 2opt
+	
+* **Evaluaciones:** Numero maximo de funciones de evaluacion calculadas. Por defecto se utiliza 1000
+	 * (-e o --evaluations **entero**). **Ejemplo:** python tspf.py -e 2000
+
+* **Iteraciones:** Numero maximo de iteraciones para el ciclo principal de un algoritmo. Por defecto se utiliza 20
+	 * (-e o --evaluations **entero**). **Ejemplo:** python tspf.py -it 500
+	 
+* **Archivo de solucion y trayectoria:** Nombre del archivo de salida para almacenar la solucion y la trayectoria. Por defecto se utiliza solution.txt y trajectory.csv
+	 * (-sol o --solution <PATH>). **Ejemplo:** python tspf.py --solution test
+	 
+* **Visualizacion de trayectoria:** Parametro de tipo flag que indica si se quiere o no visualizar la trayectoria de la solucion.
+	 * (-vi o --visualize). **Ejemplo:** python tspf.py --visualize
+
+* **Modo Interfaz Grafica:** Parametro de tipo flag que indica si se quiere o no utilizar el modo interfaz grafica.
+	 * (-gui o --gui). **Ejemplo:** python tspf.py --gui
+
+Argumentos para Simulated Annealing:
+
+* **Solucion Inicial:** Tipo de solucion inicial en formato InitialSolution que se utilizara. Los valores posibles son: InitialSolution.RANDOM, InitialSolution.NEAREST_N y InitialSolution.DETERMINISTIC. Por defecto se utiliza random. 
+	* (-is o --insol **[ random | nearest_n | deterministic ]**). **Ejemplo:** python tspf.py --insol deterministic
+
+* **Enfriamiento:** Variable del tipo CoolingType que indica el tipo de esquema de enfriamiento que se utilizara para la ejecucion de Simulated Annealing. Los valores posibles son: CoolingType.GEOMETRIC, CoolingType.LOG y CoolingType.LINEAR. Por defecto se utiliza geometric.
+
+	* (-c o --cooling **[ linear | geometric | log ]**). **Ejemplo:** python tspf.py -c linear
+
+* **Alfa:** Valor del parametro alpha para el enfriamiento lineal. Por defecto 0.98.
+
+	* (-a o --alpha **]0,1]**). **Ejemplo:** python tspf.py -a 0.3
+  
+
+* **Temperatura Inicial:** Valor de la temperatura inicial t0. Por defecto 1000.0. 
+	* (-t0 o --tini **]0,FLOAT_MAX]**). **Ejemplo:** python tspf.py -t0 956.45
+
+* **Temperatura Minima:** Valor de la temperatura minima. Por defecto 900.0.
+	*  (-tmin o --tmin **]0,FLOAT_MAX]**). **Ejemplo:** python tspf.py -tmin 800.87
+
+Argumentos para Algoritmo Genetico:
+
+* **Tamano de la poblacion:** Tamano de la poblacion. Por defecto 10
+
+	* (-p o --psize **]0,INT_MAX]**). **Ejemplo:** python tspf.py -p 20
+
+* **Cantidad de hijos:** Cantidad de hijos a generar. Por defecto 20
+
+	* (-o o --osize **]0,INT_MAX]**). **Ejemplo:** python tspf.py -o 40
+
+* **Seleccion de padres:** Los valores posibles son: SelectionType.BEST, SelectionType.RANDOM, SelectionType.ROULETTE y SelectionType.TOURNAMENT. Por defecto random
+
+	* (-ps o --pselection **[ random | best | roulette | tournament ]**). **Ejemplo:** python tspf.py --pselection tournament
+
+* **Operador de cruzamiento:** Los valores posibles son: CrossoverType.OX, CrossoverType.PMX y CrossoverType.OPX. Por defecto ox
+
+	* (-cr o --crossover **[ ox | opx | pmx ]**). **Ejemplo:** python tspf.py --crossover opx
+
+* **Operador de mutacion:** Los valores posibles son: TSPMove.SWAP, TSPMove.TWO_OPT y TSPMove.THREE_OPT. Por defecto swap 
+
+	* (-mu o --mutation **[ swap | 2opt | 3opt ]**). **Ejemplo:** python tspf.py -mu 2opt
+
+* **Probabilidad de mutacion:** Valor de probabilidad de mutacion de los individuos. Por defecto 0.2.
+
+	* (-mp o --mprobability **[0,1]**). **Ejemplo:** python tspf.py -mp 0.3
+  
+* **Seleccion de poblacion:** Los valores posibles son: SelectionType.BEST, SelectionType.RANDOM, SelectionType.ROULETTE y SelectionType.TOURNAMENT. Por defecto random
+
+	* (-gs o --gselection **[ random | best | roulette | tournament ]**). **Ejemplo:** python tspf.py --gselection tournament
+
+* **Estrategia de seleccion de poblacion:** Los valores posibles son: SelectionStrategy.MULAMBDA y SelectionStrategy.MUPLUSLAMBDA. Por defecto mu,lambda
+
+	* (-g o --gstrategy **[ mu,lambda | mu+lambda ]**). **Ejemplo:** python tspf.py --gstrategy mu,lambda
+
+Argumentos para Local Search e Iterated Local Search:
+
+* **Movimiento:** Tipo de movimiento en formato TSPMove que se utilizara para la ejecucion. Los valores posibles son TSPMove.SWAP, TSPMove.TWO_OPT y TSPMove.THREE_OPT. Por defecto se utiliza swap. 
+	* (-mhm o --move **[ swap | 2opt | 3opt ]**). **Ejemplo:** python tspf.py --move 2opt
+
+* **Best Improvemet:** Parametro de tipo flag que indica si la busqueda es del tipo best improvement o first improvement, se deja por defecto first improvement.
+	 * (-b o --best). **Ejemplo:** python tspf.py --best
+	 
+* **Perturbacion:** Tipo de movimiento en formato PerturbationType que se utilizara para la perturbacion. Los valores posibles son PerturbationType.SWAP, PerturbationType.TWO_OPT, PerturbationType.THREE_OPT y PerturbationType.RANDOM. Por defecto se utiliza swap. 
+	* (-per o --perturbation **[ swap | 2opt | 3opt | random ]**). **Ejemplo:** python tspf.py --move 2opt
+	 
+* **Numero de Perturbaciones:** Numero maximo de perturbaciones por iteracion en Itarated Local Search. Por defecto se utiliza 3
+	 * (-np o --nperturbations **entero**). **Ejemplo:** python tspf.py -np 5
---
+++ packages/TSP-Framework/TSP-Framework-1.17.tar.gz/TSP-Framework-1.17/src/tspf/Algorithms/GeneticAlgorithm.py
@@ -0,0 +1,310 @@
+"""
+Modulo que contiene la clase que representa la metaheuristica de Algoritmo Genetico
+
+"""
+
+from ..Tools import utilities, bcolors, plot, Trajectory
+from . import Population, csv, datetime, Path, timer, PrettyTable, path
+from .. import Tour, Tsp, AlgorithmsOptions, SelectionStrategy
+
+class GeneticAlgorithm():
+    """ Clase Simulated Annealing la cual representa dicha metaheristica y sus metodos de busqueda
+
+        Parameters
+        ----------
+        problem : Tsp
+            Instancia del problema TSP
+        options : AlgorithmsOptions
+            Objeto de opciones para el algoritmo
+
+        Attributes
+        ----------
+        pop_size : int
+            Numero de integrantes de la poblacion
+        offspring_size : int
+            Numero de hijos para los integrantes de la poblacion
+        pselection_type : SelectionType
+            Metodo de seleccion de padres
+        crossover_type : CrossoverType
+            Metodo de cruzamiento para los padres
+        mutation_type : TSPMove
+            Metodo de mutacion para la poblacion
+        mutation_prob : float
+            Probabilidad de mutacion para la poblacion
+        selection_strategy : SelectionStrategy
+            Estrategia de seleccion para la nueva poblacion
+        gselection_type : SelectionType
+            Tipo de seleccion de la poblacion
+        best_tour : Tour
+            Instancia del mejor tour
+        evaluations : int
+            Numero de evaluaciones
+        iterations : int
+            Numero de iteraciones
+        total_time : float
+            Tiempo de ejecucion de Simulated Annealing
+        trajectory : list
+            Lista de objetos de la trayectoria de la solucion
+
+        Examples
+        --------
+        >>> options = AlgorithmsOptions()
+        >>> problem = Tsp(filename=options.instance)
+        >>> solver = GeneticAlgorithm(options=options, problem=problem)
+    """
+
+    def __init__(self, options: AlgorithmsOptions = None, problem: Tsp = None) -> None:
+
+        # Atributos de instancia
+        self.problem: Tsp # Problema 	
+
+        self.best_tour: Tour = None # Mejor tour 
+        
+        self.total_time = 0.0 # tiempo de ejecucion de Algoritmo Genetico
+        
+        self.iterations = 1 # numero de iteraciones
+        
+        self.evaluations = 0 # numero de evaluaciones
+
+        self.options: AlgorithmsOptions # Opciones
+
+        self.total_time = 0.0 # tiempo de ejecucion de Algoritmo Genetico
+
+        self.trajectory = [] # lista de listas con la trayectoria de la solucion
+
+        # Si por el objeto con las opciones no es enviado al iniciar la clase
+        if not options:
+            self.options = AlgorithmsOptions()
+        else:
+            self.options = options
+        # Si el objeto con el problema tsp no esta incluido
+        if not problem:
+            self.problem = Tsp(self.options.instance)
+        else:
+            self.problem = problem
+
+        self.pop_size = self.options.pop_size # Parametro tamaño de la poblacion 
+
+        self.offspring_size = self.options.offspring_size # Cantidad de hijos
+
+        self.pselection_type = self.options.pselection_type # Seleccion de padres
+
+        self.crossover_type = self.options.crossover_type # Cruzamiento
+
+        self.mutation_type = self.options.mutation_type # Mutacion
+
+        self.mutation_prob = self.options.mutation_prob # Probabilidad de mutacion
+
+        self.selection_strategy = self.options.selection_strategy # Estrategia de seleccion de la nueva poblacion
+
+        self.gselection_type = self.options.gselection_type # Tipo de seleccion de la poblacion
+
+        print(f"{bcolors.HEADER}\nIniciando Algortimo Genetico...{bcolors.ENDC}")
+
+
+    def print_best_solution(self) -> None:
+        """ Escribir la mejor solucion """
+        self.updateLog()
+        print()
+        print(f"\t\t{bcolors.UNDERLINE}Mejor Solucion Encontrada{bcolors.ENDC}\n")
+        self.best_tour.printSol(True)
+        print(f"{bcolors.BOLD}Total de iteraciones:{bcolors.ENDC} {bcolors.OKBLUE}{self.iterations-1}{bcolors.ENDC}")
+        print(f"{bcolors.BOLD}Total de evaluaciones:{bcolors.ENDC} {bcolors.OKBLUE}{self.evaluations-self.offspring_size}{bcolors.ENDC}")
+        print(f"{bcolors.BOLD}Tiempo total de busqueda con Algoritmo Genetico:{bcolors.ENDC} {bcolors.OKBLUE}{self.total_time:.3f} segundos{bcolors.ENDC}")
+
+
+    def search(self) -> None:
+        """ Ejecuta la busqueda del Algoritmo Genetico desde una poblacion generada aleatoriamente """
+
+        # Tabla para almacenar la informacion de la ejecucion
+        table = PrettyTable()
+        # cabezeras de la tabla
+        table.field_names = [f"{bcolors.BOLD}Iteraciones", "Evaluaciones", "Tiempo", "Minimo",
+        "Promedio", "Desv. Estandar", f"Detalles{bcolors.ENDC}"]
+        
+        parents = []
+        # Inicializar poblacion
+        print(f"{bcolors.BOLD}Generando poblacion inicial...{bcolors.ENDC}")
+        population = Population(pop_size=self.pop_size, problem=self.problem)
+        # Iniciar poblacion de hijos
+        offspring = Population(problem=self.problem)
+        
+
+        # Imprimir mejor solución encontrada 
+        print(f"{bcolors.UNDERLINE}Mejor individuo de la poblacion{bcolors.ENDC}")
+        population.getBestTour().printSol()
+        
+        # Guardar la mejor solucion en best_tour
+        if not self.best_tour:
+            self.best_tour = Tour(tour=population.getBestTour())
+        else:
+            self.best_tour.copy(population.getBestTour())
+        
+        # Guardar trayectoria
+        self.trajectory.append( Trajectory(tour=self.best_tour.current.copy(),
+                                cost=self.best_tour.cost, iterations=0, evaluations=self.evaluations,
+                                average=population.getAverage(), deviation=population.getDeviation(),
+                                worst=population.getWorstTour().cost) )
+        if not self.options.replit:
+            self.trajectory.append( Trajectory(tour=self.best_tour.current.copy(),
+                                    cost=self.best_tour.cost, iterations=0, evaluations=self.evaluations,
+                                    average=population.getAverage(), deviation=population.getDeviation(),
+                                    worst=population.getWorstTour().cost) )
+        self.evaluations += self.offspring_size
+
+        # tiempo para iteraciones y condicion de termino por tiempo
+        start = end = timer()
+        if not self.options.silent: # si esta o no el modo silencioso que muestra los cambios en cada iteracion
+            print(f"{bcolors.HEADER}\nEjecutando Algoritmo Genetico...\n{bcolors.ENDC}")
+
+
+        # Bucle principal del algoritmo
+        while (self.terminationCondition(self.iterations, self.evaluations, end-start)):
+
+            details = '' # variable de texto con los detalles 
+               
+            # Aplicar cruzamiento para generar poblacion de hijos
+            while (offspring.pop_size < self.offspring_size):
+                parents = population.selectParents(self.pselection_type)
+                offspring.add( population.crossover(parents, self.crossover_type) )
+            
+            # Aplicar mutacion
+            offspring.mutation(self.mutation_prob, self.mutation_type)
+
+        
+            # Revisar si algun hijo es la mejor solucion hasta el momento
+            if (offspring.getBestTour().cost < self.best_tour.cost):
+
+                details = f"{bcolors.OKGREEN} Mejor actual: {self.best_tour.cost} -> {offspring.getBestTour().cost} ¡Actualizado!{bcolors.ENDC}"
+                    
+                self.best_tour.copy(offspring.getBestTour())
+                
+                # Guardar trayectoria
+                self.trajectory.append( Trajectory(tour=self.best_tour.current.copy(),
+                                        cost=self.best_tour.cost, iterations=self.iterations, evaluations=self.evaluations,
+                                        average=population.getAverage(), deviation=population.getDeviation(),
+                                        worst=population.getWorstTour().cost) ) 
+                
+            else: 
+                
+                details = f"{bcolors.OKBLUE} Mejor actual: {self.best_tour.cost}{bcolors.ENDC}"
+
+            # Agregar la informacion de la iteracion a la tabla la tabla
+            table.add_row([f"{bcolors.BOLD}{self.iterations}", 
+                        f"{self.evaluations}", 
+                        f"{end-start:.4f}", 
+                        f"{offspring.getBestTour().cost}", 
+                        f"{offspring.getAverage():.2f}", 
+                        f"{offspring.getDeviation():.2f}{bcolors.ENDC}", 
+                        f"{details}"
+                        ])
+
+            # Seleccionar nueva poblacion
+            if (self.selection_strategy == SelectionStrategy.MULAMBDA):
+                # Seleccionar solo desde los hijos
+                if (self.offspring_size > self.pop_size):
+                    # Seleccionar desde los hijos
+                    offspring.selectPopulation(self.pop_size, self.gselection_type)
+                
+                population.copy(offspring)
+            elif (self.selection_strategy == SelectionStrategy.MUPLUSLAMBDA):
+                # Seleccionar desde los hijos y los padres al unir ambas poblaciones (hijos y padres)
+                offspring.joinPopulation(population)
+                # Seleccionar desde estas poblaciones
+                offspring.selectPopulation(self.pop_size, self.gselection_type)
+                population.copy(offspring)            
+                    
+            # Actualizar contadores de iteraciones y evaluaciones, luego limpiar la poblacion de hijos
+            self.evaluations += self.offspring_size
+            self.iterations += 1
+            offspring.clear()
+            end = timer() # tiempo actual de iteracion
+
+        # Mostrar la tabla con toda la informacion de la ejecucion del algoritmo
+        if not self.options.silent:
+            print(table)
+
+
+        # actualizar tiempo total de busqueda de Algoritmo Genetico
+        self.total_time = timer() - start
+
+
+    def terminationCondition(self, iterations: int, evaluations: int, time: float) -> bool:
+        """ Condicion de termino para el ciclo principal de Algoritmo Genetico, 
+        basado en los criterios de iteraciones, evaluaciones y tiempo, devuelve verdadero o falso si se debe continuar o no"""
+        
+        # Criterio de termino de las iteraciones
+        if (self.options.max_iterations > 0):
+            if (iterations > self.options.max_iterations):
+                return False
+        # Criterio de termino de las evaluciones
+        if (self.options.max_evaluations > 0):
+            if (evaluations > self.options.max_evaluations):
+                return False
+        # Criterio de termino por tiempo
+        if (self.options.max_time > 0):
+            if (time > self.options.max_time):
+                return False
+        
+        return True
+
+    def printSolFile(self, outputSol: str) -> None:
+        """ Guarda la solucion en archivo de texto"""
+        utilities.printSolToFile(outputSol, self.best_tour.current)
+
+    def printTraFile(self, outputTra: str) -> None:
+        """ Guarda la trayectoria de la solucion en archivo de texto"""
+        utilities.printTraToFile(outputTra, self.trajectory)
+
+    def updateLog(self) -> None:
+        """ Actualiza el registro de mejores soluciones con todas las caracteristicas de su ejecución """
+        # crea la carpeta en caso de que no exista (python 3.5+)
+        Path("log/").mkdir(exist_ok=True)
+        logFile = "log/GAlog.csv"
+        # usar el archivo en modo append
+        with open(logFile, "a", newline="\n") as csvfile:
+            
+            print(f"{bcolors.OKGREEN}\nActualizando log con mejores soluciones en archivo... {bcolors.ENDC}{path.abspath(logFile)}")
+            # Headers
+            fields = ["solution","cost","instance","date","pop_size","offspring_size", 
+                     "pselection_type","crossover_type","mutation_type","mutation_prob", 
+                     "selection_strategy","gselection_type","seed","move","max_evaluations",
+                     "max_iterations","max_time","initial_solution"]
+
+            writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=fields)
+            # Si la posicion de el archivo es cero se escriben los headers
+            if not csvfile.tell():
+                writer.writeheader()
+
+            # crear texto con la solucion separando cada elemento con espacios y luego guardarlo en el archivo
+            sol = " ".join([str(elem) for elem in self.best_tour.current])
+
+            # escribir la mejor solucion y todas las caracteristicas de su ejecucion
+            writer.writerow({
+                "solution": sol, 
+                "cost": self.best_tour.cost, 
+                "instance": self.options.instance, 
+                "date": datetime.today(), 
+                "pop_size": self.options.pop_size, 
+                "offspring_size": self.options.offspring_size, 
+                "pselection_type": self.options.pselection_type.value, 
+                "crossover_type": self.options.crossover_type.value, 
+                "mutation_type": self.options.mutation_type.value,
+                "mutation_prob": self.options.mutation_prob, 
+                "selection_strategy": self.options.selection_strategy.value, 
+                "gselection_type": self.options.gselection_type.value, 
+                "seed": self.options.seed, 
+                "move": self.options.move.value, 
+                "max_evaluations": self.options.max_evaluations,
+                "max_iterations": self.options.max_iterations, 
+                "max_time": self.options.max_time,
+                "initial_solution": self.options.initial_solution.value
+            })
+    
+
+    def visualize(self) -> None:
+        """ Visualiza la trayectoria de la solucion """
+        plot.Graph.replit = self.options.replit
+        plot.Graph.trajectory = self.trajectory
+        
+        plot.show(self.options.gui)
