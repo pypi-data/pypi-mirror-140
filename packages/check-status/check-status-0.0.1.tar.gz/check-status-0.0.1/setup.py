28c3216ca6408a7e76832a7a8e7f3e1419b58f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:34 +0000

    Add package check-status/0.0.1/check-status-0.0.1.tar.gz

---
+++ packages/check-status/check-status-0.0.1.tar.gz/check-status-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="check-status",
+    version='0.0.1',
+    author='kane.zhu',
+    author_email='kane.zhu@aliyun.com',
+    url='https://www.socialepoch.com',
+    description='check some status then return boolean',
+
+)
