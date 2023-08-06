731029f0dc0ee0f71803ff7ad241c4c6507e2c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:40 +0000

    Add package pyotb/1.3.1/pyotb-1.3.1.tar.gz

---
+++ packages/pyotb/pyotb-1.3.1.tar.gz/pyotb-1.3.1/pyotb/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+__version__ = "1.3"
+
+from .apps import *
+from .core import App, Output, Input, get_nbchannels, get_pixel_type
+from .functions import *
+from .tools import logger
