61b305893d8665734231175e85fccf305c5921a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:47 +0000

    Add package datadog-api-client/1.10.0/datadog_api_client-1.10.0-py3-none-any.whl

---
+++ packages/datadog-api-client/datadog_api_client-1.10.0-py3-none-any.whl/datadog_api_client/v1/model/geomap_widget_definition_style.py
@@ -0,0 +1,57 @@
+# Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2019-Present Datadog, Inc.
+
+
+from datadog_api_client.v1.model_utils import (
+    ModelNormal,
+    cached_property,
+)
+
+
+class GeomapWidgetDefinitionStyle(ModelNormal):
+
+    validations = {}
+
+    @cached_property
+    def openapi_types():
+        return {
+            "palette": (str,),
+            "palette_flip": (bool,),
+        }
+
+    attribute_map = {
+        "palette": "palette",
+        "palette_flip": "palette_flip",
+    }
+
+    read_only_vars = {}
+
+    def __init__(self, palette, palette_flip, *args, **kwargs):
+        """
+        The style to apply to the widget.
+
+        :param palette: The color palette to apply to the widget.
+        :type palette: str
+
+        :param palette_flip: Whether to flip the palette tones.
+        :type palette_flip: bool
+        """
+        super().__init__(kwargs)
+
+        self._check_pos_args(args)
+
+        self.palette = palette
+        self.palette_flip = palette_flip
+
+    @classmethod
+    def _from_openapi_data(cls, palette, palette_flip, *args, **kwargs):
+        """Helper creating a new instance from a response."""
+
+        self = super(GeomapWidgetDefinitionStyle, cls)._from_openapi_data(kwargs)
+
+        self._check_pos_args(args)
+
+        self.palette = palette
+        self.palette_flip = palette_flip
+        return self
---
+++ packages/datadog-api-client/datadog_api_client-1.10.0-py3-none-any.whl/datadog_api_client/v1/model/synthetics_assertion_json_path_target_target.py
@@ -0,0 +1,70 @@
+# Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+# This product includes software developed at Datadog (https://www.datadoghq.com/).
+# Copyright 2019-Present Datadog, Inc.
+
+
+from datadog_api_client.v1.model_utils import (
+    ModelNormal,
+    cached_property,
+    date,
+    datetime,
+    none_type,
+)
+
+
+class SyntheticsAssertionJSONPathTargetTarget(ModelNormal):
+
+    validations = {}
+
+    @cached_property
+    def openapi_types():
+        return {
+            "json_path": (str,),
+            "operator": (str,),
+            "target_value": (
+                bool,
+                date,
+                datetime,
+                dict,
+                float,
+                int,
+                list,
+                str,
+                none_type,
+            ),
+        }
+
+    attribute_map = {
+        "json_path": "jsonPath",
+        "operator": "operator",
+        "target_value": "targetValue",
+    }
+
+    read_only_vars = {}
+
+    def __init__(self, *args, **kwargs):
+        """
+        Composed target for `validatesJSONPath` operator.
+
+        :param json_path: The JSON path to assert.
+        :type json_path: str, optional
+
+        :param operator: The specific operator to use on the path.
+        :type operator: str, optional
+
+        :param target_value: The path target value to compare to.
+        :type target_value: bool, date, datetime, dict, float, int, list, str, none_type, optional
+        """
+        super().__init__(kwargs)
+
+        self._check_pos_args(args)
+
+    @classmethod
+    def _from_openapi_data(cls, *args, **kwargs):
+        """Helper creating a new instance from a response."""
+
+        self = super(SyntheticsAssertionJSONPathTargetTarget, cls)._from_openapi_data(kwargs)
+
+        self._check_pos_args(args)
+
+        return self
