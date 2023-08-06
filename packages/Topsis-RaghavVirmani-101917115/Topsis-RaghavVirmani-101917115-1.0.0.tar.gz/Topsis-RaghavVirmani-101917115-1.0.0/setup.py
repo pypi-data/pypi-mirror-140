bdce1ae8f8b22445d8b7c4687bc66fdf19614da1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:53 +0000

    Add package Topsis-RaghavVirmani-101917115/1.0.0/Topsis-RaghavVirmani-101917115-1.0.0.tar.gz

---
+++ packages/Topsis-RaghavVirmani-101917115/Topsis-RaghavVirmani-101917115-1.0.0.tar.gz/Topsis-RaghavVirmani-101917115-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+    name='Topsis-RaghavVirmani-101917115',
+    packages=['Topsis-RaghavVirmani-101917115'],
+    version='1.0.0',
+    license='MIT',
+    description='PredAnalysis Assignment 4',
+    author='Raghav Virmani',
+    author_email='rvirmani_be19@thapar.edu',
+    url='',
+    download_url='',
+    keywords=[],
+    install_requires=[
+        'validators',
+        'beautifulsoup4',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
