e6f1d5dd5bb9111ba130bb1035b9b409cdb90429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:50 +0000

    Add package pyalvarockcl/1.23/pyalvarockcl-1.23.tar.gz

---
+++ packages/pyalvarockcl/pyalvarockcl-1.23.tar.gz/pyalvarockcl-1.23/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='pyalvarockcl',
+      version='1.23',
+      description='version 1.23',
+      url='https://github.com/DataBeamCL/pyalvarockcl',
+      author='Alvaro Fuentes',
+      author_email='alvarofue@gmail.com',
+      license='Apache 2.0',
+      packages=['pyalvarockcl'],
+      zip_safe=False,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      keywords='test lib',
+      classifiers=sorted([
+            'Programming Language :: Python :: 3.10',
+      ]),    
+)
