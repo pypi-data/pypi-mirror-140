8b1cef3571a9d76096d0a149be19086fb19bad24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:32 +0000

    Add package taskel/0.0.3/taskel-0.0.3.tar.gz

---
+++ packages/taskel/taskel-0.0.3.tar.gz/taskel-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='taskel',
+      version='0.0.3',
+      description='The python library for all types of tasks',
+      long_description="Readme.md",
+      url='http://github.com/Ehnryu/taskel',
+      author='Ehnryu/Sakurai07',
+      author_email='blzzardst0rm@gmail.com',
+      license='MIT',
+      packages=['taskel'],
+    keywords=['taskel', 'tasks',"threading"])
