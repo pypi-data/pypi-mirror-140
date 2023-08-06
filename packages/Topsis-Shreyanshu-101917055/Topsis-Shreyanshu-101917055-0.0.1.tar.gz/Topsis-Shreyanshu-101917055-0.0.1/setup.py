67185ddc48dadebbafe6bb8ad2e538545b111db4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:43 +0000

    Add package Topsis-Shreyanshu-101917055/0.0.1/Topsis-Shreyanshu-101917055-0.0.1.tar.gz

---
+++ packages/Topsis-Shreyanshu-101917055/Topsis-Shreyanshu-101917055-0.0.1.tar.gz/Topsis-Shreyanshu-101917055-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Topsis-Shreyanshu-101917055",
+    version="0.0.1",
+    license="MIT",
+    description="A Python package to find TOPSIS for multi-criteria decision analysis method",
+    author="Shreyanshu Kashyap",
+    author_email="skashyap_be19@thapar.edu",
+    url="",
+    keywords=[],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['pandas', 'tabulate'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
+)
