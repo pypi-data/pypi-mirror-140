ab2f7ab49efe9abc03ad6a11c51424a99040886d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:23 +0000

    Add package ffmpeg-downloader/0.1.4/ffmpeg_downloader-0.1.4-py3-none-any.whl

---
+++ packages/ffmpeg-downloader/ffmpeg_downloader-0.1.4-py3-none-any.whl/ffmpeg_downloader/_linux.py
@@ -0,0 +1,55 @@
+from tempfile import TemporaryDirectory
+from ._download_helper import download_info, download_file, chmod
+import re, tarfile, os, shutil
+from os import path
+
+home_url = "https://johnvansickle.com/ffmpeg"
+
+
+def get_version():
+
+    return re.search(
+        r"version: (\d+\.\d+(?:\.\d+)?)",
+        download_info(f"{home_url}/release-readme.txt", "text/plain", 10),
+    )[1]
+
+
+def download_n_install(install_dir, progress=None, arch=None):
+    archs = ("amd64", "i686", "arm64", "armhf", "armel")
+    if arch is None:
+        arch = archs[0]
+    elif arch not in archs:
+        raise ValueError(f"Invalid arch specified. Must be one of {arch}")
+
+    with TemporaryDirectory() as tmpdir:
+        tarpath = path.join(tmpdir, "ffmpeg_linux.tar.xz")
+        url = f"{home_url}/releases/ffmpeg-release-{arch}-static.tar.xz"
+
+        with TemporaryDirectory() as tmpdir:
+            download_file(
+                tarpath, url, "application/x-xz", progress=progress, timeout=10
+            )
+
+        with tarfile.open(tarpath, "r") as f:
+            f.extractall(tmpdir)
+
+        _, (src_dir_name, *_), _ = next(os.walk(tmpdir))
+        src_dir_path = path.join(tmpdir, src_dir_name)
+
+        with open(path.join(src_dir_path, "VERSION"), "wt") as f:
+            f.write(get_version())
+
+        try:
+            shutil.rmtree(install_dir)
+        except:
+            pass
+
+        # os.makedirs(install_dir, exist_ok=True)
+        shutil.move(src_dir_path, install_dir)
+
+    for cmd in ("ffmpeg", "ffprobe"):
+        chmod(path.join(install_dir, cmd))
+
+
+def get_bindir(install_dir):
+    return install_dir
---
+++ packages/ffmpeg-downloader/ffmpeg_downloader-0.1.4-py3-none-any.whl/ffmpeg_downloader/_macos.py
@@ -0,0 +1,62 @@
+from tempfile import TemporaryDirectory
+from os import path
+import json, os, zipfile, shutil
+from ._download_helper import download_info, download_file, chmod, download_base
+
+home_url = "https://evermeet.cx/ffmpeg"
+
+
+def get_version():
+    return json.loads(
+        download_info(f"{home_url}/info/ffmpeg/release", "application/json")
+    )["version"]
+
+
+def download_n_install(install_dir, progress=None):
+
+    ntotal = 0
+
+    def get_nbytes(cmd):
+        response = download_base(f"{home_url}/getrelease/{cmd}/zip", "application/zip")
+        nbytes = int(response.headers["content-length"])
+        response.close()
+        return nbytes
+
+    nfiles = [get_nbytes(cmd) for cmd in ("ffmpeg", "ffprobe")]
+    ntotal = sum(nfiles)
+    n1 = nfiles[0]
+    prog = (
+        lambda nread, nbytes: progress((nread + n1) if nbytes != n1 else nread, ntotal)
+        if progress is not None
+        else None
+    )
+
+    with TemporaryDirectory() as tmpdir:
+
+        for cmd in ("ffmpeg", "ffprobe"):
+            zippath = path.join(tmpdir, f"{cmd}.zip")
+            download_file(
+                zippath,
+                f"{home_url}/getrelease/{cmd}/zip",
+                "application/zip",
+                progress=prog,
+            )
+
+            with zipfile.ZipFile(zippath, "r") as f:
+                f.extractall(tmpdir)
+
+            dst_path = path.join(install_dir, cmd)
+            try:
+                os.remove(dst_path)
+            except:
+                pass
+            os.makedirs(install_dir, exist_ok=True)
+            shutil.move(path.join(tmpdir, cmd), dst_path)
+            chmod(dst_path)
+
+        with open(path.join(install_dir, "VERSION"), "wt") as f:
+            f.write(get_version())
+
+
+def get_bindir(install_dir):
+    return install_dir
