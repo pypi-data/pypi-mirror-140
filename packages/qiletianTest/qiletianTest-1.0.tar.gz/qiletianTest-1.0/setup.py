188cbc14a8581ea852a35e87c6dcedef634c6af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:12 +0000

    Add package qiletianTest/1.0/qiletianTest-1.0.tar.gz

---
+++ packages/qiletianTest/qiletianTest-1.0.tar.gz/qiletianTest-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='qiletianTest',
+    version='1.0',
+    description='这是一个测试',
+    author = 'qiletian',
+    author_email='1452239845@qq.com',
+    py_modules=['qiletianTest.demo1','qiletianTest.demo2'],
+)
