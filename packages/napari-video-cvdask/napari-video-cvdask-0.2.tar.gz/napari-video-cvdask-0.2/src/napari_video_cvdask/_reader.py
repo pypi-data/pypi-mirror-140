84c2399d9b5e2abe8c844ca946050231e49d8e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:37 +0000

    Add package napari-video-cvdask/0.2/napari-video-cvdask-0.2.tar.gz

---
+++ packages/napari-video-cvdask/napari-video-cvdask-0.2.tar.gz/napari-video-cvdask-0.2/src/napari_video_cvdask/_reader.py
@@ -0,0 +1,19 @@
+from typing import Tuple, List, Dict, Union
+
+from dask_image.imread import imread
+
+
+def napari_get_reader(path):
+    filenames = [path] if isinstance(path, str) else path
+    # if we know we cannot read all the files, we immediately return None.
+    for filename in filenames:
+        if not any([filename.endswith(ext) for ext in [".mp4", ".mov", ".avi"]]):
+            return None
+    return reader_function
+
+
+def reader_function(path: Union[str, List[str]]) -> List[Tuple[Array, Dict, str]]:
+    """Take a path or list of paths and return a list of LayerData tuples."""
+    filenames = [path] if isinstance(path, str) else path
+    return [(imread(filename), {}, "image") for filename in filenames]
+
