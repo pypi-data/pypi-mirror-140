23036fd915388f8432b06b2e54922aa1bba1adc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:56 +0000

    Add package jaye/0.0.10/jaye-0.0.10.tar.gz

---
+++ packages/jaye/jaye-0.0.10.tar.gz/jaye-0.0.10/jaye/Jaye.py
@@ -0,0 +1,29 @@
+# output : pd.DataFrame
+# index : datetime
+# dtype : float64
+
+# from datetime import datetime, date, time, timedelta
+# from dateutil.parser import parse
+# from pytz import timezone, utc
+# from collections import namedtuple
+from .applications import *
+
+
+import json
+import requests
+import numpy as np
+import pandas as pd
+import os
+from .settings import *
+
+
+class Jaye:
+    def __init__(self):
+        self.account = Account(self)
+        self.analysis = Analysis()
+        self.trade = {}
+        self.simulation = Simulation()
+        self.version = '0.0.10'
+
+
+
