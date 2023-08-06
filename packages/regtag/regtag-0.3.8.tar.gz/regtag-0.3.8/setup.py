2c0d2b089b0d016469344871ff52a0f3c398e169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:56 +0000

    Add package regtag/0.3.8/regtag-0.3.8.tar.gz

---
+++ packages/regtag/regtag-0.3.8.tar.gz/regtag-0.3.8/setup.py
@@ -0,0 +1,47 @@
+import pathlib
+from setuptools import setup
+from setuptools import find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="regtag",
+    version="0.3.8",
+    description="Regex Tagging",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nguyenvulebinh/regtag",
+    author="Binh Nguyen",
+    author_email="nguyenvulebinh@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["regtag"],
+    package_dir={"": "src"},   # tell distutils packages are under src
+    include_package_data=True,
+    install_requires=["nltk>=3.6.5", "validators>=0.18.2"],
+    package_data={
+        # If any package contains *.txt files, include them:
+        "": ["*.txt"],
+        # And include any *.dat files found in the "data" subdirectory
+        # of the "mypkg" package, also:
+        "regtag": ["vidict.txt", 'abb_dict.json', 'en_dict.json', 'domain_extension.json', 'general_word_en.txt',
+                   'domain_names.json'],
+    }
+    # python -m build
+    # twine upload dist/*
+    # entry_points={
+    #     "console_scripts": [
+    #         "realpython=visen.__main__:main",
+    #     ]
+    # },
+
+)
