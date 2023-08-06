696ddfabda455eb2eadac7cdaacd3b5b8da8bb2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:31 +0000

    Add package auto-causality/0.0.1/auto-causality-0.0.1.tar.gz

---
+++ packages/auto-causality/auto-causality-0.0.1.tar.gz/auto-causality-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="auto-causality",
+    version="0.0.1",
+    description="AutoML for Causal Inference.",
+    long_description="# auto-causality",
+    long_description_content_type='text/markdown',
+    author="Wise",
+    url='https://github.com/transferwise/auto-causality',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+    ],
+    install_requires=[],
+    extras_require={},
+    keywords='automl',
+)
