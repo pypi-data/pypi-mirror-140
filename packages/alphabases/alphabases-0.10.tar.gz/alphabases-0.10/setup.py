a25c23a27b9fef12c44886af0174c78cbc73e3c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:43 +0000

    Add package alphabases/0.10/alphabases-0.10.tar.gz

---
+++ packages/alphabases/alphabases-0.10.tar.gz/alphabases-0.10/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+VERSION = "0.10"
+
+setup(
+    author="Ricardo",
+    author_email="gzsushixuan@corp.netease.com",
+    description="SDK of alpha@gdc group.",
+    name="alphabases",
+    packages=find_packages(),
+    zip_safe=False,
+    version=VERSION,
+    install_requires = [
+        "blackhole-sdk",
+        "requests",
+        "lxml",
+        "proc"
+    ],
+)
+
+
