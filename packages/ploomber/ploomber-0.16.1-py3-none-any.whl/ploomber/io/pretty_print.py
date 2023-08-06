a3b10e8ac5a2eaaec63531c1c5f862a886248fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:55 +0000

    Add package ploomber/0.16.1/ploomber-0.16.1-py3-none-any.whl

---
+++ packages/ploomber/ploomber-0.16.1-py3-none-any.whl/ploomber/io/pretty_print.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+
+def iterable(obj):
+    sorted_ = sorted(f"'{element}'" for element in obj)
+
+    if len(sorted_) > 1:
+        sorted_[-1] = f'and {sorted_[-1]}'
+
+    return ", ".join(sorted_)
+
+
+def them_or_name(obj):
+    return 'them' if len(obj) > 1 else f"'{list(obj)[0]}'"
+
+
+def trailing_dot(obj):
+    return ' '.join(f'{e}.' for e in obj)
+
+
+def try_relative_path(path):
+    if not isinstance(path, (str, Path)):
+        return path
+
+    path_ = Path(path)
+
+    if path_.is_absolute():
+        try:
+            return str(path_.relative_to(Path().resolve()))
+        except ValueError:
+            return str(path)
+    else:
+        return str(path)
