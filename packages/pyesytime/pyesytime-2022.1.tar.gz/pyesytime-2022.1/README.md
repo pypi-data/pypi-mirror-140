5214434e995544f9ee28faed40a4f23a604e2f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:02 +0000

    Add package pyesytime/2022.1/pyesytime-2022.1.tar.gz

---
+++ packages/pyesytime/pyesytime-2022.1.tar.gz/pyesytime-2022.1/README.md
@@ -0,0 +1,35 @@
+## PyesyTime
+
+### 一个让时间更好获取的模块
+
+#### 注意：最新版本2022.1
+
+##### 0.名称
+
+Pyesytime
+
+python easy time
+
+##### 1.下载
+
+pip install pyesytime
+
+##### 2.导入
+
+import pyesytime
+
+##### 3.函数
+
+ALL_IN_ONE()
+
+YEAR_MONTH_DAY_LINE()
+
+YEAR_MONTH_DAY_DIAGONAL()
+
+HOUR_MINUTE_SECOND_24_COLON()
+
+HOUR_MINUTE_SECOND_12_COLON()
+
+ONLY_HOUR_24()
+
+ONLY_HOUR_12()
---
+++ packages/pyesytime/pyesytime-2022.1.tar.gz/pyesytime-2022.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*- 
+import setuptools
+with open("README.md", "r",encoding='utf-8') as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="pyesytime",  # 模块名称
+    version="2022.1",  # 当前版本
+    author="SunnyLi",  # 作者
+    author_email="5327136@qq.com",  # 作者邮箱
+    description="一个让时间变得更简易的模块",  # 模块简介
+    long_description=long_description,  # 模块详细介绍
+    long_description_content_type="text/markdown",  # 模块详细介绍格式
+    # url="https://github.com/wupeiqi/fucker",  # 模块github地址
+    packages=setuptools.find_packages(),  # 自动找到项目中导入的模块
+    # 模块相关的元数据
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # 依赖模块
+    install_requires=[
+        
+    ],
+    python_requires='>=3',
+)
