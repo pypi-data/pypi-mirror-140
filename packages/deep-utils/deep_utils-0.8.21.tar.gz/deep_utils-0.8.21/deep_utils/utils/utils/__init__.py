6154e14fe556381b089c1afbc3c9e93938d65496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:07 +0000

    Add package deep-utils/0.8.21/deep_utils-0.8.21.tar.gz

---
+++ packages/deep-utils/deep_utils-0.8.21.tar.gz/deep_utils-0.8.21/deep_utils/utils/utils/__init__.py
@@ -0,0 +1,7 @@
+from .main import shift_lst, dictnamedtuple, easy_argparse
+from .pickles import dump_pickle, load_pickle
+from .unzip import unzip
+from .str_utils import color_str
+from .seeds import tf_set_seed
+from .logging_ import get_logger, log_print, value_error_log, get_conf_matrix, get_cls_report, save_params
+from .json_utils import load_json, dump_json
