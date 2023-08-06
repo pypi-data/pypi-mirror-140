7183da8e0d49504680b1b99f52ca795d9780ace5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:28 +0000

    Add package django-dkutils/1.2.0/django_dkutils-1.2.0-py3-none-any.whl

---
+++ packages/django-dkutils/django_dkutils-1.2.0-py3-none-any.whl/dkutils/mixins.py
@@ -0,0 +1,12 @@
+import waffle
+from .exceptions import ServiceUnavailable
+
+
+class FlagMixin:
+    flags = []
+
+    def dispatch(self, request, *args, **kwargs):
+        for flag in self.flags:
+            if not waffle.switch_is_active(flag):
+                raise ServiceUnavailable()
+        return super().dispatch(request, *args, **kwargs)
