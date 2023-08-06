d91e03a681305472d21fcf6eb0be57bbac296db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:55 +0000

    Add package miauth/0.9.3/miauth-0.9.3.tar.gz

---
+++ packages/miauth/miauth-0.9.3.tar.gz/miauth-0.9.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='miauth',
+    version='0.9.3',
+    url='https://github.com/dnandha/miauth',
+    license='GNU AGPL v3',
+    author='Daljeet Nandha',
+    author_email='dalj337@gmail.com',
+    description='Authenticate and interact with Xiaomi devices over BLE',
+    packages=find_packages(include=['miauth', 'miauth.*']),
+    entry_points={
+        'console_scripts': ['miauth=miauth.cli:main']
+    },
+    install_requires=[
+        'cryptography'
+    ],
+    extras_require={
+        'cli': [
+            'bluepy==1.3.0'
+        ]
+    },
+    python_requires=">=3.6",
+)
