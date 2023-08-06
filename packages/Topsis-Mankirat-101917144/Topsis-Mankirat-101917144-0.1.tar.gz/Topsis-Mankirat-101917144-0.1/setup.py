dd9b18e7020ca605679f7f418fe7c374d6c19f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:42 +0000

    Add package Topsis-Mankirat-101917144/0.1/Topsis-Mankirat-101917144-0.1.tar.gz

---
+++ packages/Topsis-Mankirat-101917144/Topsis-Mankirat-101917144-0.1.tar.gz/Topsis-Mankirat-101917144-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+  name = 'Topsis-Mankirat-101917144',        
+  version = '0.1',      
+  license='MIT',        
+  description = 'Topsis implementation / package creation',  
+  author = 'Mankirat Kaur',                   
+  author_email = 'mkaur_be19@thapar.edu',
+  url = '',     
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License', 
+    'Programming Language :: Python :: 3',     
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
