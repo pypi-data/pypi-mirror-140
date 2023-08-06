be863e9a888640238320ecc973e9168d9ad96e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:05 +0000

    Add package anilist-man/0.2.6/anilist-man-0.2.6.tar.gz

---
+++ packages/anilist-man/anilist-man-0.2.6.tar.gz/anilist-man-0.2.6/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['anilist_man']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.27.1,<3.0.0', 'typer[all]>=0.4.0,<0.5.0']
+
+entry_points = \
+{'console_scripts': ['anilist-man = anilist_man.main:app']}
+
+setup_kwargs = {
+    'name': 'anilist-man',
+    'version': '0.2.6',
+    'description': '',
+    'long_description': '# AniCli\nA python command-line tool for AniList , made by [@ayushsehrawat](https://github.com/AyushSehrawat) in Typer.\n\n---\n\n## Setup/Installation\n\nSoon\n\n---\n\n#### Todo\n\n- [ ] Add more support and commands \n- [ ] Register on pypi ( python main.py -> some_command )\n\n#### Using of Code\nIf you want to use this code , you can use it in your project but you must give credit to this repo.',
+    'author': 'Mini',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
