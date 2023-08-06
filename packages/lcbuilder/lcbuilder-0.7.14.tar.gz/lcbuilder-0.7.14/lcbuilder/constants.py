bae4cd31f6109323b19666657c534437c4d993bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:19:57 +0000

    Add package lcbuilder/0.7.14/lcbuilder-0.7.14.tar.gz

---
+++ packages/lcbuilder/lcbuilder-0.7.14.tar.gz/lcbuilder-0.7.14/lcbuilder/constants.py
@@ -0,0 +1,7 @@
+import os
+
+USER_HOME_ELEANOR_CACHE = os.path.join(os.path.expanduser('~'), '.eleanor/')
+USER_HOME_LIGHTKURVE = os.path.join(os.path.expanduser('~'), '.lightkurve-cache/')
+LIGHTKURVE_CACHE_DIR = ".lightkurve-cache/"
+ELEANOR_CACHE_DIR = ".eleanor/"
+CUTOUT_SIZE = 11
