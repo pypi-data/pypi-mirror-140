7e3d67ec48c43afcf03cb4c76f2ff01ce1515ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:45 +0000

    Add package Accuinsight/1.0.83/Accuinsight-1.0.83-py2.py3-none-any.whl

---
+++ packages/Accuinsight/Accuinsight-1.0.83-py2.py3-none-any.whl/Accuinsight/modeler/core/Lifecycle.py
@@ -0,0 +1,17 @@
+from Accuinsight.Lifecycle import keras
+from Accuinsight.Lifecycle import tensorflow
+
+
+class Lifecycle:
+    """
+    The exposed class to user
+    """
+    def __init__(self):
+        self.accu_keras = keras.accuinsight()
+        self.accu_tensorflow = tensorflow.accuinsight()
+
+    def run_keras(self, tag=None):
+        self.accu_keras.autolog(tag)
+
+    def run_tensorflow(self, tag=None):
+        self.accu_tensorflow.autolog(tag)
