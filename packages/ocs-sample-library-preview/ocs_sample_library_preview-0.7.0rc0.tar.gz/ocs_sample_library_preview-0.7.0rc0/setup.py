8bb1288b1bdfa2d6267843e689c73f78da22bfc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:19:20 +0000

    Add package ocs-sample-library-preview/0.7.0rc0/ocs_sample_library_preview-0.7.0rc0.tar.gz

---
+++ packages/ocs-sample-library-preview/ocs_sample_library_preview-0.7.0rc0.tar.gz/ocs_sample_library_preview-0.7.0rc0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='ocs_sample_library_preview',
+    version='0.7.0_preview',
+    author='OSIsoft',
+    license='Apache 2.0',
+    author_email='dendres@osisoft.com',
+    description='A preview of an ADH (Aveva Data Hub) client library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/osisoft/sample-ocs-sample_libraries-python',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests>=2.27.1',
+        'python-dateutil>=2.8.2',
+        'jsonpatch>=1.32'
+    ],
+    python_requires='>=3.7',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+)
