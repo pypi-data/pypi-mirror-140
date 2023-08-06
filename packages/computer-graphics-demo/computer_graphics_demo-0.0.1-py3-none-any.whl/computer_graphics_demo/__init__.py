ab1ac06529fd2669f3f877d3e1e2e168d79d5e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:47:26 +0000

    Add package computer-graphics-demo/0.0.1/computer_graphics_demo-0.0.1-py3-none-any.whl

---
+++ packages/computer-graphics-demo/computer_graphics_demo-0.0.1-py3-none-any.whl/computer_graphics_demo/__init__.py
@@ -0,0 +1,32 @@
+"""Computer graphics algorithms demonstration."""
+import taichi as ti
+from typing import Final
+
+VERSION: Final = "0.0.1"
+pixels = ti.Vector.field(3, float)
+
+
+def init() -> None:
+    """init.
+
+    :rtype: None
+    """
+    pass
+
+
+@ti.pyfunc
+def setpixel(x: int, y: int, R: int, G: int, B: int):
+    """setpixel.
+
+    :param x:
+    :type x: int
+    :param y:
+    :type y: int
+    :param R:
+    :type R: int
+    :param G:
+    :type G: int
+    :param B:
+    :type B: int
+    """
+    pixels[x, y] = ti.Vector([R, G, B])
