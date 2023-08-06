9d60a5f2fc8514aaa46664180cd112ec17298571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:41 +0000

    Add package NOSMaker/0.1/NOSMaker-0.1.tar.gz

---
+++ packages/NOSMaker/NOSMaker-0.1.tar.gz/NOSMaker-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='NOSMaker',                    # package name
+    version='0.1',                          # version
+    description='Package Description',      # short description
+    url='http://0nera.github.io/NOSMaker',               # package URL
+    install_requires=[],                    # list of packages this package depends
+                                            # on.
+    packages=['NOSMaker'],              # List of module names that installing
+                                            # this package will provide.
+) 
