07ce878866e330fa2f9789f96e384265ef1de56e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:58 +0000

    Add package replstories/0.4.1/replstories-0.4.1.tar.gz

---
+++ packages/replstories/replstories-0.4.1.tar.gz/replstories-0.4.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='replstories',
+    packages=find_packages(),
+    version='0.4.1',
+    description='Repl Stories is a social media platform where developers can share code and connect with the community!',
+	url="https://api.replstories.xyz/",
+    author='STGdevelopers',
+    license='MIT',
+)
