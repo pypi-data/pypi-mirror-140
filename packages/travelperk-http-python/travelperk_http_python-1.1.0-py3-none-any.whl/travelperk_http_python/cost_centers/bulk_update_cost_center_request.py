414ce22e0d051a542a321e5a5da58e1ec3a6aedf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:46 +0000

    Add package travelperk-http-python/1.1.0/travelperk_http_python-1.1.0-py3-none-any.whl

---
+++ packages/travelperk-http-python/travelperk_http_python-1.1.0-py3-none-any.whl/travelperk_http_python/cost_centers/bulk_update_cost_center_request.py
@@ -0,0 +1,32 @@
+from typing import List, TYPE_CHECKING
+from .bulk_update_cost_center_input_params import BulkUpdateCostCenterInputParams
+from travelperk_python_api_types.cost_centers.cost_centers.bulk_update_response import (
+    BulkUpdateResponse,
+)
+from travelperk_http_python.dataclass_wrapper.dataclass_wrapper import DataclassWrapper
+
+if TYPE_CHECKING:
+    from api.travelperk import TravelPerk
+
+
+class BulkUpdateCostCenterRequest:
+    def __init__(self, travelperk: "TravelPerk"):
+        self.params = BulkUpdateCostCenterInputParams()
+        self.travelperk = travelperk
+
+    def save(self) -> BulkUpdateResponse:
+        return DataclassWrapper.wrap(
+            BulkUpdateResponse,
+            self.travelperk.patch(
+                "/".join(["cost_centers", "bulk_update"]),
+                self.params.to_dict(),
+            ),
+        )
+
+    def set_ids(self, ids: List[int]) -> "BulkUpdateCostCenterRequest":
+        self.params.set_ids(ids)
+        return self
+
+    def set_archive(self, archive: bool) -> "BulkUpdateCostCenterRequest":
+        self.params.set_archive(archive)
+        return self
