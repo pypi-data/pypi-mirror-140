cf0c589427ea06fe397e4a7fe636cc688f909ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:20 +0000

    Add package Topsis-Gitesh-101903031/0.0.4/Topsis-Gitesh-101903031-0.0.4.tar.gz

---
+++ packages/Topsis-Gitesh-101903031/Topsis-Gitesh-101903031-0.0.4.tar.gz/Topsis-Gitesh-101903031-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Topsis-Gitesh-101903031",
+    version="0.0.4",
+    author="Gitesh Garg",
+    author_email="giteshgarg2002a@gmail.com",
+    description="This package is for topsis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=['Topsis-Gitesh-101903031'],
+    include_package_data = True,
+    python_requires=">=3.6",
+)
