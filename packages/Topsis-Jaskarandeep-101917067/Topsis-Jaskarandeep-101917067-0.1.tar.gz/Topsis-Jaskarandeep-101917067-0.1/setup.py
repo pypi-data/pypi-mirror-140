343fbc4f110f55b1459c0288e076072ee7a3bfca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:40 +0000

    Add package Topsis-Jaskarandeep-101917067/0.1/Topsis-Jaskarandeep-101917067-0.1.tar.gz

---
+++ packages/Topsis-Jaskarandeep-101917067/Topsis-Jaskarandeep-101917067-0.1.tar.gz/Topsis-Jaskarandeep-101917067-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+	name = "Topsis-Jaskarandeep-101917067",
+    version = "0.1",
+    license = "MIT",
+    description = "A Python package to find TOPSIS for multi-criteria decision analysis method",
+    author = "Jaskarandeep Saini",
+    author_email = "jsaini_be19@thapar.edu",
+    install_requires = ['pandas'],
+    classifiers = [
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3' 
+    ],
+    packages = ["Topsis-Jaskarandeep-101917067"]
+)
