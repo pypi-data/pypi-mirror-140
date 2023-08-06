ed6d9f38e44b2cfaee03753d8e11d128b47f1e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:41 +0000

    Add package pynamodb-polymorph/0.3.3/pynamodb-polymorph-0.3.3.tar.gz

---
+++ packages/pynamodb-polymorph/pynamodb-polymorph-0.3.3.tar.gz/pynamodb-polymorph-0.3.3/src/pynamodb_polymorph/__init__.py
@@ -0,0 +1,13 @@
+from pynamodb_polymorph.custom_attr import (
+    CompoundTemplateAttribute,
+    CopiedDiscriminatorAttribute,
+    CopiedIntegerAttribute,
+    CopiedULIDAttribute,
+    CopiedUnicodeAttribute,
+    IsoDateTime,
+    JoinedUnicodeAttribute,
+    SetSizeAttribute,
+    ULIDAttribute,
+    UpdatedIsoDateTime,
+)
+from pynamodb_polymorph.zlib_attr import CompressedAttribute
