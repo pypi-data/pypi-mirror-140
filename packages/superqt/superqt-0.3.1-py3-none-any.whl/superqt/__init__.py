862bdbd47113a9752a7660efec91d746ac1c496c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:53 +0000

    Add package superqt/0.3.1/superqt-0.3.1-py3-none-any.whl

---
+++ packages/superqt/superqt-0.3.1-py3-none-any.whl/superqt/__init__.py
@@ -0,0 +1,38 @@
+"""superqt is a collection of QtWidgets for python."""
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
+
+from ._eliding_label import QElidingLabel
+from .collapsible import QCollapsible
+from .combobox import QEnumComboBox
+from .sliders import (
+    QDoubleRangeSlider,
+    QDoubleSlider,
+    QLabeledDoubleRangeSlider,
+    QLabeledDoubleSlider,
+    QLabeledRangeSlider,
+    QLabeledSlider,
+    QRangeSlider,
+)
+from .spinbox import QLargeIntSpinBox
+from .utils import QMessageHandler, ensure_main_thread, ensure_object_thread
+
+__all__ = [
+    "ensure_main_thread",
+    "ensure_object_thread",
+    "QDoubleRangeSlider",
+    "QDoubleSlider",
+    "QElidingLabel",
+    "QLabeledDoubleRangeSlider",
+    "QLabeledDoubleSlider",
+    "QLabeledRangeSlider",
+    "QLabeledSlider",
+    "QLargeIntSpinBox",
+    "QMessageHandler",
+    "QRangeSlider",
+    "QEnumComboBox",
+    "QCollapsible",
+]
