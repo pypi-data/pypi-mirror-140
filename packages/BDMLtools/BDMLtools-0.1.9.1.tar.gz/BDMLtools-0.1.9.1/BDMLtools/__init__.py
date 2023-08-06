3c3c52a4fe9484fc8d532e0c68d975dba404632d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:27 +0000

    Add package BDMLtools/0.1.9.1/BDMLtools-0.1.9.1.tar.gz

---
+++ packages/BDMLtools/BDMLtools-0.1.9.1.tar.gz/BDMLtools-0.1.9.1/BDMLtools/__init__.py
@@ -0,0 +1,55 @@
+# -*- coding:utf-8 -*- 
+
+from BDMLtools.clearner import outliersTransformer,dtypeAllocator,nanTransformer,dtStandardization
+from BDMLtools.report import businessReport,EDAReport,varReport,varGroupsReport,varReportSinge,varGroupsPlot
+from BDMLtools.selector import binSelector,binAdjuster,binFreq,binPretty,binTree,binChi2,binKmeans
+from BDMLtools.selector import faSelector
+from BDMLtools.selector import stepLogit,cardScorer
+from BDMLtools.selector import preSelector,corrSelector,prefitModel
+#from BDMLtools.selector import RFECVSelector
+from BDMLtools.selector import lassoSelector
+from BDMLtools.plotter import BaseWoePlotter
+from BDMLtools.plotter import perfEval
+from BDMLtools.encoder import woeTransformer
+from BDMLtools.tuner import girdTuner,hgirdTuner
+from BDMLtools.tuner import BayesianXGBTuner,BayesianLgbmTuner,shapCheck
+
+
+__version__ = '0.1.9.1'
+
+__all__ = (    
+    dtStandardization,
+    outliersTransformer,
+    dtypeAllocator,
+    nanTransformer,
+    businessReport,
+    prefitModel,
+    EDAReport,
+    varReportSinge,
+    varReport,
+    varGroupsReport,
+    varGroupsPlot,
+    binSelector,
+    binAdjuster,
+    binFreq,
+    binPretty,
+    binTree,
+    binChi2,
+    binKmeans,
+    BaseWoePlotter,
+    faSelector,
+    stepLogit,
+    cardScorer,
+    preSelector,
+    corrSelector,
+    lassoSelector,
+    woeTransformer,
+    girdTuner,
+    hgirdTuner,
+    BayesianXGBTuner,
+    BayesianLgbmTuner,
+    perfEval,
+    shapCheck
+)
+
+
---
+++ packages/BDMLtools/BDMLtools-0.1.9.1.tar.gz/BDMLtools-0.1.9.1/BDMLtools/selector/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 12 18:29:30 2022
+
+@author: zengke
+"""
+
+from BDMLtools.selector.bin import binSelector,binAdjuster
+from BDMLtools.selector.bin_fun import binFreq,binPretty,binTree,binChi2,binKmeans
+from BDMLtools.selector.fa import faSelector
+from BDMLtools.selector.embeded import lassoSelector
+from BDMLtools.selector.logtit import stepLogit,cardScorer
+from BDMLtools.selector.simple import prefitModel,preSelector,corrSelector
+#from BDMLtools.selector.wrapper import RFECVSelector
+
+
+__all__ = (    
+    binSelector,
+    binAdjuster,
+    binFreq,
+    binPretty,
+    binTree,
+    binChi2,
+    binKmeans,
+    faSelector,
+    lassoSelector,
+    stepLogit,
+    cardScorer,
+    prefitModel,
+    preSelector,
+    corrSelector
+)
+
