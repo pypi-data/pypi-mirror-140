dc10d90be5db7ad1afd24289ed1556030fe71828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:37 +0000

    Add package neural-lifetimes/0.0.1/neural-lifetimes-0.0.1.tar.gz

---
+++ packages/neural-lifetimes/neural-lifetimes-0.0.1.tar.gz/neural-lifetimes-0.0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="neural-lifetimes",
+    version="0.0.1",
+    description="User behavior prediction from event data.",
+    long_description="# Neural Lifetimes",
+    long_description_content_type='text/markdown',
+    author="Wise",
+    url='https://github.com/transferwise',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+    ],
+    install_requires=[],
+    extras_require={},
+)
