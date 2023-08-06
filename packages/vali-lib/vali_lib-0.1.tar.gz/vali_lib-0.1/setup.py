50a135247f6eaaf9b637e65a600e515fe6c8e12e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:14 +0000

    Add package vali-lib/0.1/vali_lib-0.1.tar.gz

---
+++ packages/vali-lib/vali_lib-0.1.tar.gz/vali_lib-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vali_lib',  # mandatory
+    version='0.1',  # mandatory
+    packages=find_packages(),  # mandatory
+    # optional parameters
+    description='This is a test project!',
+    author='eu',
+    author_email='eu@toteu.com',
+    url='https://google.com',
+    install_requires=['requests==2.24.0', 'pymongo', 'beautifulsoup4'],
+    python_requires='>=3.10',
+)
---
+++ packages/vali-lib/vali_lib-0.1.tar.gz/vali_lib-0.1/vali_lib/pack_a/find_index.py
@@ -0,0 +1,4 @@
+def find_index(colectie, element):
+    for index, value in enumerate(colectie):
+        if value == element:
+            return index
