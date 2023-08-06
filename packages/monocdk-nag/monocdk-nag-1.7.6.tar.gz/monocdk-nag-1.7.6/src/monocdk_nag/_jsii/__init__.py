4d12c7e1c7c04cf2c3c351cea6fce07ced728abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:10 +0000

    Add package monocdk-nag/1.7.6/monocdk-nag-1.7.6.tar.gz

---
+++ packages/monocdk-nag/monocdk-nag-1.7.6.tar.gz/monocdk-nag-1.7.6/src/monocdk_nag/_jsii/__init__.py
@@ -0,0 +1,23 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.core._jsii
+import constructs._jsii
+import monocdk._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "monocdk-nag", "1.7.6", __name__[0:-6], "monocdk-nag@1.7.6.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
