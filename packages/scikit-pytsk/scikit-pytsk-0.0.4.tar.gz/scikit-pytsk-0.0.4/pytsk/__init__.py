927794920d810afd0e2eaea94c624355484aff3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:53 +0000

    Add package scikit-pytsk/0.0.4/scikit-pytsk-0.0.4.tar.gz

---
+++ packages/scikit-pytsk/scikit-pytsk-0.0.4.tar.gz/scikit-pytsk-0.0.4/pytsk/__init__.py
@@ -0,0 +1,10 @@
+import importlib.util
+
+package_name = "torch"
+spec = importlib.util.find_spec(package_name)
+if spec is None:
+    print("Warning! PyTorch package is not installed, "
+          "this package must be installed when you need to use pytsk.gradient_descent")
+
+name = "pytsk"
+__version__ = "0.0.4"
