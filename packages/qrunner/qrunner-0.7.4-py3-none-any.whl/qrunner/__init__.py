87aad9c093e1c6ef23280c59fb55c0897a1d815c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:39 +0000

    Add package qrunner/0.7.4/qrunner-0.7.4-py3-none-any.whl

---
+++ packages/qrunner/qrunner-0.7.4-py3-none-any.whl/qrunner/__init__.py
@@ -0,0 +1,10 @@
+from qrunner.running.runner import main
+from qrunner.case import TestCase, Page, H5Page
+from qrunner.core.h5.driver import H5Driver
+from qrunner.decorate import *
+from qrunner.utils.log import logger
+
+
+__version__ = "0.7.4"
+__description__ = "全栈自动化测试框架"
+
