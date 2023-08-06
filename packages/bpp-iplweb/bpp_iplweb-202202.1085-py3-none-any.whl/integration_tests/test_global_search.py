ed7918fcd6fa0feb22c1a0cdac1f951c2ed59eda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:07 +0000

    Add package bpp-iplweb/202202.1085/bpp_iplweb-202202.1085-py3-none-any.whl

---
+++ packages/bpp-iplweb/bpp_iplweb-202202.1085-py3-none-any.whl/integration_tests/test_global_search.py
@@ -0,0 +1,100 @@
+from model_mommy import mommy
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
+
+from bpp.models import Rekord, Wydawnictwo_Ciagle
+from bpp.tests import select_select2_autocomplete
+
+from django_bpp.selenium_util import LONG_WAIT_TIME, wait_for_page_load
+
+
+def test_global_search_user(
+    asgi_live_server,
+    browser,
+    transactional_db,
+):
+    rec = None
+    try:
+        rec = mommy.make(Wydawnictwo_Ciagle, tytul_oryginalny="Test")
+        Rekord.objects.full_refresh()
+
+        assert Rekord.objects.count() >= 1
+        assert Rekord.objects.filter(tytul_oryginalny__icontains="Test").exists()
+
+        with wait_for_page_load(browser):
+            browser.visit(asgi_live_server.url)
+
+        with wait_for_page_load(browser):
+            select_select2_autocomplete(
+                browser,
+                "id_global_nav_value",
+                "Test",
+                value_before_enter="Rekord",
+                wait_for_new_value=False,  # False, bo zmiana wartosci powoduje wczytanie strony
+            )
+
+        try:
+            WebDriverWait(browser, LONG_WAIT_TIME).until(
+                lambda browser: "Strona WWW" in browser.html
+            )
+        except TimeoutException:
+            raise TimeoutException(f"Browser.html dump: {browser.html}")
+    finally:
+        if rec is not None:
+            rec.delete()
+
+
+def test_global_search_logged_in(
+    asgi_live_server,
+    admin_browser,
+    transactional_db,
+):
+    rec = None
+    try:
+        browser = admin_browser
+        mommy.make(Wydawnictwo_Ciagle, tytul_oryginalny="Test")
+        Rekord.objects.full_refresh()
+
+        with wait_for_page_load(browser):
+            browser.visit(asgi_live_server.url)
+
+        with wait_for_page_load(browser):
+            select_select2_autocomplete(
+                browser,
+                "id_global_nav_value",
+                "Test",
+                value_before_enter="Rekord",
+                wait_for_new_value=False,  # False, bo zmiana wartosci powoduje wczytanie strony
+            )
+
+        try:
+            WebDriverWait(browser, LONG_WAIT_TIME).until(
+                lambda browser: "Strona WWW" in browser.html
+            )
+        except TimeoutException:
+            raise TimeoutException(f"Browser.html dump: {browser.html}")
+    finally:
+        if rec is not None:
+            rec.delete()
+
+
+def test_global_search_in_admin(asgi_live_server, admin_browser, transactional_db):
+    browser = admin_browser
+    mommy.make(Wydawnictwo_Ciagle, tytul_oryginalny="Test")
+
+    with wait_for_page_load(browser):
+        browser.visit(asgi_live_server.url + "/admin/")
+
+    with wait_for_page_load(browser):
+        select_select2_autocomplete(
+            browser,
+            "id_global_nav_value",
+            "Test",
+            value_before_enter="ydawnictwo",
+            wait_for_new_value=False,  # False, bo zmiana wartosci powoduje wczytanie strony
+        )
+
+    browser.wait_for_condition(
+        lambda browser: "Zmień wydawnictwo ciągłe" in browser.html,
+        timeout=LONG_WAIT_TIME,
+    )
---
+++ packages/bpp-iplweb/bpp_iplweb-202202.1085-py3-none-any.whl/pbn_api/admin/publisher.py
@@ -0,0 +1,34 @@
+from pbn_api.admin import MaMNISWIDFilter
+from pbn_api.admin.base import BaseMongoDBAdmin
+from pbn_api.admin.filters import OdpowiednikWydawcyWBPPFilter
+from pbn_api.models import Publisher
+
+from django.contrib import admin
+from django.contrib.postgres.search import TrigramSimilarity
+
+from bpp.const import PBN_UID_LEN
+
+
+@admin.register(Publisher)
+class PublisherAdmin(BaseMongoDBAdmin):
+    list_display = ["publisherName", "mniswId", "mongoId", "rekord_w_bpp"]
+    search_fields = ["mongoId", "publisherName", "mniswId"]
+    list_filter = [
+        OdpowiednikWydawcyWBPPFilter,
+        MaMNISWIDFilter,
+    ] + BaseMongoDBAdmin.list_filter
+
+    MIN_TRIGRAM_MATCH = 0.05
+
+    def get_search_results(self, request, queryset, search_term):
+        if not search_term or len(search_term) == PBN_UID_LEN:
+            return super().get_search_results(request, queryset, search_term)
+
+        queryset = (
+            queryset.annotate(
+                similarity=TrigramSimilarity("publisherName", search_term)
+            )
+            .filter(similarity__gte=self.MIN_TRIGRAM_MATCH)
+            .order_by("-similarity")
+        )
+        return queryset, False
