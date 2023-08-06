6a2de7811dee00f05b4be609b46c95f25f29459d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:40 +0000

    Add package CloudComputing/0.0.9/CloudComputing-0.0.9-py3-none-any.whl

---
+++ packages/CloudComputing/CloudComputing-0.0.9-py3-none-any.whl/CloudComputing/__init__.py
@@ -0,0 +1,20 @@
+from .cloud_storage import connect, change_namespace, download_file
+from .config import get_auth_token, save_auth_token, check_auth, make_auth, check_config, make_config, load_config, check_ssh_connection
+from .remote_exec import remote_exec
+from . import vars
+
+# CloudComputing version
+__version__ = "0.0.9"
+# Author (GitHub username)
+__author__ = "mp1994"
+
+## Global variables
+# cloud_storage
+vars.creds_path = check_auth(silent=True)
+creds = vars.creds_path
+# remote_exec
+check_config(silent=True)
+c = load_config()
+if not c is None:
+    vars.ssh_host = c['SSH']['host']
+    vars.ssh_port = c['SSH']['port']
