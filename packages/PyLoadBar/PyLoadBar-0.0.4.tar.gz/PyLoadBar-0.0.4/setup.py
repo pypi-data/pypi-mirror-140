606064d1f03cd9257d0ecf02eba27626f87b2343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:25 +0000

    Add package PyLoadBar/0.0.4/PyLoadBar-0.0.4.tar.gz

---
+++ packages/PyLoadBar/PyLoadBar-0.0.4.tar.gz/PyLoadBar-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(name='PyLoadBar',
+      version='0.0.4',
+      description='Simple, easy-to-use loading sequence/progress bar module.',
+      py_modules=["PyLoadBar"],
+      package_dir={'': 'src'},
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Programming Language :: Python :: 3",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+      ],
+      long_description=long_description,
+      long_description_content_type='text/plain',
+      install_requires=['setuptools>=58.1.0', 'tqdm==4.62.3'],
+      extras_require={"dev": ["pytest>=6.2.5"]},
+      url='https://github.com/schlopp96/PyLoadBar',
+      author='schlopp96',
+      author_email='schloppdaddy@gmail.com')
