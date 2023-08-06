b6a82467040316cd6514e37a1306badd9b233852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:09 +0000

    Add package IE-SAMBD2020-ADVPYTHON-GROUPE/0.1/IE_SAMBD2020_ADVPYTHON_GROUPE-0.1.tar.gz

---
+++ packages/IE-SAMBD2020-ADVPYTHON-GROUPE/IE_SAMBD2020_ADVPYTHON_GROUPE-0.1.tar.gz/IE_SAMBD2020_ADVPYTHON_GROUPE-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='IE_SAMBD2020_ADVPYTHON_GROUPE',
+      version='0.1',  # Development release
+      description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
+      #url='https://github.com/manoelgadi/pythonpackagesample',
+      url='https://github.com/janamia/IE_SAMBD2020_ADVPYTHON_GROUPE.git',
+      author='GROUPE',
+      author_email='jana.jindan@gmail.com',
+      license='MIT',
+          packages=['IE_SAMBD2020_ADVPYTHON_GROUPE'],
+      zip_safe=False)
