d1941ad5ea93286c4f5a516fe09c6f1d54aaf2a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:24 +0000

    Add package method-python/0.0.17/method_python-0.0.17-py3-none-any.whl

---
+++ packages/method-python/method_python-0.0.17-py3-none-any.whl/method/resources/Entity.py
@@ -0,0 +1,124 @@
+from typing import TypedDict, Optional, List, Dict, Any, Literal
+
+from method.resource import Resource, RequestOpts
+from method.configuration import Configuration
+from method.errors import ResourceError
+
+
+EntityTypesLiterals = Literal[
+    'individual',
+    'c_corporation',
+    's_corporation',
+    'llc',
+    'partnership',
+    'sole_proprietorship',
+    'receive_only'
+]
+
+
+EntityCapabilitiesLiterals = Literal[
+    'payments:send',
+    'payments:receive',
+    'payments:limited-send'
+]
+
+
+EntityStatusesLiterals = Literal[
+    'active',
+    'incomplete',
+    'disabled'
+]
+
+
+class EntityIndividual(TypedDict):
+    first_name: Optional[str]
+    last_name: Optional[str]
+    phone: Optional[str]
+    email: Optional[str]
+    dob: Optional[str]
+
+
+class EntityAddress(TypedDict):
+    line1: Optional[str]
+    line2: Optional[str]
+    city: Optional[str]
+    state: Optional[str]
+    zip: Optional[str]
+
+
+class EntityCorporationOwner(TypedDict):
+    first_name: Optional[str]
+    last_name: Optional[str]
+    phone: Optional[str]
+    email: Optional[str]
+    dob: Optional[str]
+    address: EntityAddress
+
+
+class EntityCorporation(TypedDict):
+    name: Optional[str]
+    dba: Optional[str]
+    ein: Optional[str]
+    owners: List[EntityCorporationOwner]
+
+
+class EntityReceiveOnly(TypedDict):
+    name: str
+    phone: Optional[str]
+    email: Optional[str]
+
+
+class Entity(TypedDict):
+    id: str
+    type: EntityTypesLiterals
+    individual: Optional[EntityIndividual]
+    corporation: Optional[EntityCorporation]
+    receive_only: Optional[EntityReceiveOnly]
+    capabilities: List[EntityCapabilitiesLiterals]
+    address: EntityAddress
+    status: EntityStatusesLiterals
+    error: Optional[ResourceError]
+    metadata: Optional[Dict[str, Any]]
+    created_at: str
+    updated_at: str
+
+
+class EntityCreateOpts(TypedDict):
+    type: EntityTypesLiterals
+    individual: Optional[EntityIndividual]
+    corporation: Optional[EntityCorporation]
+    receive_only: Optional[EntityReceiveOnly]
+    address: Optional[EntityAddress]
+    metadata: Optional[Dict[str, Any]]
+
+
+class EntityUpdateOpts(TypedDict):
+    individual: Optional[EntityIndividual]
+    corporation: Optional[EntityCorporation]
+    address: Optional[EntityAddress]
+
+
+class EntityListOpts(TypedDict):
+    to_date: Optional[str]
+    from_date: Optional[str]
+    page: Optional[str | int]
+    page_limit: Optional[str | int]
+    status: Optional[str]
+    type: Optional[str]
+
+
+class EntityResource(Resource):
+    def __init__(self, config: Configuration):
+        super(EntityResource, self).__init__(config.add_path('entities'))
+
+    def create(self, opts: EntityCreateOpts, request_opts: Optional[RequestOpts] = None) -> Entity:
+        return super(EntityResource, self)._create(opts, request_opts)
+
+    def update(self, _id: str, opts: EntityCreateOpts) -> Entity:
+        return super(EntityResource, self)._update_with_id(_id, opts)
+
+    def get(self, _id: str) -> Entity:
+        return super(EntityResource, self)._get_with_id(_id)
+
+    def list(self, params: EntityListOpts = None) -> List[Entity]:
+        return super(EntityResource, self)._list(params)
