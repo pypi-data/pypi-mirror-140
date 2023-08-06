56dbddc2915edabbacb1cc10a05fa00861f3c475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:01 +0000

    Add package jaws-libp/2.8.0/jaws_libp-2.8.0-py3-none-any.whl

---
+++ packages/jaws-libp/jaws_libp-2.8.0-py3-none-any.whl/jlab_jaws/avro/__init__.py
@@ -0,0 +1,13 @@
+"""
+   AVRO schemas (.avsc data files) and corresponding serializers/deserializers (serde).
+
+   Support data files also include schema registration information for the registry and topic names.
+
+   Data Files:
+
+    * :download:`topics.json </../../src/jlab_jaws/avro/topics.json>`
+    * :download:`schema-registry.json </../../src/jlab_jaws/avro/schema-registry.json>`
+
+   See Also:
+       - `Apache AVRO <https://avro.apache.org/>`_
+"""
