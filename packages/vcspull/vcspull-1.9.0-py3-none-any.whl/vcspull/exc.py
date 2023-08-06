72d64476418d65b47c4397d58187fec0bd8c5a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:00 +0000

    Add package vcspull/1.9.0/vcspull-1.9.0-py3-none-any.whl

---
+++ packages/vcspull/vcspull-1.9.0-py3-none-any.whl/vcspull/exc.py
@@ -0,0 +1,9 @@
+class VCSPullException(Exception):
+
+    """Standard exception raised by libvcs."""
+
+    pass
+
+
+class MultipleConfigWarning(VCSPullException):
+    message = "Multiple configs found in home directory use only one." " .yaml, .json."
