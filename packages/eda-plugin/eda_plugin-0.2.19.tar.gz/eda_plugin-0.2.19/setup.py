0f00af1df2a253155cb120ba458e60fc94d357b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:39 +0000

    Add package eda-plugin/0.2.19/eda_plugin-0.2.19.tar.gz

---
+++ packages/eda-plugin/eda_plugin-0.2.19.tar.gz/eda_plugin-0.2.19/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+packages = find_packages()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="eda_plugin",
+    version="0.2.19",
+    description="Event-driven acquisition",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wl-stepp/eda_plugin",
+    project_urls={
+        "Bug Tracker": "https://github.com/wl-stepp/eda_plugin/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    packages=packages,
+    package_data={"": ["settings.json", "utility/*.jar", "utility/models/*"]},
+    include_package_data=True,
+    install_requires=[
+        "pyqt5",
+        "pycromanager",
+        "pyqtgraph",
+        "qimage2ndarray",
+        "qdarkstyle",
+        "tifffile",
+        "zenodo_get",
+        "docstring_inheritance",
+    ],
+    author="Willi L. Stepp",
+    author_email="willi.stepp@epfl.ch",
+    python_requires=">=3.7",
+)
