2eec87990c0eadecb5939c3d918c454e826d776c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:50:48 +0000

    Add package pyobs-core/0.17.9/pyobs_core-0.17.9-py3-none-any.whl

---
+++ packages/pyobs-core/pyobs_core-0.17.9-py3-none-any.whl/pyobs/comm/commlogging.py
@@ -0,0 +1,35 @@
+import logging
+import os
+from typing import TYPE_CHECKING, Any
+
+from pyobs.events import LogEvent
+
+if TYPE_CHECKING:
+    from pyobs.comm import Comm
+
+
+class CommLoggingHandler(logging.Handler):
+    """A logging handler that sends all messages through a Comm module."""
+
+    def __init__(self, comm: "Comm"):
+        """Create a new logging handler.
+
+        Args:
+            comm: Comm module to use.
+        """
+        logging.Handler.__init__(self)
+        self._comm = comm
+
+    def emit(self, rec: Any) -> None:
+        """Send a new log entry to the comm module.
+
+        Args:
+            rec: Log record to send.
+        """
+        entry = LogEvent(
+            rec.created, rec.levelname, os.path.basename(rec.pathname), rec.funcName, rec.lineno, rec.msg % rec.args
+        )
+        self._comm.log_message(entry)
+
+
+__all__ = ["CommLoggingHandler"]
