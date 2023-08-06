7487b758bb7d057999ea56a8ddbcff5cdd76a617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:08 +0000

    Add package m3u-ipytv/0.0.12/m3u-ipytv-0.0.12.tar.gz

---
+++ packages/m3u-ipytv/m3u-ipytv-0.0.12.tar.gz/m3u-ipytv-0.0.12/ipytv/__init__.py
@@ -0,0 +1,18 @@
+from ipytv.channel import \
+    IPTVAttr, \
+    IPTVChannel
+
+from ipytv.playlist import \
+    M3UPlaylist
+
+from ipytv.exceptions import \
+    M3YouException, \
+    MalformedExtinfException, \
+    MalformedPlaylistException, \
+    URLException, \
+    WrongTypeException
+
+from ipytv.m3u_tools import \
+    M3UDoctor, \
+    IPTVChannelDoctor, \
+    M3UPlaylistDoctor
