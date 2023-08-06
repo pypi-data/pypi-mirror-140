2048d64e6187a81ae180ec6174558776c7c24276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:51 +0000

    Add package browserist/0.3.0/browserist-0.3.0.tar.gz

---
+++ packages/browserist/browserist-0.3.0.tar.gz/browserist-0.3.0/src/browserist/browser/window/__main__.py
@@ -0,0 +1,30 @@
+from ...model.browser.base.driver import BrowserDriver
+from ...model.browser.base.settings import BrowserSettings
+from ...model.driver_methods import DriverMethods
+from .fullscreen import window_fullscreen
+from .get.__main__ import WindowGetDriverMethods
+from .maximize import window_maximize
+from .minimize import window_minimize
+from .set.__main__ import WindowSetDriverMethods
+
+
+class WindowDriverMethods(DriverMethods):
+    def __init__(self, browser_driver: BrowserDriver, settings: BrowserSettings) -> None:
+        super().__init__(browser_driver, settings)
+        self.get: WindowGetDriverMethods = WindowGetDriverMethods(browser_driver, settings)
+        self.set: WindowSetDriverMethods = WindowSetDriverMethods(browser_driver, settings)
+
+    def fullscreen(self) -> None:
+        """Fills the entire screen, similar to pressing F11 in most browsers."""
+
+        window_fullscreen(self._driver)
+
+    def maximize(self) -> None:
+        """Enlarges the window. For most operating systems, the window will fill the screen, without blocking the operating system's own menus and toolbars."""
+
+        window_maximize(self._driver)
+
+    def minimize(self) -> None:
+        """Minimizes the window of current browsing context. The exact behavior of this command is specific to individual window managers. Minimize Window typically hides the window in the system tray."""
+
+        window_minimize(self._driver)
---
+++ packages/browserist/browserist-0.3.0.tar.gz/browserist-0.3.0/src/browserist/browser/window/get/__main__.py
@@ -0,0 +1,30 @@
+from ....model.browser.base.driver import BrowserDriver
+from ....model.browser.base.settings import BrowserSettings
+from ....model.driver_methods import DriverMethods
+from .current_handle import get_current_window_handle
+from .position import get_window_position
+from .size import get_window_size
+
+
+class WindowGetDriverMethods(DriverMethods):
+    def __init__(self, browser_driver: BrowserDriver, settings: BrowserSettings) -> None:
+        super().__init__(browser_driver, settings)
+
+    def current_handle(self) -> str:
+        """Get the ID of the current window."""
+
+        return get_current_window_handle(self._driver)
+
+    def position(self) -> tuple[int, int]:
+        """Get the coordinates of the top left corner of the browser window on the screen. Usage:
+
+        x, y = browser.window.get.position()"""
+
+        return get_window_position(self._driver)
+
+    def size(self) -> tuple[int, int]:
+        """Get width and height of browser window on the screen in pixels. Usage:
+
+        width, height = browser.window.get.size()"""
+
+        return get_window_size(self._driver)
