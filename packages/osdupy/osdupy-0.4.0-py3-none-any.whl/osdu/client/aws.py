c8411aa450bafbc9c44c2ce60a52696c690c0047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:22 +0000

    Add package osdupy/0.4.0/osdupy-0.4.0-py3-none-any.whl

---
+++ packages/osdupy/osdupy-0.4.0-py3-none-any.whl/osdu/client/aws.py
@@ -0,0 +1,76 @@
+import os
+import boto3
+
+from ._base import BaseOsduClient
+import boto3.session
+
+
+
+class AwsOsduClient(BaseOsduClient):
+    """Good for batch tasks that don't have an interactive front-end. Token management is handled 
+    with the boto3 library directly through the Cognito service. You have to supply additional arguments for this.
+
+    Requires: `boto3`
+    """
+
+    @property
+    def profile(self):
+        return self._profile
+
+    @profile.setter
+    def profile(self, val):
+        self._profile = val
+
+    def __init__(self, data_partition_id, api_url:str=None, client_id:str=None, secret_hash:str=None,user:str=None, password:str=None, profile:str=None) -> None:
+        """Authenticate and instantiate a new AWS OSDU client. Uses Cognito directly to obtain an access token.
+
+        :param data_partition_id:   [Required] OSDU data partition ID, e.g. 'opendes'
+        :param api_url:     Must be only the base URL, e.g. 'https://myapi.myregion.mydomain.com'
+                            If not provided as arg, client will attempt to load value from 
+                            environment variable: OSDU_API_URL.
+        :param client_id:   OSDU client ID. Must be a Cognito App Client with no client secret.
+        :param secret_hash: Amazon Cognito Secret hash. This is described here: 'https://aws.amazon.com/premiumsupport/knowledge-center/cognito-unable-to-verify-secret-hash/'
+        :param user:        OSDU username. If not provided as arg, client will attempt to load value from
+                            environment variable: OSDU_USER.
+        :param password:    OSDU password. If not provided as arg, client will attempt to load value from
+                            environment variable: OSDU_PASSWORD.
+        :param profile:     Name of AWS profile to use for AWS session to retrieve tokens form Cognito. 
+                            If not provided as arg, client will attempt to load value from 
+                            environment variable: AWS_PROFILE.
+        """
+        super().__init__(data_partition_id, api_url)
+
+        self._client_id = client_id or os.environ.get('OSDU_CLIENT_ID')
+        self._user = user or os.environ.get('OSDU_USER')
+        self._profile = profile or os.environ.get('AWS_PROFILE')
+        self._secret_hash = secret_hash or os.environ.get('AWS_SECRETHASH')
+        if password:
+            self.get_tokens(password, secret_hash)
+            password = None # Don't leave password lying around.
+        else:
+            self.get_tokens(os.environ.get('OSDU_PASSWORD'), secret_hash)
+
+    def get_tokens(self, password, secret_hash) -> None:
+        if self._profile:
+            session = boto3.Session(profile_name=self._profile)
+            print('Created boto3 session with profile: ', self._profile)
+            cognito = session.client('cognito-idp')
+        else:
+            cognito = boto3.client('cognito-idp')
+
+        auth_params = { 
+            'USERNAME': self._user, 
+            'PASSWORD': password
+        }
+        if secret_hash:
+            auth_params['SECRET_HASH'] = secret_hash
+        
+        response = cognito.initiate_auth(
+            AuthFlow='USER_PASSWORD_AUTH',
+            ClientId=self._client_id,
+            AuthParameters=auth_params
+        )
+
+
+        self._access_token = response['AuthenticationResult']['AccessToken']
+        self._refresh_token = response['AuthenticationResult']['RefreshToken']
---
+++ packages/osdupy/osdupy-0.4.0-py3-none-any.whl/osdu/services/storage.py
@@ -0,0 +1,91 @@
+""" Provides a simple Python interface to the OSDU Storage API.
+"""
+from typing import List
+import requests
+from .base import BaseService
+
+
+class StorageService(BaseService):
+
+    def __init__(self, client):
+        super().__init__(client, service_name='storage', service_version=2)
+
+    def get_record(self, record_id: str):
+        """Returns the latest version of the given record."""
+        url = f'{self._service_url}/records/{record_id}'
+        response = self.__execute_request('get', url)
+
+        return response.json()
+
+    def get_records(self, record_ids: List[str], attributes: List[str] = []):
+        """Fetches multiple records at once.
+
+        :param record_ids:  List of record ids. Each record id must follow the naming convention {OSDU-Account-Id}:{dataset-name}:{record-type}:{version}.
+                            example: tenant1:well:123456789
+        :param attributes:  Filter attributes to restrict the returned fields of the record. Usage: data.{record-data-field-name}.
+                            example: data.wellName
+        """
+        url = f'{self._service_url}/query/records'
+        payload = {'records': record_ids, 'attributes': attributes}
+        response = self.__execute_request('post', url, json=payload)
+
+        return response.json()
+
+    def query_all_kinds(self):
+        """Returns a list of all kinds in the current data partition."""
+        url = f'{self._service_url}/query/kinds'
+        response = self.__execute_request('get', url)
+
+        return response.json()
+
+    def store_records(self, records: list):
+        """Create and/or update records. When no record id is provided or when the provided id is not already present 
+        in the Data Ecosystem, then a new record is created. If the id is related to an existing record in the Data 
+        Ecosystemthen an update operation takes place and a new version of the record is created.
+
+        """
+        url = f'{self._service_url}/records'
+        response = self.__execute_request('put', url, json=records)
+
+        return response.json()
+
+    def delete_record(self, record_id: str) -> bool:
+        """Performs a logical deletion of the given record. This operation can be reverted later.
+        
+        :returns:   True if record deleted successfully. Otherwise False.
+        """
+        url = f'{self._service_url}/records/{record_id}:delete'
+        response = self.__execute_request('post', url)
+
+        return response.status_code == 204
+
+    def purge_record(self, record_id: str) -> bool:
+        """performs the physical deletion of the given record and all of its versions. This operation cannot be undone.
+        
+        :returns:   True if record was purged successfully. Otherwise False.
+        """
+        url = f'{self._service_url}/records/{record_id}'
+        response = self.__execute_request('delete', url)
+
+        return response.status_code == 204
+
+    def get_all_record_versions(self, record_id: str):
+        """Returns a list containing all versions for the given record id."""
+        url = f'{self._service_url}/records/versions/{record_id}'
+        response = self.__execute_request('get', url)
+
+        return response.json()
+
+    def get_record_version(self, record_id: str, version: str):
+        """Retrieves the specific version of the given record."""
+        url = f'{self._service_url}/records/{record_id}/{version}'
+        response = self.__execute_request('get', url)
+
+        return response.json()
+
+    def __execute_request(self, method: str, url: str, json=None):
+        headers = self._headers()
+        response = requests.request(method, url, headers=headers, json=json)
+        response.raise_for_status()
+
+        return response
