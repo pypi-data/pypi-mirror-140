4d38b4f12f5e6e500e2317ba195ff07cf3ca81bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:41 +0000

    Add package tidy3d-beta/1.1.1/tidy3d-beta-1.1.1.tar.gz

---
+++ packages/tidy3d-beta/tidy3d-beta-1.1.1.tar.gz/tidy3d-beta-1.1.1/tidy3d/components/mode.py
@@ -0,0 +1,60 @@
+"""Defines specification for mode solver."""
+
+from typing import Tuple
+
+import pydantic as pd
+
+from ..constants import MICROMETER
+from .base import Tidy3dBaseModel
+from .types import Axis2D
+from ..log import SetupError
+
+
+class ModeSpec(Tidy3dBaseModel):
+    """Stores specifications for the mode solver to find an electromagntic mode.
+    Note, the planar axes are found by popping the propagation axis from {x,y,z}.
+    For example, if propagation axis is y, the planar axes are ordered {x,z}.
+
+    Example
+    -------
+    >>> mode_spec = ModeSpec(num_modes=3, target_neff=1.5)
+    """
+
+    num_modes: pd.PositiveInt = pd.Field(
+        1, title="Number of modes", description="Number of modes returned by mode solver."
+    )
+
+    target_neff: pd.PositiveFloat = pd.Field(
+        None, title="Target effective index", description="Guess for effective index of the mode."
+    )
+
+    num_pml: Tuple[pd.NonNegativeInt, pd.NonNegativeInt] = pd.Field(
+        (0, 0),
+        title="Number of PML layers",
+        description="Number of standard pml layers to add in the two tangential axes.",
+    )
+
+    bend_radius: float = pd.Field(
+        None,
+        title="Bend radius",
+        description="A curvature radius for simulation of waveguide bends. Can be negative, in "
+        "which case the mode plane center has a smaller value than the curvature center along the "
+        "tangential axis perpendicular to the bend axis.",
+        units=MICROMETER,
+    )
+
+    bend_axis: Axis2D = pd.Field(
+        None,
+        title="Bend axis",
+        description="Index into the first two non-propagating axes defining the normal to the "
+        "plane in which the bend lies. This must be provided if ``bend_radius`` is not ``None``. "
+        "For example, for a ring in the global xy-plane, and a mode plane in either the xz or the "
+        "yz plane, the ``bend_axis`` is always 1 (the global z axis).",
+    )
+
+    @pd.validator("bend_axis", always=True)
+    def bend_axis_given(cls, val, values):
+        """check that ``bend_axis`` is provided if ``bend_radius`` is not ``None``"""
+        if val is None and values.get("bend_radius") is not None:
+            raise SetupError("bend_axis must also be defined if bend_radius is defined.")
+        return val
