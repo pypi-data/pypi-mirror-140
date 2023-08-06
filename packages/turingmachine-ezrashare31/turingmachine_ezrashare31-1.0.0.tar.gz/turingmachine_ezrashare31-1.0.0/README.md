d4f22d40087f9c806d794be548cac1beda270bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:25 +0000

    Add package turingmachine-ezrashare31/1.0.0/turingmachine_ezrashare31-1.0.0.tar.gz

---
+++ packages/turingmachine-ezrashare31/turingmachine_ezrashare31-1.0.0.tar.gz/turingmachine_ezrashare31-1.0.0/README.md
@@ -0,0 +1,7 @@
+# TuringMachine
+
+TuringMachine runs on a infinate tape.
+
+the intruction set is at github.com/ezrashare21/turing/docs
+
+the online replit runtime is at repl.it/@EzraMorales/turing
---
+++ packages/turingmachine-ezrashare31/turingmachine_ezrashare31-1.0.0.tar.gz/turingmachine_ezrashare31-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name = "turingmachine_ezrashare31",
+	long_description = open("README.md").read(),
+	long_description_content_type = "text/markdown",
+	version = "1.0.0",
+	license = "MIT",
+	description = "a simple turing machine with an API for some awkward reason."
+)
