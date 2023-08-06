eb6fcd66533bbd391f7097f3a911665999d94a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:59 +0000

    Add package mypy-boto3-elasticache/1.21.8/mypy-boto3-elasticache-1.21.8.tar.gz

---
+++ packages/mypy-boto3-elasticache/mypy-boto3-elasticache-1.21.8.tar.gz/mypy-boto3-elasticache-1.21.8/mypy_boto3_elasticache/__main__.py
@@ -0,0 +1,39 @@
+"""
+Main CLI entrypoint.
+"""
+import sys
+
+
+def print_info() -> None:
+    """
+    Print package info to stdout.
+    """
+    print(
+        "Type annotations for boto3.ElastiCache 1.21.8\n"
+        "Version:         1.21.8\n"
+        "Builder version: 7.1.2\n"
+        "Docs:            https://vemel.github.io/boto3_stubs_docs/mypy_boto3_elasticache//\n"
+        "Boto3 docs:      https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/elasticache.html#ElastiCache\n"
+        "Other services:  https://pypi.org/project/boto3-stubs/\n"
+        "Changelog:       https://github.com/vemel/mypy_boto3_builder/releases"
+    )
+
+
+def print_version() -> None:
+    """
+    Print package version to stdout.
+    """
+    print("1.21.8")
+
+
+def main() -> None:
+    """
+    Main CLI entrypoint.
+    """
+    if "--version" in sys.argv:
+        return print_version()
+    print_info()
+
+
+if __name__ == "__main__":
+    main()
