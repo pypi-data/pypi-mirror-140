7ff1a1ec8138ea6ef7b6636070af0e5db9b6ab05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:01 +0000

    Add package zhangtest3/0.0.0/zhangtest3-0.0.0.tar.gz

---
+++ packages/zhangtest3/zhangtest3-0.0.0.tar.gz/zhangtest3-0.0.0/setup.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from distutils.core import setup
+setup(
+    name="zhangtest3",  # 对外我们的模块名
+    verson='1.0', # 版本号
+    description="第一次发布，测试一下",  # 描述
+    author="zhanglie",
+    author_email="3277706060@qq.com",
+    py_modules=['zhangtest3.demo1']    # 要发布的模块
+
+)
