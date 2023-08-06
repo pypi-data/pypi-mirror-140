473f84c8cf42357e76a9e03bb06d3993d105fdd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:48 +0000

    Add package git-interface/0.7.2/git-interface-0.7.2.tar.gz

---
+++ packages/git-interface/git-interface-0.7.2.tar.gz/git-interface-0.7.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+# Only requirements are listed here for GitHub dependency graph
+setup(
+    name="git-interface",
+    install_requires=[
+        "aiofiles >= 0.8",
+    ],
+)
