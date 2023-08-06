912e996b45dbf565495b50f525a9a1da5a8c71ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:51 +0000

    Add package notecoin/0.10.14/notecoin-0.10.14.tar.gz

---
+++ packages/notecoin/notecoin-0.10.14.tar.gz/notecoin-0.10.14/notecoin/okex/v5/client/market.py
@@ -0,0 +1,107 @@
+import datetime
+from typing import Optional, Union
+
+import pandas as pd
+from notecoin.okex.v5.client.base import BaseClient
+from notecoin.okex.v5.consts import *
+from notecoin.okex.v5.types import CcyType, InstType
+from notecoin.okex.v5.utils import enum_to_str
+
+
+class MarketClient(BaseClient):
+
+    def __init__(self, *args, **kwargs):
+        super(MarketClient, self).__init__(*args, **kwargs)
+
+    def tickers(self, instType: InstType, uly: Optional[str] = None) -> pd.DataFrame:
+        params = {}
+        if instType is not None:
+            params['instType'] = enum_to_str(instType)
+        if uly is not None:
+            params['uly'] = uly
+        data = self._request_with_params(GET, TICKERS, params)["data"]
+
+        df = pd.DataFrame(data)
+        df = df.apply(pd.to_numeric, errors='ignore')
+        df['ts'] = df['ts'].apply(lambda x: datetime.datetime.fromtimestamp(int(x) / 1000))
+        return df
+
+    def ticker(self, instId: str) -> pd.DataFrame:
+        params = {}
+        if instId is not None:
+            params['instId'] = instId
+        data = self._request_with_params(GET, TICKER, params)["data"]
+
+        df = pd.DataFrame(data)
+        df = df.apply(pd.to_numeric, errors='ignore')
+        df['ts'] = df['ts'].apply(lambda x: datetime.datetime.fromtimestamp(int(x) / 1000))
+        return df
+
+    def index_tickers(
+            self, quoteCcy: Optional[Union[CcyType, str]] = None, instId: Optional[str] = None) -> pd.DataFrame:
+        params = {}
+        if quoteCcy is not None:
+            params['quoteCcy'] = enum_to_str(quoteCcy)
+        if instId is not None:
+            params['instId'] = instId
+        data = self._request_with_params(GET, INDEX_TICKERS, params)["data"]
+
+        df = pd.DataFrame(data)
+        df = df.apply(pd.to_numeric, errors='ignore')
+        return df
+
+    def books(self, instId: str, sz: Optional[Union[int, str]] = None):
+        params = {}
+        if instId is not None:
+            params['instId'] = instId
+        if sz is not None:
+            params['sz'] = str(sz)
+        data = self._request_with_params(GET, BOOKS, params)["data"]
+
+        return data
+
+    def candles(self, instId: str,
+                after: Optional[Union[int, str]] = None,
+                before: Optional[Union[int, str]] = None,
+                bar: Optional[str] = None,
+                limit: Optional[Union[int, str]] = None) -> pd.DataFrame:
+        params = {}
+        if instId is not None:
+            params['instId'] = str(instId)
+        if after is not None:
+            params['after'] = str(after)
+        if before is not None:
+            params['before'] = str(before)
+        if bar is not None:
+            params['bar'] = str(bar)
+        if limit is not None:
+            params['limit'] = str(limit)
+        data = self._request_with_params(GET, CANDLES, params)["data"]
+
+        df = pd.DataFrame(data, columns=["ts", "o", "h", "l", "c", "vol", "volCcy"])
+        df = df.apply(pd.to_numeric, errors='ignore')
+        df['ts'] = df['ts'].apply(lambda x: datetime.datetime.fromtimestamp(int(x) / 1000))
+        return df
+
+    def history_candles(self, instId: str,
+                        after: Optional[Union[int, str]] = None,
+                        before: Optional[Union[int, str]] = None,
+                        bar: Optional[str] = None,
+                        limit: Optional[Union[int, str]] = None) -> pd.DataFrame:
+        params = {}
+        if instId is not None:
+            params['instId'] = str(instId)
+        if after is not None:
+            params['after'] = str(after)
+        if before is not None:
+            params['before'] = str(before)
+        if bar is not None:
+            params['bar'] = str(bar)
+        if limit is not None:
+            params['limit'] = str(limit)
+        data = self._request_with_params(GET, CANDLES, params)["data"]
+
+        df = pd.DataFrame(data, columns=["ts", "o", "h", "l", "c", "vol", "volCcy"])
+        df = df.apply(pd.to_numeric, errors='ignore')
+        df['ts'] = df['ts'].apply(lambda x: datetime.datetime.fromtimestamp(int(x) / 1000))
+        return df
---
+++ packages/notecoin/notecoin-0.10.14.tar.gz/notecoin-0.10.14/notecoin/okex/v5/client/public.py
@@ -0,0 +1,43 @@
+from typing import Optional, Union
+
+import pandas as pd
+from notecoin.okex.v5.client.base import BaseClient
+from notecoin.okex.v5.consts import *
+from notecoin.okex.v5.types import InstType
+from notecoin.okex.v5.utils import enum_to_str
+
+
+class PublicClient(BaseClient):
+
+    def __init__(self, *args, **kwargs):
+        super(PublicClient, self).__init__(*args, **kwargs)
+
+    def instruments(self, instType: Union[InstType, str], uly: Optional[str] = None, instId: Optional[str] = None):
+        params = {}
+        if instType is not None:
+            params['instType'] = enum_to_str(instType)
+        if uly is not None:
+            params['uly'] = uly
+        if instId is not None:
+            params['instId'] = instId
+        data = self._request_with_params(GET, INSTRUMENTS, params)["data"]
+
+        df = pd.DataFrame(data)
+        df = df.apply(pd.to_numeric, errors='ignore')
+        return df
+
+    def delivery_exercise_history(self, instType: Union[InstType, str],
+                                  uly: str,
+                                  after: Optional[Union[int, str]] = None,
+                                  before: Optional[Union[int, str]] = None,
+                                  limit: Optional[Union[int, str]] = None):
+        pass
+
+    def open_interest(self,
+                      instType: Union[InstType, str],
+                      uly: Optional[str],
+                      instId: Optional[str]):
+        pass
+
+    def funding_rate(self, instId: str):
+        pass
