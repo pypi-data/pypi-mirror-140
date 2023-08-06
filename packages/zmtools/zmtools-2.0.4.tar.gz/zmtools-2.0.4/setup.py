5c22d60caa43e71d101d688810e60147037c59d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:39 +0000

    Add package zmtools/2.0.4/zmtools-2.0.4.tar.gz

---
+++ packages/zmtools/zmtools-2.0.4.tar.gz/zmtools-2.0.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['zmtools']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=2.0.0']
+
+setup_kwargs = {
+    'name': 'zmtools',
+    'version': '2.0.4',
+    'description': "Various tools used across Zeke Marffy's programs",
+    'long_description': '# zmtools\n\nA conglomeration of functions reused in my programs; maybe they can help you too. The docstrings should explain all you need to know.\n',
+    'author': 'Zeke Marffy',
+    'author_email': 'zmarffy@yahoo.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/zmarffy/zmtools',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
