36a6326b5ba68e474691b9e8c5c782c874edffde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:54 +0000

    Add package layrz-sdk/1.1.2/layrz-sdk-1.1.2.tar.gz

---
+++ packages/layrz-sdk/layrz-sdk-1.1.2.tar.gz/layrz-sdk-1.1.2/layrzsdk/entities/__init__.py
@@ -0,0 +1,20 @@
+""" Init file """
+# Basic entities
+from .asset import Asset
+from .sensor import Sensor
+from .asset_operation_mode import AssetOperationMode
+from .device import Device
+from .custom_field import CustomField
+from .message import Message
+from .position import Position
+from .event import Event
+from .trigger import Trigger
+from .case import Case
+from .user import User
+from .comment import Comment
+from .checkpoint import Checkpoint
+from .geofence import Geofence
+from .waypoint import Waypoint
+
+# Charta entities
+from .charts import *
