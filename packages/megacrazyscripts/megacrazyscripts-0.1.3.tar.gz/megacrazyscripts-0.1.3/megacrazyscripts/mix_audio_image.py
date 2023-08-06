0ba1ac8fb8bd48353dc1ed328df65722a30b985d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:56 +0000

    Add package megacrazyscripts/0.1.3/megacrazyscripts-0.1.3.tar.gz

---
+++ packages/megacrazyscripts/megacrazyscripts-0.1.3.tar.gz/megacrazyscripts-0.1.3/megacrazyscripts/mix_audio_image.py
@@ -0,0 +1,22 @@
+def mix_audio_image(audio, img, output):
+    
+    prefix = '(mix_audio_image)'
+
+    from moviepy.editor import AudioFileClip, ImageClip
+
+    # Import files
+    print(prefix, f'Importing {audio}, {img} ...')
+    audio = AudioFileClip(audio)
+    img = ImageClip(img)
+    
+    # Mix into video
+    print(prefix, 'Setting audio...')
+    video = image_clip.set_audio(audio)
+    print(prefix, 'Setting video duration...')
+    video.duration = audio.duration
+    print(prefix, 'Setting video at 1 fps...')
+    video_clip.fps = 1
+    print(prefix, f'Exporting {output}...')
+    video_clip.write_videofile(output)
+
+    return print(prefix, 'Done')
