3fc29fdff5c95ce72ad71587d881bfd1689843d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:11 +0000

    Add package megacrazyscripts/0.0.4/megacrazyscripts-0.0.4.tar.gz

---
+++ packages/megacrazyscripts/megacrazyscripts-0.0.4.tar.gz/megacrazyscripts-0.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Various scripts from justcow.'
+
+# Setting up
+setup(
+    name="megacrazyscripts",
+    version=VERSION,
+    author="JustCow",
+    author_email="<justcow@pm.me>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['ffmpeg-python', 'pedalboard', 'youtube_dl', 'pillow'],
+    keywords=['python', 'video', 'audio', 'youtubedl', 'justcow'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
