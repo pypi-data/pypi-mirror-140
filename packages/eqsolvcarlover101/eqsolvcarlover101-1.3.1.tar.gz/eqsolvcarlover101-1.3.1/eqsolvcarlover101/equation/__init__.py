1f4a563d94e88a353f3088a93593ecadabd7ce43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:24 +0000

    Add package eqsolvcarlover101/1.3.1/eqsolvcarlover101-1.3.1.tar.gz

---
+++ packages/eqsolvcarlover101/eqsolvcarlover101-1.3.1.tar.gz/eqsolvcarlover101-1.3.1/eqsolvcarlover101/equation/__init__.py
@@ -0,0 +1,6 @@
+from .quadsolve import quadsolve
+from .slopeint import slopeint
+from .commands import commands
+from .help import help
+from .midpoint import midpoint
+from .perpint import perpint
