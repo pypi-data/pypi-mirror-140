8233c5caba41ff6beaa69d53937264b2c3380bce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:45 +0000

    Add package hstrat/0.1.1/hstrat-0.1.1.tar.gz

---
+++ packages/hstrat/hstrat-0.1.1.tar.gz/hstrat-0.1.1/hstrat/helpers/__init__.py
@@ -0,0 +1,8 @@
+from .AnyTreeAscendingIter import AnyTreeAscendingIter
+from .attach_attrs import attach_attrs
+
+# adapted from https://stackoverflow.com/a/31079085
+__all__ = [
+    'AnyTreeAscendingIter',
+    'attach_attrs',
+]
