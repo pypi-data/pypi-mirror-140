5991bd1914b062111abdaa187f2c3c77ccca8051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:03 +0000

    Add package jft/0.0.20/jft-0.0.20.tar.gz

---
+++ packages/jft/jft-0.0.20.tar.gz/jft-0.0.20/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from pathlib import Path
+long_description = Path("./README.md").read_text()
+
+setup(
+  name='jft',
+  version='0.0.20',
+  license='MIT',
+  description='Function Test Pair Toolbox',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author='@JohnRForbes',
+  author_email='john.robert.forbes@gmail.com',
+  url='https://gitlab.com/zereiji/jft',
+  packages=['jft'],
+  keywords='jft',
+  install_requires=[],
+)
