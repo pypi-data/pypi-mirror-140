2b1faa72082bc7b9dbebd687243e6031e5fb38b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:52:01 +0000

    Add package threedi-raster-edits/0.19.dev0/threedi_raster_edits-0.19.dev0-py3-none-any.whl

---
+++ packages/threedi-raster-edits/threedi_raster_edits-0.19.dev0-py3-none-any.whl/threedi_raster_edits/examples/merging_linestrings_based_on_heights.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 13 09:15:34 2021
+
+@author: chris.kerklaan
+
+Tool is also called levee-aggregation-algorithm
+"""
+
+# imports
+from threedi_raster_edits import Vector
+
+# define linestring paths
+levees = Vector("")
+
