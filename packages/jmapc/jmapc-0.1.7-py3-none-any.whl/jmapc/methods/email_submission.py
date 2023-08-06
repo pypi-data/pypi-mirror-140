d763fbe3a9af4da6d31c3d4008ebc32b1ab81993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:38:54 +0000

    Add package jmapc/0.1.7/jmapc-0.1.7-py3-none-any.whl

---
+++ packages/jmapc/jmapc-0.1.7-py3-none-any.whl/jmapc/methods/email_submission.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any, Dict, List, Optional
+
+from .. import constants
+from ..models import EmailSubmission
+from .base import Set, SetResponse
+
+
+@dataclass
+class EmailSubmissionSet(Set):
+    name = "EmailSubmission/set"
+    using = set([constants.JMAP_URN_SUBMISSION])
+
+    create: Optional[Dict[str, EmailSubmission]] = None
+    on_success_update_email: Optional[Dict[str, Any]] = None
+    on_success_destroy_email: Optional[List[str]] = None
+
+
+@dataclass
+class EmailSubmissionSetResponse(SetResponse):
+    name = "EmailSubmission/set"
+
+    created: Optional[Dict[str, Optional[EmailSubmission]]]
+    updated: Optional[Dict[str, Optional[EmailSubmission]]]
