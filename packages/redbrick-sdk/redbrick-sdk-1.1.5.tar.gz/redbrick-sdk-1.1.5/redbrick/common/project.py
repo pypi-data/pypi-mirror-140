973125d80112a8a88a429ce9911f9d17fc15dbbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:51 +0000

    Add package redbrick-sdk/1.1.5/redbrick-sdk-1.1.5.tar.gz

---
+++ packages/redbrick-sdk/redbrick-sdk-1.1.5.tar.gz/redbrick-sdk-1.1.5/redbrick/common/project.py
@@ -0,0 +1,42 @@
+"""Interface for getting basic information about a project."""
+
+from typing import Dict, List
+from abc import ABC, abstractmethod
+
+
+class ProjectRepoInterface(ABC):
+    """Abstract interface to Project APIs."""
+
+    @abstractmethod
+    def get_project(self, org_id: str, project_id: str) -> Dict:
+        """
+        Get project name and status.
+
+        Raise an exception if project does not exist.
+        """
+
+    @abstractmethod
+    def get_stages(self, org_id: str, project_id: str) -> List[Dict]:
+        """Get stages."""
+
+    @abstractmethod
+    def create_project(
+        self, org_id: str, name: str, stages: List[dict], td_type: str, tax_name: str
+    ) -> Dict:
+        """Create a project and return project_id."""
+
+    @abstractmethod
+    def get_org(self, org_id: str) -> Dict:
+        """Get organization."""
+
+    @abstractmethod
+    def get_projects(self, org_id: str) -> List[Dict]:
+        """Get all projects in organization."""
+
+    @abstractmethod
+    def get_taxonomies(self, org_id: str) -> List[Dict]:
+        """Get a list of taxonomies."""
+
+    @abstractmethod
+    def delete_project(self, org_id: str, project_id: str) -> None:
+        """Delete Project."""
