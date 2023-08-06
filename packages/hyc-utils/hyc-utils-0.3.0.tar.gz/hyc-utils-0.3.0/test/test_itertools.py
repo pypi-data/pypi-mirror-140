2d988350b2647f857ae7e72fbc77e922ac5d0d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:10 +0000

    Add package hyc-utils/0.3.0/hyc-utils-0.3.0.tar.gz

---
+++ packages/hyc-utils/hyc-utils-0.3.0.tar.gz/hyc-utils-0.3.0/test/test_itertools.py
@@ -0,0 +1,25 @@
+import pytest
+
+import utils
+
+@pytest.mark.parametrize('s, depth, expected', [
+    ([[1,2,[3]],[],(4,5),6], 1, [1,2,[3],4,5,6]),
+    ([[1,2,[3]],[],(4,5),6], -1, [1,2,3,4,5,6]),
+    (tuple([[1,2,[3]],[],(4,5),6]), -1, (1,2,3,4,5,6)),
+])
+def test_flatten_seq(s, depth, expected):
+    assert str(utils.itertools.flatten_seq(s, depth=depth)) == str(expected)
+    
+@pytest.mark.parametrize('d, depth, expected', [
+    ({'a': {'b': {'c': 'd'}}}, 1, {('a','b'): {'c': 'd'}}),
+    ({'a': {'b': {'c': 'd'}}}, -1, {('a','b','c'):'d'}),
+])
+def test_flatten_dict(d, depth, expected):
+    assert str(utils.itertools.flatten_dict(d, depth=depth)) == str(expected)
+    
+@pytest.mark.parametrize('d, keys, value, expected', [
+    ({}, ['a', 'b'], 'c', {'a': {'b': 'c'}}),
+])
+def test_assign_dict(d, keys, value, expected):
+    utils.itertools.assign_dict(d, keys, value)
+    assert str(d) == str(expected)
