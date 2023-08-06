bacf1bf2384fb4bfdd8f98f8aef75f20e3130e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:38 +0000

    Add package Topsis-Khushi-101903244/0.1/Topsis_Khushi_101903244-0.1.tar.gz

---
+++ packages/Topsis-Khushi-101903244/Topsis_Khushi_101903244-0.1.tar.gz/Topsis_Khushi_101903244-0.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'Topsis_Khushi_101903244',         
+  packages = ['Topsis_Khushi_101903244'],  
+  version = '0.1',
+  license='MIT',
+  description = 'Calculate Topsis score and save it in a csv file',
+  author = 'Khushi Singhal',                   
+  author_email = 'khushi.mitr03@gmail.com',     
+  url = 'https://github.com/khushimitr/Topsis-Khushi-101903244',
+  download_url = 'https://github.com/khushimitr/Topsis-Khushi-101903244/archive/refs/tags/v0.1.tar.gz',
+  keywords = ['TOPSISSCORE', 'RANK', 'DATAFRAME'],
+  install_requires=[
+          'numpy',
+          'pandas',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
