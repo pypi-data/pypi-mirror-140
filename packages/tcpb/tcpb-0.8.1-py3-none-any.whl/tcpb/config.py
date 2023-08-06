294e7f77ebb87db1425acb27152d3164e85695a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:16 +0000

    Add package tcpb/0.8.1/tcpb-0.8.1-py3-none-any.whl

---
+++ packages/tcpb/tcpb-0.8.1-py3-none-any.whl/tcpb/config.py
@@ -0,0 +1,15 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    """Base Settings configuration.
+
+    Do not instantiate directly, use settings object on module
+    """
+
+    extras_qcvars_kwarg: str = "qcvars"
+    extras_job_kwarg: str = "job_extras"
+    tcfe_config_kwarg: str = "tcfe:config"
+
+
+settings = Settings()
