781d6191f84e41a4321830c5e294fb41e82d4481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:26 +0000

    Add package django-ruamel-yamlfield/0.1.2/django-ruamel-yamlfield-0.1.2.tar.gz

---
+++ packages/django-ruamel-yamlfield/django-ruamel-yamlfield-0.1.2.tar.gz/django-ruamel-yamlfield-0.1.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+setuptools.setup(
+    name="django-ruamel-yamlfield",
+    version="0.1.2",
+    description='A Django database field for storing YAML data with Ruamel yaml implementation',
+    author='Martin Kubát',
+    author_email='martin.kubat@mapotic.com',
+    url="https://github.com/Mapotic/django-ruamel-yamlfield",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.1',
+        'License :: OSI Approved :: MIT License',
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=(
+        'ruamel.yaml>=0.17'
+    ),
+)
+
