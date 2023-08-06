45d4f7d4ff65cb8ae728e1475d0ae1fe830f6abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:03 +0000

    Add package mypy-boto3-transfer/1.21.7/mypy_boto3_transfer-1.21.7-py3-none-any.whl

---
+++ packages/mypy-boto3-transfer/mypy_boto3_transfer-1.21.7-py3-none-any.whl/mypy_boto3_transfer/__init__.py
@@ -0,0 +1,56 @@
+"""
+Main interface for transfer service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_transfer import (
+        Client,
+        ListAccessesPaginator,
+        ListExecutionsPaginator,
+        ListSecurityPoliciesPaginator,
+        ListServersPaginator,
+        ListTagsForResourcePaginator,
+        ListUsersPaginator,
+        ListWorkflowsPaginator,
+        TransferClient,
+    )
+
+    session = Session()
+    client: TransferClient = session.client("transfer")
+
+    list_accesses_paginator: ListAccessesPaginator = client.get_paginator("list_accesses")
+    list_executions_paginator: ListExecutionsPaginator = client.get_paginator("list_executions")
+    list_security_policies_paginator: ListSecurityPoliciesPaginator = client.get_paginator("list_security_policies")
+    list_servers_paginator: ListServersPaginator = client.get_paginator("list_servers")
+    list_tags_for_resource_paginator: ListTagsForResourcePaginator = client.get_paginator("list_tags_for_resource")
+    list_users_paginator: ListUsersPaginator = client.get_paginator("list_users")
+    list_workflows_paginator: ListWorkflowsPaginator = client.get_paginator("list_workflows")
+    ```
+"""
+from .client import TransferClient
+from .paginator import (
+    ListAccessesPaginator,
+    ListExecutionsPaginator,
+    ListSecurityPoliciesPaginator,
+    ListServersPaginator,
+    ListTagsForResourcePaginator,
+    ListUsersPaginator,
+    ListWorkflowsPaginator,
+)
+
+Client = TransferClient
+
+
+__all__ = (
+    "Client",
+    "ListAccessesPaginator",
+    "ListExecutionsPaginator",
+    "ListSecurityPoliciesPaginator",
+    "ListServersPaginator",
+    "ListTagsForResourcePaginator",
+    "ListUsersPaginator",
+    "ListWorkflowsPaginator",
+    "TransferClient",
+)
