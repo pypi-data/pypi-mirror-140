e4f161d8b95bfde705f800b636cf268119f406aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:19:25 +0000

    Add package ddtrace/0.59.0rc2/ddtrace-0.59.0rc2-cp35-cp35m-manylinux2010_x86_64.whl

---
+++ packages/ddtrace/ddtrace-0.59.0rc2-cp35-cp35m-manylinux2010_x86_64.whl/ddtrace/contrib/mongoengine/patch.py
@@ -0,0 +1,21 @@
+import mongoengine
+
+from ...vendor.debtcollector.removals import remove
+from .trace import WrappedConnect
+
+
+# Original connect function
+_connect = mongoengine.connect
+
+
+def patch():
+    setattr(mongoengine, "connect", WrappedConnect(_connect))
+
+
+def unpatch():
+    setattr(mongoengine, "connect", _connect)
+
+
+@remove(message="Use patching instead (see the docs).", removal_version="1.0.0")
+def trace_mongoengine(*args, **kwargs):
+    return _connect
