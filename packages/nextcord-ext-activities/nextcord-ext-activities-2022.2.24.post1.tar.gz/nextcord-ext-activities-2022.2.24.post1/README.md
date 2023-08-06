2011773cab7feeba5091b0be8ac8ee825632fe16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:23 +0000

    Add package nextcord-ext-activities/2022.2.24.post1/nextcord-ext-activities-2022.2.24.post1.tar.gz

---
+++ packages/nextcord-ext-activities/nextcord-ext-activities-2022.2.24.post1.tar.gz/nextcord-ext-activities-2022.2.24.post1/README.md
@@ -0,0 +1,6 @@
+# DISCLAIMER: THIS LIBRARY IS IN BETA. I AM NOT RESPONSIBLE FOR ANYTHING DAMAGED TO YOUR BOT. USE THIS LIBRARY AT YOUR OWN RISK.
+
+# nextcord-ext-activities
+nextcord.ext.activities is an extension that helps you to launch activities on Discord.
+
+
---
+++ packages/nextcord-ext-activities/nextcord-ext-activities-2022.2.24.post1.tar.gz/nextcord-ext-activities-2022.2.24.post1/nextcord/ext/activities/enums.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class Activity(Enum):
+    poker = 1
+    betrayal = 2
+    fishington = 3
+    chess = 4
+    checker = 5
+    ocho = 6
+    youtube = 7
+    doodle = 8
+    letter_tile = 9
+    word_snacks = 10
+    sketch = 11
+    spellcast = 12
