f460787a42758c4cc78416ef71e59ddee3c1bc16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:42 +0000

    Add package Topsis-Harsimran-101903288/1.2/Topsis_Harsimran_101903288-v1.2.tar.gz

---
+++ packages/Topsis-Harsimran-101903288/Topsis_Harsimran_101903288-v1.2.tar.gz/Topsis_Harsimran_101903288-v1.2/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'Topsis_Harsimran_101903288',        
+  packages = ['Topsis_Harsimran_101903288'],  
+  version = 'v1.2',      
+  license='MIT',       
+  description = 'Topsis Score and Rank generator',
+  author = 'Harsimran Kaur',                
+  author_email = 'hkaur3_be19@thapar.edu',      
+  url = 'https://github.com/Harsimrank23/Topsis_Harsimran_101903288',   
+  download_url = 'https://github.com/Harsimrank23/Topsis_Harsimran_101903288/archive/refs/heads/main.zip',    
+  keywords = ['topsis', 'topsis score', 'rank','Thapar'],
+  install_requires=['numpy','pandas' ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',    
+    'Intended Audience :: Developers',   
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
