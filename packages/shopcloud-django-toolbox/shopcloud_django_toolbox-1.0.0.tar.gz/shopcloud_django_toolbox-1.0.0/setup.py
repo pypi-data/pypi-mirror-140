638cc4595f0849ad67375b817d726dc41c6575ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:35 +0000

    Add package shopcloud-django-toolbox/1.0.0/shopcloud_django_toolbox-1.0.0.tar.gz

---
+++ packages/shopcloud-django-toolbox/shopcloud_django_toolbox-1.0.0.tar.gz/shopcloud_django_toolbox-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup_args = dict(
+    name='shopcloud_django_toolbox',
+    version='1.0.0',
+    description='Django tool',
+    long_description_content_type="text/markdown",
+    long_description=README,
+    license='MIT',
+    packages=find_packages(),
+    author='Konstantin Stoldt',
+    author_email='konstantin.stoldt@talk-point.de',
+    keywords=['CLI'],
+    url='https://github.com/Talk-Point/shopcloud-django-toolbox',
+)
+
+install_requires = [
+    'Django>=4'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
