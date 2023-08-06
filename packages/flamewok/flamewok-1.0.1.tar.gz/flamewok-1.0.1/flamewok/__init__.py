63772fbf9bfe33e20b25cfc45ff8d2459c4ba2e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:57 +0000

    Add package flamewok/1.0.1/flamewok-1.0.1.tar.gz

---
+++ packages/flamewok/flamewok-1.0.1.tar.gz/flamewok-1.0.1/flamewok/__init__.py
@@ -0,0 +1,18 @@
+"""Curenttly, to work with flamewok, you may need to import some of
+this objects:
+
+Menu
+TextBox
+ActionBox
+Form
+Field
+"""
+
+
+from flamewok.menu import Menu, TextBox, ActionBox
+from flamewok.form import Form, Field
+from flamewok.validators import check_type
+
+
+# Version of the flamewok package
+__version__ = "1.0.1"
