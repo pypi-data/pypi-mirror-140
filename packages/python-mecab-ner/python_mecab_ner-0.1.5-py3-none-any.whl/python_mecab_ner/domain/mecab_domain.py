33954a435d24c7c24d130d980ae88dc2e9a07366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:47 +0000

    Add package python-mecab-ner/0.1.5/python_mecab_ner-0.1.5-py3-none-any.whl

---
+++ packages/python-mecab-ner/python_mecab_ner-0.1.5-py3-none-any.whl/python_mecab_ner/domain/mecab_domain.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class Category:
+    large: str
+    small: str
+
+
+@dataclass
+class MecabWordCategory:
+    category: Category
+    start_idx: Optional[int] = None
+    end_idx: Optional[int] = None
+    entity: Optional[str] = None
+
+
+@dataclass
+class MecabPatternData:
+    category: Category
+    dictionary_data: str
+    pattern: str
+    sentence: list
+    min_meaning: int = 0
+    parse_character: bool = False
+
+
+@dataclass
+class MecabWordFeature:
+    word: str
+    pos: str
+    semantic: str
+    has_jongseong: bool
+    reading: str
+    type : str
+    start_pos: str
+    end_pos: str
+    expression: str
+    space_token_idx: Optional[int] = None
+    mecab_token_idx: Optional[int] = None
+    mecab_token_compound_idx: Optional[int] = None
+
+
+@dataclass
+class MecabNerFeature:
+    word: str
+    pos: str
+    start_idx: int
+    end_idx: int
+    category: Optional[Category] = None
