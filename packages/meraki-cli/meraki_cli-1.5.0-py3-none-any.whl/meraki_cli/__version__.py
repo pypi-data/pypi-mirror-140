6866d3d78ed841821e4b41a4073809eb706f7579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:20 +0000

    Add package meraki-cli/1.5.0/meraki_cli-1.5.0-py3-none-any.whl

---
+++ packages/meraki-cli/meraki_cli-1.5.0-py3-none-any.whl/meraki_cli/__version__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+'''
+Authoritative source of current program version
+'''
+
+
+# Single-sourced release version is informed here
+version = '1.5.0'
+
+# User-Agent header used for HTTP requests
+user_agent = f'Meraki-CLI/{version} PackeTsar'
