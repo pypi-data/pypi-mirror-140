61ce1581095e5b52ddb2c43ff3f026b7879267eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:06 +0000

    Add package production-titanic-model/0.1.0/production-titanic-model-0.1.0.tar.gz

---
+++ packages/production-titanic-model/production-titanic-model-0.1.0.tar.gz/production-titanic-model-0.1.0/regression_model/train_pipeline.py
@@ -0,0 +1,41 @@
+from config.core import config
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+from regression_model.processing.data_manager import load_dataset, save_model
+from regression_model.processing.features_preprocessing import preprocess_data
+
+
+def run_training() -> None:
+
+    # Load dataset.
+    df_data = load_dataset(config.model_config.data_url)
+
+    # Select specified columns.
+    df_data = df_data[config.model_config.columns_to_select]
+
+    # Preprocess data.
+    df_data = preprocess_data(df_data)
+
+    # Specify features and target.
+    features_columns = df_data.columns.difference([config.model_config.target])
+    target_column = config.model_config.target
+
+    # Divide train and test
+    X_train, X_test, y_train, y_test = train_test_split(
+        df_data[features_columns],
+        df_data[target_column],
+        test_size=config.model_config.test_size,
+        random_state=config.model_config.random_state,
+    )
+
+    # Train model.
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+
+    # Persist model.
+    save_model(model)
+
+
+if __name__ == "__main__":
+    run_training()
