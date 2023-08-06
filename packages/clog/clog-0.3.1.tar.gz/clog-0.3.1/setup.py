a9a84bc193aa60f60c7814ae2adbebc1c52ad004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:18 +0000

    Add package clog/0.3.1/clog-0.3.1.tar.gz

---
+++ packages/clog/clog-0.3.1.tar.gz/clog-0.3.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="clog",
+    version="0.3.1",
+    author="Brad Montgomery",
+    author_email="brad@bradmontgomery.net",
+    description="pretty-print with color",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bradmontgomery/clog",
+    project_urls={
+        "Bug Tracker": "https://github.com/bradmontgomery/clog/issues",
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    license="MIT",
+    install_requires=[
+        "rich",
+    ],
+)
