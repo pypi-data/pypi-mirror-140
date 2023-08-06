a0465f4c4f755d172e26d503beff2b8e5c19e6cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:29 +0000

    Add package Coffin-Codes-NET/7.0/Coffin_Codes_NET-7.0-py3-none-any.whl

---
+++ packages/Coffin-Codes-NET/Coffin_Codes_NET-7.0-py3-none-any.whl/bsb/main.py
@@ -0,0 +1,66 @@
+import asyncio
+import sys
+from getpass import getuser
+
+import click
+import os 
+from .tunnel_http import open_http_tunnel
+from .tunnel_tcp import open_tcp_tunnel
+from . import __version__
+
+os.system("clear")
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+@click.argument('port')
+@click.option('-s', '--subdomain', default='')
+@click.option('--host', default='open.jprq.io')
+def http(**kwargs):
+    host = kwargs['host']
+    port = kwargs['port']
+    username = kwargs['subdomain'] or getuser()
+   
+    print('''\033[91m
+× Welcome to Coffin Codes V7 Hacking Tunnel 2022
+│ × We do everything like a sir !!!
+│ × Coffin Codes V7 [2022]
+│ × TCP / SSL
+╰─> BSB Developers 
+''')
+
+    print("")
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(
+            open_http_tunnel(
+                ws_uri=f'wss://{host}/_ws/?username={username}&port={port}&version={__version__}',
+                http_uri=f'http://127.0.0.1:{port}',
+            )
+        )
+    except KeyboardInterrupt:
+       print("Cₒffᵢₙ Cₒdₑₛ ᵥ₇ cₗₒₛₑd")
+
+@main.command()
+@click.argument('port', type=click.INT)
+@click.option('--host', default='tcp.jprq.io')
+def tcp(**kwargs):
+    host = kwargs['host']
+    port = kwargs['port']
+
+    #print(f"\n\033[1;35mjprq : {__version__}\033[00m \033[34m{'Press Ctrl+C to quit.':>60}\n")
+
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(
+            open_tcp_tunnel(remote_server_host=host, ws_uri=f'wss://{host}/_ws/', local_server_port=port)
+        )
+    except KeyboardInterrupt:
+        #print("\n\033[31mjprq tunnel closed\033[00m")
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
