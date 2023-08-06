7e550c5185a804361d87b3660ad9081c7dabd13d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:16 +0000

    Add package buildproj/0.6.2/buildproj-0.6.2.tar.gz

---
+++ packages/buildproj/buildproj-0.6.2.tar.gz/buildproj-0.6.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+long_description = "[Documentation](http://github.com/Ehnryu/build)"
+setup(name='buildproj',
+      version='0.6.2',
+      description='Alternative to make',
+      url='http://github.com/Ehnryu/build',
+      author='Ehnryu/Sakurai07',
+      author_email='blzzardst0rm@gmail.com',
+      license='MIT',
+      packages=['buildproj'],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      entry_points={
+        'console_scripts': [
+            'build = buildproj.cli:start',
+        ],
+    },
+    install_requires=[
+        'tqdm',
+        ],
+    keywords=['build', 'make',"project"],
+    classifiers=["Programming Language :: Python :: 3"],
+      zip_safe=False)
