bd5608114b1af8cd32223a23d98cf1c29918c960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:42 +0000

    Add package goodhound/0.8.1/goodhound-0.8.1.tar.gz

---
+++ packages/goodhound/goodhound-0.8.1.tar.gz/goodhound-0.8.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['goodhound']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pandas==1.3.5', 'py2neo==2021.2.3']
+
+entry_points = \
+{'console_scripts': ['goodhound = goodhound:main']}
+
+setup_kwargs = {
+    'name': 'goodhound',
+    'version': '0.8.1',
+    'description': 'Attackers think in graphs, defenders think in actions, management think in charts.  GoodHound operationalises Bloodhound by determining the busiest paths to high value targets and creating actionable output to prioritise remediation of attack paths.',
+    'long_description': None,
+    'author': 'Andi Morris',
+    'author_email': 'andi.morris@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/idnahacks/GoodHound',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9.7,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
