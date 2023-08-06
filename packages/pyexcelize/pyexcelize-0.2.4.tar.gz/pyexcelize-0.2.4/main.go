9753e3d4957c15834a2d46a32a31d1796ff4ac67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:43 +0000

    Add package pyexcelize/0.2.4/pyexcelize-0.2.4.tar.gz

---
+++ packages/pyexcelize/pyexcelize-0.2.4.tar.gz/pyexcelize-0.2.4/main.go
@@ -0,0 +1,365 @@
+package main
+
+/*#include <stdlib.h>
+#include <time.h>
+struct ExcelValue {
+	int int_value;
+	float float_value;
+	char* string_value;
+	int value_type;
+};
+*/
+import "C"
+
+import (
+	"bytes"
+	"time"
+	"unsafe"
+
+	"github.com/xuri/excelize/v2"
+)
+
+const ValueType_Int C.int  = 0
+const ValueType_Float C.int  = 1
+const ValueType_String C.int  = 2
+const ValueType_Bool C.int  = 3
+const ValueType_Time C.int  = 4
+const ValueType_Nil C.int  = 5
+
+var files map[int]*excelize.File
+var writers map[int]*excelize.StreamWriter
+var fIndex = 0
+var wIndex = 0
+
+
+func convert_excelvalue(val *C.struct_ExcelValue) interface{} {
+	switch val.value_type {
+	case ValueType_Int:
+		return val.int_value
+	case ValueType_Float:
+		return val.float_value
+	case ValueType_String:
+		return C.GoString(val.string_value)
+	case ValueType_Bool:
+		return val.int_value == 1
+	case ValueType_Time:
+		return time.Unix(int64(val.int_value), 0)
+	case ValueType_Nil:
+		return nil
+	default:
+		return nil
+	}
+}
+
+
+/////////////////////////////////////////////////////////
+//                      WorkBook
+/////////////////////////////////////////////////////////
+
+//export NewFile
+func NewFile() int {
+	f := excelize.NewFile()
+	if files == nil {
+		files = make(map[int]*excelize.File)
+	}
+	fIndex += 1
+	files[fIndex] = f
+	return fIndex;
+}
+
+//export OpenFile
+func OpenFile(filename *C.char) int {
+	f, err:= excelize.OpenFile(C.GoString(filename))
+	if err != nil {
+		return -1;
+	}
+	if files == nil {
+		files = make(map[int]*excelize.File)
+	}
+	fIndex += 1
+	files[fIndex] = f
+	return fIndex;
+}
+
+//export Save
+func Save(fIndex int) {
+	files[fIndex].Save()
+}
+
+//export SaveAs
+func SaveAs(fIndex int, filename *C.char) {
+	files[fIndex].SaveAs(C.GoString(filename))
+}
+
+//export Close
+func Close(fIndex int) {
+	files[fIndex].Close()
+	delete(files, fIndex)
+}
+
+//export NewSheet
+func NewSheet(fIndex int, sheetname *C.char) int {
+	return files[fIndex].NewSheet(C.GoString(sheetname))
+}
+
+//export DeleteSheet
+func DeleteSheet(fIndex int, sheetname *C.char) {
+	files[fIndex].DeleteSheet(C.GoString(sheetname))
+}
+
+//export CopySheet
+func CopySheet(fIndex int, fromSheet int, toSheet int) int {
+	err:=files[fIndex].CopySheet(fromSheet, toSheet)
+	if err != nil {
+		return -1
+	}
+	return toSheet
+}
+
+//GroupSheets
+//UnGroupSheets
+//SetSheetBackground
+
+//export SetActiveSheet
+func SetActiveSheet(fIndex int, sheetIndex int) {
+	files[fIndex].SetActiveSheet(sheetIndex)
+}
+
+//export GetActiveSheetIndex
+func GetActiveSheetIndex(fIndex int) int {
+	return files[fIndex].GetActiveSheetIndex()
+}
+
+//SetSheetVisible
+//SetSheetFormatPr
+//GetSheetFormatPr
+//SetSheetViewOptions
+//GetSheetViewOptions
+//SetPageLayout
+//GetPageLayout
+//SetPageMargins
+//GetPageMargins
+//SetWorkbookPrOptions
+//GetWorkbookPrOptions
+//SetHeaderFooter
+//GetDefinedName
+//DeleteDefinedName
+//SetAppProps
+//GetAppProps
+//SetDocProps
+//GetDocProps
+
+
+/////////////////////////////////////////////////////////
+//                      WorkSheet
+/////////////////////////////////////////////////////////
+
+//SetColVisible
+//SetColWidth
+//SetRowHeight
+//SetRowVisible
+//GetSheetName
+//GetColVisible
+//GetColWidth
+//GetRowHeight
+//GetRowVisible
+//GetSheetIndex
+//GetSheetMap
+//GetSheetList
+//SetSheetName
+//SetSheetPrOptions
+//GetSheetPrOptions
+//InsertCol
+//InsertRow
+//DuplicateRow
+//DuplicateRowTo
+//SetRowOutlineLevel
+//SetColOutlineLevel
+//GetRowOutlineLevel
+//GetColOutlineLevel
+//Cols
+//Rows
+//Next
+//Error
+//Rows
+//Columns
+//Next
+//Error
+//Close
+//SearchSheet
+//ProtectSheet
+//UnprotectSheet
+//RemoveCol
+//RemoveRow
+
+//export SetSheetRow
+func SetSheetRow(fIndex int, sheetname *C.char, axis *C.char, rowPtr *C.struct_ExcelValue, length int) {
+	values := make([]interface{}, length)
+	row := unsafe.Slice(rowPtr, length)
+	for i, x := range row {
+		values[i] = convert_excelvalue(&x)
+	}
+	files[fIndex].SetSheetRow(C.GoString(sheetname), C.GoString(axis), &values)
+}
+
+//InsertPageBreak
+//RemovePageBreak
+
+
+/////////////////////////////////////////////////////////
+//                        Cell
+/////////////////////////////////////////////////////////
+
+//SetCellValue
+//SetCellBool
+//SetCellDefault
+
+//export SetCellInt
+func SetCellInt(fIndex int, sheetname *C.char, axis *C.char, value int) {
+	files[fIndex].SetCellInt(C.GoString(sheetname), C.GoString(axis), value)
+}
+
+//export SetCellStr
+func SetCellStr(fIndex int, sheetname *C.char, axis *C.char, value *C.char) {
+	files[fIndex].SetCellStr(C.GoString(sheetname), C.GoString(axis), C.GoString(value))
+}
+
+//export SetCellStyle
+func SetCellStyle(fIndex int, sheetname *C.char, hCell *C.char, vCell *C.char, styleIndex int) {
+	files[fIndex].SetCellStyle(C.GoString(sheetname), C.GoString(hCell), C.GoString(vCell), styleIndex)
+}
+
+//SetCellHyperLink
+//SetCellRichText
+//GetCellRichText
+//export GetCellValue
+func GetCellValue(fIndex int, sheetname *C.char, axis *C.char, out *byte, outN int64) *byte {
+	val, err := files[fIndex].GetCellValue(C.GoString(sheetname), C.GoString(axis))
+	if err != nil {
+		return nil
+	}
+	outBytes := unsafe.Slice(out, outN)[:0]
+	buf := bytes.NewBuffer(outBytes)
+	buf.WriteString(val)
+	buf.WriteByte(0) // Null terminator
+	return out
+}
+//GetCellType
+//GetCols
+//GetRows
+//GetCellHyperLink
+
+//export GetCellStyle
+func GetCellStyle(fIndex int, sheetname *C.char, axis *C.char) int {
+	index, err := files[fIndex].GetCellStyle(C.GoString(sheetname), C.GoString(axis))
+	if err != nil {
+		return -1
+	}
+	return index;
+}
+
+//MergeCell
+//UnmergeCell
+//GetMergeCells
+//GetCellValue
+//GetStartAxis
+//GetEndAxis
+//AddComment
+//GetComments
+//SetCellFormula
+//GetCellFormula
+//CalcCellValue
+
+
+/////////////////////////////////////////////////////////
+//                        Graph
+/////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////
+//                        Image
+/////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////
+//                        Shape
+/////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////
+//                      Sparkline
+/////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////
+//                        Style
+/////////////////////////////////////////////////////////
+
+//NewStyle
+//SetColStyle
+//SetRowStyle
+//SetDefaultFont
+//GetDefaultFont
+
+
+/////////////////////////////////////////////////////////
+//                     StreamWriter
+/////////////////////////////////////////////////////////
+//export NewStreamWriter
+func NewStreamWriter(fIndex int, sheetname *C.char) int {
+	writer, err := files[fIndex].NewStreamWriter(C.GoString(sheetname))
+	if err != nil {
+		return -1
+	}
+	wIndex ++
+	if writers == nil {
+		writers = make(map[int]*excelize.StreamWriter)
+	}
+	writers[wIndex] = writer
+	return wIndex
+}
+
+//export SetRow
+func SetRow(wIndex int, axis *C.char, rowPtr *C.struct_ExcelValue, length int) int {
+	values := make([]interface{}, length)
+	row := unsafe.Slice(rowPtr, length)
+	for i, x := range row {
+		values[i] = convert_excelvalue(&x)
+	}
+	err := writers[wIndex].SetRow(C.GoString(axis), values)
+	if err != nil {
+		return -1;
+	}
+	return length
+}
+
+//export AddTable
+func AddTable(wIndex int, hCell *C.char, vCell *C.char, format *C.char) {
+	writers[wIndex].AddTable(C.GoString(hCell), C.GoString(vCell), C.GoString(format))
+}
+
+//MergeCell
+//SetColWidth
+//export Flush
+func Flush(wIndex int) {
+	writers[wIndex].Flush()
+	delete(writers, wIndex)
+}
+
+
+/////////////////////////////////////////////////////////
+//　                  Data Validation
+/////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////
+//　                    Pivot Table
+/////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////
+//　                       Tools
+/////////////////////////////////////////////////////////
+
+func main() {
+}
---
+++ packages/pyexcelize/pyexcelize-0.2.4.tar.gz/pyexcelize-0.2.4/pyexcelize.py
@@ -0,0 +1,155 @@
+import json
+from datetime import datetime, date
+from ctypes import CDLL, c_int, c_char_p, create_string_buffer, Structure, c_float, sizeof
+from pathlib import Path
+import os
+
+from distutils.sysconfig import get_config_var
+
+
+here = Path(__file__).absolute().parent
+ext_suffix = get_config_var("EXT_SUFFIX")
+so_file = f'{here}/excelize{ext_suffix}'
+lib = CDLL(so_file)
+ENCODE = 'utf-8'
+
+
+__version__ = '0.1.0'
+
+
+class PyExcelizeError(Exception):
+    pass
+
+
+class ExcelValue(Structure):
+    _fields_ = [
+        ('int_value', c_int),
+        ('float_value', c_float),
+        ('str_value', c_char_p),
+        ('value_type', c_int),
+    ]
+
+ValueType_Int = 0
+ValueType_Float = 1
+ValueType_String = 2
+ValueType_Bool = 3
+ValueType_Time = 4
+ValueType_Nil = 5
+
+
+def _param_to_excel_value(value, param):
+    if isinstance(value, int):
+        param.int_value = value
+        param.value_type = ValueType_Int
+    elif isinstance(value, str):
+        param.str_value = value.encode(ENCODE)
+        param.value_type = ValueType_String
+    elif isinstance(value, float):
+        param.float_value = value
+        param.value_type = ValueType_Float
+    elif isinstance(value, bool):
+        param.int_value = 1 if value else 0
+        param.value_type = ValueType_Bool
+    elif isinstance(value, (datetime, date)):
+        param.str_value = value.isoformat().encode(ENCODE)
+        param.value_type = ValueType_Time
+    elif value is None:
+        param.value_type = ValueType_Nil
+    else:
+        raise PyExcelizeError('unsupported type')
+
+
+def new_file() -> int:
+    return lib.NewFile()
+
+
+def open_file(filename: str) -> int:
+    return lib.OpenFile(filename.encode(ENCODE))
+
+
+def save(file_index: int) -> None:
+    lib.Save(file_index)
+
+
+def save_as(file_index: int, filename: str) -> None:
+    lib.SaveAs(file_index, filename.encode(ENCODE))
+
+
+def close(file: int) -> None:
+    lib.Close(file)
+
+
+def new_sheet(file_index: int, sheet_name: str) -> None:
+    lib.NewSheet(file_index, sheet_name.encode(ENCODE))
+
+
+def delete_sheet(file_index: int, sheet_name: str) -> None:
+    lib.DeleteSheet(file_index, sheet_name.encode(ENCODE))
+
+
+def copy_sheet(file_index: int, fromIndex: int, toIndex: int) -> int:
+    result = lib.CopySheet(file_index, fromIndex, toIndex)
+    if result < 0:
+        raise PyExcelizeError('copy sheet error')
+    return result
+
+
+def set_active_sheet(file_index: int, index: int) -> None:
+    lib.SetActiveSheet(file_index, index)
+
+
+def get_active_sheet_index(file_index: int) -> int:
+    lib.GetActiveSheetIndex.restype = c_int
+    return lib.GetActiveSheetIndex(file_index)
+
+
+def set_sheet_row(file_index:int, sheet_name: str, axis: str, values):
+    ExcelValues = ExcelValue * len(values)
+    param = ExcelValues()
+    for v, p in zip(values, param):
+        _param_to_excel_value(v, p)
+    lib.SetSheetRow(file_index, sheet_name.encode(ENCODE), axis.encode(ENCODE), param, len(values))
+
+
+def set_cell_int(file_index:int, sheet_name: str, axis: str, value: int) -> None:
+    lib.SetCellInt(file_index, sheet_name.encode(ENCODE), axis.encode(ENCODE), value)
+
+
+def set_cell_str(file_index: int, sheet_name: str, axis: str, value: str) -> None:
+    lib.SetCellStr(file_index, sheet_name.encode(ENCODE), axis.encode(ENCODE), value.encode(ENCODE))
+
+
+def set_cell_style(file_index: int, sheet_name: str, h_cell: str, v_cell:str, style: int) -> None:
+    lib.SetCellStyle(file_index, sheet_name.encode(ENCODE), h_cell.encode(ENCODE), v_cell.encode(ENCODE), style)
+
+
+def get_cell_style(file_index: int, sheet_name: str, axis: str) -> int:
+    lib.GetCellStyle.restype = c_int
+    return lib.GetCellStyle(file_index, sheet_name.encode(ENCODE), axis.encode(ENCODE))
+
+
+def get_cell_value(file_index: int, sheet_name: str, axis: str) -> str:
+    buf_size = 2048
+    buf = create_string_buffer(buf_size)
+    lib.GetCellValue.restype = c_char_p
+    return lib.GetCellValue(file_index, sheet_name.encode(ENCODE), axis.encode(ENCODE), buf, buf_size).decode(ENCODE)
+
+
+def new_stream_writer(file_index: int, sheet_name: str) -> int:
+    return lib.NewStreamWriter(file_index, sheet_name.encode(ENCODE))
+
+
+def set_row(writer_index: int, axis: str, row: list) -> None:
+    ExcelValues = ExcelValue * len(row)
+    param = ExcelValues()
+    for v, p in zip(row, param):
+        _param_to_excel_value(v, p)
+    lib.SetRow(writer_index, axis.encode(ENCODE), param, len(row))
+
+
+def add_table(writer_index: int, h, v, format:dict):
+    lib.AddTable(writer_index, h.encode(ENCODE), v.encode(ENCODE), json.dumps(format).encode(ENCODE))
+
+
+def flush(writer_index: int) -> None:
+    lib.Flush(writer_index)
