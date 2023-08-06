400c3eebf85f31b46d97967fa32594563f088b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:03 +0000

    Add package onez/0.0.10/onez-0.0.10.tar.gz

---
+++ packages/onez/onez-0.0.10.tar.gz/onez-0.0.10/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "onez",
+    version = "0.0.10",
+
+    keywords = ("pip", "testpypi"),
+    description = "test pip module",
+    long_description = "test how to define pip module and upload to pypi",
+    license = "MIT",
+
+    url = "https://99onez.com",          # your module home page, such as
+    author = "99onez",                         # your name
+    author_email = "99onez@qq.com",    # your email
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
