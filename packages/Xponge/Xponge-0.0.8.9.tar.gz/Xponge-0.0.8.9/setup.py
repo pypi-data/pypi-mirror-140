28649e7d0930e30e192feed6b0837933b47e25b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:06 +0000

    Add package Xponge/0.0.8.9/Xponge-0.0.8.9.tar.gz

---
+++ packages/Xponge/Xponge-0.0.8.9.tar.gz/Xponge-0.0.8.9/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+ 
+f = open("README.md", "r", encoding="utf-8")
+long_description = f.read()
+f.close()
+
+#for formal
+setuptools.setup(
+    name="Xponge",
+    version="0.0.8.9",
+    author="Yijie Xia",  
+    author_email="yijiexia@pku.edu.cn", 
+    description="A package for building molecular dynamics inputs for SPONGE",
+    long_description=long_description, 
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/gao_hyp_xyj_admin/xponge",
+    packages=setuptools.find_packages(),
+    package_data = {"":['*.mol2', '*.frcmod', '*.dat', '*.itp']},
+    install_requires = ["numpy", "pubchempy", "NetCDF4"],
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6', 
+)
