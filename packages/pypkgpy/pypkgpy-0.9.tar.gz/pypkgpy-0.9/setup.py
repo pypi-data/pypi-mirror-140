47fc9489e771cbd8e5063124b811002612b71d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:54 +0000

    Add package pypkgpy/0.9/pypkgpy-0.9.tar.gz

---
+++ packages/pypkgpy/pypkgpy-0.9.tar.gz/pypkgpy-0.9/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="pypkgpy",
+version="0.9",
+description="This is pypkgpy package",
+long_description = "This is a very very long description",
+author="Henil",
+packages=['pythonpkg'],
+install_requires=[]
+)
