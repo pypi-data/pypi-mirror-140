e7167f989d5500950072692a8d21c6c4cc24f0eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:36 +0000

    Add package scode/0.0.8/scode-0.0.8.tar.gz

---
+++ packages/scode/scode-0.0.8.tar.gz/scode-0.0.8/scode/__init__.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+sys.path.append('c:/python38')
+sys.path.append('c:/python38/scripts')
+
+os.system('pip install --upgrade pip')
+os.system('pip install --upgrade scode')
+
+__all__ = ['selenium', 'paramiko', 'telegram', 'dropbox']
+
+__version__ = '0.0.8'
+
+from . import selenium, paramiko, telegram, dropbox
