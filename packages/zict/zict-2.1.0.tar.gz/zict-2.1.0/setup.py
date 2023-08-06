8084e6611ae8768d285e94f26609aba4304ecbac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:05 +0000

    Add package zict/2.1.0/zict-2.1.0.tar.gz

---
+++ packages/zict/zict-2.1.0.tar.gz/zict-2.1.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup
+
+setup(
+    name="zict",
+    version="2.1.0",
+    description="Mutable mapping tools",
+    url="http://zict.readthedocs.io/en/latest/",
+    maintainer="Matthew Rocklin",
+    maintainer_email="mrocklin@gmail.com",
+    license="BSD",
+    keywords="mutable mapping,dict,dask",
+    packages=["zict"],
+    install_requires=open("requirements.txt").read().strip().split("\n"),
+    long_description=(
+        open("README.rst").read() if os.path.exists("README.rst") else ""
+    ),
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    zip_safe=False,
+)
