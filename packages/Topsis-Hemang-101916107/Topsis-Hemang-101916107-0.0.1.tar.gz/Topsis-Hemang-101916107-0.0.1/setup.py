21694fa1015262327768d5744159852c2d317537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:42 +0000

    Add package Topsis-Hemang-101916107/0.0.1/Topsis-Hemang-101916107-0.0.1.tar.gz

---
+++ packages/Topsis-Hemang-101916107/Topsis-Hemang-101916107-0.0.1.tar.gz/Topsis-Hemang-101916107-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Topsis implementation'
+setup(
+    name="Topsis-Hemang-101916107",
+    version=VERSION,
+    author="Hemang",
+    author_email="<hemang.popli7@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['topsispy', 'numpy', 'pandas'],
+    keywords=['python', 'topsis', 'impacts','video','stream','video stream','camera stream','sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
