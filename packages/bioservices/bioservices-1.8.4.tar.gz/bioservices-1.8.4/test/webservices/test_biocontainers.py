d0d1e3abdde3e7b619c43c9e83e927900e5bb1a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:56 +0000

    Add package bioservices/1.8.4/bioservices-1.8.4.tar.gz

---
+++ packages/bioservices/bioservices-1.8.4.tar.gz/bioservices-1.8.4/test/webservices/test_biocontainers.py
@@ -0,0 +1,16 @@
+from bioservices import Biocontainers
+import pytest
+
+skiptravis = pytest.mark.skipif( "TRAVIS_PYTHON_VERSION" in os.environ,
+    reason="too slow for travis")
+
+
+@skiptravis
+@pytest.mark.xfail
+def test_biocontainers():
+
+    b = Biocontainers()
+    stats = b.get_stats()
+    b.get_tools(limit=10)
+    b.get_versions_one_tool("bioservices")
+
