eb6e156e8c02eb5b37831ba8550a16bab61060df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:00 +0000

    Add package yhproject-pkg-0.02/0.0.2/yhproject_pkg_0.02-0.0.2.tar.gz

---
+++ packages/yhproject-pkg-0.02/yhproject_pkg_0.02-0.0.2.tar.gz/yhproject_pkg_0.02-0.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    setuptools.setup(
+        name="yhproject_pkg_0.02",
+        version="0.0.2",
+        author="HUXIAOFENG",
+        author_email="474025548@qq.com",
+        description="A small example package",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/pypa/sampleproject",
+        packages=setuptools.find_packages(),
+        classifiers=[
+              "Programming Language :: Python :: 3",
+              "License :: OSI Approved :: MIT License",
+              "Operating System :: OS Independent",
+        ],
+    )
