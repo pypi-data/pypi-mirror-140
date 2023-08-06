3aeab342a0304c4eb8c91e1ff33894f847ada8ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:18:32 +0000

    Add package phytoClassifier/1.1.0/phytoClassifier-1.1.0-py3-none-any.whl

---
+++ packages/phytoClassifier/phytoClassifier-1.1.0-py3-none-any.whl/phytoClassifier/__init__.py
@@ -0,0 +1,5 @@
+from .main import getClassBySmiles, getClassByName, getClassByCid, getClassByInchi, getClassByInChiKey
+
+__version__ = '1.0.0'
+__author__ = 'TaoJiang'
+__description__ = 'A package for phytoChemicals classification'
