915f981e55e3fd7fcf333f723784a183c825f629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:53 +0000

    Add package Topsis-Mahira-101903536/1.2/Topsis_Mahira_101903536-v1.2.tar.gz

---
+++ packages/Topsis-Mahira-101903536/Topsis_Mahira_101903536-v1.2.tar.gz/Topsis_Mahira_101903536-v1.2/Topsis_Mahira_101903536/__init__.py
@@ -0,0 +1,2 @@
+from Topsis_Mahira_101903536.topsis import rank
+__version__='v1.2'
---
+++ packages/Topsis-Mahira-101903536/Topsis_Mahira_101903536-v1.2.tar.gz/Topsis_Mahira_101903536-v1.2/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'Topsis_Mahira_101903536',        
+  packages = ['Topsis_Mahira_101903536'],  
+  version = 'v1.2',      
+  license='MIT',       
+  description = 'Topsis Score and Rank generator',
+  author = 'Mahira Modi',                
+  author_email = 'mmodi_be19@thapar.edu',      
+  url = 'https://github.com/Mahiramodi/Topsis_Mahira_101903536',   
+  download_url = '',    
+  keywords = ['topsis','topsis score','rank','Thapar'],
+  install_requires=['numpy','pandas' ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',    
+    'Intended Audience :: Developers',   
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
