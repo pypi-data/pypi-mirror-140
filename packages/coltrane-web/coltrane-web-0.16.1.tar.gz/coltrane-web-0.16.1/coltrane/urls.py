4b159b10272be98d705b8d97d2d30d4e6ba5e357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:31 +0000

    Add package coltrane-web/0.16.1/coltrane-web-0.16.1.tar.gz

---
+++ packages/coltrane-web/coltrane-web-0.16.1.tar.gz/coltrane-web-0.16.1/coltrane/urls.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from django.contrib.sitemaps.views import sitemap
+from django.urls import include, path, re_path
+
+from coltrane.sitemaps import ContentSitemap
+
+from . import views
+
+
+app_name = "coltrane"
+
+sitemaps = {"content": ContentSitemap}
+
+urlpatterns = []
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
+
+urlpatterns += [
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
+    re_path(r"^(?P<slug>(\w|-|\/)*)", views.content, name="content"),
+]
