eef062e9e6aaed82b379ce29219cfccd7419c79c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:39 +0000

    Add package pacchettoDiG/0.0.2/pacchettoDiG-0.0.2-py3-none-any.whl

---
+++ packages/pacchettoDiG/pacchettoDiG-0.0.2-py3-none-any.whl/pckg/__init__.py
@@ -0,0 +1,12 @@
+import sys
+from twiggy import quick_setup,log
+import numpy as np
+from scipy import stats
+from scipy.special import factorial
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from scipy.fft import fft, ifft
+import pckg.module2 as bob
+
+quick_setup()
+logger= log.name('lossaggregation')
---
+++ packages/pacchettoDiG/pacchettoDiG-0.0.2-py3-none-any.whl/pckg/module1.py
@@ -0,0 +1,13 @@
+class MyClass:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+
+    def mymethod(self):
+        return bob.myfn(a)*np.arange(self.a,self.b)
+
+
+
+import pacchettodig
+
+pacchettoDiG.MyClass(a=2,b=9)
