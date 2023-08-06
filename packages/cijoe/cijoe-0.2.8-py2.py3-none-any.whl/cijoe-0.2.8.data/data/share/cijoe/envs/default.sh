3f5f6d6b4fdeb91cd885413cfbbc9ec970a235cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:12 +0000

    Add package cijoe/0.2.8/cijoe-0.2.8-py2.py3-none-any.whl

---
+++ packages/cijoe/cijoe-0.2.8-py2.py3-none-any.whl/cijoe-0.2.8.data/data/share/cijoe/envs/default.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#
+# Exists but does not define anything
+#
+# See the reference environments "local.sh" and "remote.sh" for examples of configuring *cijoe* for
+# either local or remote command execution
+#
