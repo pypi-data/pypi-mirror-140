d65cda954512cd331db32720f7f42cc0cfeef486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:48 +0000

    Add package topsis-101903169/2.1.0/topsis_101903169-2.1.0.tar.gz

---
+++ packages/topsis-101903169/topsis_101903169-2.1.0.tar.gz/topsis_101903169-2.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="topsis_101903169",
+    version="2.1.0",
+    description="topsis",
+    long_description="topsis 101903169",
+    long_description_content_type="text/markdown",
+    author="Parmeet Singh",
+    author_email="parmeet845@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["topsis_101903169"],
+    include_package_data=True,
+    install_requires=[],
+    
+)
