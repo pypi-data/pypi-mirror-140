aa5af9e29fc17658c1f37a6c53afe76d631dc782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:54 +0000

    Add package pypkgpy/0.8/pypkgpy-0.8.tar.gz

---
+++ packages/pypkgpy/pypkgpy-0.8.tar.gz/pypkgpy-0.8/pythonpkg/__init__.py
@@ -0,0 +1,8 @@
+class Add:
+    """Add class"""
+
+    def add(self, num1, num2):
+        """add() method"""
+
+        print("sum is: ", num1 + num2)
+        return num1 + num2
---
+++ packages/pypkgpy/pypkgpy-0.8.tar.gz/pypkgpy-0.8/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="pypkgpy",
+version="0.8",
+description="This is pypkgpy package",
+long_description = "This is a very very long description",
+author="Harry",
+packages=['pythonpkg'],
+install_requires=[]
+)
