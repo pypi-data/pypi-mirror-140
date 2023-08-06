4bb737502ea40f1b7ddc86c78d93d72b8d3b7e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:48 +0000

    Add package r2k/0.7.4/r2k-0.7.4-py3-none-any.whl

---
+++ packages/r2k/r2k-0.7.4-py3-none-any.whl/r2k/ebook/single_article.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+
+from r2k.feeds import Article
+
+
+class SingleArticle(Article):
+    """Represents a single (i.e. not part of a feed) article for sending it to kindle"""
+
+    def __init__(self, url: str) -> None:
+        """Constructor"""
+        super().__init__({"link": url})
+        reqs = requests.get(self.link)
+        self._soup = BeautifulSoup(reqs.text, "html.parser")
+
+        self.set_title()
+        self.set_author()
+
+    def set_title(self) -> None:
+        """Set the article's title"""
+        titles = self._soup.find_all("title")
+        if titles:
+            self.title = titles[0].get_text().strip()
+        else:
+            self.title = self.link.strip()
+
+    def set_author(self) -> None:
+        """Set the article's author"""
+        metas = self._soup.find_all("meta")
+        for meta in metas:
+            if meta.attrs.get("name") == "author":
+                self.author = meta.attrs.get("content").strip()
