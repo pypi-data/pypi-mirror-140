9148d404c9271df675720d7a414f1ce76bb9a9ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:47 +0000

    Add package Topsis-Utkarsh-101903139/1.0.3/Topsis-Utkarsh-101903139-1.0.3.tar.gz

---
+++ packages/Topsis-Utkarsh-101903139/Topsis-Utkarsh-101903139-1.0.3.tar.gz/Topsis-Utkarsh-101903139-1.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+    name="Topsis-Utkarsh-101903139",
+    version="1.0.3",
+    description="A Python package implementing TOPSIS technique.",
+    author="Utkarsh Sangwan",
+    author_email="utkarshsangwan7@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["topsis"],
+    include_package_data=True,
+    install_requires=['numpy',
+                      'pandas',
+                     
+                      ],
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis.101903139:main",
+        ]
+    },
+)
