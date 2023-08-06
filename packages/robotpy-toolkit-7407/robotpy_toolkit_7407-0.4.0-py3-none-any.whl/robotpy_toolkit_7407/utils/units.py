ef2f012952ef08040befffeb307a099a01b94946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:02 +0000

    Add package robotpy-toolkit-7407/0.4.0/robotpy_toolkit_7407-0.4.0-py3-none-any.whl

---
+++ packages/robotpy-toolkit-7407/robotpy_toolkit_7407-0.4.0-py3-none-any.whl/robotpy_toolkit_7407/utils/units.py
@@ -0,0 +1,15 @@
+from fast_unit import units, add_unit, Unum
+
+m = units.m
+ft = units.ft
+inch = units.inch
+mile = units.mile
+
+s = units.s
+ms = units.ms
+minute = units.minute
+hour = 60 * minute
+
+rad = units.rad
+deg = units.deg
+rev = units.rev
