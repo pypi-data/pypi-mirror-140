141466b492b24117eb6fe7f8d3bd86ecf46936f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:36 +0000

    Add package hfradarpy/0.1.4.7/hfradarpy-0.1.4.7-py2.py3-none-any.whl

---
+++ packages/hfradarpy/hfradarpy-0.1.4.7-py2.py3-none-any.whl/hfradarpy/cli.py
@@ -0,0 +1,28 @@
+from argparse import ArgumentParser
+from hfradarpy import __version__
+
+def cli(args=None):
+    p = ArgumentParser(
+        description="Toolbox to read in High Frequency Radar (HFR) files written in CODAR Tabular Format (CTF).",
+        conflict_handler='resolve'
+    )
+    p.add_argument(
+        '-V', '--version',
+        action='version',
+        help='Show the conda-prefix-replacement version number and exit.',
+        version="hfradarpy %s" % __version__,
+    )
+
+    args, unknown = p.parse_known_args(args)
+
+    # do something with the args
+    print("CLI template - fix me up!")
+
+    # No return value means no error.
+    # Return a value of 1 or higher to signify an error.
+    # See https://docs.python.org/3/library/sys.html#sys.exit
+
+
+if __name__ == '__main__':
+    import sys
+    cli(sys.argv[1:])
