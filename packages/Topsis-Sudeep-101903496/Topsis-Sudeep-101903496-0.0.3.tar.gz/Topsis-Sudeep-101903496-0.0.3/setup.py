a1ff0e29f2d7626b25961246be859a951daf78ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:31 +0000

    Add package Topsis-Sudeep-101903496/0.0.3/Topsis-Sudeep-101903496-0.0.3.tar.gz

---
+++ packages/Topsis-Sudeep-101903496/Topsis-Sudeep-101903496-0.0.3.tar.gz/Topsis-Sudeep-101903496-0.0.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Topsis-Sudeep-101903496",
+    version="0.0.3",
+    author="Sudeep Singh",
+    author_email="singhsudeep0202@gmail.com",
+    description="A package for calculating Topsis Score and Rank them accordingly",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/sudeepk07/Topsis-Sudeep-101903496",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["Topsis_Sudeep_101903496"],
+    include_package_data=True,
+    install_requires='pandas',
+    entry_points={
+        "console_scripts": [
+            "topsis=Topsis_Sudeep_101903496.topsis:main",
+        ]
+    },
+)
