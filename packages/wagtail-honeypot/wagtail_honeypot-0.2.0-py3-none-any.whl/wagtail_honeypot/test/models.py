267507680823f1bb48212c09dc64df2daff92d26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:23 +0000

    Add package wagtail-honeypot/0.2.0/wagtail_honeypot-0.2.0-py3-none-any.whl

---
+++ packages/wagtail-honeypot/wagtail_honeypot-0.2.0-py3-none-any.whl/wagtail_honeypot/test/models.py
@@ -0,0 +1,54 @@
+from modelcluster.fields import ParentalKey
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    FieldRowPanel,
+    InlinePanel,
+    MultiFieldPanel,
+    ObjectList,
+    TabbedInterface,
+)
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page
+
+from wagtail_honeypot.models import HoneypotMixin
+
+
+class HomePage(Page):
+    template = "home_page.html"
+
+
+class FormField(AbstractFormField):
+    page = ParentalKey("FormPage", related_name="form_fields")
+
+
+class FormPage(HoneypotMixin):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FieldPanel("intro", classname="full"),
+        InlinePanel("form_fields", label="Form fields"),
+        FieldPanel("thank_you_text", classname="full"),
+        MultiFieldPanel(
+            [
+                FieldRowPanel(
+                    [
+                        FieldPanel("from_address", classname="col6"),
+                        FieldPanel("to_address", classname="col6"),
+                    ]
+                ),
+                FieldPanel("subject"),
+            ],
+            "Email",
+        ),
+    ]
+
+    edit_handler = TabbedInterface(
+        [
+            ObjectList(content_panels, heading="Content"),
+            ObjectList(HoneypotMixin.honeypot_panels, heading="Honeypot"),
+            ObjectList(Page.promote_panels, heading="Promote"),
+            ObjectList(Page.settings_panels, heading="Settings", classname="settings"),
+        ]
+    )
