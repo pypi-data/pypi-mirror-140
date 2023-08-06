5f20c3c87df932d612105e2c3cedd6e1116820a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:48 +0000

    Add package django-river-ml/0.0.11/django-river-ml-0.0.11.tar.gz

---
+++ packages/django-river-ml/django-river-ml-0.0.11.tar.gz/django-river-ml-0.0.11/django_river_ml/secret_key.py
@@ -0,0 +1,2 @@
+SHELVE_SECRET_KEY = 'c&c8ht1e+7rrsmv0gpjy@igkp4_+c!g_3h_^_2wdzj5uak@6sz'
+JWT_SERVER_SECRET = '7w7km8^j@t&hhxsiz)n5fbn2ku*at9m27*i)56=6jdz8)j8syc'
---
+++ packages/django-river-ml/django-river-ml-0.0.11.tar.gz/django-river-ml-0.0.11/django_river_ml/views/__init__.py
@@ -0,0 +1,6 @@
+from .auth import GetAuthToken
+from .base import ServiceInfo
+from .learn import LearnView
+from .metrics import MetricsView, stream_metrics, stream_events, StatsView
+from .model import ModelView, ModelsView, ModelDownloadView
+from .predict import PredictView
