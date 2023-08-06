9db2a84084c4e5c84579a8b25cbe17ad60f60017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:39:27 +0000

    Add package nepali-sentiment-analysis/1.0/nepali_sentiment_analysis-1.0.tar.gz

---
+++ packages/nepali-sentiment-analysis/nepali_sentiment_analysis-1.0.tar.gz/nepali_sentiment_analysis-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    packages=find_packages(),
+    install_requires=[
+        "numpy==1.22.1",
+        "pandas==1.4.0",
+        "torch==1.10.2",
+        "scikit_learn",
+        "transformers==4.16.1",
+    ],
+)
