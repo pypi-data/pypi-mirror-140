fa6b3b1546192b8d8566c8032ba9407bbf06dc05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:11 +0000

    Add package podaac-data-subscriber/1.8.0/podaac_data_subscriber-1.8.0-py3-none-any.whl

---
+++ packages/podaac-data-subscriber/podaac_data_subscriber-1.8.0-py3-none-any.whl/subscriber/podaac_data_subscriber.py
@@ -0,0 +1,285 @@
+#!/usr/bin/env python3
+
+# # Access Sentinel-6 MF Data using a script
+# This script shows a simple way to maintain a local time series of Sentinel-6
+# data using the
+# [CMR Search API]
+# (https://cmr.earthdata.nasa.gov/search/site/docs/search/api.html).
+# It downloads granules the ingested since
+#   the previous run to a designated data
+#   folder and overwrites a hidden file inside with the timestamp of the
+# CMR Search request on success.
+# Before you beginning this tutorial, make sure you have an Earthdata account:
+# [https://urs.earthdata.nasa.gov] .
+# Accounts are free to create and take just a moment to set up.
+import argparse
+import logging
+import os
+from os import makedirs
+from os.path import isdir, basename, join, isfile
+from urllib.request import urlretrieve
+from datetime import datetime, timedelta
+
+from subscriber import podaac_access as pa
+
+__version__ = pa.__version__
+
+LOGLEVEL = os.environ.get('PODAAC_LOGLEVEL', 'WARNING').upper()
+logging.basicConfig(level=LOGLEVEL)
+logging.debug("Log level set to " + LOGLEVEL)
+
+page_size = 2000
+
+edl = pa.edl
+cmr = pa.cmr
+token_url = pa.token_url
+
+
+def get_update_file(data_dir, collection_name):
+    if isfile(data_dir + "/.update__" + collection_name):
+        return data_dir + "/.update__" + collection_name
+    elif isfile(data_dir + "/.update"):
+        print("WARNING: found a deprecated use of '.update' file at {0}. After this run it will be renamed to {1}".format(data_dir + "/.update", data_dir + "/.update__" + collection_name))
+        return data_dir + "/.update"
+
+    return None
+
+
+def validate(args):
+    if args.minutes is None and args.startDate is False and args.endDate is False:
+        raise ValueError("Error parsing command line arguments: one of --start-date, --end-date or --minutes are required")
+
+
+def create_parser():
+    # Initialize parser
+    parser = argparse.ArgumentParser(prog='PO.DAAC data subscriber')
+
+    # Adding Required arguments
+    parser.add_argument("-c", "--collection-shortname", dest="collection",required=True, help = "The collection shortname for which you want to retrieve data.")  # noqa E501
+    parser.add_argument("-d", "--data-dir", dest="outputDirectory", required=True, help = "The directory where data products will be downloaded.")  # noqa E501
+
+    # Adding optional arguments
+
+    # spatiotemporal arguments
+    parser.add_argument("-sd", "--start-date", dest="startDate", help = "The ISO date time before which data should be retrieved. For Example, --start-date 2021-01-14T00:00:00Z", default=False)  # noqa E501
+    parser.add_argument("-ed", "--end-date", dest="endDate", help = "The ISO date time after which data should be retrieved. For Example, --end-date 2021-01-14T00:00:00Z", default=False)   # noqa E501
+    parser.add_argument("-b", "--bounds", dest="bbox", help = "The bounding rectangle to filter result in. Format is W Longitude,S Latitude,E Longitude,N Latitude without spaces. Due to an issue with parsing arguments, to use this command, please use the -b=\"-180,-90,180,90\" syntax when calling from the command line. Default: \"-180,-90,180,90\".", default="-180,-90,180,90")  # noqa E501
+
+    # Arguments for how data are stored locally - much processing is based on
+    # the underlying directory structure (e.g. year/Day-of-year)
+    parser.add_argument("-dc", dest="cycle", action="store_true", help = "Flag to use cycle number for directory where data products will be downloaded.")  # noqa E501
+    parser.add_argument("-dydoy", dest="dydoy", action="store_true", help = "Flag to use start time (Year/DOY) of downloaded data for directory where data products will be downloaded.")  # noqa E501
+    parser.add_argument("-dymd", dest="dymd", action="store_true", help = "Flag to use start time (Year/Month/Day) of downloaded data for directory where data products will be downloaded.")  # noqa E501
+    parser.add_argument("-dy", dest="dy", action="store_true", help = "Flag to use start time (Year) of downloaded data for directory where data products will be downloaded.")  # noqa E501
+    parser.add_argument("--offset", dest="offset", help = "Flag used to shift timestamp. Units are in hours, e.g. 10 or -10.")  # noqa E501
+
+    parser.add_argument("-m", "--minutes", dest="minutes", help = "How far back in time, in minutes, should the script look for data. If running this script as a cron, this value should be equal to or greater than how often your cron runs.", type=int, default=None)  # noqa E501
+    parser.add_argument("-e", "--extensions", dest="extensions", help = "The extensions of products to download. Default is [.nc, .h5, .zip]", default=None, action='append')  # noqa E501
+    parser.add_argument("--process", dest="process_cmd", help="Processing command to run on each downloaded file (e.g., compression). Can be specified multiple times.", action='append')
+
+    parser.add_argument("--version", action="version", version='%(prog)s ' + __version__, help="Display script version information and exit.")  # noqa E501
+    parser.add_argument("--verbose", dest="verbose", action="store_true", help="Verbose mode.")    # noqa E501
+
+    parser.add_argument("-p", "--provider", dest="provider", default='POCLOUD', help="Specify a provider for collection search. Default is POCLOUD.")    # noqa E501
+    return parser
+
+
+def run():
+    parser = create_parser()
+    args = parser.parse_args()
+
+    try:
+        pa.validate(args)
+        validate(args)
+    except ValueError as v:
+        print(v)
+        exit()
+
+    pa.setup_earthdata_login_auth(edl)
+    token = pa.get_token(token_url, 'podaac-subscriber', edl)
+
+    mins = args.minutes  # In this case download files ingested in the last 60 minutes -- change this to whatever setting is needed
+    provider = args.provider
+    start_date_time = args.startDate
+    end_date_time = args.endDate
+    short_name = args.collection
+    extensions = args.extensions
+    process_cmd = args.process_cmd
+    data_path = args.outputDirectory
+
+    defined_time_range = False
+    if start_date_time or end_date_time:
+        defined_time_range = True
+
+    ts_shift = 0
+    if args.offset:
+        ts_shift = timedelta(hours=int(args.offset))
+
+    # Error catching for output directory specifications
+    # Must specify -d output path or one time-based output directory flag
+
+    if sum([args.cycle, args.dydoy, args.dymd, args.dy]) > 1:
+        parser.error('Too many output directory flags specified, '
+                     'Please specify exactly one flag '
+                     'from -dc, -dy, -dydoy, or -dymd')
+
+    # **The search retrieves granules ingested during the last `n` minutes.
+    # ** A file in your local data dir  file that tracks updates to your data directory,
+    # if one file exists.
+
+    # This is the default way of finding data if no other
+    if defined_time_range:
+        data_within_last_timestamp = start_date_time if start_date_time else end_date_time
+    else:
+        data_within_last_timestamp = (datetime.utcnow() - timedelta(minutes=mins)).strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    # This cell will replace the timestamp above with the one read from the `.update` file in the data directory, if it exists.
+
+    if not isdir(data_path):
+        print("NOTE: Making new data directory at " + data_path + "(This is the first run.)")
+        makedirs(data_path, exist_ok=True)
+
+    else:
+        update_file = get_update_file(data_path, short_name)
+        if update_file is not None:
+            try:
+                with open(update_file, "r") as f:
+                    data_within_last_timestamp = f.read().strip()
+                    print("NOTE: Update found in the data directory. (The last run was at " + data_within_last_timestamp + ".)")
+            except FileNotFoundError:
+                print("WARN: No .update in the data directory. (Is this the first run?)")
+        else:
+            print("WARN: No .update__" + short_name + " in the data directory. (Is this the first run?)")
+
+    # Change this to whatever extent you need. Format is W Longitude,S Latitude,E Longitude,N Latitude
+    bounding_extent = args.bbox
+
+    # There are several ways to query for CMR updates that occured during a given timeframe. Read on in the CMR Search documentation:
+    # * https://cmr.earthdata.nasa.gov/search/site/docs/search/api.html#c-with-new-granules (Collections)
+    # * https://cmr.earthdata.nasa.gov/search/site/docs/search/api.html#c-with-revised-granules (Collections)
+    # * https://cmr.earthdata.nasa.gov/search/site/docs/search/api.html#g-production-date (Granules)
+    # * https://cmr.earthdata.nasa.gov/search/site/docs/search/api.html#g-created-at (Granules)
+    # The `created_at` parameter works for our purposes. It's a granule search parameter that returns the records ingested since the input timestamp.
+
+    if defined_time_range:
+        # if(data_since):
+        temporal_range = pa.get_temporal_range(start_date_time, end_date_time, datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"))  # noqa E501
+
+    params = {
+        'scroll': "true",
+        'page_size': page_size,
+        'sort_key': "-start_date",
+        'provider': provider,
+        'ShortName': short_name,
+        'updated_since': data_within_last_timestamp,
+        'token': token,
+        'bounding_box': bounding_extent,
+    }
+
+    if defined_time_range:
+        params = {
+            'scroll': "true",
+            'page_size': page_size,
+            'sort_key': "-start_date",
+            'provider': provider,
+            'updated_since': data_within_last_timestamp,
+            'ShortName': short_name,
+            'temporal': temporal_range,
+            'token': token,
+            'bounding_box': bounding_extent,
+        }
+
+        if args.verbose:
+            print("Temporal Range: " + temporal_range)
+
+    if args.verbose:
+        print("Provider: " + provider)
+        print("Updated Since: " + data_within_last_timestamp)
+
+    results = pa.get_search_results(args, params)
+
+    if args.verbose:
+        print(str(results['hits'])+" new granules found for "+short_name+" since "+data_within_last_timestamp)   # noqa E501
+
+    if any([args.dy, args.dydoy, args.dymd]):
+        file_start_times = pa.parse_start_times(results)
+    elif args.cycle:
+        cycles = pa.parse_cycles(results)
+
+    timestamp = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    downloads_all = []
+    downloads_data = [[u['URL'] for u in r['umm']['RelatedUrls'] if u['Type'] == "GET DATA" and ('Subtype' not in u or u['Subtype'] != "OPENDAP DATA")] for r in results['items']]
+    downloads_metadata = [[u['URL'] for u in r['umm']['RelatedUrls'] if u['Type'] == "EXTENDED METADATA"] for r in results['items']]
+
+    for f in downloads_data:
+        downloads_all.append(f)
+    for f in downloads_metadata:
+        downloads_all.append(f)
+
+    downloads = [item for sublist in downloads_all for item in sublist]
+
+    if len(downloads) >= page_size:
+        print("Warning: only the most recent " + str(page_size) + " granules will be downloaded; try adjusting your search criteria (suggestion: reduce time period or spatial region of search) to ensure you retrieve all granules.")
+
+    # filter list based on extension
+    if not extensions:
+        extensions = pa.extensions
+    filtered_downloads = []
+    for f in downloads:
+        for extension in extensions:
+            if f.lower().endswith(extension):
+                filtered_downloads.append(f)
+
+    downloads = filtered_downloads
+
+    # https://github.com/podaac/data-subscriber/issues/33
+    # Make this a non-verbose message
+    # if args.verbose:
+    print("Found " + str(len(downloads)) + " total files to download")
+    if args.verbose:
+        print("Downloading files with extensions: " + str(extensions))
+
+    # NEED TO REFACTOR THIS, A LOT OF STUFF in here
+    # Finish by downloading the files to the data directory in a loop.
+    # Overwrite `.update` with a new timestamp on success.
+    success_cnt = failure_cnt = 0
+    for f in downloads:
+        try:
+            # -d flag, args.outputDirectory
+            output_path = join(data_path, basename(f))
+            # -dy, args.dy, -dydoy, args.dydoy and -dymd, args.dymd
+            if any([args.dy, args.dydoy, args.dymd]):
+                output_path = pa.prepare_time_output(
+                    file_start_times, data_path, f, args, ts_shift)
+            # -dc flag
+            if args.cycle:
+                output_path = pa.prepare_cycles_output(
+                    cycles, data_path, f)
+            urlretrieve(f, output_path)
+            pa.process_file(process_cmd, output_path, args)
+            print(str(datetime.now()) + " SUCCESS: " + f)
+            success_cnt = success_cnt + 1
+        except Exception as e:
+            print(str(datetime.now()) + " FAILURE: " + f)
+            failure_cnt = failure_cnt + 1
+            print(e)
+
+    # If there were updates to the local time series during this run and no
+    # exceptions were raised during the download loop, then overwrite the
+    #  timestamp file that tracks updates to the data folder
+    #   (`resources/nrt/.update`):
+    if len(results['items']) > 0:
+        if not failure_cnt > 0:
+            with open(data_path + "/.update__" + short_name, "w") as f:
+                f.write(timestamp)
+
+    print("Downloaded: " + str(success_cnt) + " files\n")
+    print("Files Failed to download:" + str(failure_cnt) + "\n")
+    pa.delete_token(token_url, token)
+    print("END \n\n")
+    exit(0)
+
+
+if __name__ == '__main__':
+    run()
