976e7ef6cdf4ac3ae63b71ede888b4cecce2e5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:27 +0000

    Add package psstdata/0.0.0a5/psstdata-0.0.0a5.tar.gz

---
+++ packages/psstdata/psstdata-0.0.0a5.tar.gz/psstdata-0.0.0a5/psstdata/__init__.py
@@ -0,0 +1,8 @@
+from psstdata.consts import *
+from psstdata.tasks import PAD, UNK, SIL, SPN, VOCAB_ARPABET_JSON, VOCAB_ARPABET, CORRECT_PRONUNCIATIONS_BNT, \
+    CORRECT_PRONUNCIATIONS_VNT, PROMPTS_BNT, PROMPTS_VNT
+from psstdata.datastructures import PSSTUtterance, PSSTData, PSSTUtteranceCollection
+
+from psstdata.loading import load
+from psstdata.logs import logger
+
