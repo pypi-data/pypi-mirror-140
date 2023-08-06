bbf775b6acb8d5101dfde4290ebfec9486e8a304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:57 +0000

    Add package argeasy/1.1.1/argeasy-1.1.1.tar.gz

---
+++ packages/argeasy/argeasy-1.1.1.tar.gz/argeasy-1.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from argeasy import __version__
+
+setup(
+    name='argeasy',
+    version=__version__,
+    author='Jaedson Silva',
+    author_email='imunknowuser@protonmail.com',
+    packages=['argeasy'],
+    project_urls={
+        'Código fonte': 'https://github.com/jaedsonpys/argeasy',
+        'Licença': 'https://github.com/jaedsonpys/argeasy/blob/master/LICENSE'
+    },
+    license='GNU General Public License v3.0'
+)
