d8eef3e845b5d7e5d90c19d8215b00c245af71f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:12 +0000

    Add package Topsis-Shikhar-102083055/0.12/Topsis_Shikhar_102083055-0.12.tar.gz

---
+++ packages/Topsis-Shikhar-102083055/Topsis_Shikhar_102083055-0.12.tar.gz/Topsis_Shikhar_102083055-0.12/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'Topsis_Shikhar_102083055',         
+  packages = ['Topsis_Shikhar_102083055'],   
+  version = '0.12',     
+  license='MIT',       
+  description = 'Topsis Calculation',   
+  author = 'Shikhar Saini',                  
+  author_email = 'shikharsaini25@gmail.com', 
+  url = 'https://github.com/shikharsaini',   
+  keywords = ['Topsis', 'TopsisPackage'],   
+  install_requires=[     
+           'numpy',
+          'pandas',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Developers',     
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
